cadb7fb0bd11b5fe2464b01fb4cd5a1372302d36
hi@timeto.me <hi@timeto.me>
2023-01-12 12:56:15 +0300

    Init shared build.gradle.kts

---
+++ shared/build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("com.squareup.sqldelight")
+}
+
+kotlin {
+
+    android()
+
+    listOf(
+        // iOS
+        iosArm64(),
+        iosSimulatorArm64(),
+        // WatchOS
+        watchosX64(),
+        watchosArm32(),
+        watchosArm64(),
+        watchosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+
+        val ktor_version = "2.1.3"
+        val sqldelight_version = "1.5.4" // WARNING Sync version with project build.gradle.kts
+
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-core:$ktor_version")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+                implementation("com.squareup.sqldelight:runtime:$sqldelight_version")
+                implementation("com.squareup.sqldelight:coroutines-extensions:$sqldelight_version")
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+                implementation("com.squareup.sqldelight:android-driver:$sqldelight_version")
+                implementation("androidx.security:security-crypto:1.0.0")
+            }
+        }
+
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktor_version")
+                implementation("com.squareup.sqldelight:native-driver:$sqldelight_version")
+            }
+        }
+
+        val watchosX64Main by getting
+        val watchosArm32Main by getting
+        val watchosArm64Main by getting
+        val watchosSimulatorArm64Main by getting
+        val watchosMain by creating {
+            dependsOn(commonMain)
+            watchosX64Main.dependsOn(this)
+            watchosArm32Main.dependsOn(this)
+            watchosArm64Main.dependsOn(this)
+            watchosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktor_version")
+                implementation("com.squareup.sqldelight:native-driver:$sqldelight_version")
+            }
+        }
+    }
+}
+
+android {
+    namespace = "timeto.shared"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 33
+    }
+}
+
+sqldelight {
+    database("TimetoDB") {
+        packageName = "timeto.dbsq"
+    }
+}
