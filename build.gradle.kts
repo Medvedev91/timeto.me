acf520771333d55b7c5a73a20168c93ecc90ec93
hi@timeto.me <hi@timeto.me>
2023-01-13 12:42:15 +0300

    build.gradle.kts code style

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-        // WARNING Sync version with shared build.gradle.kts
-        classpath("com.squareup.sqldelight:gradle-plugin:1.5.4")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.4") // TRICK Sync with shared build.gradle.kts
     }
 }
 
