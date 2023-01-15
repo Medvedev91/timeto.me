15cbf7171f48cb3ccc60b767e7c1263105c8e8b6
hi@timeto.me <hi@timeto.me>
2023-01-15 08:09:58 +0300

    Init DaytimeUI.kt secondsToString()

--- shared/src/commonMain/kotlin/timeto/shared/vm/ui/DaytimeUI.kt
+++ shared/src/commonMain/kotlin/timeto/shared/vm/ui/DaytimeUI.kt
@@ -1,7 +1,7 @@
 package timeto.shared.vm.ui
 
-import timeto.shared.ColorNative
-import timeto.shared.TextFeatures
+import timeto.shared.*
+import kotlin.math.absoluteValue
 
 class DaytimeUI(
     daytime: Int,
@@ -9,3 +9,32 @@ class DaytimeUI(
     val daytimeText = TextFeatures.daytimeToString(daytime)
     val color: ColorNative = ColorNative.blue
 }
+
+private fun secondsToString(
+    secondsAnySign: Int,
+    isShort: Boolean,
+): String {
+    val (h, m) = secondsAnySign.absoluteValue.toHms()
+
+    if (isShort) {
+        if (h == 0)
+            return if (m == 0) "~1 minute"
+            else m.toStringEndingMinutes()
+        if (h <= 2)
+            return if (m == 0) h.toStringEndingHours()
+            else "${h.toStringEndingHours()} $m min"
+        return h.toStringEndingHours()
+    }
+
+    if ((h == 0) && (m == 0))
+        return "less than a minute"
+
+    val strings = mutableListOf<String>()
+    if (h > 0) strings.add(h.toStringEndingHours())
+    if (h <= 2 && m > 0) strings.add(m.toStringEndingMinutes())
+    val separator = if (m <= 5) " and " else " "
+    return strings.joinToString(separator)
+}
+
+private fun Int.toStringEndingHours() = toStringEnding(true, "hour", "hours")
+private fun Int.toStringEndingMinutes() = toStringEnding(true, "minute", "minutes")
