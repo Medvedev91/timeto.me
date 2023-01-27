291f875776bd4358e78bf4f7c61d95f7bd95cf98
hi@timeto.me <hi@timeto.me>
2023-01-27 17:04:33 +0300

    In .. days / .. days overdue

--- shared/src/commonMain/kotlin/timeto/shared/ui/TimeUI.kt
+++ shared/src/commonMain/kotlin/timeto/shared/ui/TimeUI.kt
@@ -28,13 +28,19 @@ class TimeUI(
 private fun secondsInToString(seconds: Int): String {
     // With roundToNextMinute it's impossible to (h == 0 && m == 0)
     val (h, m) = seconds.toHms(roundToNextMinute = true)
-    return if (h > 0) "In ${h.toStringEndingHours()}"
-    else "In ${m.toStringEnding(true, "minute", "min")}"
+    val d = h / 24
+    return when {
+        d >= 1 -> "In ${d.toStringEndingDays()}"
+        h > 0 -> "In ${h.toStringEndingHours()}"
+        else -> "In ${m.toStringEnding(true, "minute", "min")}"
+    }
 }
 
 private fun secondsOverdueToString(seconds: Int): String {
     val (h, m) = seconds.absoluteValue.toHms()
+    val d = h / 24
     return when {
+        d >= 1 -> d.toStringEndingDays() + " overdue"
         h > 0 -> h.toStringEndingHours() + " overdue"
         m == 0 -> "Now! 🙀"
         else -> "$m min overdue"
@@ -42,3 +48,4 @@ private fun secondsOverdueToString(seconds: Int): String {
 }
 
 private fun Int.toStringEndingHours() = toStringEnding(true, "hour", "hours")
+private fun Int.toStringEndingDays() = toStringEnding(true, "day", "days")
