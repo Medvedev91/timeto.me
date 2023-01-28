28ca8ae75e880b4551b7bb67619faae270b4ae57
hi@timeto.me <hi@timeto.me>
2023-01-28 19:50:41 +0300

    Remove packagingOptions from build.gradle.kts

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -28,11 +28,6 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.3.2"
     }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
