f4dd985d4d54dced26c9865d4a4db1c184dc7152
hi@timeto.me <hi@timeto.me>
2023-01-31 13:21:07 +0300

    Up to org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     }
 
     buildFeatures.compose = true
-    composeOptions.kotlinCompilerExtensionVersion = "1.3.2"
+    composeOptions.kotlinCompilerExtensionVersion = "1.4.0"
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.5") // TRICK Sync with shared build.gradle.kts
     }
 }
