6bc419cfcf379308d278ab877b62a65e0772359b
hi@timeto.me <hi@timeto.me>
2023-02-02 16:25:35 +0300

    Remove TriggersView__FormView__Deprecated android

--- android_app/src/main/java/app/time_to/timeto/ui/TasksListView.kt
+++ android_app/src/main/java/app/time_to/timeto/ui/TasksListView.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
@@ -22,9 +21,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.TextFieldValue
@@ -62,9 +59,6 @@ fun TasksListView(
         )
     }
 
-    // Without the default value - animation on open
-    val taskFormHeight = remember { mutableStateOf(TASKS_LIST_ITEM_MIN_HEIGHT + taskListSectionPadding * 2) }
-
     Box(
         contentAlignment = Alignment.BottomCenter,
     ) {
@@ -81,7 +75,10 @@ fun TasksListView(
         ) {
 
             item {
-                Box(modifier = Modifier.height(taskFormHeight.value))
+                TaskFormView(
+                    folder = activeFolder,
+                    listState = listState,
+                )
             }
 
             val tasksUI = state.tasksUI
@@ -280,23 +277,14 @@ fun TasksListView(
                 }
             }
         }
-
-        TaskFormView(
-            height = taskFormHeight,
-            folder = activeFolder,
-            listState = listState,
-            editedTask = remember { mutableStateOf(null) }, // todo
-        )
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun TaskFormView(
-    height: MutableState<Dp>,
     folder: TaskFolderModel,
     listState: LazyListState,
-    editedTask: MutableState<TaskModel?>,
 ) {
     val scope = rememberCoroutineScope()
     val errorDialog = LocalErrorDialog.current
@@ -304,69 +292,23 @@ private fun TaskFormView(
     var isFocused by remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
     val focusRequester = remember { FocusRequester() }
-    val keyboardController = LocalSoftwareKeyboardController.current
 
     val triggersState = TriggersView__State__TextField.asState(initText = "")
-
-    LaunchedEffect(editedTask.value) {
-        triggersState.reInit(editedTask.value?.text ?: "")
-        if (editedTask.value != null) {
-            focusRequester.requestFocus()
-            // In case if field already focused but ime is not opened
-            keyboardController?.show()
-        }
-    }
 
     // Input UI
     val isLight = MaterialTheme.colors.isLight
     val inputBg = if (isLight) c.background2.copy(alpha = 0.9f) else c.background.copy(alpha = 0.85f)
     // Border has almost the same color for the light theme
     val inputBorder = if (isLight) c.background else c.white.copy(alpha = 0.4f)
-    val paddingStart = TAB_TASKS_PADDING_START - if (isLight) 1.dp else 0.dp
-    val paddingEnd = TAB_TASKS_PADDING_END - if (isLight) 1.dp else 0.dp
 
     Column(
         modifier = Modifier
             .pointerInput(Unit) { } // Ignore clicks through
-            .onSizeChanged {
-                height.value = pxToDp(it.height).dp
-            }
             .padding(
                 top = taskListSectionPadding,
                 bottom = taskListSectionPadding,
-                start = paddingStart,
-                end = paddingEnd
             )
     ) {
-
-        /**
-         * todo if (isFocused && WindowInsets.isImeVisible)
-         * The "if" UI works faster than AnimatedVisibility, but I
-         * get a crash when adding a task. I do not know the reason,
-         * but if I don't use isFocused it doesn't crash.
-         */
-
-        val animationSpecIntSize: FiniteAnimationSpec<IntSize> = spring(
-            stiffness = Spring.StiffnessHigh,
-            visibilityThreshold = IntSize.VisibilityThreshold
-        )
-        val animationSpecFloat: FiniteAnimationSpec<Float> = spring(stiffness = Spring.StiffnessHigh)
-
-        AnimatedVisibility(
-            visible = isFocused && WindowInsets.isImeVisible,
-            modifier = Modifier
-                .fillMaxWidth(), // Fix slide from start animation
-            enter = expandVertically(animationSpecIntSize) + fadeIn(animationSpecFloat),
-            exit = shrinkVertically(animationSpecIntSize) + fadeOut(animationSpecFloat),
-        ) {
-            TriggersView__FormView__Deprecated(
-                triggersState = triggersState,
-                modifier = Modifier
-                    .padding(bottom = 12.dp)
-            )
-        }
-
-        /****/
 
         Row(
             modifier = Modifier
@@ -412,22 +354,6 @@ private fun TaskFormView(
                     }
             )
 
-            if (editedTask.value != null) {
-                Text(
-                    text = "Cancel",
-                    modifier = Modifier
-                        .padding(end = 4.dp)
-                        .clip(RoundedCornerShape(99f))
-                        .clickable {
-                            editedTask.value = null
-                            focusManager.clearFocus()
-                        }
-                        .padding(horizontal = 8.dp, vertical = 6.dp),
-                    color = c.textSecondary,
-                    fontSize = 14.sp,
-                )
-            }
-
             Box(
                 modifier = Modifier
                     .padding(top = 5.dp, bottom = 5.dp, end = 5.dp)
@@ -446,21 +372,15 @@ private fun TaskFormView(
                             try {
                                 val newText = triggersState.textWithTriggers()
 
-                                val editedTaskValue = editedTask.value
-                                if (editedTaskValue != null) {
-                                    editedTaskValue.upTextWithValidation(newText)
-                                    editedTask.value = null
-                                } else {
-                                    TaskModel.addWithValidation(newText, folder)
+                                TaskModel.addWithValidation(newText, folder)
 
-                                    scope.launchEx {
-                                        listState.animateScrollBy(
-                                            -dpToPx(TASKS_LIST_ITEM_MIN_HEIGHT.value).toFloat(),
-                                            animationSpec = spring(stiffness = Spring.StiffnessMediumLow)
-                                        )
-                                        // In case if it's higher than TASKS_LIST_ITEM_MIN_HEIGHT
-                                        listState.animateScrollToItem(0)
-                                    }
+                                scope.launchEx {
+                                    listState.animateScrollBy(
+                                        -dpToPx(TASKS_LIST_ITEM_MIN_HEIGHT.value).toFloat(),
+                                        animationSpec = spring(stiffness = Spring.StiffnessMediumLow)
+                                    )
+                                    // In case if it's higher than TASKS_LIST_ITEM_MIN_HEIGHT
+                                    listState.animateScrollToItem(0)
                                 }
 
                                 triggersState.textField.value = TextFieldValue()
@@ -482,7 +402,7 @@ private fun TaskFormView(
                 contentAlignment = Alignment.CenterStart
             ) {
                 Text(
-                    if (editedTask.value == null) "SAVE" else "UP",
+                    "SAVE",
                     color = c.white,
                     fontSize = 14.sp,
                     fontWeight = FontWeight.W600
