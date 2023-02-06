5b5475764657571f92cb6e7757733ccb5e6c1828
hi@timeto.me <hi@timeto.me>
2023-02-06 19:32:14 +0300

    Init TabTasksVM.kt

---
+++ shared/src/commonMain/kotlin/timeto/shared/vm/TabTasksVM.kt
@@ -0,0 +1,27 @@
+package timeto.shared.vm
+
+import kotlinx.coroutines.flow.*
+import timeto.shared.DI
+import timeto.shared.db.TaskFolderModel
+import timeto.shared.db.TaskFolderModel.Companion.sortFolders
+import timeto.shared.onEachExIn
+
+class TabTasksVM : __VM<TabTasksVM.State>() {
+
+    data class State(
+        val folders: List<TaskFolderModel>,
+    )
+
+    override val state = MutableStateFlow(
+        State(
+            folders = DI.taskFolders.sortFolders()
+        )
+    )
+
+    override fun onAppear() {
+        val scope = scopeVM()
+        TaskFolderModel.getAscBySortFlow().onEachExIn(scope) { folders ->
+            state.update { it.copy(folders = folders.sortFolders()) }
+        }
+    }
+}
