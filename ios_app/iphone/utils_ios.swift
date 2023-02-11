89ebf933c1f08bd6f0b6ac9a01dd95f3d97d097d
hi@timeto.me <hi@timeto.me>
2023-02-11 08:49:18 +0300

    Init RoundedCorner.cornerRadius()

--- ios_app/iphone/utils_ios.swift
+++ ios_app/iphone/utils_ios.swift
@@ -198,3 +198,27 @@ extension View {
         return self
     }
 }
+
+///
+/// Rounded corners https://stackoverflow.com/a/58606176/5169420
+
+private struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+
+    func cornerRadius(
+            _ radius: CGFloat,
+            corners: UIRectCorner
+    ) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
