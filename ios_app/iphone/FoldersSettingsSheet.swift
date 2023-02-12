a0239948b523493f800a7193d3e69935c32254a1
hi@timeto.me <hi@timeto.me>
2023-02-12 08:52:05 +0300

    Init FoldersSettingsSheet__FolderItem ios

--- ios_app/iphone/FoldersSettingsSheet.swift
+++ ios_app/iphone/FoldersSettingsSheet.swift
@@ -22,10 +22,60 @@ struct FoldersSettingsSheet: View {
                 isPresented = false
             }
 
+            ZStack {}.frame(height: 20)
+
             ScrollViewWithVListener(showsIndicators: false, vScroll: $sheetHeaderScroll) {
 
+                VStack(spacing: 0) {
+
+                    let folders = state.folders
+                    ForEach(folders, id: \.id) { folder in
+                        let isFirst = folders.first == folder
+                        MyListView__ItemView(
+                                isFirst: isFirst,
+                                isLast: folders.last == folder,
+                                withTopDivider: !isFirst
+                        ) {
+                            FoldersSettingsSheet__FolderItem(
+                                    vm: vm,
+                                    folder: folder
+                            )
+                        }
+                    }
+                }
             }
         }
                 .background(Color(.mySheetFormBg))
+    }
+}
+
+private struct FoldersSettingsSheet__FolderItem: View {
+
+    let vm: FoldersSettingsVM
+    let folder: TaskFolderModel
+
+    @State private var isEditPresented = false
+
+    var body: some View {
+
+        MyListView__ItemView__ButtonView(
+                text: folder.name,
+                rightView: AnyView(
+                        Button(
+                                action: {
+                                    vm.sortUp(folder: folder)
+                                },
+                                label: {
+                                    Image(systemName: "arrow.up")
+                                            .font(.system(size: 14))
+                                            .foregroundColor(.blue)
+                                }
+                        )
+                                .buttonStyle(.plain)
+                                .padding(.trailing, MyListView.PADDING_INNER_HORIZONTAL)
+                )
+        ) {
+
+        }
     }
 }
