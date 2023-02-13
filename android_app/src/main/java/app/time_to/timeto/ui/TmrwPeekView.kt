0caced45bc7ece7675580f1d5d0a32c46ec93224
hi@timeto.me <hi@timeto.me>
2023-02-13 15:54:39 +0300

    Todo TmrwPeekView.kt

---
+++ android_app/src/main/java/app/time_to/timeto/ui/TmrwPeekView.kt
@@ -0,0 +1,17 @@
+package app.time_to.timeto.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TmrwPeekView() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text("ff")
+    }
+}
