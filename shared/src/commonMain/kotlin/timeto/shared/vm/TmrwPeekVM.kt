ac893b73d1ef38ace2bcee09fba887c0dc2d7c78
hi@timeto.me <hi@timeto.me>
2023-02-14 17:08:03 +0300

    Init TmrwPeekVM

---
+++ shared/src/commonMain/kotlin/timeto/shared/vm/TmrwPeekVM.kt
@@ -0,0 +1,74 @@
+package timeto.shared.vm
+
+import kotlinx.coroutines.flow.*
+import timeto.shared.*
+import timeto.shared.db.EventModel
+import timeto.shared.db.RepeatingModel
+import timeto.shared.db.TaskFolderModel
+import timeto.shared.db.TaskModel
+import timeto.shared.ui.sortedByFolder
+
+/**
+ * todo Live update
+ */
+class TmrwPeekVM : __VM<TmrwPeekVM.State>() {
+
+    class TaskUI(task: TaskModel) : timeto.shared.ui.TaskUI(task)
+
+    data class State(
+        val tasksUI: List<TaskUI>,
+    )
+
+    override val state = MutableStateFlow(
+        State(
+            tasksUI = listOf()
+        )
+    )
+
+    override fun onAppear() {
+        val scope = scopeVM()
+        scope.launchEx {
+
+            val rawTasks = mutableListOf<TaskModel>()
+            val unixTmrwDS = UnixTime(utcOffset = localUtcOffsetWithDayStart).inDays(1)
+            val tmrwDSDay = unixTmrwDS.localDay
+            var lastFakeTaskId = unixTmrwDS.localDayStartTime()
+
+            // Tasks
+            TaskModel.getAsc()
+                .filter { it.isTmrw }
+                .let { rawTasks.addAll(it) }
+
+            // Repeatings
+            RepeatingModel.getAsc()
+                .filter { it.getNextDay() == tmrwDSDay }
+                .forEach { repeating ->
+                    rawTasks.add(
+                        TaskModel(
+                            id = ++lastFakeTaskId,
+                            text = repeating.prepTextForTask(tmrwDSDay),
+                            folder_id = TaskFolderModel.ID_TODAY,
+                        )
+                    )
+                }
+
+            // Events
+            EventModel.getAscByTime()
+                .filter { it.getLocalTime().localDay == tmrwDSDay }
+                .forEach { event ->
+                    rawTasks.add(
+                        TaskModel(
+                            id = ++lastFakeTaskId,
+                            text = event.prepTextForTask(),
+                            folder_id = TaskFolderModel.ID_TODAY,
+                        )
+                    )
+                }
+
+            rawTasks
+                .map { TaskUI(it) }
+                .sortedByFolder(DI.getTodayFolder())
+                .forEach { zlog(it.listText) }
+        }
+    }
+}
