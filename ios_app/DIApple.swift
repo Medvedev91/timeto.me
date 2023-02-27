f498a737dbcac02627934f339d49e44ef39a928c
hi@timeto.me <hi@timeto.me>
2023-02-27 20:46:01 +0300

    Remove DIApple.checklistItems

--- ios_app/DIApple.swift
+++ ios_app/DIApple.swift
@@ -11,22 +11,17 @@ extension View {
 
 class DIApple: ObservableObject {
 
-    @Published var checklistItems = [ChecklistItemModel]()
     @Published var lastInterval: IntervalModel? = nil
 }
 
 private struct DIApple__Modifier: ViewModifier {
 
-    private typealias pubArray = AnyPublisher<NSArray, Never>
-
     @StateObject private var diApple = DIApple()
 
-    private let checklistItems: pubArray = ChecklistItemModel.companion.getAscFlow().toPublisher()
     private let lastInterval: AnyPublisher<IntervalModel, Never> = IntervalModel.companion.getLastOneOrNullFlow().toPublisher()
 
     func body(content: Content) -> some View {
         content
-                .onReceive(checklistItems) { diApple.checklistItems = $0 as! [ChecklistItemModel] }
                 .onReceive(lastInterval) { diApple.lastInterval = $0 as! IntervalModel? }
                 .environmentObject(diApple)
     }
