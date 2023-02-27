4a7b8dc81a23ee533331832aed7eff67248874f8
hi@timeto.me <hi@timeto.me>
2023-02-27 20:35:58 +0300

    Init ChecklistDialogVM.kt

---
+++ shared/src/commonMain/kotlin/timeto/shared/vm/ChecklistDialogVM.kt
@@ -0,0 +1,31 @@
+package timeto.shared.vm
+
+import kotlinx.coroutines.flow.*
+import timeto.shared.DI
+import timeto.shared.db.ChecklistItemModel
+import timeto.shared.db.ChecklistModel
+import timeto.shared.onEachExIn
+
+class ChecklistDialogVM(
+    val checklist: ChecklistModel,
+) : __VM<ChecklistDialogVM.State>() {
+
+    data class State(
+        val items: List<ChecklistItemModel>,
+    )
+
+    override val state = MutableStateFlow(
+        State(items = DI.checklistItems.prepChecklistItems(checklist))
+    )
+
+    override fun onAppear() {
+        val scope = scopeVM()
+        ChecklistItemModel.getAscFlow().onEachExIn(scope) { items ->
+            state.update { it.copy(items = items.prepChecklistItems(checklist)) }
+        }
+    }
+}
+
+private fun List<ChecklistItemModel>.prepChecklistItems(
+    checklist: ChecklistModel
+): List<ChecklistItemModel> = this.filter { it.list_id == checklist.id }
