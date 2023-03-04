3798b40d879f04fbb019696d79300c6c97246ae5
hi@timeto.me <hi@timeto.me>
2023-03-04 15:22:25 +0300

    Init MyDialog.showDatePicker()

--- android_app/src/main/java/app/time_to/timeto/ui/MyDialog.kt
+++ android_app/src/main/java/app/time_to/timeto/ui/MyDialog.kt
@@ -1,21 +1,39 @@
 package app.time_to.timeto.ui
 
+import android.widget.CalendarView
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import app.time_to.timeto.LocalWrapperViewLayers
+import app.time_to.timeto.toUnixTime
+import timeto.shared.UnixTime
+import timeto.shared.toHms
+import java.text.SimpleDateFormat
+import java.util.*
 
 object MyDialog {
 
@@ -45,6 +63,166 @@ object MyDialog {
                 }
             }
         ).showOneTime(layers)
+    }
+
+    fun showDatePicker(
+        layers: MutableList<WrapperView__LayerData>,
+        defaultTime: UnixTime? = null,
+        minPickableDay: Int, // Available to pick
+        minSavableDay: Int, // Active add button
+        maxDay: Int = UnixTime.MAX_DAY,
+        title: String? = null,
+        withTimeBtnText: String? = null,
+        onSelect: (UnixTime) -> Unit,
+    ) {
+        show(
+            layers = layers,
+            margin = PaddingValues(horizontal = 30.dp)
+        ) { layer ->
+
+            val defHms = if (defaultTime != null)
+                (defaultTime.time - defaultTime.localDayStartTime()).toHms()
+            else null
+
+            var timeHIndex by remember { mutableStateOf(defHms?.get(0) ?: 0) }
+            var timeMIndex by remember { mutableStateOf(defHms?.get(1) ?: 0) }
+
+            var selectedDay by remember { mutableStateOf(defaultTime?.localDay ?: minPickableDay) }
+
+            fun onSelectAndClose() {
+                onSelect(UnixTime.byLocalDay(selectedDay).inSeconds((timeHIndex * 3600) + (timeMIndex * 60)))
+                layer.close()
+            }
+            Column(
+                modifier = Modifier
+                    .background(c.background2)
+            ) {
+
+                if (title != null) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(c.datePickerTitleBg)
+                    ) {
+                        Text(
+                            title,
+                            modifier = Modifier
+                                .padding(top = 19.dp, bottom = 19.dp, start = 20.dp, end = 20.dp)
+                                .align(Alignment.Center),
+                            fontSize = 19.sp,
+                            fontWeight = FontWeight.Medium,
+                            textAlign = TextAlign.Center,
+                            color = c.text,
+                        )
+
+                        Divider(
+                            Modifier
+                                .align(Alignment.BottomCenter)
+                                .fillMaxWidth()
+                                .height(0.8.dp),
+                            color = c.dividerBackground2,
+                        )
+                    }
+                }
+
+                AndroidView(
+                    { CalendarView(it) },
+                    modifier = Modifier.padding(top = 4.dp),
+                    update = { calendarView ->
+                        calendarView.date = UnixTime.byLocalDay(selectedDay).time * 1000L
+
+                        calendarView.maxDate = UnixTime.byLocalDay(maxDay).time * 1000L
+                        calendarView.minDate = UnixTime.byLocalDay(minPickableDay).time * 1000L
+
+                        calendarView.setOnDateChangeListener { _, cYear, cMonthIndex, cDay ->
+                            val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+                            val newDate = formatter.parse("$cYear-${cMonthIndex + 1}-${cDay}")!!
+                            selectedDay = newDate.toUnixTime().localDay
+                            if (withTimeBtnText == null)
+                                onSelectAndClose()
+                        }
+                    }
+                )
+
+                if (withTimeBtnText != null)
+                    Box {
+                        Row(
+                            modifier = Modifier.padding(bottom = 24.dp, start = 24.dp, end = 23.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            Box(
+                                modifier = Modifier
+                            ) {
+                                MyPicker(
+                                    items = (0..23).map { "$it".padStart(2, '0') },
+                                    containerWidth = 30.dp,
+                                    containerHeight = 30.dp,
+                                    itemHeight = 20.dp,
+                                    selectedIndex = timeHIndex,
+                                    onChange = { index, _ ->
+                                        timeHIndex = index
+                                    },
+                                )
+                            }
+
+                            Text(
+                                ":",
+                                modifier = Modifier
+                                    .padding(horizontal = 4.dp)
+                                    .offset(y = (-2.2).dp),
+                                fontWeight = FontWeight.Black,
+                                fontSize = 22.sp,
+                            )
+
+                            Box(
+                                modifier = Modifier
+                            ) {
+                                MyPicker(
+                                    items = (0..59).map { "$it".padStart(2, '0') },
+                                    containerWidth = 30.dp,
+                                    containerHeight = 30.dp,
+                                    itemHeight = 20.dp,
+                                    selectedIndex = timeMIndex,
+                                    onChange = { index, _ ->
+                                        timeMIndex = index
+                                    },
+                                )
+                            }
+
+                            Box(Modifier.weight(1f))
+
+                            MyButton(
+                                text = withTimeBtnText,
+                                isEnabled = selectedDay in minSavableDay..maxDay,
+                                modifier = Modifier,
+                                backgroundColor = c.blue,
+                                extraPaddings = -4 to 0
+                            ) {
+                                onSelectAndClose()
+                            }
+                        }
+
+                        Icon(
+                            painterResource(id = app.time_to.timeto.R.drawable.ic_round_close_24),
+                            "Close",
+                            tint = c.textSecondary,
+                            modifier = Modifier
+                                .offset(x = (-22).dp, y = (-42).dp)
+                                .alpha(0.7f)
+                                .align(Alignment.TopEnd)
+                                .padding(end = 4.dp)
+                                .size(30.dp)
+                                .clip(RoundedCornerShape(99.dp))
+                                .background(c.transparent)
+                                .clickable {
+                                    layer.close()
+                                }
+                                .padding(4.dp)
+                        )
+                    }
+            }
+        }
     }
 }
 
