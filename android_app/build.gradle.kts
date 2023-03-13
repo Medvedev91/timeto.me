0e395bd5e5d5ad52b95fa0a2f5e33aff70bf3cd4
hi@timeto.me <hi@timeto.me>
2023-03-13 07:51:55 +0300

    Code style build.gradle.kts

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.material:material:1.3.1")
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.compose.material:material:1.3.1")
     implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 }
