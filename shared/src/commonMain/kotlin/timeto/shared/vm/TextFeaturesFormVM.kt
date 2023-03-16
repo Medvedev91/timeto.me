88719bb4e60445ccc9da6fbab8f24c0c11e867a7
hi@timeto.me <hi@timeto.me>
2023-03-16 21:38:20 +0300

    R: TextFeaturesFormVM.State

--- shared/src/commonMain/kotlin/timeto/shared/vm/TextFeaturesFormVM.kt
+++ shared/src/commonMain/kotlin/timeto/shared/vm/TextFeaturesFormVM.kt
@@ -14,9 +14,9 @@ class TextFeaturesFormVM(
     ) {
 
         val titleChecklist = "Checklists"
-        val titleShortcuts = "Shortcuts"
-
         val noteChecklists = textFeatures.checklists.takeIf { it.isNotEmpty() }?.joinToString(", ") { it.name } ?: "None"
+
+        val titleShortcuts = "Shortcuts"
         val noteShortcuts = textFeatures.shortcuts.takeIf { it.isNotEmpty() }?.joinToString(", ") { it.name } ?: "None"
     }
 
@@ -26,13 +26,13 @@ class TextFeaturesFormVM(
         )
     )
 
-    fun upChecklists(checklists: List<ChecklistModel>) : TextFeatures {
+    fun upChecklists(checklists: List<ChecklistModel>): TextFeatures {
         val newTextFeatures = state.value.textFeatures.copy(checklists = checklists)
         state.update { it.copy(textFeatures = newTextFeatures) }
         return newTextFeatures
     }
 
-    fun upShortcuts(shortcuts: List<ShortcutModel>) : TextFeatures {
+    fun upShortcuts(shortcuts: List<ShortcutModel>): TextFeatures {
         val newTextFeatures = state.value.textFeatures.copy(shortcuts = shortcuts)
         state.update { it.copy(textFeatures = newTextFeatures) }
         return newTextFeatures
