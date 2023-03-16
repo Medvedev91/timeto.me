a5f556a4d416be3df216b6687ac31cbe07888c17
hi@timeto.me <hi@timeto.me>
2023-03-16 21:52:25 +0300

    I: TimerFormView()

--- android_app/src/main/java/app/time_to/timeto/ui/TextFeaturesFormView.kt
+++ android_app/src/main/java/app/time_to/timeto/ui/TextFeaturesFormView.kt
@@ -2,8 +2,11 @@ package app.time_to.timeto.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import app.time_to.timeto.rememberVM
+import app.time_to.timeto.toColor
 import timeto.shared.TextFeatures
 import timeto.shared.vm.TextFeaturesFormVM
 
@@ -74,6 +77,73 @@ private fun TriggersFormView(
                 ) {
                     onChange(vm.upShortcuts(it))
                 }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun TimerFormView(
+    vm: TextFeaturesFormVM,
+    state: TextFeaturesFormVM.State,
+    onChange: (TextFeatures) -> Unit,
+) {
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    MyListView__ItemView(
+        isFirst = true,
+        isLast = false,
+    ) {
+        MyListView__ItemView__ButtonView(
+            text = state.activityTitle,
+            withArrow = true,
+            rightView = {
+                MyListView__ItemView__ButtonView__RightText(
+                    text = state.activityNote,
+                    paddingEnd = 2.dp,
+                    color = state.activityColorOrNull?.toColor(),
+                )
+            }
+        ) {
+            Sheet.show { layer ->
+                ActivityPickerSheet(
+                    layer = layer,
+                ) {
+                    onChange(vm.upActivity(it))
+                }
+            }
+        }
+    }
+
+    MyListView__ItemView(
+        isFirst = false,
+        isLast = true,
+        withTopDivider = true,
+    ) {
+        MyListView__ItemView__ButtonView(
+            text = state.timerTitle,
+            withArrow = true,
+            rightView = {
+                MyListView__ItemView__ButtonView__RightText(
+                    text = state.timerNote,
+                    paddingEnd = 2.dp,
+                    color = state.timerColorOrNull?.toColor(),
+                )
+            }
+        ) {
+            keyboardController?.hide()
+            Sheet.show { layer ->
+                TimerPickerSheet(
+                    layer = layer,
+                    title = "Timer",
+                    doneText = "Done",
+                    defMinutes = 30,
+                    onPick = { seconds ->
+                        onChange(vm.upTimer(seconds))
+                    }
+                )
             }
         }
     }
