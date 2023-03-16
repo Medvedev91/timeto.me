a506bac579c8363d50b79ac14dc364f29cac95a2
hi@timeto.me <hi@timeto.me>
2023-03-16 12:25:57 +0300

    I: ShortcutsPickerSheetVM.kt

---
+++ shared/src/commonMain/kotlin/timeto/shared/vm/ShortcutsPickerSheetVM.kt
@@ -0,0 +1,43 @@
+package timeto.shared.vm
+
+import kotlinx.coroutines.flow.*
+import timeto.shared.DI
+import timeto.shared.db.ShortcutModel
+
+class ShortcutsPickerSheetVM(
+    selectedShortcuts: List<ShortcutModel>,
+) : __VM<ShortcutsPickerSheetVM.State>() {
+
+    data class State(
+        val shortcutsUI: List<ShortcutUI>,
+    ) {
+        val headerTitle = "Shortcuts"
+        val doneTitle = "Done"
+    }
+
+    data class ShortcutUI(
+        val shortcut: ShortcutModel,
+        val isSelected: Boolean,
+    ) {
+        val text = shortcut.name
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            shortcutsUI = DI.shortcuts.map {
+                ShortcutUI(it, it.id in selectedShortcuts.map { it.id })
+            },
+        )
+    )
+
+    fun toggleShortcut(shortcutUI: ShortcutUI) {
+        val shortcutsUI = state.value.shortcutsUI.toMutableList()
+        val idx = shortcutsUI.indexOf(shortcutUI) // todo report if -1
+        shortcutsUI[idx] = shortcutUI.copy(isSelected = !shortcutUI.isSelected)
+        state.update { it.copy(shortcutsUI = shortcutsUI) }
+    }
+
+    fun getSelectedShortcuts() = state.value.shortcutsUI
+        .filter { it.isSelected }
+        .map { it.shortcut }
+}
