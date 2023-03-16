240b48b55f587632d2ad0f44cc3eb7a7affbb0fb
hi@timeto.me <hi@timeto.me>
2023-03-16 10:42:37 +0300

    I: ChecklistsPickerSheetVM.kt

---
+++ shared/src/commonMain/kotlin/timeto/shared/vm/ChecklistsPickerSheetVM.kt
@@ -0,0 +1,43 @@
+package timeto.shared.vm
+
+import kotlinx.coroutines.flow.*
+import timeto.shared.DI
+import timeto.shared.db.ChecklistModel
+
+class ChecklistsPickerSheetVM(
+    selectedChecklists: List<ChecklistModel>,
+) : __VM<ChecklistsPickerSheetVM.State>() {
+
+    data class State(
+        val checklistsUI: List<ChecklistUI>,
+    ) {
+        val headerTitle = "Checklists"
+        val doneTitle = "Done"
+    }
+
+    data class ChecklistUI(
+        val checklist: ChecklistModel,
+        val isSelected: Boolean,
+    ) {
+        val text = checklist.name
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            checklistsUI = DI.checklists.map {
+                ChecklistUI(it, it.id in selectedChecklists.map { it.id })
+            },
+        )
+    )
+
+    fun toggleChecklist(checklistUI: ChecklistUI) {
+        val checklistsUI = state.value.checklistsUI.toMutableList()
+        val idx = checklistsUI.indexOf(checklistUI) // todo report if -1
+        checklistsUI[idx] = checklistUI.copy(isSelected = !checklistUI.isSelected)
+        state.update { it.copy(checklistsUI = checklistsUI) }
+    }
+
+    fun getSelectedChecklists() = state.value.checklistsUI
+        .filter { it.isSelected }
+        .map { it.checklist }
+}
