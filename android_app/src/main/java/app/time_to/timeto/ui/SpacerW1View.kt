ab619be5f4c98896b49265c3bab6e9e3e1b41a52
hi@timeto.me <hi@timeto.me>
2023-03-19 17:00:57 +0300

    Init fun ColumnScope.SpacerW1()

--- android_app/src/main/java/app/time_to/timeto/ui/SpacerW1View.kt
+++ android_app/src/main/java/app/time_to/timeto/ui/SpacerW1View.kt
@@ -1,11 +1,17 @@
 package app.time_to.timeto.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun RowScope.SpacerW1() {
+    Box(Modifier.weight(1f))
+}
+
+@Composable
+fun ColumnScope.SpacerW1() {
     Box(Modifier.weight(1f))
 }
