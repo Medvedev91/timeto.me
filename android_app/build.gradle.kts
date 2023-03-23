89b0d44a7ebed1a29acb2161755b23c7aa11b1d7
hi@timeto.me <hi@timeto.me>
2023-03-23 17:39:32 +0300

    Up to androidx.activity:activity-compose:1.7.0

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
     implementation("androidx.compose.material:material:1.4.0")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.navigation:navigation-compose:2.5.3")
