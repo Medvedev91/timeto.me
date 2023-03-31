54a0ad44e8f230c2e6d8b836aeda8e4f898bf21a
hi@timeto.me <hi@timeto.me>
2023-03-31 13:09:25 +0300

    I: FullScreenVM.State.allTasksUI

--- shared/src/commonMain/kotlin/timeto/shared/vm/FullScreenVM.kt
+++ shared/src/commonMain/kotlin/timeto/shared/vm/FullScreenVM.kt
@@ -6,7 +6,9 @@ import kotlinx.coroutines.launch
 import timeto.shared.*
 import timeto.shared.db.ChecklistItemModel
 import timeto.shared.db.IntervalModel
+import timeto.shared.db.TaskModel
 import timeto.shared.vm.ui.TimerDataUI
+import timeto.shared.vm.ui.sortedByFolder
 import timeto.shared.vm.ui.toChecklistUI
 
 class FullScreenVM : __VM<FullScreenVM.State>() {
@@ -16,6 +18,7 @@ class FullScreenVM : __VM<FullScreenVM.State>() {
         val allChecklistItems: List<ChecklistItemModel>,
         val isTaskCancelVisible: Boolean,
         val isCountdown: Boolean,
+        val allTasksUI: List<TaskUI>,
         val idToUpdate: Long,
     ) {
         val timerData = TimerDataUI(interval, isCountdown, ColorNative.white)
@@ -58,6 +61,7 @@ class FullScreenVM : __VM<FullScreenVM.State>() {
             allChecklistItems = DI.checklistItems,
             isTaskCancelVisible = false,
             isCountdown = true,
+            allTasksUI = listOf(), // todo
             idToUpdate = 0,
         )
     )
@@ -75,6 +79,17 @@ class FullScreenVM : __VM<FullScreenVM.State>() {
             .getAscFlow()
             .onEachExIn(scope) { items ->
                 state.update { it.copy(allChecklistItems = items) }
+            }
+        TaskModel
+            .getAscFlow()
+            .map { it.filter { task -> task.isToday } }
+            .onEachExIn(scope) { tasks ->
+                state.update {
+                    it.copy(allTasksUI = tasks
+                        .sortedByFolder(DI.getTodayFolder())
+                        .map { task -> TaskUI.prepTask(task) }
+                    )
+                }
             }
         scope.launch {
             while (true) {
@@ -113,5 +128,76 @@ class FullScreenVM : __VM<FullScreenVM.State>() {
             IntervalModel.cancelCurrentInterval()
             state.update { it.copy(isTaskCancelVisible = false) }
         }
+    }
+
+    //////
+
+    sealed class TaskUI(
+        val task: TaskModel,
+    ) {
+
+        companion object {
+
+            fun prepTask(task: TaskModel): TaskUI {
+                val textFeatures = task.text.textFeatures()
+                val timeData = textFeatures.timeData
+                val grayRgba = ColorRgba(150, 150, 150, 255) // todo
+
+                if (timeData == null) {
+                    return RegularTaskUI(
+                        task = task,
+                        text = textFeatures.textNoFeatures,
+                        textColor = grayRgba,
+                    )
+                }
+
+                val timeLeftText = timeData.timeLeftText()
+
+                if (timeData.isImportant) {
+                    val dateText = timeData.unixTime.getStringByComponents(
+                        UnixTime.StringComponent.dayOfMonth,
+                        UnixTime.StringComponent.space,
+                        UnixTime.StringComponent.month3,
+                        UnixTime.StringComponent.comma,
+                        UnixTime.StringComponent.space,
+                        UnixTime.StringComponent.hhmm24,
+                    )
+                    val backgroundColor = when (timeData.status) {
+                        TextFeatures.TimeData.STATUS.IN, // todo?
+                        TextFeatures.TimeData.STATUS.NEAR -> ColorRgba(0, 122, 255, 255) // todo
+                        TextFeatures.TimeData.STATUS.OVERDUE -> ColorRgba(255, 59, 48) // todo
+                    }
+                    return ImportantTaskUI(
+                        task = task,
+                        type = timeData.type,
+                        text = "$dateText ${textFeatures.textNoFeatures} - $timeLeftText",
+                        backgroundColor = backgroundColor,
+                    )
+                }
+                val textColor = when (timeData.status) {
+                    TextFeatures.TimeData.STATUS.IN -> grayRgba
+                    TextFeatures.TimeData.STATUS.NEAR -> ColorRgba(0, 122, 255, 180) // todo
+                    TextFeatures.TimeData.STATUS.OVERDUE -> ColorRgba(255, 59, 48, 160) // todo
+                }
+                return RegularTaskUI(
+                    task = task,
+                    text = "${textFeatures.textNoFeatures} - $timeLeftText",
+                    textColor = textColor,
+                )
+            }
+        }
+
+        class RegularTaskUI(
+            task: TaskModel,
+            val text: String,
+            val textColor: ColorRgba,
+        ) : TaskUI(task)
+
+        class ImportantTaskUI(
+            task: TaskModel,
+            val type: TextFeatures.TimeData.TYPE,
+            val text: String,
+            val backgroundColor: ColorRgba,
+        ) : TaskUI(task)
     }
 }
