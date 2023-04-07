e03f2089b7160013e0329ca9e68e9cc70e11048c
hi@timeto.me <hi@timeto.me>
2023-04-07 23:24:27 +0300

    Up to androidx.compose.material:material:1.4.1

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 dependencies {
     implementation(project(":shared"))
     implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.compose.material:material:1.4.0")
+    implementation("androidx.compose.material:material:1.4.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.navigation:navigation-compose:2.5.3")
 }
