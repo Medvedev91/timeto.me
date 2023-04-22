32b54b215ec2f896a63b34b8667b0aa5d8257756
hi@timeto.me <hi@timeto.me>
2023-04-22 08:22:23 +0300

    Remove: FullScreenVM.ChecklistUI.collapsedTitle

--- shared/src/commonMain/kotlin/timeto/shared/vm/FullScreenVM.kt
+++ shared/src/commonMain/kotlin/timeto/shared/vm/FullScreenVM.kt
@@ -172,8 +172,6 @@ class FullScreenVM : __VM<FullScreenVM.State>() {
         val stateUI = ChecklistStateUI.build(checklist, items)
         val itemsUI = items.map { ItemUI(it) }
 
-        val collapsedTitle = checklist.name + " - ${items.count { it.isChecked }}/${items.size}"
-
         class ItemUI(
             val item: ChecklistItemModel,
         ) {
