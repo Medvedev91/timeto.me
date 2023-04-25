cb1b97eaf4d464c3b18ca8ee214a81efbc5c1c38
hi@timeto.me <hi@timeto.me>
2023-04-25 16:18:53 +0300

    UI: Up Strings.AUTO_FS_FORM_TITLE

--- shared/src/commonMain/kotlin/timeto/shared/Strings.kt
+++ shared/src/commonMain/kotlin/timeto/shared/Strings.kt
@@ -2,5 +2,5 @@ package timeto.shared
 
 object Strings {
 
-    internal const val AUTO_FS_FORM_TITLE = "Auto-Open in Full Screen"
+    internal const val AUTO_FS_FORM_TITLE = "Auto-Start Focus Mode"
 }
