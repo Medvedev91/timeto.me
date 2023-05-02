dd1445a324e4d786e0859ef730d69e2bf86d3af8
hi@timeto.me <hi@timeto.me>
2023-05-02 17:20:13 +0300

    I: fun List<TextFeatures.Trigger>.filterNoChecklists()

--- shared/src/commonMain/kotlin/timeto/shared/TextFeatures.kt
+++ shared/src/commonMain/kotlin/timeto/shared/TextFeatures.kt
@@ -109,6 +109,9 @@ data class TextFeatures(
     }
 }
 
+fun List<TextFeatures.Trigger>.filterNoChecklists() =
+    this.filter { it !is TextFeatures.Trigger.Checklist }
+
 fun String.textFeatures(): TextFeatures = parseLocal(this)
 
 //////
