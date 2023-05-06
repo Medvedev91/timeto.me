3bb7283b54a323aa0498f12d80c0e39967921d8e
hi@timeto.me <hi@timeto.me>
2023-05-06 17:35:03 +0300

    b194 v2023.05.06

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 33
-        versionCode = 193
+        versionCode = 194
         versionName = "2023.05.06"
     }
 
