c9d689569896ab931d3065761733bfc9029536b6
hi@timeto.me <hi@timeto.me>
2023-05-18 13:00:59 +0300

    R: Clean ActivityColorPickerSheet.kt

--- android_app/src/main/java/me/timeto/app/ui/ActivityColorPickerSheet.kt
+++ android_app/src/main/java/me/timeto/app/ui/ActivityColorPickerSheet.kt
@@ -17,13 +17,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import me.timeto.app.R
 import me.timeto.app.onePx
 import me.timeto.app.rememberVM
 import me.timeto.app.toColor
