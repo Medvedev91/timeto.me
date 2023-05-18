45dd8566e532c74bd977a715178c5469693b62cc
hi@timeto.me <hi@timeto.me>
2023-05-18 18:57:32 +0300

    I: DividerBg2(isVisible)

--- android_app/src/main/java/me/timeto/app/ui/DividerBg2.kt
+++ android_app/src/main/java/me/timeto/app/ui/DividerBg2.kt
@@ -1,9 +1,16 @@
 package me.timeto.app.ui
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 
 @Composable
-fun DividerBg2() {
-    Divider(color = c.dividerBg2)
+fun DividerBg2(
+    isVisible: Boolean = true,
+) {
+    val animateColor = animateColorAsState(
+        if (isVisible) c.dividerBg2
+        else c.transparent
+    )
+    Divider(color = animateColor.value)
 }
