6d539ae62652e1392bcb8442383074c012509dc5
hi@timeto.me <hi@timeto.me>
2023-05-18 18:33:12 +0300

    I: DividerBg2.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/DividerBg2.kt
@@ -0,0 +1,9 @@
+package me.timeto.app.ui
+
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DividerBg2() {
+    Divider(color = c.dividerBackground2)
+}
