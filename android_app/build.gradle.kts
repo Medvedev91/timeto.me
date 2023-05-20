162a5dc2288b16b4835820e5000685c8261f0d22
hi@timeto.me <hi@timeto.me>
2023-05-20 20:08:41 +0300

    I: implementation("androidx.compose.material:material-icons-extended:1.4.3")

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation(project(":shared"))
     implementation("androidx.activity:activity-compose:1.7.1")
     implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.compose.material:material-icons-extended:1.4.3")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.5.3")
 }
