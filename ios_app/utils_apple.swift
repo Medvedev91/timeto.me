afd36199d5084246169fb85dfd41bb4556dbfd2c
hi@timeto.me <hi@timeto.me>
2023-05-23 17:53:15 +0300

    I: Int limitMin()/limitMax()/limitMinMax()

--- ios_app/utils_apple.swift
+++ ios_app/utils_apple.swift
@@ -200,6 +200,18 @@ extension Int {
     func min(_ test: Int) -> Int {
         self < test ? self : test
     }
+
+    func limitMin(_ value: Int) -> Int {
+        self < value ? value : self
+    }
+
+    func limitMax(_ value: Int) -> Int {
+        self > value ? value : self
+    }
+
+    func limitMinMax(_ min: Int, _ max: Int) -> Int {
+        limitMin(min).limitMax(max)
+    }
 }
 
 extension KotlinInt {
