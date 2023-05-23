7e215b6cc455b1d0710f94d3be8aa51831a2f58c
hi@timeto.me <hi@timeto.me>
2023-05-23 17:33:14 +0300

    R: Sheet.HeaderView() animation

--- android_app/src/main/java/me/timeto/app/ui/Sheet.kt
+++ android_app/src/main/java/me/timeto/app/ui/Sheet.kt
@@ -3,16 +3,16 @@ package me.timeto.app.ui
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
@@ -29,8 +30,10 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import me.timeto.app.ZStack
+import me.timeto.app.onePx
 import me.timeto.app.statusBarHeight
-import me.timeto.shared.min
+import me.timeto.shared.limitMinMax
 
 object Sheet {
 
@@ -84,15 +87,15 @@ object Sheet {
     ) {
 
         val isLight = MaterialTheme.colors.isLight
-        val bg = remember(isLight) { if (isLight) Color(0xFFF9F9F9) else Color(0xFF191919) }
-        val bgDivider = remember(isLight) { if (isLight) Color(0xFFE9E9E9) else Color(0xFF1F1F1F) }
-
-        val bgAlpha = (scrollToHeader.toFloat() / 50).min(1f)
-        val bgAnimate = animateColorAsState(bg.copy(alpha = bgAlpha))
-        val bgDividerAnimate = animateColorAsState(bgDivider.copy(alpha = bgAlpha))
+        val bgColor = remember(isLight) { if (isLight) Color(0xFFF9F9F9) else Color(0xFF191919) }
+        val dividerColor = remember(isLight) { if (isLight) Color(0xFFE9E9E9) else Color(0xFF1F1F1F) }
+        val alphaAnimate = animateFloatAsState((scrollToHeader.toFloat() / 50).limitMinMax(0f, 1f))
 
         Box(
-            modifier = Modifier.background(bgAnimate.value),
+            modifier = Modifier
+                .drawBehind {
+                    drawRect(color = bgColor.copy(alpha = alphaAnimate.value))
+                },
             contentAlignment = Alignment.BottomCenter // For divider
         ) {
 
@@ -148,9 +151,13 @@ object Sheet {
                 )
             }
 
-            Divider(
-                color = bgDividerAnimate.value,
-                thickness = 1.dp
+            ZStack(
+                modifier = Modifier
+                    .height(onePx)
+                    .fillMaxWidth()
+                    .drawBehind {
+                        drawRect(color = dividerColor.copy(alpha = alphaAnimate.value))
+                    },
             )
         }
     }
