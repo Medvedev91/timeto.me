d42160c0cbfecaac9ace09ed592a02be8d11ce88
hi@timeto.me <hi@timeto.me>
2023-05-30 16:53:42 +0300

    R: Clean FullScreenVM.kt

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/FullScreenVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/FullScreenVM.kt
@@ -30,6 +30,7 @@ class FullScreenVM : __VM<FullScreenVM.State>() {
         val timerData = TimerDataUI(interval, isCountdown, ColorNative.white)
 
         val activity = interval.getActivityDI()
+
         // todo or use interval.getTriggers()
         val textFeatures = (interval.note ?: activity.name).textFeatures()
         val title = textFeatures.textUi(withActivityEmoji = false, withTimer = false)
