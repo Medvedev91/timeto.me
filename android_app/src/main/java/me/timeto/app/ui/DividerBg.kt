627704f8af817beeff3be6d6ca531c5b30c4ad50
hi@timeto.me <hi@timeto.me>
2023-06-10 20:41:15 +0300

    I: DividerBg.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/DividerBg.kt
@@ -0,0 +1,24 @@
+package me.timeto.app.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import me.timeto.app.ZStack
+import me.timeto.app.onePx
+
+@Composable
+fun DividerBg(
+    modifier: Modifier = Modifier,
+    isVisible: Boolean = true,
+) {
+    ZStack(
+        modifier = modifier
+            .alpha(if (isVisible) 1f else 0f)
+            .height(onePx)
+            .fillMaxWidth()
+            .background(c.dividerBg),
+    )
+}
