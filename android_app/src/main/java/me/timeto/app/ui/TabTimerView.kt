35274047a1244726462d2fc8f08c8e91890cd073
hi@timeto.me <hi@timeto.me>
2023-06-29 11:46:31 +0300

    R: Clean TabTimerView.kt

--- android_app/src/main/java/me/timeto/app/ui/TabTimerView.kt
+++ android_app/src/main/java/me/timeto/app/ui/TabTimerView.kt
@@ -5,7 +5,6 @@ import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
