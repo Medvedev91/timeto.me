9b61501e5243d195676cfd6448cdbba1761e9380
hi@timeto.me <hi@timeto.me>
2023-07-02 13:33:20 +0300

    I: FocusModeVM.State.restartText

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/FocusModeVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/FocusModeVM.kt
@@ -30,6 +30,8 @@ class FocusModeVM : __VM<FocusModeVM.State>() {
 
         val activity = interval.getActivityDI()
 
+        val restartText = interval.deadline.toTimerHintNote(isShort = true)
+
         // todo or use interval.getTriggers()
         val textFeatures = (interval.note ?: activity.name).textFeatures()
         val title = textFeatures.textUi(withActivityEmoji = false, withTimer = false)
