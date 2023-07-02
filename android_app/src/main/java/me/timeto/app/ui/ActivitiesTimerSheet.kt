fa2fd8f15f311f7e70508d131204c2f4c9276667
hi@timeto.me <hi@timeto.me>
2023-07-02 08:30:32 +0300

    R: Clean ActivitiesTimerSheet.kt

--- android_app/src/main/java/me/timeto/app/ui/ActivitiesTimerSheet.kt
+++ android_app/src/main/java/me/timeto/app/ui/ActivitiesTimerSheet.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
