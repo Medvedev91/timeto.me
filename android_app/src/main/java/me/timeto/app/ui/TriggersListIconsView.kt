0362700e81656bf02ff6a84102ff433ef952a7c5
hi@timeto.me <hi@timeto.me>
2023-07-19 10:15:01 +0300

    UI: TriggersListIconsView.kt no clickable

--- android_app/src/main/java/me/timeto/app/ui/TriggersListIconsView.kt
+++ android_app/src/main/java/me/timeto/app/ui/TriggersListIconsView.kt
@@ -1,15 +1,12 @@
 package me.timeto.app.ui
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import me.timeto.app.HStack
-import me.timeto.app.roundedShape
 import me.timeto.shared.TextFeatures
 
 @Composable
@@ -28,10 +25,6 @@ fun TriggersListIconsView(
             Text(
                 text = trigger.emoji,
                 modifier = Modifier
-                    .clip(roundedShape)
-                    .clickable {
-                        trigger.performUI()
-                    }
                     .padding(horizontal = 3.dp),
                 fontSize = fontSize,
             )
