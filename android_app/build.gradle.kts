a0b6de0dda214b487398dfc44680317896f58b3a
hi@timeto.me <hi@timeto.me>
2023-07-31 08:21:26 +0300

    R: Remove androidx.navigation:navigation-compose

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -34,5 +34,4 @@ dependencies {
     implementation("androidx.compose.material:material:1.4.3")
     implementation("androidx.compose.material:material-icons-extended:1.4.3")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.navigation:navigation-compose:2.5.3")
 }
