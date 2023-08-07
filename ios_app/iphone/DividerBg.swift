82fc83ac04a2c53457df8e1abdc8f583d83a0d24
hi@timeto.me <hi@timeto.me>
2023-08-07 20:46:03 +0300

    R: By Divider.swift DividerBg.swift/DividerFg.swift

--- ios_app/iphone/DividerBg.swift
+++ ios_app/iphone/DividerBg.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct DividerBg: View {
 
     var body: some View {
-        c.dividerBg
-                .frame(height: onePx)
+        Divider(color: c.dividerBg)
     }
 }
--- ios_app/iphone/DividerFg.swift
+++ ios_app/iphone/DividerFg.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct DividerFg: View {
 
     var body: some View {
-        c.dividerFg
-                .frame(height: onePx)
+        Divider(color: c.dividerFg)
     }
 }
