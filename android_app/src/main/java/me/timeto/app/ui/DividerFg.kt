e55d7f430d5e19ddd2dcfa60b4388ff6ba8339e7
hi@timeto.me <hi@timeto.me>
2023-08-07 20:58:33 +0300

    R: By Divider.kt DividerBg.kt/DividerFg.kt

--- android_app/src/main/java/me/timeto/app/ui/DividerFg.kt
+++ android_app/src/main/java/me/timeto/app/ui/DividerFg.kt
@@ -1,25 +1,13 @@
 package me.timeto.app.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import me.timeto.app.ZStack
 import me.timeto.app.c
-import me.timeto.app.onePx
 
 @Composable
 fun DividerFg(
     modifier: Modifier = Modifier,
     isVisible: Boolean = true,
 ) {
-    ZStack(
-        modifier = modifier
-            .alpha(if (isVisible) 1f else 0f)
-            .height(onePx)
-            .fillMaxWidth()
-            .background(c.dividerFg),
-    )
+    Divider(c.dividerFg, modifier, isVisible)
 }
