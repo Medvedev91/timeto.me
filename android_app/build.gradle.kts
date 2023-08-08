a9bdbaac6059cc502a87ffae72f934fe206b4b4b
hi@timeto.me <hi@timeto.me>
2023-08-09 02:15:14 +0300

    Release: b361 v2023.08.07 ios

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 33
-        versionCode = 360
-        versionName = "2023.08.07"
+        versionCode = 362
+        versionName = "2023.08.08"
     }
 
     buildTypes {
