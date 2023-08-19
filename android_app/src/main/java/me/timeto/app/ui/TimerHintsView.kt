683d0817f0ff381b4f9c2542b9c68c2a1e370e79
hi@timeto.me <hi@timeto.me>
2023-08-19 10:12:49 +0300

    UI: Remove TimerHintsView.kt isPrimary

--- android_app/src/main/java/me/timeto/app/ui/TimerHintsView.kt
+++ android_app/src/main/java/me/timeto/app/ui/TimerHintsView.kt
@@ -1,6 +1,5 @@
 package me.timeto.app.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
@@ -12,7 +11,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import me.timeto.app.HStack
 import me.timeto.app.c
 import me.timeto.app.roundedShape
@@ -29,24 +27,20 @@ fun TimerHintsView(
 ) {
     HStack(modifier = modifier) {
         timerHintsUI.forEach { hintUI ->
-            val isPrimary = hintUI.isPrimary
-            val hPadding = if (isPrimary) 6.dp else hintHPadding
             Text(
                 text = hintUI.text,
                 modifier = Modifier
-                    .padding(start = if (isPrimary) 2.dp else 0.dp)
                     .clip(roundedShape)
                     .align(Alignment.CenterVertically)
-                    .background(if (isPrimary) c.blue else c.transparent)
                     .clickable {
                         hintUI.startInterval {
                             onStart()
                         }
                     }
-                    .padding(start = hPadding, end = hPadding, top = 3.dp, bottom = 4.dp),
-                color = if (isPrimary) c.white else c.blue,
-                fontSize = if (isPrimary) (fontSize.value - 1).sp else fontSize,
-                fontWeight = if (isPrimary) FontWeight.Medium else fontWeight,
+                    .padding(start = hintHPadding, end = hintHPadding, top = 3.dp, bottom = 4.dp),
+                color = c.blue,
+                fontSize = fontSize,
+                fontWeight = fontWeight,
             )
         }
     }
