28cfaee06c1d837a383ba66c44261874ee018fbc
hi@timeto.me <hi@timeto.me>
2023-09-02 22:22:17 +0300

    UI: MainView.kt tasks text top padding

--- android_app/src/main/java/me/timeto/app/ui/MainView.kt
+++ android_app/src/main/java/me/timeto/app/ui/MainView.kt
@@ -362,7 +362,7 @@ fun MainView() {
                         text = state.tasksText,
                         modifier = Modifier
                             .height(taskCountsHeight)
-                            .padding(top = 2.dp),
+                            .padding(top = 3.dp),
                         color = menuColor,
                         fontSize = 13.sp,
                         fontWeight = FontWeight.Light,
