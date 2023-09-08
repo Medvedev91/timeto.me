6a942b970b8c7a7f0e2abf6f302a913a969fdbaa
hi@timeto.me <hi@timeto.me>
2023-09-08 12:21:52 +0300

    UI: ActivitiesTimerSheet.swift let topContentPadding = 8.0

--- ios_app/iphone/ActivitiesTimerSheet.swift
+++ ios_app/iphone/ActivitiesTimerSheet.swift
@@ -34,7 +34,7 @@ extension TimetoSheet {
 
 private let bgColor = c.sheetBg
 private let listItemHeight = 46.0
-private let topContentPadding = 2.0
+private let topContentPadding = 8.0
 private let bottomContentPadding = 36.0
 
 private let activityItemEmojiWidth = 30.0
