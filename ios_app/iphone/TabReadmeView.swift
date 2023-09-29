92446d4cebb205a7ab8b82acf45cef4fa2854f17
hi@timeto.me <hi@timeto.me>
2023-09-29 08:57:03 +0300

    R: TabReadmeView.swift

--- ios_app/iphone/TabReadmeView.swift
+++ ios_app/iphone/TabReadmeView.swift
@@ -23,8 +23,8 @@ struct TabReadmeView: View {
             RQuote("All we need to be productive is mental energy, not extra time."),
             RParagraph(
                     Text("It is the most important point. ") +
-                            Text("We have enough time, but not enough energy. ").rBold() +
-                            Text("Even if you have extra time, you can't do anything without energy.")
+                    Text("We have enough time, but not enough energy. ").rBold() +
+                    Text("Even if you have extra time, you can't do anything without energy.")
             ),
             RParagraph(Text("Unfortunately, it is almost impossible to increase mental energy, but there are ways to save it. I will show you where we lose the most energy and how to prevent it.")),
             RParagraph(Text("The system is a set of practices on how to save mental energy, focus on tasks and follow long-term goals.")),
@@ -41,7 +41,7 @@ struct TabReadmeView: View {
 
             RParagraph(
                     Text("Don't skip practices. ").rBold() +
-                            Text("All practices depend on each other. The first two eliminate energy leaks and distractions. The third one helps you focus on tasks. The fourth frees your mind from storing everything."),
+                    Text("All practices depend on each other. The first two eliminate energy leaks and distractions. The third one helps you focus on tasks. The fourth frees your mind from storing everything."),
                     paddingTop: 20
             ),
 
@@ -53,13 +53,13 @@ struct TabReadmeView: View {
             RParagraph(Text("Don't try to get rid of this addiction completely. It will get worse. It's like gravity. The longer you hang on the bar, the harder it is to resist. Eventually, you will fall.")),
             RParagraph(
                     Text("To limit social media, schedule time for it. ").rBold() +
-                            Text("Read news, watch youtube, tv, etc., always at a certain time. I do it once a day at 8 p.m.")
+                    Text("Read news, watch youtube, tv, etc., always at a certain time. I do it once a day at 8 p.m.")
             ),
             RParagraph(Text("Be careful, at first, you will feel uncomfortable and try to replace social media. After a few days, you will get used to it. As a bonus, you will notice that you enjoy social media more.")),
             RParagraph(Text("I don't use any software to limit social media. It brings additional problems. For example, if I need to watch a lesson on youtube, I have to fight the software.")),
             RParagraph(
                     Text("What should I do if I find myself on social media? ").rBold() +
-                            Text("It still happens to me. When I notice it, I think, \"Stop! I'll wait until 8 p.m. to continue.\"")
+                    Text("It still happens to me. When I notice it, I think, \"Stop! I'll wait until 8 p.m. to continue.\"")
             ),
             RParagraph(Text("It's especially bad to use social media in the morning, even for a minute. This is when you have the most energy, so spend it on useful things.")),
             RParagraph(Text("Don't use social media to rest, it drains your energy. You will feel even worse.")),
@@ -68,20 +68,20 @@ struct TabReadmeView: View {
             RParagraph(Text("Notifications became part of our lives. But it also has a negative side.")),
             RParagraph(
                     Text("Notifications provoke thoughts irrelevant to the task. ")
-                            + Text("So if you see or hear a notification, you are already interrupted.").rBold()
+                    + Text("So if you see or hear a notification, you are already interrupted.").rBold()
             ),
 
             RParagraph(Text("This is how I manage notifications:")),
 
             RParagraph(
                     Text("PC: ").rBold() +
-                            Text("all communication tools are always closed. Including email, messaging apps, etc. Except when they are required for the current task. I check email once a day at 12 noon.")
+                    Text("all communication tools are always closed. Including email, messaging apps, etc. Except when they are required for the current task. I check email once a day at 12 noon.")
             ),
             RParagraph(
                     Text("Smartphone: ").rBold() +
-                            Text("always in \"Do Not Disturb\" mode. Exception when I'm waiting for a call or message. I added ") +
-                            Text("Wyou ").rBold() +
-                            Text("app to \"ALLOWED NOTIFICATIONS\" to receive notifications from the timer. The rest of the notifications, including IM, I check between tasks.")
+                    Text("always in \"Do Not Disturb\" mode. Exception when I'm waiting for a call or message. I added ") +
+                    Text("Wyou ").rBold() +
+                    Text("app to \"ALLOWED NOTIFICATIONS\" to receive notifications from the timer. The rest of the notifications, including IM, I check between tasks.")
             ),
             RParagraph(Text("At first, turn off notifications for a few hours a day. Then increases until you turn it off completely. You may feel anxious, that's okay. In a few days, you will feel better than before.")),
             RParagraph(Text("Turning off notifications is a hard but necessary step to eliminate distractions. Just imagine: nothing distracts you, you can fully focus on your tasks.")),
@@ -93,8 +93,8 @@ struct TabReadmeView: View {
             RParagraph(Text("The main feature of this app is that there is no \"stop\" option. Once you have completed one activity, you have to set a timer for the next one, even if it's a \"sleeping\" activity.")),
             RParagraph(
                     Text("This time tracking approach provides real data on how long everything takes. You can see it on the ")
-                            + Text("Chart").rBold()
-                            + Text(" screen. It's always surprising.")
+                    + Text("Chart").rBold()
+                    + Text(" screen. It's always surprising.")
             ),
             RParagraph(Text("By default, the app contains the activities that I use: meditation, work, music, personal development, exercises, walk, getting ready, sleep / rest, other. That's all I need, but you can change it.")),
             RParagraph(Text("Tips:")),
@@ -106,27 +106,27 @@ struct TabReadmeView: View {
             RParagraph(Text("Task list frees your mind from storing everything. It is the most important part of the system.")),
             RParagraph(
                     Text("I designed this app to cover the best practices I use. Everything to manage the task list is under the ")
-                            + Text("Tasks").rBold()
-                            + Text(" tab. There are 5 folders:")
+                    + Text("Tasks").rBold()
+                    + Text(" tab. There are 5 folders:")
             ),
 
             RParagraph(Text("1. Calendar").rBold(), paddingTop: 40),
             RParagraph(
                     Text("Typical calendar. Set a date and the task will automatically move to the ")
-                            + Text("Today").rBold()
-                            + Text(" on the set date.")
+                    + Text("Today").rBold()
+                    + Text(" on the set date.")
             ),
 
             RParagraph(Text("2. Repeating tasks").rBold(), paddingTop: 40),
             RParagraph(
                     Text("Tasks will be automatically moved to the ")
-                            + Text("Today").rBold()
-                            + Text(" at specified intervals (every day, day of week, etc).")
+                    + Text("Today").rBold()
+                    + Text(" at specified intervals (every day, day of week, etc).")
             ),
             RParagraph(
                     Text("By default, the app contains some of repeating tasks that I use. In fact, there are more than 20 tasks on my ")
-                            + Text("repeating").rBold()
-                            + Text(" list, like pay for the Internet, water the cactus, etc. Try to write out all your repeating tasks, I'm sure there will be as many. Just imagine how much you keep in your head. Free your mind from that.")
+                    + Text("repeating").rBold()
+                    + Text(" list, like pay for the Internet, water the cactus, etc. Try to write out all your repeating tasks, I'm sure there will be as many. Just imagine how much you keep in your head. Free your mind from that.")
             ),
 
             RParagraph(Text("3. Inbox").rBold(), paddingTop: 40),
@@ -136,8 +136,8 @@ struct TabReadmeView: View {
             RParagraph(Text("Adding everything to the task list is a habit that takes a long time to develop. It takes months. But it's useful right from the start.")),
             RParagraph(
                     Text("This is right if ")
-                            + Text("Inbox").rBold()
-                            + Text(" has a lot of tasks, if not it signals that you don't write everything. I have up to 50 tasks in the inbox. Just imagine if I kept it all in my head.")
+                    + Text("Inbox").rBold()
+                    + Text(" has a lot of tasks, if not it signals that you don't write everything. I have up to 50 tasks in the inbox. Just imagine if I kept it all in my head.")
             ),
 
             RParagraph(Text("4. Week").rBold(), paddingTop: 40),
@@ -149,20 +149,20 @@ struct TabReadmeView: View {
             RParagraph(Text("This is the most used list, I make it every morning and look at it every hour.")),
             RParagraph(
                     Text("I try to do everything from ")
-                            + Text("Today").rBold()
-                            + Text(" in the first 6 hours after I wake up. If I have done everything from ")
-                            + Text("Today").rBold()
-                            + Text(", I do tasks from ")
-                            + Text("Week").rBold()
-                            + Text(" or ")
-                            + Text("Inbox.").rBold()
+                    + Text("Today").rBold()
+                    + Text(" in the first 6 hours after I wake up. If I have done everything from ")
+                    + Text("Today").rBold()
+                    + Text(", I do tasks from ")
+                    + Text("Week").rBold()
+                    + Text(" or ")
+                    + Text("Inbox.").rBold()
             ),
             RDivider(paddingTop: 40),
 
             RParagraph(Text("Before you start using the task list, I want to warn you about the biggest danger - hidden task lists.")),
             RParagraph(
                     Text("Hidden task lists ")
-                            + Text(" is where you actually get tasks from. It can be a messenger, email, your brain, and so on. The more tasks you take from hidden lists, the less relevant the right list is. Eventually you quit using the right list. To avoid this, put all tasks into one list and take tasks only from it.")
+                    + Text(" is where you actually get tasks from. It can be a messenger, email, your brain, and so on. The more tasks you take from hidden lists, the less relevant the right list is. Eventually you quit using the right list. To avoid this, put all tasks into one list and take tasks only from it.")
             ),
             RParagraph(Text("Task list is powerful tool. The longer you use it, the more useful it is. Once I got used to it, my life changed. Please give this practice most of your attention, it is worth it.")),
 
@@ -205,13 +205,7 @@ struct TabReadmeView: View {
 
                     }
             )
-                    .background(
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 99, style: .circular)
-                                        .fill(Color(.systemBackground))
-                            }
-                                    .frame(width: 35, height: 35)
-                    )
+                    .frame(width: 35, height: 35)
                     .padding(.trailing, 30)
         }
     }
