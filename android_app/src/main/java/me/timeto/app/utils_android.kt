f8c3edfdff4d5f0e2e3e8fff3909ecb15c1b1644
hi@timeto.me <hi@timeto.me>
2023-10-01 17:44:24 +0300

    I: utils_android.kt c.homeGoalBg

--- android_app/src/main/java/me/timeto/app/utils_android.kt
+++ android_app/src/main/java/me/timeto/app/utils_android.kt
@@ -217,6 +217,7 @@ object c {
 
     val homeFontSecondary = ColorRgba.homeFontSecondary.toColor()
     val homeMenuTime = ColorRgba.homeMenuTime.toColor()
+    val homeGoalBg = ColorRgba.homeGoalBg.toColor()
 
     val tasksDropFocused = ColorRgba.tasksDropFocused.toColor()
     val formButtonRightNoteText = ColorRgba.formButtonRightNoteText.toColor()
