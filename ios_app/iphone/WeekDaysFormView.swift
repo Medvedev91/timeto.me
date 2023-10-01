9c4ec8be2e6ced2dfa6d6bfe9ad4ddd1d30d87cf
hi@timeto.me <hi@timeto.me>
2023-10-01 21:20:46 +0300

    I: WeekDaysFormView.swift

---
+++ ios_app/iphone/WeekDaysFormView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import shared
+
+struct WeekDaysFormView: View {
+
+    let weekDays: [KotlinInt]
+    let size: CGFloat
+    let onChange: ([KotlinInt]) -> Void
+
+    var body: some View {
+
+        let formUI = WeekDaysFormUI(weekDays: weekDays)
+
+        HStack(spacing: 10) {
+
+            ForEach(formUI.weekDaysUI, id: \.idx) { weekDayUI in
+
+                let isSelected = weekDayUI.isSelected
+
+                Button(
+                        action: {
+                            withAnimation {
+                                onChange(formUI.toggleWeekDay(idx: weekDayUI.idx))
+                            }
+                        },
+                        label: {
+                            Text(weekDayUI.title)
+                                    .font(.system(size: 16))
+                        }
+                )
+                        .foregroundColor(isSelected ? .white : .primary)
+                        .frame(width: 32, height: 32)
+                        .background(
+                                ZStack {
+                                    if (isSelected) {
+                                        roundedShape.fill(.blue)
+                                    } else {
+                                        roundedShape.stroke(.primary, lineWidth: 1)
+                                    }
+                                }
+                        )
+            }
+
+            Spacer()
+        }
+    }
+}
