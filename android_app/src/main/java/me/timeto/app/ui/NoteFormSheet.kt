ae23be9b21bed253a5edc99e7f7490503d1e8d03
hi@timeto.me <hi@timeto.me>
2023-10-03 13:53:42 +0300

    I: NoteFormSheet.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/NoteFormSheet.kt
@@ -0,0 +1,63 @@
+package me.timeto.app.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import me.timeto.app.VStack
+import me.timeto.app.c
+import me.timeto.app.rememberVM
+import me.timeto.shared.db.NoteModel
+import me.timeto.shared.vm.NoteFormSheetVM
+
+@Composable
+fun NoteFormSheet(
+    layer: WrapperView.Layer,
+    note: NoteModel?,
+) {
+
+    val (vm, state) = rememberVM(note) { NoteFormSheetVM(note) }
+
+    VStack(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(c.sheetBg)
+    ) {
+
+        Sheet__HeaderView(
+            title = state.headerTitle,
+            scrollState = null,
+            bgColor = c.sheetBg,
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+
+            MyListView__ItemView(
+                isFirst = true,
+                isLast = true,
+            ) {
+                MyListView__ItemView__TextInputView(
+                    placeholder = state.inputTextPlaceholder,
+                    text = state.inputTextValue,
+                    onTextChanged = { newText -> vm.setInputText(newText) },
+                )
+            }
+        }
+
+        Sheet__BottomViewDefault(
+            primaryText = state.doneTitle,
+            primaryAction = {
+                vm.save {
+                    layer.close()
+                }
+            },
+            secondaryText = "Cancel",
+            secondaryAction = {
+                layer.close()
+            },
+        )
+    }
+}
