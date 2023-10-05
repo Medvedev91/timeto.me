006c8256e4d9449bd13aca6a7faf5687b7ab9ad5
hi@timeto.me <hi@timeto.me>
2023-10-05 16:21:14 +0300

    UI: SummarySheet.kt period hints

--- android_app/src/main/java/me/timeto/app/ui/SummarySheet.kt
+++ android_app/src/main/java/me/timeto/app/ui/SummarySheet.kt
@@ -2,9 +2,7 @@ package me.timeto.app.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -16,11 +14,17 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.timeto.app.*
+import me.timeto.app.R
 import me.timeto.shared.UnixTime
 import me.timeto.shared.vm.SummarySheetVM
+
+private val periodHintsHeight = 40.dp
+private val periodHintShape = SquircleShape(len = 50f)
+private val periodHintBg = c.sheetBg.copy(alpha = 0.8f)
 
 @Composable
 fun SummarySheet(
@@ -35,7 +39,35 @@ fun SummarySheet(
             .background(c.sheetBg),
     ) {
 
-        SpacerW1()
+        ZStack(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+        ) {
+
+            HStack(
+                modifier = Modifier
+                    .height(periodHintsHeight)
+                    .align(Alignment.BottomCenter),
+            ) {
+                state.periodHints.forEach { period ->
+                    Text(
+                        period.title,
+                        modifier = Modifier
+                            .clip(periodHintShape)
+                            .clickable {
+                                vm.setPeriod(period.pickerTimeStart, period.pickerTimeFinish)
+                            }
+                            .background(periodHintBg)
+                            .padding(start = 8.dp, end = 8.dp, top = 6.dp, bottom = 7.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 12.sp,
+                        fontWeight = if (period.isActive) FontWeight.Black else FontWeight.Light,
+                        color = if (period.isActive) c.white else c.text,
+                    )
+                }
+            }
+        }
 
         Sheet__BottomView {
 
