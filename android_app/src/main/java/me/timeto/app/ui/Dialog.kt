1bacae802e001e9e9e57011ad71e899c8a31bdd6
hi@timeto.me <hi@timeto.me>
2023-10-05 12:38:24 +0300

    R: Clean Dialog.kt

--- android_app/src/main/java/me/timeto/app/ui/Dialog.kt
+++ android_app/src/main/java/me/timeto/app/ui/Dialog.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
