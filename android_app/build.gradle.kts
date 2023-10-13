ded04b0b06aa058cac105b280c2e571f50c81435
hi@timeto.me <hi@timeto.me>
2023-10-13 17:19:56 +0300

    Build 2023.10.13.433 F-Droid

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 33
-        versionCode = 432
+        versionCode = 433
         versionName = "2023.10.13"
     }
 
