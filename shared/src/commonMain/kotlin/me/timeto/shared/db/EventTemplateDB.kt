836b442367131964377e9c370164ba6ced1927da
hi@timeto.me <hi@timeto.me>
2023-11-16 14:34:38 +0300

    EventTemplateDB.kt: Init EventTemplateDB.Companion.selectAscSortedFlow()

--- shared/src/commonMain/kotlin/me/timeto/shared/db/EventTemplateDB.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/db/EventTemplateDB.kt
@@ -1,6 +1,12 @@
 package me.timeto.shared.db
 
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import dbsq.EventTemplateSQ
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 data class EventTemplateDB(
     val id: Int,
@@ -14,6 +20,9 @@ data class EventTemplateDB(
         suspend fun selectAscSorted(): List<EventTemplateDB> = dbIO {
             db.eventTemplateQueries.selectAscSorted().executeAsList().map { it.toDB() }
         }
+
+        fun selectAscSortedFlow(): Flow<List<EventTemplateDB>> = db.eventTemplateQueries
+            .selectAscSorted().asFlow().mapToList(Dispatchers.IO).map { list -> list.map { it.toDB() } }
     }
 }
 
