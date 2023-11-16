49bffe7204fa44c7bd3b51aaa8d84f96f2ee96d2
hi@timeto.me <hi@timeto.me>
2023-11-16 17:34:20 +0300

    I: proguard-rules.pro

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
---
+++ android_app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# ktor https://youtrack.jetbrains.com/issue/KTOR-5528
+-dontwarn org.slf4j.impl.StaticLoggerBinder
