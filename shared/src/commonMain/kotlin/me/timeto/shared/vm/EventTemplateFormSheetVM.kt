48cb9f050b1df41879cdc3b05b64d1b17c4b2cde
hi@timeto.me <hi@timeto.me>
2023-11-17 15:50:30 +0300

    EventTemplateFormSheetVM.kt: Rename to EventTemplateFormSheetVM.State.daytime

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/EventTemplateFormSheetVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/EventTemplateFormSheetVM.kt
@@ -12,7 +12,7 @@ class EventTemplateFormSheetVM(
         val headerTitle: String,
         val doneText: String,
         val textFeatures: TextFeatures,
-        val dayTime: Int?,
+        val daytime: Int?,
     ) {
         val inputTextValue = textFeatures.textNoFeatures
         val deleteText = "Delete Template"
@@ -23,7 +23,7 @@ class EventTemplateFormSheetVM(
             headerTitle = if (eventTemplateDB != null) "Edit Template" else "New Template",
             doneText = "Save",
             textFeatures = (eventTemplateDB?.text ?: "").textFeatures(),
-            dayTime = eventTemplateDB?.daytime,
+            daytime = eventTemplateDB?.daytime,
         )
     )
 
@@ -40,7 +40,7 @@ class EventTemplateFormSheetVM(
     ) {
         scopeVM().launchEx {
             try {
-                val daytime = state.value.dayTime ?: throw UIException("The time is not set")
+                val daytime = state.value.daytime ?: throw UIException("The time is not set")
                 val textFeatures = state.value.textFeatures
                 val textWithFeatures = textFeatures.textWithFeatures()
                 if (textFeatures.textNoFeatures.isBlank())
