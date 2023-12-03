156b9d281647dd1d476c6a969ffd47b9f41a6351
hi@timeto.me <hi@timeto.me>
2023-12-03 23:49:52 +0300

    Release Android 2023.12.03.449

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 448
+        versionCode = 449
         versionName = "2023.12.03"
     }
 
