c1dfbffb83ed86103b8f9c6350e51a368a0cd2fc
hi@timeto.me <hi@timeto.me>
2023-12-03 23:57:41 +0300

    atm_kmp.kt: json

--- shared/src/commonMain/kotlin/me/timeto/shared/atm_kmp.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/atm_kmp.kt
@@ -1,3 +1,31 @@
 package me.timeto.shared
 
+import kotlinx.serialization.json.*
+
 fun zlog(message: Any?) = println(";; ${message.toString().replace("\n", "\n;; ")}")
+
+///
+/// Json
+
+fun JsonObject.getInt(key: String): Int = this[key]!!.jsonPrimitive.int
+fun JsonObject.getString(key: String): String = this[key]!!.jsonPrimitive.content
+fun JsonObject.getStringOrNull(key: String): String? = this[key]!!.jsonPrimitive.contentOrNull
+fun JsonObject.getBoolean(key: String): Boolean = this[key]!!.jsonPrimitive.boolean
+fun JsonObject.getIntArray(key: String): List<Int> = this[key]!!.jsonArray.map { it.jsonPrimitive.int }
+
+fun JsonArray.getInt(index: Int): Int = this[index].jsonPrimitive.int
+fun JsonArray.getIntOrNull(index: Int): Int? = this[index].jsonPrimitive.intOrNull
+fun JsonArray.getString(index: Int): String = this[index].jsonPrimitive.content
+fun JsonArray.getStringOrNull(index: Int): String? = this[index].jsonPrimitive.contentOrNull
+
+fun List<*>.toJsonArray() = JsonArray(
+    this.map { item ->
+        when (item) {
+            is JsonElement -> item
+            is String -> JsonPrimitive(item)
+            is Int -> JsonPrimitive(item)
+            null -> JsonNull
+            else -> throw Exception() // todo report
+        }
+    }
+)
--- shared/src/commonMain/kotlin/me/timeto/shared/utils.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/utils.kt
@@ -11,7 +11,6 @@ import io.ktor.utils.io.core.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.datetime.*
-import kotlinx.serialization.json.*
 import me.timeto.appdbsq.TimetomeDB
 import me.timeto.shared.db.*
 import me.timeto.shared.db.KVModel.Companion.asDayStartOffsetSeconds
@@ -120,32 +119,6 @@ fun String.removeDuplicateSpaces() = this.replace(duplicateSpacesRegex, " ")
 
 fun getSoundTimerExpiredFileName(withExtension: Boolean): String =
     "sound_timer_expired${if (withExtension) ".mp3" else ""}"
-
-///
-/// Json
-
-fun JsonObject.getInt(key: String): Int = this[key]!!.jsonPrimitive.int
-fun JsonObject.getString(key: String): String = this[key]!!.jsonPrimitive.content
-fun JsonObject.getStringOrNull(key: String): String? = this[key]!!.jsonPrimitive.contentOrNull
-fun JsonObject.getBoolean(key: String): Boolean = this[key]!!.jsonPrimitive.boolean
-fun JsonObject.getIntArray(key: String): List<Int> = this[key]!!.jsonArray.map { it.jsonPrimitive.int }
-
-fun JsonArray.getInt(index: Int): Int = this[index].jsonPrimitive.int
-fun JsonArray.getIntOrNull(index: Int): Int? = this[index].jsonPrimitive.intOrNull
-fun JsonArray.getString(index: Int): String = this[index].jsonPrimitive.content
-fun JsonArray.getStringOrNull(index: Int): String? = this[index].jsonPrimitive.contentOrNull
-
-fun List<*>.toJsonArray() = JsonArray(
-    this.map { item ->
-        when (item) {
-            is JsonElement -> item
-            is String -> JsonPrimitive(item)
-            is Int -> JsonPrimitive(item)
-            null -> JsonNull
-            else -> throw Exception() // todo report
-        }
-    }
-)
 
 /**
  * File names without extension must be unique!
