88d3149e1c18cb5304a4d2d67f3097cdc7fd202f
hi@timeto.me <hi@timeto.me>
2023-12-15 11:21:30 +0300

    atm_apple.swift:

--- ios_app/atm_apple.swift
+++ ios_app/atm_apple.swift
@@ -34,9 +34,9 @@ struct VMView<VMState: AnyObject, Content: View>: View {
     private let stack: StackType
 
     init(
-            vm: __VM<VMState>,
-            stack: StackType = .ZStack(),
-            @ViewBuilder content: @escaping (VMState) -> Content
+        vm: __VM<VMState>,
+        stack: StackType = .ZStack(),
+        @ViewBuilder content: @escaping (VMState) -> Content
     ) {
         self.vm = vm
         state = vm.state.value as! VMState
@@ -49,23 +49,29 @@ struct VMView<VMState: AnyObject, Content: View>: View {
         ZStack {
             switch stack {
             case .ZStack(let p1):
-                ZStack(alignment: p1) { content(state) }
+                ZStack(alignment: p1) {
+                    content(state)
+                }
             case .VStack(let p1, let p2):
-                VStack(alignment: p1, spacing: p2) { content(state) }
+                VStack(alignment: p1, spacing: p2) {
+                    content(state)
+                }
             case .HStack(let p1, let p2):
-                HStack(alignment: p1, spacing: p2) { content(state) }
+                HStack(alignment: p1, spacing: p2) {
+                    content(state)
+                }
             }
         }
-                /// In onAppear() because init() is called frequently even the
-                /// view is not showed. "Unnecessary" calls is 90% times.
-                /// Yes, onAppear() calls too late but the default values DI saves.
+            // In onAppear() because init() is called frequently even the
+            // view is not showed. "Unnecessary" calls is 90% times.
+            // Yes, onAppear() calls too late but the default values DI saves.
                 .onAppear {
                     vm.onAppear()
                 }
                 .onDisappear {
                     vm.onDisappear()
                 }
-                //////
+                ////
                 .onReceive(publisher) { res in
                     state = res
                 }
