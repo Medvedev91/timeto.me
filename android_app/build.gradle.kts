3f6814a67b0ba5452277f1e7830b20eaa623c9ac
hi@timeto.me <hi@timeto.me>
2023-12-18 10:23:35 +0300

    F-Droid Release: 2023.12.18.458

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 457
+        versionCode = 458
         versionName = "2023.12.18"
     }
 
