c755a24f7f9ae260b2df6c5c588b05454d6a1d99
hi@timeto.me <hi@timeto.me>
2024-01-19 12:01:59 +0300

    build.gradle.kts: outputFileName = "$name.apk"

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     applicationVariants.all {
         outputs.all {
             this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
-            outputFileName = "$flavorName-release.apk"
+            outputFileName = "$name.apk"
         }
     }
 
