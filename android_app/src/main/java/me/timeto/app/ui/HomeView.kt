79af18dcf865e17ed1260f774a9f9d0c8cb7d3d9
hi@timeto.me <hi@timeto.me>
2024-01-28 16:23:39 +0300

    HomeView.kt:

--- android_app/src/main/java/me/timeto/app/ui/HomeView.kt
+++ android_app/src/main/java/me/timeto/app/ui/HomeView.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ExpandCircleDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.*
 import androidx.compose.ui.graphics.Brush
