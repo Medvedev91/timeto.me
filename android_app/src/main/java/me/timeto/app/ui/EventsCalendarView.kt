24818ed603115cf771f9f10041e6dee1146f7d61
hi@timeto.me <hi@timeto.me>
2024-02-07 22:24:21 +0300

    EventsCalendarView.kt:

--- android_app/src/main/java/me/timeto/app/ui/EventsCalendarView.kt
+++ android_app/src/main/java/me/timeto/app/ui/EventsCalendarView.kt
@@ -4,14 +4,12 @@ import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
