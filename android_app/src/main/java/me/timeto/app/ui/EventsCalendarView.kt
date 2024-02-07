75d5f047a85cd29d2dc1ae41e8118667a2fd3e42
hi@timeto.me <hi@timeto.me>
2024-02-07 10:37:11 +0300

    EventsCalendarView.kt: UI

--- android_app/src/main/java/me/timeto/app/ui/EventsCalendarView.kt
+++ android_app/src/main/java/me/timeto/app/ui/EventsCalendarView.kt
@@ -1,20 +1,88 @@
 package me.timeto.app.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import me.timeto.app.VStack
-import me.timeto.app.c
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.timeto.app.*
+import me.timeto.shared.vm.EventsCalendarVM
 
 @Composable
 fun EventsCalendarView(
     modifier: Modifier,
 ) {
 
+    val (_, state) = rememberVM { EventsCalendarVM() }
+
     VStack(
-        modifier = modifier,
+        modifier = modifier
+            .padding(start = H_PADDING, end = TasksView__PADDING_END),
     ) {
 
-        Text("todo", color = c.white)
+        LazyColumn {
+
+            state.months.forEach { month ->
+
+                item {
+
+                    HStack {
+
+                        repeat(month.emptyStartDaysCount) {
+                            SpacerW1()
+                        }
+
+                        Text(
+                            text = month.title,
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(top = 16.dp, bottom = 8.dp),
+                            color = c.white,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                        )
+
+                        repeat(month.emptyEndDaysCount) {
+                            SpacerW1()
+                        }
+                    }
+
+                    month.weeks.forEach { week ->
+
+                        HStack {
+
+                            week.forEach { day ->
+
+                                if (day == null) {
+                                    SpacerW1()
+                                }
+                                else {
+
+                                    VStack(
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .padding(bottom = 2.dp),
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                    ) {
+
+                                        DividerBg(Modifier.padding(bottom = 6.dp))
+
+                                        Text(
+                                            text = day.title,
+                                            color = c.white,
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
