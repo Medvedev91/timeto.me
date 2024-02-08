93a1e85bc963ed7c790225512233ea69685f8520
hi@timeto.me <hi@timeto.me>
2024-02-08 20:39:58 +0300

    EventsListEventView.swift

---
+++ ios_app/iphone/EventsListEventView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import shared
+
+struct EventsListEventView: View {
+    
+    let eventUi: EventsListVM.EventUi
+    let bgColor: Color
+    let paddingStart: CGFloat
+    let paddingEnd: CGFloat
+    let withTopDivider: Bool
+    
+    var body: some View {
+        
+        Text("ee")
+    }
+}
