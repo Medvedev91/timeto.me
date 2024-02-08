fb031c28a256e7b1433447d83673a0b8109b5c30
hi@timeto.me <hi@timeto.me>
2024-02-08 15:05:16 +0300

    EventsCalendarView.swift: Title headers

--- ios_app/iphone/EventsCalendarView.swift
+++ ios_app/iphone/EventsCalendarView.swift
@@ -2,13 +2,36 @@ import SwiftUI
 import shared
 
 struct EventsCalendarView: View {
-    
+
+    @State private var vm = EventsCalendarVM()
+
     var body: some View {
-        
-        Text("temp")
-        
-        VStack {
-            Spacer()
+
+        VMView(vm: vm) { state in
+
+            VStack {
+
+                VStack {
+
+                    HStack {
+
+                        ForEachIndexed(state.weekTitles) { _, weekTitle in
+
+                            HStack {
+                                Spacer()
+                                Text(weekTitle.title)
+                                        .font(.system(size: 11))
+                                        .foregroundColor(weekTitle.isBusiness ? c.text : c.textSecondary)
+                                Spacer()
+                            }
+                        }
+                    }
+
+                    DividerBg()
+                            .padding(.top, 2.0)
+                }
+            }
+                    .padding(.horizontal, H_PADDING)
         }
     }
 }
