9d06b53127d0d54a37103644583339546cfbe1fa
hi@timeto.me <hi@timeto.me>
2024-02-08 22:53:08 +0300

    EventsListEventView.swift:

--- ios_app/iphone/EventsListEventView.swift
+++ ios_app/iphone/EventsListEventView.swift
@@ -2,15 +2,71 @@ import SwiftUI
 import shared
 
 struct EventsListEventView: View {
-    
+
     let eventUi: EventsListVM.EventUi
     let bgColor: Color
     let paddingStart: CGFloat
     let paddingEnd: CGFloat
     let withTopDivider: Bool
-    
+
+    @EnvironmentObject private var nativeSheet: NativeSheet
+
     var body: some View {
-        
-        Text("ee")
+
+        MyListSwipeToActionItem(
+            deletionHint: eventUi.event.text,
+            deletionConfirmationNote: eventUi.deletionNote,
+            onEdit: {
+                nativeSheet.EventFormSheet__show(editedEvent: eventUi.event) {
+                }
+            },
+            onDelete: {
+                withAnimation {
+                    eventUi.delete()
+                }
+            }
+        ) {
+
+            AnyView(safeView)
+                    .padding(.leading, H_PADDING)
+                    // todo remove after removing MyListSwipeToActionItem()
+                    .background(c.bg)
+        }
+    }
+
+    private var safeView: some View {
+
+        VStack {
+
+            HStack {
+
+                Text(eventUi.dateString)
+                        .font(.system(size: 14, weight: .light))
+                        .foregroundColor(.secondary)
+
+                Spacer()
+
+                Text(eventUi.dayLeftString)
+                        .font(.system(size: 14, weight: .light))
+                        .foregroundColor(.secondary)
+            }
+
+            HStack {
+
+                Text(eventUi.listText)
+                        .lineSpacing(4)
+                        .multilineTextAlignment(.leading)
+                        .myMultilineText()
+
+                Spacer()
+
+                TriggersListIconsView(triggers: eventUi.textFeatures.triggers, fontSize: 15)
+            }
+                    .padding(.top, 4)
+        }
+                .padding(.top, 10)
+                .padding(.bottom, 10)
+                .foregroundColor(.primary)
+                .id("\(eventUi.event.id) \(eventUi.event.text)") /// #TruncationDynamic
     }
 }
