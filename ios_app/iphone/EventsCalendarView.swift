03e57149ad36c89319f127669fc2ab22a06b0933
hi@timeto.me <hi@timeto.me>
2024-02-08 13:32:02 +0300

    EventsCalendarView.swift

---
+++ ios_app/iphone/EventsCalendarView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import shared
+
+struct EventsCalendarView: View {
+    
+    var body: some View {
+        
+        Text("temp")
+        
+        VStack {
+            Spacer()
+        }
+    }
+}
