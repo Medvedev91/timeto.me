60eb3503ba17185da0b616796c681b90d3fd9e4a
hi@timeto.me <hi@timeto.me>
2024-02-11 00:31:54 +0300

    PrivacySheet.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/PrivacySheet.kt
@@ -0,0 +1,94 @@
+package me.timeto.app.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.timeto.app.H_PADDING
+import me.timeto.app.VStack
+import me.timeto.app.c
+import me.timeto.app.rememberVM
+import me.timeto.shared.vm.PrivacyVM
+
+@Composable
+fun PrivacySheet(
+    layer: WrapperView.Layer,
+) {
+
+    val (_, state) = rememberVM { PrivacyVM() }
+
+    VStack(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(c.sheetBg),
+    ) {
+
+        val scrollState = rememberScrollState()
+
+        Sheet__HeaderView(
+            title = state.headerTitle,
+            scrollState = scrollState,
+            bgColor = c.sheetBg,
+        )
+
+        VStack(
+            modifier = Modifier
+                .verticalScroll(state = scrollState)
+                .padding(bottom = 20.dp)
+                .weight(1f),
+        ) {
+
+            PView(state.text1, topPadding = 8.dp)
+
+            PView(state.text2)
+
+            PView(state.text3)
+
+            PView(state.text4, fontWeight = FontWeight.Bold)
+
+            PView(state.text5)
+
+            VStack(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .background(c.sheetFg)
+                    .padding(top = 4.dp, bottom = 12.dp),
+            ) {
+                state.sendItems.forEach { sendItem ->
+                    PView(sendItem, topPadding = 4.dp)
+                }
+            }
+
+            PView(state.text6)
+        }
+
+        Sheet__BottomViewClose {
+            layer.close()
+        }
+    }
+}
+
+@Composable
+private fun PView(
+    text: String,
+    topPadding: Dp = 16.dp,
+    fontWeight: FontWeight = FontWeight.Normal,
+) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .padding(horizontal = H_PADDING)
+            .padding(top = topPadding),
+        color = c.white,
+        lineHeight = 22.sp,
+        fontWeight = fontWeight,
+    )
+}
