c2b7056fc4bf3bd884cf0a5eb1a5813d7949a4a7
hi@timeto.me <hi@timeto.me>
2024-02-12 20:40:39 +0300

    PrivacySheetVM:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/PrivacySheetVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/PrivacySheetVM.kt
@@ -17,12 +17,12 @@ class PrivacySheetVM : __VM<PrivacySheetVM.State>() {
 
         val sendReportsTitle = "Send Reports${if (isSendReportsEnabled) "" else "  $prayEmoji"}"
 
-        val text1 = PrivacySheetVM.text1
-        val text2 = PrivacySheetVM.text2
-        val text3 = PrivacySheetVM.text3
-        val text4 = PrivacySheetVM.text4
-        val text5 = PrivacySheetVM.text5
-        val text6 = PrivacySheetVM.text6
+        val text1 = PrivacySheetVM.textHi
+        val text2 = PrivacySheetVM.textHere
+        val text3 = PrivacySheetVM.textRule
+        val text4 = PrivacySheetVM.textNever
+        val text5 = PrivacySheetVM.textOnly
+        val text6 = PrivacySheetVM.textKindly
 
         val sendItems: List<String> = PrivacySheetVM.sendItems
     }
@@ -57,12 +57,12 @@ class PrivacySheetVM : __VM<PrivacySheetVM.State>() {
 
         const val prayEmoji = "🙏"
 
-        const val text1 = "Hi!"
-        const val text2 = "Developer is here."
-        const val text3 = "The strongest rule I follow while making the app is \"Privacy with no compromise\". I believe that task management is extremely sensitive information. No one should have access to it."
-        const val text4 = "The app never sends any personal information!"
-        const val text5 = "The only thing the app sends is technical reports. That's all the data the app sends:"
-        const val text6 = "I kindly ask you 🙏 not to turn off sending reports. It is the only way I can know I have such great users like you, nothing else motivates me to keep going."
+        const val textHi = "Hi!"
+        const val textHere = "Developer is here."
+        const val textRule = "The strongest rule I follow while making the app is \"Privacy with no compromise\". I believe that task management is extremely sensitive information. No one should have access to it."
+        const val textNever = "The app never sends any personal information!"
+        const val textOnly = "The only thing the app sends is technical reports. That's all the data the app sends:"
+        const val textKindly = "I kindly ask you 🙏 not to turn off sending reports. It is the only way I can know I have such great users like you, nothing else motivates me to keep going."
 
         val sendItems: List<String> = listOf(
             "OS: ${deviceData.os}",
