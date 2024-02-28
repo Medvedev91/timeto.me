14e729aaed70de15b930532103de9fae067cb713
hi@timeto.me <hi@timeto.me>
2024-02-28 09:51:14 +0300

    Android Build: 2024.02.28.485

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 484
-        versionName = "2024.02.24"
+        versionCode = 485
+        versionName = "2024.02.28"
     }
 
     buildTypes {
