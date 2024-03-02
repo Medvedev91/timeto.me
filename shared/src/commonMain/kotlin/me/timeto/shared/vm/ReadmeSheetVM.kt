34a343128412708a09b330b6b8f4a6bd9151d8ce
hi@timeto.me <hi@timeto.me>
2024-03-02 15:52:58 +0300

    ReadmeSheetVM.kt: prepParagraphs():

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/ReadmeSheetVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/ReadmeSheetVM.kt
@@ -31,5 +31,9 @@ private fun prepParagraphs(): List<ReadmeSheetVM.Paragraph> {
     val paragraphs = mutableListOf<ReadmeSheetVM.Paragraph>()
     paragraphs.add(PText("Hi!"))
     paragraphs.add(PText("Developer is here  $developerEmoji"))
+    paragraphs.add(PText("I built this app to manage my productivity. Here I will show how I use it."))
+    paragraphs.add(PText("First of all, it is a pragmatic guide. I mean, it is NOT just a list of features, but my real day-to-day experience."))
+    paragraphs.add(PText("I will start with the most important things: timer, repeating tasks and checklists. That is 90% I use."))
+    paragraphs.add(PText("Enjoy!"))
     return paragraphs
 }
