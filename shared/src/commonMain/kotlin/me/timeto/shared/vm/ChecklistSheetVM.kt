e5c024e0d92fc1dc3781fb0489d739f757f46316
hi@timeto.me <hi@timeto.me>
2024-03-25 11:50:51 +0300

    ChecklistSheetVM.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/ChecklistSheetVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/ChecklistSheetVM.kt
@@ -4,32 +4,28 @@ import kotlinx.coroutines.flow.*
 import me.timeto.shared.DI
 import me.timeto.shared.db.ChecklistItemDb
 import me.timeto.shared.db.ChecklistDb
-import me.timeto.shared.launchExDefault
 import me.timeto.shared.onEachExIn
 
-// todo actions from view to vm
 class ChecklistSheetVM(
-    val checklist: ChecklistDb,
+    private val checklistDb: ChecklistDb,
 ) : __VM<ChecklistSheetVM.State>() {
 
     data class State(
+        val checklistDb: ChecklistDb,
         val items: List<ChecklistItemDb>,
     )
 
     override val state = MutableStateFlow(
-        State(items = DI.checklistItems.prepChecklistItems(checklist))
+        State(
+            checklistDb = checklistDb,
+            items = DI.checklistItems.prepChecklistItems(checklistDb),
+        )
     )
 
     override fun onAppear() {
         val scope = scopeVM()
         ChecklistItemDb.getAscFlow().onEachExIn(scope) { items ->
-            state.update { it.copy(items = items.prepChecklistItems(checklist)) }
-        }
-    }
-
-    fun uncheck() {
-        launchExDefault {
-            ChecklistItemDb.toggleByList(checklist, false)
+            state.update { it.copy(items = items.prepChecklistItems(checklistDb)) }
         }
     }
 }
