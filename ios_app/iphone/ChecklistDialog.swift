8213659bfac27b7a9c0b4ac26512264d2655cb7b
hi@timeto.me <hi@timeto.me>
2024-03-31 16:53:54 +0300

    ChecklistDialog.swift:

--- ios_app/iphone/ChecklistDialog.swift
+++ ios_app/iphone/ChecklistDialog.swift
@@ -2,145 +2,39 @@ import SwiftUI
 import shared
 
 struct ChecklistDialog: View {
-
-    @State private var vm: ChecklistDialogVM
-
+    
+    @State private var vm: ChecklistSheetVM
+    
     @Binding private var isPresented: Bool
-    private var checklist: ChecklistDb
-
-    @State private var isAddItemPresented = false
-
+    
     init(
-            isPresented: Binding<Bool>,
-            checklist: ChecklistDb
+        isPresented: Binding<Bool>,
+        checklist: ChecklistDb
     ) {
         _isPresented = isPresented
-        self.checklist = checklist
-        _vm = State(initialValue: ChecklistDialogVM(checklist: checklist))
+        _vm = State(initialValue: ChecklistSheetVM(checklistDb: checklist))
     }
-
+    
     var body: some View {
-
+        
         VMView(vm: vm, stack: .ZStack(alignment: .top)) { state in
-
-            let items = state.items
-
-            ScrollView(showsIndicators: false) {
-
-                VStack(spacing: 0) {
-
-                    Text(checklist.name)
-                            .font(.system(size: 28, weight: .bold))
-                            .padding(.top, 70)
-                            .padding(.bottom, 15)
-
-                    ForEach(items, id: \.id) { item in
-                        let isFirst = items.first == item
-                        MyListView__ItemView(
-                                isFirst: isFirst,
-                                isLast: items.last == item,
-                                withTopDivider: !isFirst
-                        ) {
-                            ChecklistView__ItemView(item: item)
-                        }
-                    }
-                }
-            }
-
-            HStack {
-
-                Button(
-                        action: {
-                            isPresented = false
-                        },
-                        label: { Text("Back") }
-                )
-                        .foregroundColor(.blue)
-                        .padding(.leading, 25)
-
-                Spacer()
-
-                let isCheckedExists = !items.filter { item in item.isChecked }.isEmpty
-                if isCheckedExists {
-                    Button(
-                            action: {
-                                vm.uncheck()
-                            },
-                            label: { Text("Uncheck") }
-                    )
-                            .foregroundColor(.blue)
-                            .padding(.trailing, 15)
-                }
-
-                Button(
-                        action: {
-                            isAddItemPresented = true
-                        },
-                        label: {
-                            Image(systemName: "plus")
-                                    .foregroundColor(.blue)
-                                    .padding(.trailing, 30)
-                        }
-                )
-            }
+            
+            VStack {
+                
+                Text(state.checklistDb.name)
+                    .font(.system(size: 28, weight: .bold))
                     .padding(.top, 20)
-        }
-                .background(c.sheetBg)
-                .sheetEnv(isPresented: $isAddItemPresented) {
-                    ChecklistItemFormSheet(isPresented: $isAddItemPresented, checklist: checklist, checklistItem: nil)
-                }
-    }
-}
-
-struct ChecklistView__ItemView: View {
-
-    let item: ChecklistItemDb
-
-    @State private var isEditPresented = false
-
-    var body: some View {
-        MyListSwipeToActionItem(
-                deletionHint: item.text,
-                deletionConfirmationNote: nil,
-                onEdit: {
-                    isEditPresented = true
-                },
-                onDelete: {
-                    withAnimation {
-                        item.delete { _ in
-                            // todo
-                        }
-                    }
-                }
-        ) {
-            Button(
-                    action: {
-                        item.toggle { _ in
-                            // todo report
-                        }
+                    .padding(.bottom, 15)
+                
+                ChecklistView(
+                    checklistDb: state.checklistDb,
+                    onDelete: {
+                        // todo
                     },
-                    label: {
-                        HStack {
-                            Text(item.text)
-                            Spacer()
-                            if item.isChecked {
-                                Image(systemName: "checkmark")
-                                        .foregroundColor(.blue)
-                                        .offset(x: 4)
-                            }
-                        }
-                                .frame(minHeight: MyListView.ITEM_MIN_HEIGHT)
-                    }
-            )
-                    .foregroundColor(.primary)
-                    .padding(.horizontal, H_PADDING)
+                    bottomPadding: 0
+                )
+            }
         }
-                .sheetEnv(isPresented: $isEditPresented) {
-                    ChecklistItemFormSheet(
-                            isPresented: $isEditPresented,
-                            checklist: DI.checklists.filter { $0.id == item.list_id }.first!,
-                            checklistItem: item
-                    )
-                }
+        .background(c.sheetBg)
     }
 }
