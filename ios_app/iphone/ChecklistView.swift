e0c2c4a07c36e56569d41f44d4f03da585b9b079
hi@timeto.me <hi@timeto.me>
2024-04-01 12:22:16 +0300

    ChecklistView.swift:

--- ios_app/iphone/ChecklistView.swift
+++ ios_app/iphone/ChecklistView.swift
@@ -4,17 +4,17 @@ import shared
 private let checkboxSize = 20.0
 private let checklistItemMinHeight = 46.0
 
-struct ChecklistView : View {
-    
+struct ChecklistView: View {
+
     @State private var vm: ChecklistVM
-    
+
     private let onDelete: () -> Void
     private let bottomPadding: CGFloat
-    
+
     //
-    
+
     @State private var vScroll = 0
-    
+
     init(
         checklistDb: ChecklistDb,
         onDelete: @escaping () -> Void,
@@ -24,33 +24,33 @@ struct ChecklistView : View {
         self.onDelete = onDelete
         self.bottomPadding = bottomPadding
     }
-    
+
     var body: some View {
-        
+
         VMView(vm: vm) { state in
-            
+
             VStack {
-                
+
                 HStack(alignment: .top) {
-                    
+
                     ScrollViewWithVListener(showsIndicators: false, vScroll: $vScroll) {
-                        
+
                         VStack {
-                            
+
                             ForEach(state.checklistUI.itemsUI, id: \.item.id) { itemUI in
-                                
+
                                 Button(
                                     action: {
                                         itemUI.toggle()
                                     },
                                     label: {
                                         HStack {
-                                            
+
                                             Image(systemName: itemUI.item.isChecked ? "checkmark.square.fill" : "square")
                                                 .foregroundColor(Color.white)
                                                 .font(.system(size: checkboxSize, weight: .regular))
                                                 .padding(.trailing, 12)
-                                            
+
                                             Text(itemUI.item.text)
                                                 .padding(.vertical, 4)
                                                 .foregroundColor(.white)
@@ -63,15 +63,21 @@ struct ChecklistView : View {
                                 )
                             }
                         }
-                        
+
                         Spacer()
                     }
-                    
+
                     let stateUI = state.checklistUI.stateUI
                     let stateIconResource: String = {
-                        if stateUI is ChecklistStateUI.Completed { return "checkmark.square.fill" }
-                        if stateUI is ChecklistStateUI.Empty { return "square" }
-                        if stateUI is ChecklistStateUI.Partial { return "minus.square.fill" }
+                        if stateUI is ChecklistStateUI.Completed {
+                            return "checkmark.square.fill"
+                        }
+                        if stateUI is ChecklistStateUI.Empty {
+                            return "square"
+                        }
+                        if stateUI is ChecklistStateUI.Partial {
+                            return "minus.square.fill"
+                        }
                         fatalError()
                     }()
                     Button(
@@ -88,7 +94,7 @@ struct ChecklistView : View {
                 }
                 .padding(.horizontal, H_PADDING)
             }
-            
+
         }
     }
 }
