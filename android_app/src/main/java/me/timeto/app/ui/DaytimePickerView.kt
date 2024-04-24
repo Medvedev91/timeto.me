eeedca4a64896608241d25aa50bc30a698db5d9f
hi@timeto.me <hi@timeto.me>
2024-04-24 13:58:30 +0300

    DaytimePickerView.kt: Todo

--- android_app/src/main/java/me/timeto/app/ui/DaytimePickerView.kt
+++ android_app/src/main/java/me/timeto/app/ui/DaytimePickerView.kt
@@ -3,16 +3,21 @@ package me.timeto.app.ui
 import android.widget.NumberPicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.zIndex
 import me.timeto.app.*
 import me.timeto.shared.vm.ui.DaytimePickerUi
 
 private val circleSize = 20.dp
+private val circleDefaultOffset = H_PADDING - (circleSize / 2) + 2.dp
 
 @Composable
 fun DayTimePickerView(
@@ -44,28 +49,74 @@ private fun SliderView(
     onChange: (Int) -> Unit,
 ) {
 
-    ZStack(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(circleSize),
-    ) {
+    ZStack {
 
         ZStack(
             modifier = Modifier
-                .align(Alignment.Center)
+                .zIndex(1f)
+                .height(circleSize),
+        ) {
+
+            ZStack(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(horizontal = H_PADDING)
+                    .fillMaxWidth()
+                    .height(4.dp)
+                    .clip(roundedShape)
+                    .background(c.sheetFg),
+            )
+
+            ZStack(
+                modifier = Modifier
+                    .offset(x = circleDefaultOffset)
+                    .size(circleSize)
+                    .clip(roundedShape)
+                    .background(c.blue),
+            )
+        }
+
+        HStack(
+            modifier = Modifier
+                .zIndex(0f)
+                .padding(top = 16.dp)
                 .padding(horizontal = H_PADDING)
-                .fillMaxWidth()
-                .height(4.dp)
-                .clip(roundedShape)
-                .background(c.sheetFg),
-        )
+                .fillMaxWidth(),
+        ) {
 
-        ZStack(
-            modifier = Modifier
-                .size(circleSize)
-                .clip(roundedShape)
-                .background(c.blue),
-        )
+            ticks.forEach { tick ->
+
+                val tickText = tick.text
+                if (tickText != null) {
+                    val isFirst = (tick.value == 0)
+                    VStack(
+                        modifier = Modifier
+                            .weight(if (isFirst) 0.5f else 1f),
+                        horizontalAlignment = if (isFirst) Alignment.Start else Alignment.CenterHorizontally,
+                    ) {
+
+                        ZStack(
+                            modifier = Modifier
+                                .offset(x = if (isFirst) 2.dp else 0.dp, y = 1.dp)
+                                .width(1.dp)
+                                .height(5.dp)
+                                .background(c.sheetFg),
+                        )
+
+                        Text(
+                            text = tickText,
+                            modifier = Modifier
+                                .offset(x = if (isFirst) (-1).dp else 0.dp),
+                            color = c.textSecondary,
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Light,
+                        )
+                    }
+                }
+            }
+
+            ZStack(Modifier.weight(0.4f))
+        }
     }
 }
 
