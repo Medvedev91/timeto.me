a7a6f875414a227aef9b6dfdf2d5cc706ed90785
hi@timeto.me <hi@timeto.me>
2024-04-26 21:27:01 +0300

    DaytimePickerSheet(daytimePickerUi)

--- android_app/src/main/java/me/timeto/app/ui/DaytimePickerSheet.kt
+++ android_app/src/main/java/me/timeto/app/ui/DaytimePickerSheet.kt
@@ -2,7 +2,6 @@ package me.timeto.app.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
@@ -13,24 +12,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import me.timeto.app.VStack
 import me.timeto.app.c
 import me.timeto.app.roundedShape
+import me.timeto.shared.vm.ui.DaytimePickerUi
 
 @Composable
 fun DaytimePickerSheet(
     layer: WrapperView.Layer,
     title: String,
     doneText: String,
-    defMinute: Int,
-    defHour: Int,
+    daytimePickerUi: DaytimePickerUi,
     withRemove: Boolean,
-    onPick: (Int) -> Unit,
+    onPick: (DaytimePickerUi) -> Unit,
     onRemove: () -> Unit,
 ) {
-    val selectedHour = remember { mutableIntStateOf(defHour) }
-    val selectedMinute = remember { mutableIntStateOf(defMinute) }
 
-    Column(
+    val selectedHour = remember { mutableIntStateOf(daytimePickerUi.hour) }
+    val selectedMinute = remember { mutableIntStateOf(daytimePickerUi.minute) }
+
+    VStack(
         Modifier.background(c.sheetBg)
     ) {
 
@@ -41,11 +42,15 @@ fun DaytimePickerSheet(
             isDoneEnabled = true,
             scrollState = null,
         ) {
-            onPick((selectedHour.intValue * 3_600) + (selectedMinute.intValue * 60))
+            val newDaytimePickerUi = DaytimePickerUi(
+                hour = selectedHour.intValue,
+                minute = selectedMinute.intValue,
+            )
+            onPick(newDaytimePickerUi)
             layer.close()
         }
 
-        Column(
+        VStack(
             modifier = Modifier
                 .padding(top = 12.dp, bottom = 20.dp)
                 .navigationBarsPadding(),
@@ -59,7 +64,7 @@ fun DaytimePickerSheet(
                 onMinuteChanged = { minute -> selectedMinute.intValue = minute },
             )
 
-            if (withRemove)
+            if (withRemove) {
                 Text(
                     text = "Remove",
                     modifier = Modifier
@@ -72,6 +77,7 @@ fun DaytimePickerSheet(
                         .padding(horizontal = 12.dp, vertical = 4.dp),
                     color = c.red,
                 )
+            }
         }
     }
 }
