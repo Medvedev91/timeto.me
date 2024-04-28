435082b45516aa317b46cda2d501684ab3602c7b
hi@timeto.me <hi@timeto.me>
2024-04-28 21:56:43 +0300

    EventFormSheet.swift:

--- ios_app/iphone/EventFormSheet.swift
+++ ios_app/iphone/EventFormSheet.swift
@@ -4,17 +4,17 @@ import shared
 extension NativeSheet {
 
     func EventFormSheet__show(
-            editedEvent: EventDb?,
-            defText: String? = nil,
-            defTime: Int? = nil,
-            onSave: @escaping () -> Void
+        editedEvent: EventDb?,
+        defText: String? = nil,
+        defTime: Int? = nil,
+        onSave: @escaping () -> Void
     ) {
         self.show { isPresented in
             EventFormSheet(
-                    isPresented: isPresented,
-                    editedEvent: editedEvent,
-                    defText: defText ?? "",
-                    defTime: defTime
+                isPresented: isPresented,
+                editedEvent: editedEvent,
+                defText: defText ?? "",
+                defTime: defTime
             ) {
                 onSave()
             }
@@ -34,16 +34,17 @@ private struct EventFormSheet: View {
     @State private var sheetHeaderScroll = 0
 
     init(
-            isPresented: Binding<Bool>,
-            editedEvent: EventDb?,
-            defText: String? = nil,
-            defTime: Int? = nil,
-            onSave: @escaping () -> Void = {}
+        isPresented: Binding<Bool>,
+        editedEvent: EventDb?,
+        defText: String? = nil,
+        defTime: Int? = nil,
+        onSave: @escaping () -> Void = {
+        }
     ) {
         _vm = State(initialValue: EventFormSheetVM(
-                event: editedEvent,
-                defText: defText,
-                defTime: defTime?.toKotlinInt()
+            event: editedEvent,
+            defText: defText,
+            defTime: defTime?.toKotlinInt()
         ))
         self.onSave = onSave
         _isPresented = isPresented
@@ -54,69 +55,69 @@ private struct EventFormSheet: View {
         VMView(vm: vm, stack: .VStack(spacing: 0)) { state in
 
             SheetHeaderView(
-                    onCancel: { isPresented.toggle() },
-                    title: state.headerTitle,
-                    doneText: state.headerDoneText,
-                    isDoneEnabled: state.isHeaderDoneEnabled,
-                    scrollToHeader: sheetHeaderScroll
+                onCancel: { isPresented.toggle() },
+                title: state.headerTitle,
+                doneText: state.headerDoneText,
+                isDoneEnabled: state.isHeaderDoneEnabled,
+                scrollToHeader: sheetHeaderScroll
             ) {
                 vm.save {
                     onSave()
                     isPresented = false
                 }
             }
-                    .onAppear {
-                        date = Date(timeIntervalSince1970: Double(state.selectedTime))
-                    }
-                    .onChange(of: date) { _ in
-                        vm.setTime(time: date.toUnixTime().time)
-                        hideKeyboard()
-                    }
+            .onAppear {
+                date = Date(timeIntervalSince1970: Double(state.selectedTime))
+            }
+            .onChange(of: date) { _ in
+                vm.setTime(time: date.toUnixTime().time)
+                hideKeyboard()
+            }
 
             ScrollViewWithVListener(showsIndicators: false, vScroll: $sheetHeaderScroll) {
 
                 VStack(spacing: 0) {
 
                     MyListView__ItemView(
-                            isFirst: true,
-                            isLast: true
+                        isFirst: true,
+                        isLast: true
                     ) {
 
                         MyListView__ItemView__TextInputView(
-                                text: state.inputTextValue,
-                                placeholder: "Title",
-                                // Autofocus only for new events
-                                isAutofocus: state.isAutoFocus
+                            text: state.inputTextValue,
+                            placeholder: "Title",
+                            // Autofocus only for new events
+                            isAutofocus: state.isAutoFocus
                         ) { newValue in
                             vm.setInputTextValue(text: newValue)
                         }
                     }
-                            .padding(.top, 1)
+                    .padding(.top, 1)
 
                     DatePicker(
-                            "Start Date",
-                            selection: $date,
-                            in: Date().startOfDay()...,
-                            displayedComponents: [.date]
+                        "Start Date",
+                        selection: $date,
+                        in: Date().startOfDay()...,
+                        displayedComponents: [.date]
                     )
-                            .labelsHidden()
-                            .padding(.horizontal, 28)
-                            .padding(.top, 8)
-                            .datePickerStyle(.graphical)
+                        .labelsHidden()
+                        .padding(.horizontal, 28)
+                        .padding(.top, 8)
+                        .datePickerStyle(.graphical)
 
                     DatePicker(
-                            "Start Date",
-                            selection: $date,
-                            displayedComponents: [.hourAndMinute]
+                        "Start Date",
+                        selection: $date,
+                        displayedComponents: [.hourAndMinute]
                     )
-                            .labelsHidden()
-                            .datePickerStyle(.wheel)
+                        .labelsHidden()
+                        .datePickerStyle(.wheel)
 
                     Spacer()
-                            .frame(minHeight: 20)
+                        .frame(minHeight: 20)
                 }
             }
         }
-                .background(c.sheetBg)
+        .background(c.sheetBg)
     }
 }
