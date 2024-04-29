e77694d17a79fd3fe3d6f172638e21e874d89602
hi@timeto.me <hi@timeto.me>
2024-04-29 21:12:18 +0300

    DaytimePickerSliderView.swift

---
+++ ios_app/iphone/DaytimePickerSliderView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import shared
+
+struct DaytimePickerSliderView: View {
+    
+    let daytimeModel: DaytimeModel
+    let onChange: (DaytimeModel) -> Void
+
+    var body: some View {
+
+        Text("todo")
+    }
+}
