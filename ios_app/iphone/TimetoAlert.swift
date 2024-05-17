bc190f9c8e9ef75d9d728ef4d393292509d3ce9b
hi@timeto.me <hi@timeto.me>
2024-05-17 14:56:13 +0300

    TimetoAlert.swift:

--- ios_app/iphone/TimetoAlert.swift
+++ ios_app/iphone/TimetoAlert.swift
@@ -20,41 +20,41 @@ private struct TimetoAlert__Modifier: ViewModifier {
     func body(content: Content) -> some View {
 
         content
-                .alert(
-                        timetoAlert.data?.title ?? "",
-                        isPresented: $isPresented
-                ) {
-                    if let data = timetoAlert.data {
-                        let buttons = data.buttons
-                        ForEach(0..<buttons.count, id: \.self) { idx in
-                            buttons[idx]
-                        }
-                    }
+        .alert(
+            timetoAlert.data?.title ?? "",
+            isPresented: $isPresented
+        ) {
+            if let data = timetoAlert.data {
+                let buttons = data.buttons
+                ForEach(0..<buttons.count, id: \.self) { idx in
+                    buttons[idx]
                 }
-                .environmentObject(timetoAlert)
-                .onChange(of: timetoAlert.data?.id) { _ in
-                    if timetoAlert.data != nil {
-                        /// In case of nested calls
-                        isPresented = false
-                        DispatchQueue.main.async {
-                            isPresented = true
-                        }
-                        //////
-                    } else {
-                        isPresented = false
-                    }
+            }
+        }
+        .environmentObject(timetoAlert)
+        .onChange(of: timetoAlert.data?.id) { _ in
+            if timetoAlert.data != nil {
+                /// In case of nested calls
+                isPresented = false
+                DispatchQueue.main.async {
+                    isPresented = true
                 }
-                .onReceive(alertPublisher) { data in
-                    timetoAlert.alert(data.message)
-                }
-                .onReceive(confirmationPublisher) { data in
-                    timetoAlert.confirm(
-                            data.text,
-                            confirmationText: data.buttonText,
-                            onConfirm: data.onConfirm,
-                            isDestructive: data.isRed
-                    )
-                }
+                //////
+            } else {
+                isPresented = false
+            }
+        }
+        .onReceive(alertPublisher) { data in
+            timetoAlert.alert(data.message)
+        }
+        .onReceive(confirmationPublisher) { data in
+            timetoAlert.confirm(
+                data.text,
+                confirmationText: data.buttonText,
+                onConfirm: data.onConfirm,
+                isDestructive: data.isRed
+            )
+        }
     }
 }
 
@@ -63,40 +63,44 @@ class TimetoAlert: ObservableObject {
     @Published fileprivate var data: TimetoAlert__Data?
 
     func alert(
-            _ title: String,
-            btnText: String = "OK",
-            onClick: (() -> Void)? = nil
+        _ title: String,
+        btnText: String = "OK",
+        onClick: (() -> Void)? = nil
     ) {
         data = TimetoAlert__Data(
-                id: UUID(),
-                title: title,
-                buttons: [
-                    Button(btnText, role: .cancel) {
-                        if let onClick = onClick { onClick() }
+            id: UUID(),
+            title: title,
+            buttons: [
+                Button(btnText, role: .cancel) {
+                    if let onClick = onClick {
+                        onClick()
                     }
-                ]
+                }
+            ]
         )
     }
 
     func confirm(
-            _ title: String,
-            confirmationText: String,
-            onConfirm: @escaping () -> Void,
-            isDestructive: Bool = false,
-            cancelText: String = "Cancel",
-            onCancel: (() -> Void)? = nil
+        _ title: String,
+        confirmationText: String,
+        onConfirm: @escaping () -> Void,
+        isDestructive: Bool = false,
+        cancelText: String = "Cancel",
+        onCancel: (() -> Void)? = nil
     ) {
         data = TimetoAlert__Data(
-                id: UUID(),
-                title: title,
-                buttons: [
-                    Button(confirmationText, role: isDestructive ? .destructive : .none) {
-                        onConfirm()
-                    },
-                    Button(cancelText, role: .cancel) {
-                        if let onCancel = onCancel { onCancel() }
+            id: UUID(),
+            title: title,
+            buttons: [
+                Button(confirmationText, role: isDestructive ? .destructive : .none) {
+                    onConfirm()
+                },
+                Button(cancelText, role: .cancel) {
+                    if let onCancel = onCancel {
+                        onCancel()
                     }
-                ]
+                }
+            ]
         )
     }
 }
@@ -110,25 +114,25 @@ struct TimetoAlert__Data: Identifiable {
 extension View {
 
     func sheetEnv<Content>(
-            isPresented: Binding<Bool>,
-            onDismiss: (() -> Void)? = nil,
-            hideKeyboardOnPresent: Bool = true,
-            @ViewBuilder content: @escaping () -> Content
+        isPresented: Binding<Bool>,
+        onDismiss: (() -> Void)? = nil,
+        hideKeyboardOnPresent: Bool = true,
+        @ViewBuilder content: @escaping () -> Content
     ) -> some View where Content: View {
         sheet(
-                isPresented: isPresented,
-                onDismiss: onDismiss,
-                content: {
-                    content()
-                            .attachTimetoAlert()
-                            .attachTimetoSheet()
-                            .attachNativeSheet()
-                }
+            isPresented: isPresented,
+            onDismiss: onDismiss,
+            content: {
+                content()
+                    .attachTimetoAlert()
+                    .attachTimetoSheet()
+                    .attachNativeSheet()
+            }
         )
-                .onChange(of: isPresented.wrappedValue) { newValue in
-                    if newValue && hideKeyboardOnPresent {
-                        hideKeyboard()
-                    }
-                }
+        .onChange(of: isPresented.wrappedValue) { newValue in
+            if newValue && hideKeyboardOnPresent {
+                hideKeyboard()
+            }
+        }
     }
 }
