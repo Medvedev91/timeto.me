a425eae1bcf92187fd4485a22551c332ac75c432
hi@timeto.me <hi@timeto.me>
2024-05-19 15:33:53 +0300

    EventsListView.kt: Remove event creation button

--- android_app/src/main/java/me/timeto/app/ui/EventsListView.kt
+++ android_app/src/main/java/me/timeto/app/ui/EventsListView.kt
@@ -1,8 +1,5 @@
 package me.timeto.app.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -10,14 +7,11 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.timeto.app.*
 import me.timeto.shared.vm.EventsListVM
-
-private val bgColor = c.bg
 
 @Composable
 fun EventsListView(
@@ -35,11 +29,7 @@ fun EventsListView(
         ),
     ) {
 
-        val minHeight = 42.dp
-
         item {
-
-            val saveBtnPadding = 4.dp
 
             Column {
 
@@ -47,48 +37,6 @@ fun EventsListView(
                     spaceAround = H_PADDING - 2.dp,
                     paddingTop = TasksView__LIST_SECTION_PADDING,
                 )
-
-                Box(
-                    Modifier
-                        .padding(
-                            start = H_PADDING - 2.dp,
-                            top = 16.dp,
-                            bottom = TasksView__LIST_SECTION_PADDING,
-                        )
-                        .height(minHeight)
-                        .border(onePx, c.dividerBg, TasksView__INPUT_SHAPE)
-                        .clip(TasksView__INPUT_SHAPE)
-                        .background(bgColor)
-                        .clickable {
-                            EventFormSheet__show(editedEvent = null) {}
-                        }
-                        .padding(vertical = saveBtnPadding),
-                ) {
-
-                    Text(
-                        "Event",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = MyListView.PADDING_INNER_HORIZONTAL)
-                            .align(Alignment.CenterStart),
-                        color = c.textSecondary.copy(alpha = 0.4f)
-                    )
-
-                    Text(
-                        "DATE",
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(end = saveBtnPadding)
-                            .clip(squircleShape)
-                            .background(c.blue.copy(alpha = 0.5f))
-                            .padding(horizontal = 12.dp)
-                            .align(Alignment.CenterEnd)
-                            .wrapContentSize(), // vertical center
-                        color = c.white,
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.W600,
-                    )
-                }
             }
         }
 
@@ -115,7 +63,7 @@ fun EventsListView(
         ) { idx, eventUi ->
             EventsListEventView(
                 eventUi = eventUi,
-                bgColor = bgColor,
+                bgColor = c.bg,
                 paddingStart = H_PADDING,
                 paddingEnd = 0.dp,
                 dividerColor = c.dividerBg,
