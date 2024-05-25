2259614f12ad4d3f167af1423825b238f55c3186
hi@timeto.me <hi@timeto.me>
2024-05-25 17:37:05 +0300

    Fs.swift: Fs__Header/Fs__HeaderAction

--- ios_app/iphone/Fs.swift
+++ ios_app/iphone/Fs.swift
@@ -76,3 +76,92 @@ private struct Fs__Modifier: ViewModifier {
         .environmentObject(fs)
     }
 }
+
+///
+
+struct Fs__Header<Content: View>: View {
+
+    let scrollToHeader: Int
+    @ViewBuilder var content: () -> Content
+
+    private var bgAlpha: Double {
+        (Double(scrollToHeader) / 30).limitMinMax(0, 1)
+    }
+
+    var body: some View {
+
+        // .bottom for divider
+        ZStack(alignment: .bottom) {
+
+            content()
+
+            DividerBg()
+                .opacity(bgAlpha)
+                .padding(.horizontal, H_PADDING)
+        }
+        .safeAreaPadding(.top)
+    }
+}
+
+
+struct Fs__HeaderAction: View {
+
+    let title: String
+    let actionText: String
+    let scrollToHeader: Int
+    let onCancel: () -> Void
+    let onDone: () -> Void
+
+    private var bgAlpha: Double {
+        (Double(scrollToHeader) / 30).limitMinMax(0, 1)
+    }
+
+    var body: some View {
+
+        Fs__Header(
+            scrollToHeader: scrollToHeader
+        ) {
+
+            VStack(alignment: .leading) {
+
+                Button(
+                    action: {
+                        onCancel()
+                    },
+                    label: {
+                        Text("Cancel")
+                            .foregroundColor(c.textSecondary)
+                            .font(.system(size: 15, weight: .light))
+                            .padding(.leading, H_PADDING)
+                    }
+                )
+                .padding(.bottom, 4)
+
+                HStack {
+
+                    Text(title)
+                        .font(.system(size: 26, weight: .semibold))
+                        .padding(.leading, H_PADDING)
+
+                    Spacer()
+
+                    Button(
+                        action: {
+                            onDone()
+                        },
+                        label: {
+                            Text(actionText)
+                                .foregroundColor(c.white)
+                                .font(.system(size: 16, weight: .semibold))
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 5)
+                                .background(roundedShape.fill(c.blue))
+                        }
+                    )
+                    .padding(.trailing, H_PADDING)
+                }
+                .padding(.bottom, 8)
+            }
+        }
+    }
+}
