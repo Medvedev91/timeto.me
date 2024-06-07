ea625c42b50fe00cdba7679025dee62e0de45e28
hi@timeto.me <hi@timeto.me>
2024-06-07 12:30:14 +0300

    utils_ios.swift:

--- ios_app/iphone/utils_ios.swift
+++ ios_app/iphone/utils_ios.swift
@@ -6,7 +6,8 @@ import shared
 let H_PADDING = 16.0
 let H_PADDING_HALF = H_PADDING / 2
 
-let onePx = 1 / UIScreen.main.scale
+private let scale: CGFloat = UIScreen.main.scale
+let onePx: CGFloat = 1 / scale
 let halfDp = CGFloat(Int(UIScreen.main.scale / 2)) / UIScreen.main.scale
 
 func hideKeyboard() {
