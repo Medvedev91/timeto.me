c39d9b096800db18a07aa09e8fcd4f495209c45a
hi@timeto.me <hi@timeto.me>
2024-06-11 23:14:35 +0300

    ReadmeSheetVM.kt: Up text

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/ReadmeSheetVM.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/ReadmeSheetVM.kt
@@ -66,56 +66,43 @@ private fun prepParagraphs(): List<ReadmeSheetVM.Paragraph> {
     val paragraphs = mutableListOf<ReadmeSheetVM.Paragraph>()
 
     paragraphs.add(PText("Hi,"))
-    paragraphs.add(PText("Developer is here  $developerEmoji"))
+    paragraphs.add(PText("Developer is here."))
     paragraphs.add(PText("I built this app to manage my productivity. Here I will show how I use it."))
-    paragraphs.add(PTextHighlight("This guide is not just a list of features, but my real day-to-day experience."))
-    paragraphs.add(PText("I will start with the most important things: timer, repeating tasks and checklists, that is 90% I use. Then tasks, calendar, goals, shortcuts, etc."))
+    paragraphs.add(PTextHighlight("It is not just a list of features, it is my real day-to-day experience."))
     paragraphs.add(PText("Enjoy!"))
 
     paragraphs.add(PTitle("Timer"))
     paragraphs.add(PText("You must set a timer for each activity, like eating, working, reading, etc."))
-    paragraphs.add(PTextHighlight("There is NO stop option! To finish the current activity, you have to start the next one."))
-    paragraphs.add(PText("In other words, once you complete one activity, you must start the timer for the next activity, even sleep or breakfast."))
-    paragraphs.add(PText("It helps me to always stay focused on what I have to do. Most of the time my screen looks like a typical pomodoro timer:"))
+    paragraphs.add(PTextHighlight("Timer is running all the time, even for sleep or breakfast."))
+    paragraphs.add(PText("There is NO stop option! To stop the current activity, you have to start the next one."))
+    paragraphs.add(PText("In other words, once you complete one activity, you must start the timer for the next one."))
+    paragraphs.add(PText("This way I always remember what I have to do. Most of the time my screen looks like a typical pomodoro timer:"))
     paragraphs.add(ReadmeSheetVM.Paragraph.TimerTypical())
-    paragraphs.add(PText("This way also provides real 24/7 data on how long everything takes:"))
+    paragraphs.add(PText("This way also provides 24/7 data on how long everything takes:"))
     paragraphs.add(ReadmeSheetVM.Paragraph.TimerCharts())
-    paragraphs.add(PText("The app has some activities by default, but you can add your own. Here are mine:"))
+    paragraphs.add(PText("The app has some activities by default. Here are mine:"))
     paragraphs.add(ReadmeSheetVM.Paragraph.TimerMyActivities())
     paragraphs.add(PTitle("Practice"))
-    paragraphs.add(PText("Try to start a new activity. To do so, back to the home screen, tap the timer at the bottom left, tap \"Work\" and start the timer."))
+    paragraphs.add(PText("Let's start a 45 min timer to work."))
     paragraphs.add(ReadmeSheetVM.Paragraph.TimerPractice1())
-    paragraphs.add(PText("The timer starts. Then tap the timer again to see the \"Summary\" and \"History\"."))
+    paragraphs.add(PText("The timer starts. Let's see the summary and history."))
     paragraphs.add(ReadmeSheetVM.Paragraph.TimerPractice2())
 
     paragraphs.add(PTitle("Repeating Tasks"))
-    paragraphs.add(PText("You may think choosing activity and timer for each task is overwhelming. This is where repeating tasks come into play."))
-    paragraphs.add(PText("How it works for me:"))
+    paragraphs.add(PText("You may think choosing activity and timer for each task is overwhelming. This is where repeating tasks come in."))
+    paragraphs.add(PText("How I use it:"))
     paragraphs.add(PText("Most of my activities are repeated. I wake up at the same time, then 1 hour to get ready, 2 hours working, 1 hour eating, etc. So I created a repeating task for each of these."))
     paragraphs.add(ReadmeSheetVM.Paragraph.RepeatingsMy())
     paragraphs.add(PText("Each day, these tasks move to the \"Today\" folder. Tasks are sorted by time of day and show how much time is left. It's like a schedule:"))
     paragraphs.add(ReadmeSheetVM.Paragraph.RepeatingsToday())
-    paragraphs.add(PTextHighlight("The most important, when I press it, it automatically starts a timer with the right activity."))
-    paragraphs.add(PText("You can create not only everyday tasks. For example, watering a cactus once a week or paying for internet once a month, etc."))
+    paragraphs.add(PTextHighlight("The most important, when I press it, it automatically starts a timer."))
+    paragraphs.add(PText("You can create not only everyday tasks. Like watering a cactus once a week or paying for internet once a month."))
     paragraphs.add(PText("I believe this is the main feature of the app. I recommend using it to the max."))
     paragraphs.add(PTitle("Practice"))
     paragraphs.add(PText("Let's create a repeating task for a daily workout at 18:00."))
-    paragraphs.add(PText("On the home screen, tap at the bottom center to open tasks, tap the repeating icon, tap \"New Repeating Task\" and fill the form:"))
-    paragraphs.add(
-        ReadmeSheetVM.Paragraph.ListDash(
-            items = listOf(
-                "Task: Workout;",
-                "Activity: Exercises / Health;",
-                "Timer: 1h;",
-                "Time of the Day: 18:00;",
-                "Repetition Period: Every Day;",
-                "Tap \"Create\".",
-            ),
-        )
-    )
     paragraphs.add(ReadmeSheetVM.Paragraph.RepeatingsPractice1())
     paragraphs.add(PText("Every day, a \"Workout\" task will be added to the \"Today\" folder."))
-    paragraphs.add(PText("This task is already created for today. Open \"Today\" and tap the \"Workout\" task. The timer will automatically start with the right activity."))
+    paragraphs.add(PText("This task is already created for today. Open \"Today\" and tap the \"Workout\" task. The timer will start automatically."))
     paragraphs.add(ReadmeSheetVM.Paragraph.RepeatingsPractice2())
 
     paragraphs.add(PTitle("Checklists"))
