aca0741391e5e34af586bc9f4bc09e12f9dafb09
hi@timeto.me <hi@timeto.me>
2024-06-19 23:55:56 +0300

    TimerDataUI.prolongHints

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/ui/TimerDataUI.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/ui/TimerDataUI.kt
@@ -21,6 +21,8 @@ class TimerDataUI(
     val timerText: String
     val timerColor: ColorRgba
 
+    val prolongHints: List<ProlongHint> = prolongHintsLocal
+
     ///
 
     private val activity: ActivityDb = interval.getActivityDI()
@@ -48,6 +50,36 @@ class TimerDataUI(
 
     enum class PomodoroIcon {
         PAUSE, PLAY, FORWARD,
+    }
+
+    class ProlongHint(
+        val timer: Int,
+        val text: String,
+    ) {
+
+        fun prolong() {
+            launchExDefault {
+                val interval = IntervalDb.getLastOneOrNull()!!
+                val newTf: TextFeatures = run {
+                    val oldTf = (interval.note ?: "").textFeatures()
+                    val prolonged = oldTf.prolonged ?: TextFeatures.Prolonged(interval.timer)
+                    oldTf.copy(prolonged = prolonged)
+                }
+                val newTimer: Int = run {
+                    val now = time()
+                    val secondsToEnd = interval.id + interval.timer - now
+                    if (secondsToEnd > 0)
+                        interval.timer + timer
+                    else
+                        now + timer - interval.id
+                }
+                interval.up(
+                    timer = newTimer,
+                    note = newTf.textWithFeatures(),
+                    activityDb = interval.getActivity(),
+                )
+            }
+        }
     }
 
     init {
@@ -101,6 +133,13 @@ class TimerDataUI(
 }
 
 private val defControlsColor = ColorRgba(255, 255, 255, 180)
+
+private val prolongHintsLocal: List<TimerDataUI.ProlongHint> = listOf(
+    TimerDataUI.ProlongHint(5 * 60, "5"),
+    TimerDataUI.ProlongHint(15 * 60, "15"),
+    TimerDataUI.ProlongHint(30 * 60, "30"),
+    TimerDataUI.ProlongHint(60 * 60, "1h"),
+)
 
 private fun secondsToString(seconds: Int): String {
     val hms = seconds.absoluteValue.toHms()
