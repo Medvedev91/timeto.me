c31b8f4c0754ff1af40239ccf63d47c4201cac06
hi@timeto.me <hi@timeto.me>
2024-06-19 09:40:16 +0300

    androidx.compose.material:material:1.6.8

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 dependencies {
     implementation(project(":shared"))
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.compose.material:material:1.6.7")
-    implementation("androidx.compose.material:material-icons-extended:1.6.7")
+    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
     implementation("com.google.android.material:material:1.12.0")
 }
