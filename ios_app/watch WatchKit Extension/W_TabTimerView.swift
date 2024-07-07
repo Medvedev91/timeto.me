bbf982987e5eb90c452f0857dfb057ba825f86b7
hi@timeto.me <hi@timeto.me>
2024-07-07 12:12:14 +0300

    W_TabTimerView.swift:

--- ios_app/watch WatchKit Extension/W_TabTimerView.swift
+++ ios_app/watch WatchKit Extension/W_TabTimerView.swift
@@ -15,24 +15,24 @@ struct W_TabTimerView: View {
 
                     // Tried by List, but listRowBackground() does not work on screen transition.
                     ScrollView(.vertical, showsIndicators: false) {
-                        
+
                         VStack(spacing: 5) {
-                            
+
                             TimerView()
-                                    .id("timer_view")
-                                    .padding(.bottom, 16)
+                                .id("timer_view")
+                                .padding(.bottom, 16)
 
                             ForEach(state.activitiesUI, id: \.activity.id) { activityUI in
                                 ActivityView(activityUI: activityUI)
-                                        .id("aid__\(activityUI.activity.id)")
+                                    .id("aid__\(activityUI.activity.id)")
                             }
                         }
                     }
-                            .animation(.default)
+                    .animation(.default)
                 }
-                        .onChange(of: state.lastInterval) { newLastInterval in
-                            scrollProxy.scrollTo("timer_view", anchor: .top)
-                        }
+                .onChange(of: state.lastInterval) { newLastInterval in
+                    scrollProxy.scrollTo("timer_view", anchor: .top)
+                }
             }
         }
     }
@@ -42,21 +42,21 @@ struct W_TabTimerView: View {
         @State private var vm = WatchTimerVM()
 
         var body: some View {
-            
+
             VMView(vm: vm, stack: .VStack()) { state in
-                
+
                 Text(state.timerData.note)
                     .foregroundColor(state.timerData.noteColor.toColor())
                     .font(.system(size: 15))
-                
+
                 Text(state.timerData.timerText)
-                        .font(.system(size: 35, weight:.bold, design: .monospaced))
-                        .fontWeight(.medium)
-                        .foregroundColor(state.timerData.timerColor.toColor())
+                    .font(.system(size: 35, weight: .bold, design: .monospaced))
+                    .fontWeight(.medium)
+                    .foregroundColor(state.timerData.timerColor.toColor())
             }
-                    .onTapGesture {
-                        vm.toggleIsPurple()
-                    }
+            .onTapGesture {
+                vm.toggleIsPurple()
+            }
         }
     }
 
@@ -70,56 +70,57 @@ struct W_TabTimerView: View {
         var body: some View {
 
             Button(
-                    action: {
-                        isTickerPresented = true
-                    },
-                    label: {
-                        
-                        VStack {
+                action: {
+                    isTickerPresented = true
+                },
+                label: {
 
-                            HStack(alignment: .center) {
+                    VStack {
 
-                                    Text(activityUI.text)
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                            .lineLimit(1)
-                                            .truncationMode(.middle)
-                            }
+                        HStack(alignment: .center) {
 
-                            if !activityUI.timerHints.isEmpty {
-                                HStack(spacing: 6) {
-                                    ForEach(activityUI.timerHints, id: \.seconds) { hintUI in
-                                        Button(
-                                                action: {
-                                                    hintUI.startInterval {}
-                                                },
-                                                label: {
-                                                    Text(hintUI.text)
-                                                            .font(.system(size: 13, weight: .medium))
-                                                            .foregroundColor(.white)
-                                                }
-                                        )
-                                                .buttonStyle(.borderless)
-                                    }
-                                    Spacer()
+                            Text(activityUI.text)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .lineLimit(1)
+                                .truncationMode(.middle)
+                        }
+
+                        if !activityUI.timerHints.isEmpty {
+                            HStack(spacing: 6) {
+                                ForEach(activityUI.timerHints, id: \.seconds) { hintUI in
+                                    Button(
+                                        action: {
+                                            hintUI.startInterval {
+                                            }
+                                        },
+                                        label: {
+                                            Text(hintUI.text)
+                                                .font(.system(size: 13, weight: .medium))
+                                                .foregroundColor(.white)
+                                        }
+                                    )
+                                    .buttonStyle(.borderless)
                                 }
+                                Spacer()
                             }
                         }
-                                .background(defBgColor.opacity(0.001))
                     }
+                    .background(defBgColor.opacity(0.001))
+                }
             )
-                    .frame(minHeight: 36) // Approximately
-                    .padding([.horizontal], 8)
-                    .padding([.vertical], 4)
-                    .buttonStyle(.plain)
-                    .background(squircleShape.fill(defBgColor))
-                    .sheet(isPresented: $isTickerPresented) {
-                        W_TickerDialog(
-                                activity: activityUI.activity,
-                                task: nil
-                        ) {
-                            isTickerPresented = false
-                        }
-                    }
+            .frame(minHeight: 36) // Approximately
+            .padding([.horizontal], 8)
+            .padding([.vertical], 4)
+            .buttonStyle(.plain)
+            .background(squircleShape.fill(defBgColor))
+            .sheet(isPresented: $isTickerPresented) {
+                W_TickerDialog(
+                    activity: activityUI.activity,
+                    task: nil
+                ) {
+                    isTickerPresented = false
+                }
+            }
         }
     }
 }
