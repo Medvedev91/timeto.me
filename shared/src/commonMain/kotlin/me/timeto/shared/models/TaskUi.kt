acb62e767b705ab9450d1cc2fb4bd43a18d28509
hi@timeto.me <hi@timeto.me>
2024-07-17 16:38:39 +0300

    TaskUi.taskTf

--- shared/src/commonMain/kotlin/me/timeto/shared/models/TaskUi.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/models/TaskUi.kt
@@ -1,7 +1,12 @@
 package me.timeto.shared.models
 
+import me.timeto.shared.TextFeatures
 import me.timeto.shared.db.TaskDb
+import me.timeto.shared.textFeatures
 
 data class TaskUi(
     val taskDb: TaskDb,
-)
+) {
+
+    val taskTf: TextFeatures = taskDb.text.textFeatures()
+}
