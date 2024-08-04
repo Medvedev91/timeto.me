e569e8bb3e661f32040fd1c152b071ccca57af84
hi@timeto.me <hi@timeto.me>
2024-08-04 19:09:28 +0300

    WhatsNewVm.Companion.prepHistoryItemsUi(): Today on Home Screen

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/WhatsNewVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/WhatsNewVm.kt
@@ -31,6 +31,7 @@ class WhatsNewVm : __VM<WhatsNewVm.State>() {
     companion object {
 
         fun prepHistoryItemsUi(): List<HistoryItemUi> = listOf(
+            HistoryItemUi(19939, "Today on Home Screen", text = "Can be disabled in settings."),
             HistoryItemUi(19912, "New Pomodoro", buttonUi = HistoryItemUi.ButtonUi.pomodoro),
             HistoryItemUi(19870, "Calendar Templates"),
             HistoryItemUi(19858, "Optional Pomodoro"),
