d0bea0b3976556fe8520d15652182cd22c6655c1
hi@timeto.me <hi@timeto.me>
2024-09-26 18:11:15 +0300

    DaytimeUi.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/models/DaytimeUi.kt
@@ -0,0 +1,21 @@
+package me.timeto.shared.models
+
+import me.timeto.shared.toHms
+
+data class DaytimeUi(
+    val hour: Int,
+    val minute: Int,
+) {
+
+    val seconds: Int = (hour * 3_600) + (minute * 60)
+    val text: String = hour.toString().padStart(2, '0') + ":" +
+                       minute.toString().padStart(2, '0')
+
+    companion object {
+
+        fun byDaytime(daytime: Int): DaytimeUi {
+            val (h, m) = daytime.toHms()
+            return DaytimeUi(hour = h, minute = m)
+        }
+    }
+}
