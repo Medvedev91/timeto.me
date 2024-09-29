a9fdc74fa9a57682fc6d1d12ebcf7810e0d12c8b
hi@timeto.me <hi@timeto.me>
2024-09-29 17:01:19 +0300

    __db.kt: Use Dispatchers.IO

--- shared/src/commonMain/kotlin/me/timeto/shared/db/__db.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/db/__db.kt
@@ -2,6 +2,7 @@ package me.timeto.shared.db
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 import me.timeto.appdbsq.TimetomeDB
 
@@ -9,5 +10,5 @@ internal const val DB_NAME = "timetome.db"
 internal lateinit var db: TimetomeDB
 
 internal suspend fun <T> dbIo(
-    block: suspend CoroutineScope.() -> T
-): T = withContext(Dispatchers.Default, block)
+    block: suspend CoroutineScope.() -> T,
+): T = withContext(Dispatchers.IO, block)
