a992e14c7b69db1ba3f5aa0110010d65dddd32ed
hi@timeto.me <hi@timeto.me>
2024-10-03 21:14:30 +0300

    __db.kt: asListFlow()

--- shared/src/commonMain/kotlin/me/timeto/shared/db/__db.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/db/__db.kt
@@ -1,8 +1,13 @@
 package me.timeto.shared.db
 
+import app.cash.sqldelight.Query
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import me.timeto.appdbsq.TimetomeDB
 
@@ -12,3 +17,6 @@ internal lateinit var db: TimetomeDB
 internal suspend fun <T> dbIo(
     block: suspend CoroutineScope.() -> T,
 ): T = withContext(Dispatchers.IO, block)
+
+fun <T : Any, R : Any> Query<T>.asListFlow(mapper: (T) -> R): Flow<List<R>> =
+    asFlow().mapToList(Dispatchers.IO).map { list -> list.map { mapper(it) } }
