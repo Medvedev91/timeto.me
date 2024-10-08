a69b4e765efd4c64b19b87b7f06e490a936f5a1c
hi@timeto.me <hi@timeto.me>
2024-10-08 15:54:38 +0300

    GoalsFormFs.kt:

---
+++ android_app/src/main/java/me/timeto/app/ui/GoalsFormFs.kt
@@ -0,0 +1,46 @@
+package me.timeto.app.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import me.timeto.app.VStack
+import me.timeto.app.c
+import me.timeto.app.rememberVm
+import me.timeto.shared.vm.ActivityFormSheetVm
+import me.timeto.shared.vm.GoalsFormVm
+
+@Composable
+fun GoalsFormFs(
+    _layer: WrapperView.Layer,
+    _initGoalsVmUi: List<ActivityFormSheetVm.GoalFormUi>,
+    _onSelected: (List<ActivityFormSheetVm.GoalFormUi>) -> Unit,
+) {
+
+    val (vm, state) = rememberVm(_initGoalsVmUi) {
+        GoalsFormVm(_initGoalsVmUi)
+    }
+
+    VStack(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(c.bg),
+    ) {
+
+        val scrollState = rememberScrollState()
+
+        Fs__HeaderAction(
+            title = state.headerTitle,
+            actionText = state.headerDoneText,
+            onCancel = { _layer.close() },
+            scrollState = scrollState,
+            onDone = {
+                _onSelected(state.goalsVmUi)
+                _layer.close()
+            },
+        )
+
+        SpacerW1()
+    }
+}
