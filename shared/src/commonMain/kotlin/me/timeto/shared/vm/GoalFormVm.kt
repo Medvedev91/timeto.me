8255c2eb4b87c9a19bb6a0db8eb09e5061edb46f
hi@timeto.me <hi@timeto.me>
2024-10-11 21:00:52 +0300

    GoalFormVm.kt: Finished text

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/GoalFormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/GoalFormVm.kt
@@ -9,23 +9,30 @@ class GoalFormVm(
     data class State(
         val id: Int?,
         val note: String,
+        val finishedText: String,
     ) {
 
         val headerTitle: String = if (id != null) "Edit Goal" else "New Goal"
         val headerDoneText = "Done"
 
         val notePlaceholder = "Note (optional)"
+        val finishedTitle = "Finished Emoji"
     }
 
     override val state = MutableStateFlow(
         State(
             id = initGoalFormUi?.id,
             note = initGoalFormUi?.note ?: "",
+            finishedText = initGoalFormUi?.finishText ?: "👍"
         )
     )
 
     fun setNote(note: String) {
         state.update { it.copy(note = note) }
+    }
+
+    fun setFinishedText(text: String) {
+        state.update { it.copy(finishedText = text) }
     }
 
     fun buildFormUi(
