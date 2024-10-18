3ea06d5649c31cba270797177d12a620c08740ef
hi@timeto.me <hi@timeto.me>
2024-10-18 21:45:42 +0300

    GoalFormVm.State.textFeatures

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/GoalFormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/GoalFormVm.kt
@@ -1,6 +1,8 @@
 package me.timeto.shared.vm
 
 import kotlinx.coroutines.flow.*
+import me.timeto.shared.TextFeatures
+import me.timeto.shared.textFeatures
 
 class GoalFormVm(
     initGoalFormUi: ActivityFormSheetVm.GoalFormUi?,
@@ -8,7 +10,7 @@ class GoalFormVm(
 
     data class State(
         val id: Int?,
-        val note: String,
+        val textFeatures: TextFeatures,
         val finishedText: String,
     ) {
 
@@ -16,19 +18,24 @@ class GoalFormVm(
         val headerDoneText = "Done"
 
         val notePlaceholder = "Note (optional)"
+        val note: String = textFeatures.textNoFeatures
+
         val finishedTitle = "Finished Emoji"
     }
 
     override val state = MutableStateFlow(
         State(
             id = initGoalFormUi?.id,
-            note = initGoalFormUi?.note ?: "",
+            textFeatures = (initGoalFormUi?.note ?: "").textFeatures(),
             finishedText = initGoalFormUi?.finishText ?: "👍"
         )
     )
 
     fun setNote(note: String) {
-        state.update { it.copy(note = note) }
+        state.update {
+            val tf = it.textFeatures.copy(textNoFeatures = note)
+            it.copy(textFeatures = tf)
+        }
     }
 
     fun setFinishedText(text: String) {
