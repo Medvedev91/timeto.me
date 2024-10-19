ead5d9f08fca41b2295d8d6b44b47b75fe4c88eb
hi@timeto.me <hi@timeto.me>
2024-10-19 23:57:39 +0300

    GoalFormVm.kt: Timer

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/GoalFormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/GoalFormVm.kt
@@ -1,8 +1,10 @@
 package me.timeto.shared.vm
 
 import kotlinx.coroutines.flow.*
+import me.timeto.shared.ColorRgba
 import me.timeto.shared.TextFeatures
 import me.timeto.shared.textFeatures
+import me.timeto.shared.toTimerHintNote
 
 class GoalFormVm(
     initGoalFormUi: ActivityFormSheetVm.GoalFormUi?,
@@ -20,6 +22,13 @@ class GoalFormVm(
         val notePlaceholder = "Note (optional)"
         val note: String = textFeatures.textNoFeatures
 
+        private val timer: Int? = textFeatures.timer
+        val timerTitle = "Timer on Bar Press"
+        val timerNote: String = timer?.toTimerHintNote(isShort = false) ?: "None"
+        val timerNoteColor: ColorRgba? = if (timer == null) ColorRgba.red else null
+        val timerPickerSheetTitle = "Timer"
+        val timerDefaultMinutes: Int = (if (timer != null) (timer / 60) else 45)
+
         val finishedTitle = "Finished Emoji"
     }
 
@@ -34,6 +43,13 @@ class GoalFormVm(
     fun setNote(note: String) {
         state.update {
             val tf = it.textFeatures.copy(textNoFeatures = note)
+            it.copy(textFeatures = tf)
+        }
+    }
+
+    fun setTimer(timer: Int) {
+        state.update {
+            val tf = it.textFeatures.copy(timer = timer)
             it.copy(textFeatures = tf)
         }
     }
