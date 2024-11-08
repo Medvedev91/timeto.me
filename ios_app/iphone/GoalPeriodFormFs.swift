0fea05ca775463a5ef3e3428a992bb5a2fa9f7f3
hi@timeto.me <hi@timeto.me>
2024-11-09 01:43:22 +0300

    GoalPeriodFormFs.swift: UI items

--- ios_app/iphone/GoalPeriodFormFs.swift
+++ ios_app/iphone/GoalPeriodFormFs.swift
@@ -42,6 +42,55 @@ struct GoalPeriodFormFs: View {
                 
                 VStack {
                     
+                    MyListView__PaddingFirst()
+                    
+                    MyListView__ItemView(
+                        isFirst: true,
+                        isLast: true,
+                        bgColor: c.fg
+                    ) {
+                        
+                        VStack {
+                            
+                            MyListView__ItemView__RadioView(
+                                text: state.daysOfWeekTitle,
+                                isActive: state.isDaysOfWeekSelected,
+                                onClick: {
+                                    vm.setTypeDaysOfWeek()
+                                }
+                            )
+                            
+                            if state.isDaysOfWeekSelected {
+                                WeekDaysFormView(
+                                    weekDays: state.daysOfWeek.map { $0.toInt().toKotlinInt() },
+                                    size: 36,
+                                    onChange: { newWeekDays in
+                                        vm.setDaysOfWeek(daysOfWeek: Set(newWeekDays))
+                                    }
+                                )
+                                .padding(.leading, H_PADDING)
+                                .padding(.top, 4)
+                                .padding(.bottom, 16)
+                            }
+                        }
+                    }
+                    
+                    MyListView__Padding__SectionSection()
+                    
+                    MyListView__ItemView(
+                        isFirst: true,
+                        isLast: true,
+                        bgColor: c.fg
+                    ) {
+                        
+                        MyListView__ItemView__RadioView(
+                            text: state.weeklyTitle,
+                            isActive: state.isWeeklySelected,
+                            onClick: {
+                                vm.setTypeWeekly()
+                            }
+                        )
+                    }
                 }
             }
         }
