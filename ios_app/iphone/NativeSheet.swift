ca9a79b66448b1103e1feba77e7b49d4141fba82
hi@timeto.me <hi@timeto.me>
2024-12-08 21:02:26 +0300

    NativeSheet.swift:

--- ios_app/iphone/NativeSheet.swift
+++ ios_app/iphone/NativeSheet.swift
@@ -1,24 +1,24 @@
 import SwiftUI
 
 class NativeSheet: ObservableObject {
-
+    
     @Published var items = [NativeSheet__Item<AnyView>]()
-
+    
     func show<Content: View>(
-            @ViewBuilder content: @escaping (Binding<Bool>) -> Content
+        @ViewBuilder content: @escaping (Binding<Bool>) -> Content
     ) {
         items.append(
-                NativeSheet__Item(
-                        content: { isPresented in
-                            AnyView(content(isPresented))
-                        }
-                )
+            NativeSheet__Item(
+                content: { isPresented in
+                    AnyView(content(isPresented))
+                }
+            )
         )
     }
 }
 
 extension View {
-
+    
     func attachNativeSheet() -> some View {
         modifier(NativeSheet__Modifier())
     }
@@ -28,35 +28,35 @@ extension View {
 ///
 
 struct NativeSheet__Item<Content>: View, Identifiable where Content: View {
-
+    
     @ViewBuilder var content: (Binding<Bool>) -> Content
-
+    
     let id = UUID().uuidString
-
+    
     @EnvironmentObject private var nativeSheet: NativeSheet
     @State private var isPresented = false
-
+    
     var body: some View {
-
+        
         ZStack {}
-                .sheetEnv(isPresented: $isPresented) {
-                    content($isPresented)
+            .sheetEnv(isPresented: $isPresented) {
+                content($isPresented)
+            }
+            .onAppear {
+                isPresented = true
+            }
+            .onChange(of: isPresented) { newValue in
+                if !newValue {
+                    nativeSheet.items.removeAll { $0.id == id }
                 }
-                .onAppear {
-                    isPresented = true
-                }
-                .onChange(of: isPresented) { newValue in
-                    if !newValue {
-                        nativeSheet.items.removeAll { $0.id == id }
-                    }
-                }
+            }
     }
 }
 
 private struct NativeSheet__Modifier: ViewModifier {
-
+    
     @StateObject private var nativeSheet = NativeSheet()
-
+    
     func body(content: Content) -> some View {
         ZStack {
             content
@@ -64,6 +64,6 @@ private struct NativeSheet__Modifier: ViewModifier {
                 wrapper
             }
         }
-                .environmentObject(nativeSheet)
+        .environmentObject(nativeSheet)
     }
 }
