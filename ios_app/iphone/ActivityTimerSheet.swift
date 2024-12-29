4db88fe8c5039ffdc61a5ed3cfb18735a701ca91
hi@timeto.me <hi@timeto.me>
2024-12-29 17:44:41 +0300

    ActivityTimerSheet.swift

--- ios_app/iphone/ActivityTimerSheet.swift
+++ ios_app/iphone/ActivityTimerSheet.swift
@@ -26,117 +26,117 @@ extension NativeSheet {
 }
 
 private struct ActivityTimerSheet: View {
-
+    
     @State private var vm: ActivityTimerSheetVm
-
+    
     @Binding private var isPresented: Bool
     private let onStart: () -> ()
-
+    
     @State private var formTimeItemsIdx: Int32 = 0.toInt32()
-
+    
     init(
-            activity: ActivityDb,
-            isPresented: Binding<Bool>,
-            timerContext: ActivityTimerSheetVm.TimerContext?,
-            onStart: @escaping () -> ()
+        activity: ActivityDb,
+        isPresented: Binding<Bool>,
+        timerContext: ActivityTimerSheetVm.TimerContext?,
+        onStart: @escaping () -> ()
     ) {
         self._isPresented = isPresented
         self.onStart = onStart
         _vm = State(initialValue: ActivityTimerSheetVm(activity: activity, timerContext: timerContext))
     }
-
+    
     var body: some View {
-
+        
         VMView(vm: vm, stack: .VStack()) { state in
-
+            
             HStack(spacing: 4) {
-
+                
                 Button(
-                        action: { isPresented.toggle() },
-                        label: { Text("Cancel") }
+                    action: { isPresented.toggle() },
+                    label: { Text("Cancel") }
                 )
-
+                
                 Spacer()
-
+                
                 Text(state.title)
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .multilineTextAlignment(.center)
-
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                
                 Spacer()
-
+                
                 Button(
-                        action: {
-                            vm.start {
-                                onStart()
-                            }
-                        },
-                        label: {
-                            Text("Start")
-                                    .fontWeight(.bold)
+                    action: {
+                        vm.start {
+                            onStart()
                         }
+                    },
+                    label: {
+                        Text("Start")
+                            .fontWeight(.bold)
+                    }
                 )
             }
-                    .padding(.horizontal, 25)
-                    .padding(.top, 24)
-
+            .padding(.horizontal, 25)
+            .padding(.top, 24)
+            
             // Plus padding from picker
             if let note = state.note {
                 Text(note)
-                        .font(.system(size: 14))
-                        .foregroundColor(.secondary)
-                        .multilineTextAlignment(.center)
-                        .padding(.top, 6)
+                    .font(.system(size: 14))
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 6)
             }
-
+            
             ZStack {
-
+                
                 VStack {
-
+                    
                     Spacer()
-
+                    
                     Picker(
-                            "Time",
-                            selection: $formTimeItemsIdx
+                        "Time",
+                        selection: $formTimeItemsIdx
                     ) {
                         ForEach(state.timeItems, id: \.idx) { item in
                             Text(item.title)
                         }
                     }
-                            .onChange(of: formTimeItemsIdx) { newValue in
-                                vm.setFormTimeItemIdx(newIdx: newValue)
-                            }
-                            .onAppear {
-                                formTimeItemsIdx = state.formTimeItemIdx
-                            }
-                            .pickerStyle(.wheel)
-                            .foregroundColor(.primary)
-                            .padding(.bottom, state.note != nil ? 30 : 5)
-
+                    .onChange(of: formTimeItemsIdx) { newValue in
+                        vm.setFormTimeItemIdx(newIdx: newValue)
+                    }
+                    .onAppear {
+                        formTimeItemsIdx = state.formTimeItemIdx
+                    }
+                    .pickerStyle(.wheel)
+                    .foregroundColor(.primary)
+                    .padding(.bottom, state.note != nil ? 30 : 5)
+                    
                     Spacer()
                 }
-
+                
                 VStack {
-
+                    
                     Spacer()
-
+                    
                     HStack {
                         TimerHintsView(
-                                timerHintsUI: state.timerHints,
-                                hintHPadding: 8.0,
-                                fontSize: 16.0,
-                                fontWeight: .light,
-                                onStart: {
-                                    onStart()
-                                }
+                            timerHintsUI: state.timerHints,
+                            hintHPadding: 8.0,
+                            fontSize: 16.0,
+                            fontWeight: .light,
+                            onStart: {
+                                onStart()
+                            }
                         )
                     }
-                            .padding(.bottom, 8)
+                    .padding(.bottom, 8)
                 }
-                        .safeAreaPadding(.bottom)
+                .safeAreaPadding(.bottom)
             }
         }
-                .presentationDetentsHeightIf16(400, withDragIndicator: true)
-                .ignoresSafeArea()
+        .presentationDetentsHeightIf16(400, withDragIndicator: true)
+        .ignoresSafeArea()
     }
 }
