a0546b491eb8c38e651814377f5f26f0477ee5a1
hi@timeto.me <hi@timeto.me>
2024-12-29 20:24:31 +0300

    view_extensions.swift: Plain list extensions

--- ios_app/common/view_extensions.swift
+++ ios_app/common/view_extensions.swift
@@ -6,3 +6,23 @@ extension View {
         frame(maxWidth: .infinity, alignment: alignment)
     }
 }
+
+// List
+
+extension View {
+    
+    func plainList() -> some View {
+        self
+            .listStyle(.plain)
+            .buttonStyle(PlainButtonStyle()) // Disable cell clickable
+            .environment(\.defaultMinListRowHeight, 0)
+    }
+    
+    func plainListItem() -> some View {
+        self
+            #if os(iOS)
+            .listRowSeparator(.hidden)
+            #endif
+            .listRowInsets(EdgeInsets())
+    }
+}
