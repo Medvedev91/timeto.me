2e7f255bbfcd10b84ab5a0a8e740fc6d7c2164b8
hi@timeto.me <hi@timeto.me>
2024-12-29 17:50:44 +0300

    ActivityFormSheet.swift:

--- ios_app/iphone/ActivityFormSheet.swift
+++ ios_app/iphone/ActivityFormSheet.swift
@@ -2,21 +2,21 @@ import SwiftUI
 import shared
 
 struct ActivityFormSheet: View {
-
+    
     @EnvironmentObject private var fs: Fs
     @EnvironmentObject private var nativeSheet: NativeSheet
-
+    
     @State private var vm: ActivityFormSheetVm
-
+    
     @Binding private var isPresented: Bool
     private let onSave: () -> ()
-
+    
     @State private var isAddCustomHintPresented = false
     @State private var isEmojiSheetPresented = false
     @State private var isColorPickerSheetPresented = false
-
+    
     @State private var sheetHeaderScroll = 0
-
+    
     init(
         isPresented: Binding<Bool>,
         activity: ActivityDb?,
@@ -26,11 +26,11 @@ struct ActivityFormSheet: View {
         _isPresented = isPresented
         vm = ActivityFormSheetVm(activity: activity)
     }
-
+    
     var body: some View {
-
+        
         VMView(vm: vm, stack: .VStack()) { state in
-
+            
             SheetHeaderView(
                 onCancel: { isPresented.toggle() },
                 title: state.headerTitle,
@@ -43,24 +43,24 @@ struct ActivityFormSheet: View {
                     onSave()
                 }
             }
-
+            
             ScrollViewWithVListener(showsIndicators: false, vScroll: $sheetHeaderScroll) {
-
+                
                 VStack {
-
+                    
                     VStack {
-
+                        
                         MyListView__Padding__SectionHeader()
-
+                        
                         MyListView__HeaderView(title: state.inputNameHeader)
-
+                        
                         MyListView__Padding__HeaderSection()
-
+                        
                         MyListView__ItemView(
                             isFirst: true,
                             isLast: true
                         ) {
-
+                            
                             MyListView__ItemView__TextInputView(
                                 text: state.inputNameValue,
                                 placeholder: state.inputNamePlaceholder,
@@ -70,30 +70,30 @@ struct ActivityFormSheet: View {
                                 }
                             )
                         }
-
+                        
                         MyListView__Padding__SectionSection()
-
+                        
                         TextFeaturesTriggersFormView(
                             textFeatures: state.textFeatures
                         ) { textFeatures in
                             vm.setTextFeatures(newTextFeatures: textFeatures)
                         }
-
+                        
                         MyListView__Padding__SectionSection()
                     }
-
+                    
                     MyListView__ItemView(
                         isFirst: true,
                         isLast: false
                     ) {
-
+                        
                         MyListView__ItemView__ButtonView(
                             text: state.emojiTitle,
                             withArrow: true,
                             rightView: AnyView(
-
+                                
                                 HStack {
-
+                                    
                                     if let selectedEmoji = state.emoji {
                                         Text(selectedEmoji)
                                             .font(.system(size: 30))
@@ -110,13 +110,13 @@ struct ActivityFormSheet: View {
                             isEmojiSheetPresented = true
                         }
                     }
-
+                    
                     MyListView__ItemView(
                         isFirst: false,
                         isLast: false,
                         withTopDivider: true
                     ) {
-
+                        
                         MyListView__ItemView__ButtonView(
                             text: state.colorTitle,
                             withArrow: false,
@@ -130,7 +130,7 @@ struct ActivityFormSheet: View {
                             isColorPickerSheetPresented = true
                         }
                     }
-
+                    
                     MyListView__ItemView(
                         isFirst: false,
                         isLast: false,
@@ -143,7 +143,7 @@ struct ActivityFormSheet: View {
                             vm.toggleKeepScreenOn()
                         }
                     }
-
+                    
                     MyListView__ItemView(
                         isFirst: false,
                         isLast: false,
@@ -168,9 +168,9 @@ struct ActivityFormSheet: View {
                             }
                         }
                     }
-
+                    
                     VStack {
-
+                        
                         MyListView__ItemView(
                             isFirst: false,
                             isLast: true,
@@ -200,36 +200,36 @@ struct ActivityFormSheet: View {
                             }
                         }
                     }
-
+                    
                     //////
-
+                    
                     MyListView__Padding__SectionSection()
-
+                    
                     MyListView__HeaderView(title: state.timerHintsHeader)
-
+                    
                     MyListView__Padding__HeaderSection()
-
+                    
                     VStack {
-
+                        
                         let hintsTypeName: [(title: String, type: ActivityDb__Data.TimerHintsHINT_TYPE)] = [
                             ("By History", .history),
                             ("Custom", .custom),
                         ]
-
+                        
                         ForEach(hintsTypeName, id: \.type) { pair in
-
+                            
                             let isActive = state.activityData.timer_hints.type == pair.type
-
+                            
                             let isFirst = hintsTypeName.first! == pair
-
+                            
                             MyListView__ItemView(
                                 isFirst: isFirst,
                                 isLast: hintsTypeName.last! == pair,
                                 withTopDivider: !isFirst
                             ) {
-
+                                
                                 VStack {
-
+                                    
                                     MyListView__ItemView__RadioView(
                                         text: pair.title,
                                         isActive: isActive
@@ -238,20 +238,20 @@ struct ActivityFormSheet: View {
                                             vm.setTimerHintsType(type: pair.type)
                                         }
                                     }
-
+                                    
                                     if isActive {
-
+                                        
                                         if (pair.type == .custom) {
-
+                                            
                                             VStack(alignment: .leading, spacing: 0) {
-
+                                                
                                                 // Because it can be by mistake not unique I do through count
                                                 ForEach(0..<state.timerHintsCustomItems.count, id: \.self) { idx in
-
+                                                    
                                                     let customItem = state.timerHintsCustomItems[idx]
-
+                                                    
                                                     HStack(spacing: 10) {
-
+                                                        
                                                         Button(
                                                             action: {
                                                                 vm.delCustomTimerHint(seconds: customItem.seconds)
@@ -261,12 +261,12 @@ struct ActivityFormSheet: View {
                                                                     .foregroundColor(.red)
                                                             }
                                                         )
-
+                                                        
                                                         Text(customItem.text)
                                                     }
                                                     .padding(.bottom, 10)
                                                 }
-
+                                                
                                                 Button(
                                                     action: {
                                                         isAddCustomHintPresented = true
@@ -286,16 +286,16 @@ struct ActivityFormSheet: View {
                         }
                     }
                 }
-
+                
                 if vm.activity != nil {
-
+                    
                     MyListView__Padding__SectionSection()
-
+                    
                     MyListView__ItemView(
                         isFirst: true,
                         isLast: true
                     ) {
-
+                        
                         MyListView__ItemView__ActionView(
                             text: state.deleteText
                         ) {
