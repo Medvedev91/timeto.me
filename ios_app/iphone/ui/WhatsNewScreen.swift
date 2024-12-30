d831b4e33ba0bd29a2c69ca0af2ff686cdceb0ab
hi@timeto.me <hi@timeto.me>
2024-12-30 08:19:38 +0300

    WhatsNewScreen.swift

--- ios_app/iphone/ui/WhatsNewScreen.swift
+++ ios_app/iphone/ui/WhatsNewScreen.swift
@@ -16,53 +16,57 @@ struct WhatsNewScreen: View {
             
             List {
                 
-                ForEach(state.historyItemsUi, id: \.self) { historyItemUi in
+                Section {
                     
-                    VStack {
+                    ForEach(state.historyItemsUi, id: \.self) { historyItemUi in
                         
-                        HStack {
+                        VStack {
                             
-                            Text(historyItemUi.dateText)
-                                .foregroundColor(.secondary)
-                                .font(.system(size: secondaryFontSize))
+                            HStack {
+                                
+                                Text(historyItemUi.dateText)
+                                    .foregroundColor(.secondary)
+                                    .font(.system(size: secondaryFontSize))
+                                
+                                Spacer()
+                                
+                                Text(historyItemUi.timeAgoText)
+                                    .foregroundColor(.secondary)
+                                    .font(.system(size: secondaryFontSize))
+                            }
                             
-                            Spacer()
-                            
-                            Text(historyItemUi.timeAgoText)
-                                .foregroundColor(.secondary)
-                                .font(.system(size: secondaryFontSize))
-                        }
-                        
-                        Text(historyItemUi.title)
-                            .foregroundColor(.primary)
-                            .font(.system(size: 17, weight: .bold))
-                            .padding(.top, 6)
-                            .textAlign(.leading)
-                        
-                        if let text = historyItemUi.text {
-                            Text(text)
-                                .foregroundColor(.secondary)
-                                .font(.system(size: secondaryFontSize))
+                            Text(historyItemUi.title)
+                                .foregroundColor(.primary)
+                                .font(.system(size: 17, weight: .bold))
                                 .padding(.top, 6)
                                 .textAlign(.leading)
-                        }
-                        
-                        if let buttonUi = historyItemUi.buttonUi {
-                            Text(buttonUi.text)
-                                .padding(.top, 6)
-                                .foregroundColor(.blue)
-                                .textAlign(.leading)
-                                .onTapGesture {
-                                    if (buttonUi == WhatsNewVm.HistoryItemUiButtonUi.pomodoro) {
-                                        fs.ReadmeSheet__open(defaultItem: .pomodoro)
-                                    } else {
-                                        fatalError()
+                            
+                            if let text = historyItemUi.text {
+                                Text(text)
+                                    .foregroundColor(.secondary)
+                                    .font(.system(size: secondaryFontSize))
+                                    .padding(.top, 6)
+                                    .textAlign(.leading)
+                            }
+                            
+                            if let buttonUi = historyItemUi.buttonUi {
+                                Text(buttonUi.text)
+                                    .padding(.top, 6)
+                                    .foregroundColor(.blue)
+                                    .textAlign(.leading)
+                                    .onTapGesture {
+                                        if (buttonUi == WhatsNewVm.HistoryItemUiButtonUi.pomodoro) {
+                                            fs.ReadmeSheet__open(defaultItem: .pomodoro)
+                                        } else {
+                                            fatalError()
+                                        }
                                     }
-                                }
+                            }
                         }
+                        .padding(.vertical, 2)
                     }
-                    .padding(.vertical, 2)
                 }
+                .listSectionSeparator(.hidden, edges: .top)
             }
             .listStyle(.plain)
             .navigationTitle(state.headerTitle)
