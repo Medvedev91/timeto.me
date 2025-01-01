46423ac4aa9d549101077d1e7d98dba891e17c0e
hi@timeto.me <hi@timeto.me>
2025-01-01 23:06:16 +0300

    view_extensions.swift: fillMax..()

--- ios_app/common/view_extensions.swift
+++ ios_app/common/view_extensions.swift
@@ -26,3 +26,20 @@ extension View {
             .listRowInsets(EdgeInsets())
     }
 }
+
+// fillMax..()
+
+extension View {
+    
+    func fillMaxWidth() -> some View {
+        frame(minWidth: 0, maxWidth: .infinity)
+    }
+    
+    func fillMaxHeight() -> some View {
+        frame(minHeight: 0, maxHeight: .infinity)
+    }
+    
+    func fillMaxSize() -> some View {
+        fillMaxWidth().fillMaxHeight()
+    }
+}
