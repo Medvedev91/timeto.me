bd6ea26c5aa5fc08dd53dd44fc8a1e04314f1081
hi@timeto.me <hi@timeto.me>
2025-01-02 17:53:21 +0300

    __vm.kt

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/__vm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/__vm.kt
@@ -4,14 +4,6 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.StateFlow
 import me.timeto.shared.defaultScope
 
-/**
- * WARNING Rules of use due to the specifics of implementation:
- * - Do not launch coroutines inside init() of view models;
- * - Do launch coroutines only in onAppear();
- * - To create coroutines inside life cycle use scopeVM();
- * - onDisappear() does not mean the destroy of view models, on iOS
- *   it can be restarted after onDisappear() by calling onAppear().
- */
 abstract class __Vm<T> {
 
     abstract val state: StateFlow<T>
