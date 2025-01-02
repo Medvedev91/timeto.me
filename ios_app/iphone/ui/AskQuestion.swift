8dea89faf9cc1f5be834a09197251df3f58ab85e
hi@timeto.me <hi@timeto.me>
2025-01-02 11:30:41 +0300

    AskQuestion.swift

---
+++ ios_app/iphone/ui/AskQuestion.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+import MessageUI
+import shared
+
+struct AskQuestion<Content: View>: View {
+    
+    let subject: String
+    @ViewBuilder let content: () -> Content
+    
+    @State private var isMailViewPresented = false
+    @State private var mailViewResult: Result<MFMailComposeResult, Error>? = nil
+
+    var body: some View {
+        Button(
+            action: {
+                if (MFMailComposeViewController.canSendMail()) {
+                    isMailViewPresented.toggle()
+                } else {
+                    let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                    let url = URL(string: "mailto:\(Utils_kmpKt.HI_EMAIL)?subject=\(subjectEncoded)")!
+                    UIApplication.shared.open(url)
+                }
+            },
+            label: {
+                content()
+            }
+        )
+    }
+}
