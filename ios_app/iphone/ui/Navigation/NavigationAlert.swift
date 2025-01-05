54a9d50fb36d3614be23c2859f1c1368356f82d0
hi@timeto.me <hi@timeto.me>
2025-01-06 00:55:17 +0300

    NavigationAlert.swift

---
+++ ios_app/iphone/ui/Navigation/NavigationAlert.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct NavigationAlert: View, Identifiable {
+    
+    let id: String = UUID().uuidString
+    let message: String
+    let onRemove: (_ id: String) -> Void
+    
+    @State private var isPresented = true
+    
+    var body: some View {
+        ZStack {}
+            .alert(
+                "",
+                isPresented: $isPresented,
+                actions: {
+                    Button("OK", role: .cancel) {
+                        onRemove(id)
+                    }
+                },
+                message: {
+                    Text(message)
+                }
+            )
+    }
+}
