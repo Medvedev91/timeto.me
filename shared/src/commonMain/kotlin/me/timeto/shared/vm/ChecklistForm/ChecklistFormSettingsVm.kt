1d69d59c18e236fd623babf61e9f29eb7c184299
hi@timeto.me <hi@timeto.me>
2025-01-05 21:05:02 +0300

    ChecklistFormSettingsVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/ChecklistForm/ChecklistFormSettingsVm.kt
@@ -0,0 +1,53 @@
+package me.timeto.shared.vm.ChecklistForm
+
+import kotlinx.coroutines.flow.*
+import me.timeto.shared.UIException
+import me.timeto.shared.db.ChecklistDb
+import me.timeto.shared.launchEx
+import me.timeto.shared.misc.DialogsManager
+import me.timeto.shared.vm.__Vm
+
+class ChecklistFormSettingsVm(
+    checklistDb: ChecklistDb?,
+) : __Vm<ChecklistFormSettingsVm.State>() {
+
+    data class State(
+        val checklistDb: ChecklistDb?,
+        val name: String,
+    ) {
+
+        val title: String = checklistDb?.name ?: "New Checklist"
+        val isSaveEnabled: Boolean = name.isNotBlank()
+
+        val namePlaceholder = "Name"
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            checklistDb = checklistDb,
+            name = checklistDb?.name ?: "",
+        )
+    )
+
+    fun setName(name: String) {
+        state.update { it.copy(name = name) }
+    }
+
+    fun save(
+        dialogsManager: DialogsManager,
+        onSuccess: (ChecklistDb) -> Unit,
+    ) {
+        scopeVm().launchEx {
+            try {
+                val oldChecklistDb: ChecklistDb? = state.value.checklistDb
+                val newChecklistDb: ChecklistDb = if (oldChecklistDb != null)
+                    oldChecklistDb.upNameWithValidation(state.value.name)
+                else
+                    ChecklistDb.addWithValidation(state.value.name)
+                onSuccess(newChecklistDb)
+            } catch (e: UIException) {
+                dialogsManager.alert(title = null, message = e.uiMessage)
+            }
+        }
+    }
+}
