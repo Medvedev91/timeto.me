32d8b1526b79f1316b8e9eedfb89605080e41e1a
hi@timeto.me <hi@timeto.me>
2025-01-06 13:17:05 +0300

    ChecklistFormSettingsScreen.swift

---
+++ ios_app/iphone/ui/ChecklistForm/ChecklistFormSettingsScreen.swift
@@ -0,0 +1,78 @@
+import SwiftUI
+import shared
+
+struct ChecklistFormSettingsScreen: View {
+    
+    let checklistDb: ChecklistDb?
+    let onSave: (ChecklistDb) -> Void
+    
+    var body: some View {
+        
+        VmView({
+            ChecklistFormSettingsVm(checklistDb: checklistDb)
+        }) { vm, state in
+            ChecklistFormSettingsScreenInner(
+                vm: vm,
+                state: state,
+                name: state.name,
+                onSave: onSave
+            )
+        }
+    }
+}
+
+private struct ChecklistFormSettingsScreenInner: View {
+    
+    let vm: ChecklistFormSettingsVm
+    let state: ChecklistFormSettingsVm.State
+    @State var name: String
+    let onSave: (ChecklistDb) -> Void
+    
+    ///
+    
+    @FocusState private var isFocused: Bool
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject private var navigation: Navigation
+    
+    var body: some View {
+        
+        List {
+            TextField(
+                text: $name,
+                prompt: Text(state.namePlaceholder)
+            ) {
+            }
+            .focused($isFocused)
+            .onChange(of: name) { _, new in
+                vm.setName(name: new)
+            }
+        }
+        .contentMargins(.top, 14)
+        .interactiveDismissDisabled()
+        .toolbarTitleDisplayMode(.inline)
+        .navigationTitle(state.title)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button(state.saveButtonText) {
+                    vm.save(
+                        dialogsManager: navigation,
+                        onSuccess: { newChecklistDb in
+                            dismiss()
+                            onSave(newChecklistDb)
+                        }
+                    )
+                }
+                .fontWeight(.bold)
+                .disabled(!state.isSaveEnabled)
+            }
+        }
+        .onAppear {
+            isFocused = true
+        }
+    }
+}
