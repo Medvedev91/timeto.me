57730cb47cd11eca53ae9bdbc9b86e01dcf0731b
hi@timeto.me <hi@timeto.me>
2025-01-06 10:01:57 +0300

    ChecklistDb.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/db/ChecklistDb.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/db/ChecklistDb.kt
@@ -17,7 +17,7 @@ data class ChecklistDb(
         fun anyChangeFlow() = db.checklistQueries.anyChange().asFlow()
 
         suspend fun selectAsc(): List<ChecklistDb> = dbIo {
-            db.checklistQueries.selectAsc().executeAsList().map { it.toDb() }
+            db.checklistQueries.selectAsc().asList { toDb() }
         }
 
         fun selectAscFlow(): Flow<List<ChecklistDb>> =
@@ -35,27 +35,8 @@ data class ChecklistDb(
             sqModel.toDb()
         }
 
-        private suspend fun validateName(
-            name: String,
-            exIds: Set<Int> = setOf(),
-        ): String {
-
-            val validatedName = name.trim()
-            if (validatedName.isEmpty())
-                throw UIException("Empty name")
-
-            selectAsc()
-                .filter { it.id !in exIds }
-                .forEach { checklist ->
-                    if (checklist.name.equals(validatedName, ignoreCase = true))
-                        throw UIException("$validatedName already exists.")
-                }
-
-            return validatedName
-        }
-
-        ///
-        /// Backupable Holder
+        //
+        // Backupable Holder
 
         override fun backupable__getAll(): List<Backupable__Item> =
             db.checklistQueries.selectAsc().executeAsList().map { it.toDb() }
@@ -94,8 +75,8 @@ data class ChecklistDb(
         db.checklistQueries.deleteById(id)
     }
 
-    ///
-    /// Backupable Item
+    //
+    // Backupable Item
 
     override fun backupable__getId(): String = id.toString()
 
@@ -116,6 +97,27 @@ data class ChecklistDb(
     }
 }
 
+///
+
 private fun ChecklistSQ.toDb() = ChecklistDb(
     id = id, name = name,
 )
+
+private suspend fun validateName(
+    name: String,
+    exIds: Set<Int> = setOf(),
+): String {
+
+    val validatedName = name.trim()
+    if (validatedName.isEmpty())
+        throw UIException("Empty name")
+
+    ChecklistDb.selectAsc()
+        .filter { it.id !in exIds }
+        .forEach { checklist ->
+            if (checklist.name.equals(validatedName, ignoreCase = true))
+                throw UIException("$validatedName already exists.")
+        }
+
+    return validatedName
+}
