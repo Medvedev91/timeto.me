6c6e7a62cac1a953705063704c3533c8e3c1478a
hi@timeto.me <hi@timeto.me>
2025-01-07 10:13:26 +0300

    view_extensions.swift: Fix textAlign()

--- ios_app/common/view_extensions.swift
+++ ios_app/common/view_extensions.swift
@@ -2,8 +2,15 @@ import SwiftUI
 
 extension View {
     
-    func textAlign(_ alignment: Alignment) -> some View {
-        frame(maxWidth: .infinity, alignment: alignment)
+    func textAlign(_ alignment: TextAlignment) -> some View {
+        let frameAlignment: Alignment = switch alignment {
+        case .leading: .leading
+        case .center:  .center
+        case .trailing: .trailing
+        }
+        return self
+            .frame(maxWidth: .infinity, alignment: frameAlignment)
+            .multilineTextAlignment(.leading)
     }
 }
 
