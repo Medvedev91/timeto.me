fc78e9c7943e8b3f43f1783a5e0a69bb8f545929
hi@timeto.me <hi@timeto.me>
2025-01-07 11:50:14 +0300

    NavigationPath.checklist

--- ios_app/iphone/ui/Navigation/NavigationPath.swift
+++ ios_app/iphone/ui/Navigation/NavigationPath.swift
@@ -5,6 +5,25 @@ enum NavigationPath: Hashable {
     
     case readme(defaultItem: ReadmeVm.DefaultItem)
     case whatsNew
+    case checklist(checklistDb: ChecklistDb, maxLines: Int, onDelete: () -> Void)
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .readme(let defaultItem):
+            hasher.combine("readme")
+            hasher.combine(defaultItem)
+        case .whatsNew:
+            hasher.combine("whatsNew")
+        case .checklist(let checklistDb, let maxLines, _):
+            hasher.combine("checklist")
+            hasher.combine(checklistDb)
+            hasher.combine(maxLines)
+        }
+    }
+    
+    static func == (lhs: NavigationPath, rhs: NavigationPath) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
 }
 
 extension NavigationPath {
@@ -15,6 +34,8 @@ extension NavigationPath {
             ReadmeScreen(defaultItem: defaultItem)
         case .whatsNew:
             WhatsNewScreen()
+        case .checklist(let checklistDb, let maxLines, let onDelete):
+            ChecklistScreen(checklistDb: checklistDb, maxLines: maxLines, onDelete: onDelete)
         }
     }
 }
