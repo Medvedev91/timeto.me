cd5677c1a307136c7ce07883d37c874a02a3ddf3
hi@timeto.me <hi@timeto.me>
2025-01-08 12:34:27 +0300

    DialogsManager.confirmation()

--- shared/src/commonMain/kotlin/me/timeto/shared/misc/DialogsManager.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/misc/DialogsManager.kt
@@ -3,4 +3,10 @@ package me.timeto.shared.misc
 interface DialogsManager {
 
     fun alert(message: String)
+
+    fun confirmation(
+        message: String,
+        buttonText: String,
+        onConfirm: () -> Unit,
+    )
 }
