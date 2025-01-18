08e264d49695d6ed790d0bae4fddd84fada46548
hi@timeto.me <hi@timeto.me>
2025-01-18 13:41:36 +0300

    ShortcutFormVm.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/ShortcutFormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/ShortcutFormVm.kt
@@ -3,30 +3,34 @@ package me.timeto.shared.vm
 import kotlinx.coroutines.flow.*
 import me.timeto.shared.db.ShortcutDb
 import me.timeto.shared.launchEx
+import me.timeto.shared.launchExIo
 import me.timeto.shared.misc.DialogsManager
 import me.timeto.shared.misc.UiException
 
 class ShortcutFormVm(
-    private val shortcutDb: ShortcutDb?,
+    shortcutDb: ShortcutDb?,
 ) : __Vm<ShortcutFormVm.State>() {
 
     data class State(
-        val title: String,
-        val saveText: String,
+        val shortcutDb: ShortcutDb?,
         val name: String,
         val uri: String,
     ) {
+
+        val title: String = if (shortcutDb != null) "Edit Shortcut" else "New Shortcut"
+        val saveText: String = if (shortcutDb != null) "Save" else "Create"
         val isSaveEnabled = (name.isNotBlank() && uri.isNotBlank())
+
         val nameHeader = "SHORTCUT NAME"
         val namePlaceholder = "Name"
+
         val uriHeader = "SHORTCUT LINK"
         val uriPlaceholder = "Link"
     }
 
     override val state = MutableStateFlow(
         State(
-            title = if (shortcutDb != null) "Edit Shortcut" else "New Shortcut",
-            saveText = if (shortcutDb != null) "Save" else "Create",
+            shortcutDb = shortcutDb,
             name = shortcutDb?.name ?: "",
             uri = shortcutDb?.uri ?: "",
         )
@@ -53,8 +57,9 @@ class ShortcutFormVm(
         try {
             val name: String = state.value.name
             val uri: String = state.value.name
-            val newShortcutDb: ShortcutDb = if (shortcutDb != null)
-                shortcutDb.updateWithValidation(name = name, uri = uri)
+            val oldShortcutDb: ShortcutDb? = state.value.shortcutDb
+            val newShortcutDb: ShortcutDb = if (oldShortcutDb != null)
+                oldShortcutDb.updateWithValidation(name = name, uri = uri)
             else
                 ShortcutDb.insertWithValidation(name = name, uri = uri)
             onUi { onSuccess(newShortcutDb) }
