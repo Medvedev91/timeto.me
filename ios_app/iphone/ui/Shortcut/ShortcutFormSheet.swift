cf0a28f1d7b20054c61aaebec83c3e3fecb1a303
hi@timeto.me <hi@timeto.me>
2025-01-18 08:51:04 +0300

    ShortcutFormSheet.swift:

--- ios_app/iphone/ui/Shortcut/ShortcutFormSheet.swift
+++ ios_app/iphone/ui/Shortcut/ShortcutFormSheet.swift
@@ -3,125 +3,136 @@ import shared
 
 struct ShortcutFormSheet: View {
     
-    @State private var vm: ShortcutFormSheetVm
-    @Binding private var isPresented: Bool
-    @State private var sheetHeaderScroll = 0
-    
-    init(
-        isPresented: Binding<Bool>,
-        editedShortcut: ShortcutDb?
-    ) {
-        _isPresented = isPresented
-        vm = ShortcutFormSheetVm(shortcut: editedShortcut)
+    let shortcutDb: ShortcutDb?
+    let onSave: (ShortcutDb) -> Void
+
+    var body: some View {
+        VmView({
+            ShortcutFormSheetVm(
+                initShortcutDb: shortcutDb
+            )
+        }) { vm, state in
+            ShortcutFormSheetInner(
+                vm: vm,
+                state: state,
+                name: state.name,
+                uri: state.uri,
+                onSave: onSave
+            )
+        }
     }
+}
+
+private struct ShortcutFormSheetInner: View {
+    
+    let vm: ShortcutFormSheetVm
+    let state: ShortcutFormSheetVm.State
+    
+    @State var name: String
+    @State var uri: String
+    
+    let onSave: (ShortcutDb) -> Void
+    
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(Navigation.self) private var navigation
     
     var body: some View {
         
-        VMView(vm: vm, stack: .VStack(spacing: 0)) { state in
+        List {
             
-            SheetHeaderView(
-                onCancel: { isPresented.toggle() },
-                title: state.headerTitle,
-                doneText: state.headerDoneText,
-                isDoneEnabled: state.isHeaderDoneEnabled,
-                scrollToHeader: sheetHeaderScroll
-            ) {
-                vm.save {
-                    isPresented = false
+            Section(state.nameHeader) {
+                TextField(
+                    state.namePlaceholder,
+                    text: $name
+                )
+                .onChange(of: name) { _, new in
+                    vm.setName(name: new)
                 }
             }
             
-            ScrollViewWithVListener(showsIndicators: false, vScroll: $sheetHeaderScroll) {
+            Section(state.uriHeader) {
+                TextField(
+                    state.uriPlaceholder,
+                    text: $uri
+                )
+                .onChange(of: uri) { _, new in
+                    vm.setUri(uri: new)
+                }
+            }
+            
+            
+            Section("EXAMPLES") {
                 
-                VStack(spacing: 0) {
+                ForEach(examples, id: \.self) { example in
                     
-                    VStack(spacing: 0) {
-                        
-                        MyListView__Padding__SectionSection()
-                        
-                        MyListView__HeaderView(title: state.inputNameHeader)
-                        
-                        MyListView__Padding__HeaderSection()
-                        
-                        MyListView__ItemView(
-                            isFirst: true,
-                            isLast: true
-                        ) {
+                    let isSelected: Bool = state.uri == example.uri
+                    
+                    Button(
+                        action: {
+                            name = example.name
+                            uri = example.uri
+                            hideKeyboard()
+                        },
+                        label: {
                             
-                            MyListView__ItemView__TextInputView(
-                                text: state.inputNameValue,
-                                placeholder: state.inputNamePlaceholder,
-                                isAutofocus: false,
-                                onValueChanged: { newValue in
-                                    vm.setInputNameValue(text: newValue)
+                            HStack {
+                                
+                                Text(example.name)
+                                    .foregroundColor(.primary)
+                                
+                                Spacer()
+                                
+                                Text(example.hint)
+                                    .foregroundColor(.secondary)
+                                
+                                if isSelected {
+                                    Image(systemName: "checkmark")
+                                        .font(.system(size: 16, weight: .medium))
+                                        .foregroundColor(.green)
+                                        .padding(.leading, 8)
+                                        .transition(.opacity)
                                 }
-                            )
-                        }
-                    }
-                    
-                    MyListView__HeaderView(title: state.inputUriHeader)
-                        .padding(.top, 30)
-                    
-                    MyListView__Padding__HeaderSection()
-                    
-                    MyListView__ItemView(
-                        isFirst: true,
-                        isLast: true
-                    ) {
-                        
-                        MyListView__ItemView__TextInputView(
-                            text: state.inputUriValue,
-                            placeholder: state.inputUriPlaceholder,
-                            isAutofocus: false,
-                            onValueChanged: { newValue in
-                                vm.setInputUriValue(text: newValue)
                             }
-                        )
-                    }
-                    
-                    MyListView__HeaderView(title: "EXAMPLES")
-                        .padding(.top, 60)
-                    
-                    MyListView__Padding__HeaderSection()
-                    
-                    ForEach(examples) { example in
-                        let isFirst = examples.first!.id == example.id
-                        MyListView__ItemView(
-                            isFirst: isFirst,
-                            isLast: examples.last!.id == example.id,
-                            withTopDivider: !isFirst
-                        ) {
-                            MyListView__ItemView__ButtonView(
-                                text: example.name,
-                                rightView: AnyView(
-                                    HStack {
-                                        
-                                        Text(example.hint)
-                                            .foregroundColor(.secondary)
-                                        
-                                        if (state.inputUriValue == example.uri) {
-                                            Image(systemName: "checkmark")
-                                                .font(.system(size: 16, weight: .medium))
-                                                .foregroundColor(.green)
-                                        }
-                                    }
-                                        .padding(.trailing, 14)
-                                )
-                            ) {
-                                vm.setInputNameValue(text: example.name)
-                                vm.setInputUriValue(text: example.uri)
-                                hideKeyboard()
-                            }
+                            .animation(.spring(response: 0.1), value: isSelected)
                         }
-                    }
-                    
-                    Spacer()
-                        .frame(minHeight: 20)
+                    )
                 }
             }
         }
-        .background(c.sheetBg)
+        .interactiveDismissDisabled()
+        .toolbarTitleDisplayMode(.inline)
+        .navigationTitle(state.title)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button(state.saveText) {
+                    vm.save(
+                        dialogsManager: navigation,
+                        onSuccess: { newShortcutDb in
+                            onSave(newShortcutDb)
+                            dismiss()
+                        }
+                    )
+                }
+                .fontWeight(.bold)
+                .disabled(!state.isSaveEnabled)
+            }
+        }
     }
+}
+
+///
+
+private struct ShortcutExample: Hashable {
+    let name: String
+    let hint: String
+    let uri: String
 }
 
 private let examples: [ShortcutExample] = [
@@ -129,23 +140,3 @@ private let examples: [ShortcutExample] = [
     ShortcutExample(name: "Podcasts", hint: "Open App", uri: "https://podcasts.apple.com"),
     ShortcutExample(name: "Play a Song 😈", hint: "Music App", uri: "https://music.apple.com/ru/album/highway-to-hell/574043989?i=574044008&l=en"),
 ]
-
-private struct ShortcutExample: Identifiable {
-    
-    let id: String
-    
-    let name: String
-    let hint: String
-    let uri: String
-    
-    init(
-        name: String,
-        hint: String,
-        uri: String
-    ) {
-        id = "\(name)-\(hint)-\(uri)"
-        self.name = name
-        self.hint = hint
-        self.uri = uri
-    }
-}
