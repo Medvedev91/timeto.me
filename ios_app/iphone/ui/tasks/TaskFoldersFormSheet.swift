b09e1d0118626658b4835172d54f880420807eef
hi@timeto.me <hi@timeto.me>
2025-01-19 20:54:13 +0300

    TaskFoldersFormSheet.swift:

--- ios_app/iphone/ui/tasks/TaskFoldersFormSheet.swift
+++ ios_app/iphone/ui/tasks/TaskFoldersFormSheet.swift
@@ -3,121 +3,76 @@ import shared
 
 struct TaskFoldersFormSheet: View {
     
-    @Binding var isPresented: Bool
+    var body: some View {
+        VmView({
+            FoldersSettingsVm()
+        }) { vm, state in
+            TaskFoldersFormSheetInner(
+                vm: vm,
+                state: state
+            )
+        }
+    }
+}
+
+private struct TaskFoldersFormSheetInner: View {
     
-    @State private var vm = FoldersSettingsVm()
-    @State private var sheetHeaderScroll = 0
+    let vm: FoldersSettingsVm
+    let state: FoldersSettingsVm.State
     
-    @State private var isAddSheetPresented = false
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(Navigation.self) private var navigation
+    
+    @State private var editMode: EditMode = .active
     
     var body: some View {
         
-        VMView(vm: vm, stack: .VStack(spacing: 0)) { state in
+        List {
             
-            SheetHeaderView(
-                onCancel: { isPresented.toggle() },
-                title: state.headerTitle,
-                doneText: nil,
-                isDoneEnabled: false,
-                scrollToHeader: sheetHeaderScroll,
-                cancelText: "Back"
-            ) {}
+            ForEach(state.foldersDb, id: \.id) { folderDb in
+                Button(folderDb.name) {
+                }
+                .foregroundColor(.primary)
+            }
+            .onMoveVm { from, to in
+                vm.moveIos(from: from, to: to)
+            }
+            .onDeleteVm { idx in
+                vm.delete(
+                    folderDb: state.foldersDb[idx],
+                    dialogsManager: navigation
+                )
+            }
             
-            ScrollViewWithVListener(showsIndicators: false, vScroll: $sheetHeaderScroll) {
+            Section {
                 
-                VStack {
-                    
-                    Padding(vertical: 20)
-                    
-                    let folders = state.folders
-                    ForEach(folders, id: \.id) { folder in
-                        let isFirst = folders.first == folder
-                        MyListView__ItemView(
-                            isFirst: isFirst,
-                            isLast: folders.last == folder,
-                            withTopDivider: !isFirst
-                        ) {
-                            FoldersSettingsSheet__FolderItem(
-                                vm: vm,
-                                folder: folder
-                            )
-                        }
-                    }
-                    
-                    MyListView__Padding__SectionSection()
-                    
-                    MyListView__ItemView(
-                        isFirst: true,
-                        isLast: true
-                    ) {
-                        MyListView__ItemView__ButtonView(
-                            text: "New Folder"
-                        ) {
-                            isAddSheetPresented = true
-                        }
-                        .sheetEnv(isPresented: $isAddSheetPresented) {
-                            FolderFormSheet(
-                                isPresented: $isAddSheetPresented,
-                                folder: nil
-                            )
-                        }
-                    }
-                    
-                    if let tmrwButtonUi = state.tmrwButtonUi {
-                        
-                        MyListView__Padding__SectionSection()
-                        
-                        MyListView__ItemView(
-                            isFirst: true,
-                            isLast: true
-                        ) {
-                            MyListView__ItemView__ButtonView(
-                                text: tmrwButtonUi.text
-                            ) {
-                                tmrwButtonUi.add()
-                            }
-                        }
+                Button("New Folder") {
+                }
+            }
+            
+            if let tmrwButtonUi = state.tmrwButtonUi {
+                Section {
+                    Button(tmrwButtonUi.text) {
+                        tmrwButtonUi.add(
+                            dialogsManager: navigation
+                        )
                     }
                 }
             }
         }
-        .background(c.sheetBg)
-    }
-}
-
-private struct FoldersSettingsSheet__FolderItem: View {
-    
-    let vm: FoldersSettingsVm
-    let folder: TaskFolderDb
-    
-    @State private var isEditPresented = false
-    
-    var body: some View {
-        
-        MyListView__ItemView__ButtonView(
-            text: folder.name,
-            rightView: AnyView(
-                Button(
-                    action: {
-                        vm.sortUp(folder: folder)
-                    },
-                    label: {
-                        Image(systemName: "arrow.up")
-                            .font(.system(size: 14))
-                            .foregroundColor(.blue)
-                    }
-                )
-                .buttonStyle(.plain)
-                .padding(.trailing, H_PADDING)
-            )
-        ) {
-            isEditPresented = true
-        }
-        .sheetEnv(isPresented: $isEditPresented) {
-            FolderFormSheet(
-                isPresented: $isEditPresented,
-                folder: folder
-            )
+        .environment(\.editMode, $editMode)
+        .contentMargins(.top, 14)
+        .toolbarTitleDisplayMode(.inline)
+        .navigationTitle(state.title)
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button("Done") {
+                    dismiss()
+                }
+                .fontWeight(.bold)
+            }
         }
     }
 }
