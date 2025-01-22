d9d45f249eb95f07f021ea46ee25107451e10bfc
hi@timeto.me <hi@timeto.me>
2025-01-22 09:46:56 +0300

    TaskFolderFormSheet.swift:

--- ios_app/iphone/ui/tasks/TaskFolderFormSheet.swift
+++ ios_app/iphone/ui/tasks/TaskFolderFormSheet.swift
@@ -2,83 +2,90 @@ import SwiftUI
 import shared
 
 struct TaskFolderFormSheet: View {
-
-    @State private var vm: FolderFormSheetVm
-    @Binding private var isPresented: Bool
-    @State private var sheetHeaderScroll = 0
-
-    init(
-            isPresented: Binding<Bool>,
-            folder: TaskFolderDb?
-    ) {
-        _isPresented = isPresented
-        vm = FolderFormSheetVm(folder: folder)
-    }
-
+    
+    let taskFolderDb: TaskFolderDb?
+    
     var body: some View {
+        VmView({
+            FolderFormSheetVm(
+                folderDb: taskFolderDb
+            )
+        }) { vm, state in
+            TaskFolderFormSheetInner(
+                vm: vm,
+                state: state,
+                name: state.name
+            )
+        }
+    }
+}
 
-        VMView(vm: vm, stack: .VStack(spacing: 0)) { state in
-
-            SheetHeaderView(
-                    onCancel: { isPresented.toggle() },
-                    title: state.headerTitle,
-                    doneText: state.headerDoneText,
-                    isDoneEnabled: state.isHeaderDoneEnabled,
-                    scrollToHeader: sheetHeaderScroll
-            ) {
-                vm.save {
-                    isPresented = false
-                }
+private struct TaskFolderFormSheetInner: View {
+    
+    let vm: FolderFormSheetVm
+    let state: FolderFormSheetVm.State
+    
+    @State var name: String
+    
+    ///
+    
+    @FocusState private var isFocused: Bool
+    @Environment(\.dismiss) private var dismiss
+    @Environment(Navigation.self) private var navigation
+    
+    var body: some View {
+        
+        List {
+            
+            TextField(
+                state.namePlaceholder,
+                text: $name
+            )
+            .focused($isFocused)
+            .onChange(of: name) { _, new in
+                vm.setName(name: new)
             }
-
-            ScrollViewWithVListener(showsIndicators: false, vScroll: $sheetHeaderScroll) {
-
-                VStack(spacing: 0) {
-
-                    VStack(spacing: 0) {
-
-                        MyListView__HeaderView(title: state.inputNameHeader)
-                                .padding(.top, 20)
-
-                        MyListView__Padding__HeaderSection()
-
-                        MyListView__ItemView(
-                                isFirst: true,
-                                isLast: true
-                        ) {
-
-                            MyListView__ItemView__TextInputView(
-                                    text: state.inputNameValue,
-                                    placeholder: state.inputNamePlaceholder,
-                                    isAutofocus: false,
-                                    onValueChanged: { newValue in
-                                        vm.setInputNameValue(name: newValue)
-                                    }
-                            )
-                        }
-
-                        if let folder = vm.folder, !folder.isToday {
-
-                            MyListView__Padding__SectionSection()
-
-                            MyListView__ItemView(
-                                    isFirst: true,
-                                    isLast: true
-                            ) {
-
-                                MyListView__ItemView__ActionView(
-                                        text: state.deleteFolderText
-                                ) {
-                                    vm.delete {
-                                        isPresented = false
-                                    }
-                                }
+            
+            if let folderDb = state.folderDb {
+                Section {
+                    Button(state.deleteText) {
+                        vm.delete(
+                            folderDb: folderDb,
+                            dialogsManager: navigation,
+                            onDelete: {
+                                dismiss()
                             }
-                        }
+                        )
                     }
+                    .foregroundColor(.red)
                 }
             }
         }
-                .background(c.sheetBg)
+        .contentMargins(.top, 14)
+        .interactiveDismissDisabled()
+        .toolbarTitleDisplayMode(.inline)
+        .navigationTitle(state.title)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button(state.saveText) {
+                    vm.save(
+                        dialogsManager: navigation,
+                        onSuccess: {
+                            dismiss()
+                        }
+                    )
+                }
+                .fontWeight(.bold)
+                .disabled(!state.isSaveEnabled)
+            }
+        }
+        .onAppear {
+            isFocused = true
+        }
     }
 }
