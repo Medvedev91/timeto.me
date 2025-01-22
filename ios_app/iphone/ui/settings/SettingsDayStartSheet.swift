73f45ec888a619acfbedb8ae02ae8a8ef8bdb9ba
hi@timeto.me <hi@timeto.me>
2025-01-22 20:23:53 +0300

    SettingsDayStartSheet.swift

---
+++ ios_app/iphone/ui/settings/SettingsDayStartSheet.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+import shared
+
+struct SettingsDayStartSheet: View {
+    
+    let vm: SettingsVm
+    let state: SettingsVm.State
+    
+    @State var dayStart: Int32
+    
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        
+        VStack {
+            
+            Picker("", selection: $dayStart) {
+                ForEach(state.dayStartListItems, id: \.seconds) { item in
+                    Text(item.note)
+                        .tag(item.seconds)
+                }
+            }
+            .pickerStyle(.wheel)
+        }
+        .presentationDetents([.medium])
+        .interactiveDismissDisabled()
+        .toolbarTitleDisplayMode(.inline)
+        .navigationTitle("Day Start")
+        .toolbar {
+            
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .primaryAction) {
+                Button("Save") {
+                    vm.upDayStartOffsetSeconds(seconds: dayStart) {
+                        dismiss()
+                    }
+                }
+                .fontWeight(.bold)
+            }
+        }
+    }
+}
