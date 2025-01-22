a5415f29df02efc25cccc8976f558a1cd12fd963
hi@timeto.me <hi@timeto.me>
2025-01-22 12:30:06 +0300

    FolderFormSheetVm.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/FolderFormSheetVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/FolderFormSheetVm.kt
@@ -4,85 +4,81 @@ import kotlinx.coroutines.flow.*
 import me.timeto.shared.*
 import me.timeto.shared.db.TaskFolderDb
 import me.timeto.shared.db.TaskDb
+import me.timeto.shared.ui.DialogsManager
 import me.timeto.shared.ui.UiException
 
 class FolderFormSheetVm(
-    val folder: TaskFolderDb?
+    folderDb: TaskFolderDb?,
 ) : __Vm<FolderFormSheetVm.State>() {
 
     data class State(
-        val folder: TaskFolderDb?,
-        val inputNameValue: String,
+        val folderDb: TaskFolderDb?,
+        val name: String,
     ) {
-        val headerTitle = if (folder != null) "Edit Folder" else "New Folder"
-        val headerDoneText = if (folder != null) "Done" else "Create"
-        val inputNameHeader = "FOLDER NAME"
-        val inputNamePlaceholder = "Folder Name"
-        val isHeaderDoneEnabled = inputNameValue.isNotBlank()
-        val deleteFolderText = "Delete Folder"
+
+        val title: String =
+            if (folderDb != null) "Edit Folder" else "New Folder"
+
+        val saveText: String =
+            if (folderDb != null) "Done" else "Create"
+
+        val namePlaceholder = "Folder Name"
+        val isSaveEnabled: Boolean = name.isNotBlank()
+        val deleteText = "Delete Folder"
     }
 
     override val state = MutableStateFlow(
         State(
-            folder = folder,
-            inputNameValue = folder?.name ?: "",
+            folderDb = folderDb,
+            name = folderDb?.name ?: "",
         )
     )
 
-    fun setInputNameValue(name: String) = state.update {
-        it.copy(inputNameValue = name)
+    fun setName(name: String) {
+        state.update { it.copy(name = name) }
     }
 
     fun save(
-        onSuccess: () -> Unit
+        dialogsManager: DialogsManager,
+        onSuccess: () -> Unit,
     ): Unit = launchExIo {
         try {
-            val name = state.value.inputNameValue
-            if (folder != null) {
-                folder.updateNameWithValidation(name)
-            } else {
+            val name: String = state.value.name
+            val folderDb: TaskFolderDb? = state.value.folderDb
+            if (folderDb != null)
+                folderDb.updateNameWithValidation(name)
+            else
                 TaskFolderDb.insertWithValidation(name)
-            }
             onUi { onSuccess() }
         } catch (e: UiException) {
-            showUiAlert(e.uiMessage)
+            dialogsManager.alert(e.uiMessage)
         }
     }
 
     fun delete(
-        onSuccess: () -> Unit
-    ) = launchExDefault {
-        try {
-            val folder = folder
-            if (folder == null) {
-                reportApi("FolderFormSheetVM no folder. WTF??!!")
-                return@launchExDefault
+        folderDb: TaskFolderDb,
+        dialogsManager: DialogsManager,
+        onDelete: () -> Unit,
+    ): Unit = launchExIo {
+
+        if (folderDb.isToday) {
+            dialogsManager.alert("It's impossible to delete \"Today\" folder")
+            return@launchExIo
+        }
+
+        if (TaskDb.getAsc().any { it.folder_id == folderDb.id }) {
+            dialogsManager.alert("The folder must be empty before deletion")
+            return@launchExIo
+        }
+
+        dialogsManager.confirmation(
+            message = "Are you sure you want to delete \"${folderDb.name}\" folder",
+            buttonText = "Delete",
+        ) {
+            launchExIo {
+                folderDb.delete()
+                onUi { onDelete() }
             }
-
-            if (folder.isToday)
-                throw UIException("It's impossible to delete \"Today\" folder")
-
-            if (TaskDb.getAsc().any { it.folder_id == folder.id })
-                throw UIException("The folder must be empty before deletion")
-
-            showUiConfirmation(
-                UIConfirmationData(
-                    text = "Are you sure you want to delete \"${folder.name}\" folder",
-                    buttonText = "Delete",
-                    isRed = true,
-                ) {
-                    launchExDefault {
-                        try {
-                            folder.backupable__delete()
-                            onSuccess()
-                        } catch (e: UIException) {
-                            showUiAlert(e.uiMessage)
-                        }
-                    }
-                }
-            )
-        } catch (e: UIException) {
-            showUiAlert(e.uiMessage)
         }
     }
 }
