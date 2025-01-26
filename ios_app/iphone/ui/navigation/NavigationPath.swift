31c665acacdff52cd4e35b799a96ffe686992099
hi@timeto.me <hi@timeto.me>
2025-01-26 12:19:25 +0300

    NavigationPath.swift:

--- ios_app/iphone/ui/navigation/NavigationPath.swift
+++ ios_app/iphone/ui/navigation/NavigationPath.swift
@@ -3,19 +3,14 @@ import shared
 
 enum NavigationPath: Hashable {
     
-    case readme(defaultItem: ReadmeVm.DefaultItem)
-    case whatsNew
     case checklist(checklistDb: ChecklistDb, maxLines: Int, onDelete: () -> Void)
     case note(noteDb: NoteDb, onDelete: () -> Void)
     case privacy
+    case readme(defaultItem: ReadmeVm.DefaultItem)
+    case whatsNew
     
     func hash(into hasher: inout Hasher) {
         switch self {
-        case .readme(let defaultItem):
-            hasher.combine("readme")
-            hasher.combine(defaultItem)
-        case .whatsNew:
-            hasher.combine("whats_new")
         case .checklist(let checklistDb, _, _):
             hasher.combine("checklist")
             hasher.combine(checklistDb.id)
@@ -24,6 +19,11 @@ enum NavigationPath: Hashable {
             hasher.combine(noteDb.id)
         case .privacy:
             hasher.combine("privacy")
+        case .readme(let defaultItem):
+            hasher.combine("readme")
+            hasher.combine(defaultItem)
+        case .whatsNew:
+            hasher.combine("whats_new")
         }
     }
     
@@ -36,16 +36,16 @@ extension NavigationPath {
     
     func rend() -> any View {
         switch self {
-        case .readme(let defaultItem):
-            ReadmeScreen(defaultItem: defaultItem)
-        case .whatsNew:
-            WhatsNewScreen()
         case .checklist(let checklistDb, let maxLines, let onDelete):
             ChecklistScreen(checklistDb: checklistDb, maxLines: maxLines, onDelete: onDelete)
         case .note(let noteDb, let onDelete):
             NoteScreen(noteDb: noteDb, onDelete: onDelete)
         case .privacy:
             PrivacyScreen(titleDisplayMode: .inline, scrollBottomMargin: HomeTabBar__HEIGHT)
+        case .readme(let defaultItem):
+            ReadmeScreen(defaultItem: defaultItem)
+        case .whatsNew:
+            WhatsNewScreen()
         }
     }
 }
