4eaefe17d2df662b49182e072a92968f07ecff02
hi@timeto.me <hi@timeto.me>
2025-01-27 09:26:11 +0300

    HomeTabBar.swift: Use onTouchGesture()

--- ios_app/iphone/ui/Home/HomeTabBar.swift
+++ ios_app/iphone/ui/Home/HomeTabBar.swift
@@ -16,12 +16,18 @@ struct HomeTabBar: View {
     
     let vm: HomeVm
     let state: HomeVm.State
+    
     @Binding var tabSelected: HomeTabSelected
     
     ///
     
     private var showBackground: Bool {
         tabSelected != .main || state.isTasksVisible
+    }
+    
+    private var background: AnyShapeStyle {
+        // Not .clear to tap area for onTouchGesture()
+        showBackground ? AnyShapeStyle(.bar) : AnyShapeStyle(.black)
     }
     
     @EnvironmentObject private var nativeSheet: NativeSheet
@@ -32,84 +38,74 @@ struct HomeTabBar: View {
             
             HStack {
                 
-                Button(
-                    action: {
+                Image(systemName: "timer")
+                    .fillMaxSize()
+                    .foregroundColor(c.homeFontSecondary)
+                    .font(.system(size: 30, weight: .thin))
+                    .background(background)
+                    .onTouchGesture {
                         nativeSheet.showActivitiesTimerSheet(
                             timerContext: nil,
                             withMenu: true,
                             onStart: {}
                         )
-                    },
-                    label: {
-                        Image(systemName: "timer")
-                            .fillMaxSize()
-                            .foregroundColor(c.homeFontSecondary)
-                            .font(.system(size: 30, weight: .thin))
                     }
-                )
                 
-                Button(
-                    action: {
-                        if tabSelected == .main {
-                            vm.toggleIsTasksVisible()
-                        } else {
-                            vm.setIsTaskVisible(isVisible: false)
-                            tabSelected = .main
-                        }
-                    },
-                    label: {
-                        
-                        VStack(alignment: .center) {
-                            
-                            Text(state.menuTime)
-                                .foregroundColor(c.homeMenuTime)
-                                .font(menuTimeFont)
-                                .padding(.top, 3)
-                                .padding(.bottom, 7)
-                            
-                            HStack {
-                                
-                                let batteryUi = state.batteryUi
-                                let batteryTextColor = batteryUi.colorRgba.toColor()
-                                
-                                Image(systemName: "bolt.fill")
-                                    .foregroundColor(batteryTextColor)
-                                    .font(.system(size: 12, weight: batteryUi.isHighlighted ? .regular : .ultraLight))
-                                
-                                Text(batteryUi.text)
-                                    .foregroundColor(batteryTextColor)
-                                    .font(.system(size: 13, weight: batteryUi.isHighlighted ? .bold : .regular))
-                                
-                                Image(systemName: "smallcircle.filled.circle")
-                                    .foregroundColor(c.homeFontSecondary)
-                                    .font(.system(size: 11 + halfDpCeil, weight: .regular))
-                                    .padding(.leading, 6)
-                                    .padding(.trailing, 1 + halfDpFloor)
-                                
-                                Text(state.menuTasksNote)
-                                    .foregroundColor(c.homeFontSecondary)
-                                    .font(.system(size: 13, weight: .regular))
-                            }
-                            .padding(.trailing, 2)
-                        }
+                VStack(alignment: .center) {
+                    
+                    Text(state.menuTime)
+                        .foregroundColor(c.homeMenuTime)
+                        .font(menuTimeFont)
                         .padding(.top, 3)
-                        .frame(height: HomeTabBar__HEIGHT)
-                        .frame(maxWidth: .infinity)
+                        .padding(.bottom, 7)
+                    
+                    HStack {
+                        
+                        let batteryUi = state.batteryUi
+                        let batteryTextColor = batteryUi.colorRgba.toColor()
+                        
+                        Image(systemName: "bolt.fill")
+                            .foregroundColor(batteryTextColor)
+                            .font(.system(size: 12, weight: batteryUi.isHighlighted ? .regular : .ultraLight))
+                        
+                        Text(batteryUi.text)
+                            .foregroundColor(batteryTextColor)
+                            .font(.system(size: 13, weight: batteryUi.isHighlighted ? .bold : .regular))
+                        
+                        Image(systemName: "smallcircle.filled.circle")
+                            .foregroundColor(c.homeFontSecondary)
+                            .font(.system(size: 11 + halfDpCeil, weight: .regular))
+                            .padding(.leading, 6)
+                            .padding(.trailing, 1 + halfDpFloor)
+                        
+                        Text(state.menuTasksNote)
+                            .foregroundColor(c.homeFontSecondary)
+                            .font(.system(size: 13, weight: .regular))
                     }
-                )
+                    .padding(.trailing, 2)
+                }
+                .padding(.top, 3)
+                .frame(height: HomeTabBar__HEIGHT)
+                .frame(maxWidth: .infinity)
+                .background(background)
+                .onTouchGesture {
+                    if tabSelected == .main {
+                        vm.toggleIsTasksVisible()
+                    } else {
+                        vm.setIsTaskVisible(isVisible: false)
+                        tabSelected = .main
+                    }
+                }
                 
-                Button(
-                    action: {
+                Image(systemName: "ellipsis.circle")
+                    .fillMaxSize()
+                    .foregroundColor(tabSelected == .settings ? .blue : c.homeFontSecondary)
+                    .font(.system(size: 30, weight: .thin))
+                    .background(background)
+                    .onTouchGesture {
                         vm.setIsTaskVisible(isVisible: false)
                         tabSelected = (tabSelected == .settings ? .main : .settings)
-                    },
-                    label: {
-                        Image(systemName: "ellipsis.circle")
-                            .fillMaxSize()
-                            .foregroundColor(tabSelected == .settings ? .blue : c.homeFontSecondary)
-                            .font(.system(size: 30, weight: .thin))
                     }
-                )
             }
             .fillMaxWidth()
             .frame(height: HomeTabBar__HEIGHT)
@@ -118,6 +114,5 @@ struct HomeTabBar: View {
                 DividerBg()
             }
         }
-        .background(showBackground ? AnyShapeStyle(.bar) : AnyShapeStyle(.clear))
     }
 }
