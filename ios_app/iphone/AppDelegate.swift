d69fe3a182187e8661248c51d2fd0c7f31f3140c
hi@timeto.me <hi@timeto.me>
2025-02-01 23:08:26 +0300

    AppDelegate.swift:

--- ios_app/iphone/AppDelegate.swift
+++ ios_app/iphone/AppDelegate.swift
@@ -3,46 +3,46 @@ import WatchConnectivity
 import shared
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-
+    
     func application(
-            _ application: UIApplication,
-            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         setupWCSession(self)
         return true
     }
 }
 
-///
-/// https://www.youtube.com/watch?v=_Gkp3H6Mnfs
-///
-extension AppDelegate: WCSessionDelegate {
+//
+// https://www.youtube.com/watch?v=_Gkp3H6Mnfs
 
+extension AppDelegate: WCSessionDelegate {
+    
     func session(
-            _ session: WCSession,
-            activationDidCompleteWith activationState: WCSessionActivationState,
-            error: Error?
+        _ session: WCSession,
+        activationDidCompleteWith activationState: WCSessionActivationState,
+        error: Error?
     ) {
         // todo log if error != nil
     }
-
+    
     func sessionDidBecomeInactive(_ session: WCSession) {
     }
-
+    
     // todo what is it?
     func sessionDidDeactivate(_ session: WCSession) {
         WCSession.default.activate()
     }
-
+    
     public func session(
-            _ session: WCSession,
-            didReceiveMessageData messageData: Data,
-            replyHandler: @escaping (Data) -> ()
+        _ session: WCSession,
+        didReceiveMessageData messageData: Data,
+        replyHandler: @escaping (Data) -> ()
     ) {
         // Otherwise a lot of logs about background
         DispatchQueue.main.async {
             IosToWatchSync.shared.didReceiveMessageData(
-                    jString: String(decoding: messageData, as: UTF8.self)
+                jString: String(decoding: messageData, as: UTF8.self)
             ) { jRes in
                 replyHandler(Data(jRes.utf8))
             }
