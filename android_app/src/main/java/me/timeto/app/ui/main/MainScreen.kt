fc67d41c39854583f715d93d44ec2a7a98fa7bb2
hi@timeto.me <hi@timeto.me>
2025-02-08 23:26:09 +0300

    MainScreen.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/main/MainScreen.kt
@@ -0,0 +1,57 @@
+package me.timeto.app.ui.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import me.timeto.app.VStack
+import me.timeto.app.ZStack
+import me.timeto.app.ui.TasksView
+import me.timeto.app.ui.home.HomeScreen
+import me.timeto.app.ui.navigation.NavigationScreen
+import me.timeto.app.ui.settings.SettingsScreen
+
+@Composable
+fun MainScreen() {
+
+    val tab = remember {
+        mutableStateOf(MainTabEnum.home)
+    }
+
+    VStack {
+
+        ZStack(
+            modifier = Modifier
+                .weight(1f),
+        ) {
+            when (tab.value) {
+                MainTabEnum.home -> {
+                    NavigationScreen {
+                        HomeScreen()
+                    }
+                }
+                MainTabEnum.tasks -> {
+                    NavigationScreen {
+                        TasksView(
+                            onClose = {
+                                tab.value = MainTabEnum.home
+                            },
+                        )
+                    }
+                }
+                MainTabEnum.settings -> {
+                    NavigationScreen {
+                        SettingsScreen()
+                    }
+                }
+            }
+        }
+
+        MainTabsView(
+            tab = tab.value,
+            onTabChanged = { newTab ->
+                tab.value = newTab
+            },
+        )
+    }
+}
