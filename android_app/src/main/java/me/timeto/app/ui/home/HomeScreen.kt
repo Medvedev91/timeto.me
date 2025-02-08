a56ccc8488d3a085bc96166a574f7b89ca699cb2
hi@timeto.me <hi@timeto.me>
2025-02-08 20:29:39 +0300

    HomeScreen.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/home/HomeScreen.kt
@@ -0,0 +1,18 @@
+package me.timeto.app.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val HomeScreen__primaryFontSize = 16.sp
+
+val HomeScreen__itemHeight = 36.dp
+val HomeScreen__itemCircleHPadding = 6.dp
+val HomeScreen__itemCircleHeight = 22.dp
+val HomeScreen__itemCircleFontSize = 13.sp
+val HomeScreen__itemCircleFontWeight = FontWeight.SemiBold
+
+@Composable
+fun HomeScreen() {
+}
