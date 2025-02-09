9831f955ef5eb27e642f0e7e4a74537f53c6330a
hi@timeto.me <hi@timeto.me>
2025-02-09 15:36:56 +0300

    FormPaddingSectionHeader.kt:

---
+++ android_app/src/main/java/me/timeto/app/ui/form/FormPaddingSectionHeader.kt
@@ -0,0 +1,12 @@
+package me.timeto.app.ui.form
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormPaddingSectionHeader() {
+    Box(Modifier.height(30.dp))
+}
