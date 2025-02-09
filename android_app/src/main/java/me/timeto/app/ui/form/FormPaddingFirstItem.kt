d100524d840e72daac4619c2cd48cdfadd75b38c
hi@timeto.me <hi@timeto.me>
2025-02-09 11:40:43 +0300

    FormPaddingFirstItem.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/form/FormPaddingFirstItem.kt
@@ -0,0 +1,12 @@
+package me.timeto.app.ui.form
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormPaddingFirstItem() {
+    Box(Modifier.height(14.dp))
+}
