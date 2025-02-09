b20fb79d22533fe5fbd7cb6ee76223ed3bf62dc7
hi@timeto.me <hi@timeto.me>
2025-02-09 17:05:01 +0300

    FormHeader.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/form/FormHeader.kt
@@ -0,0 +1,24 @@
+package me.timeto.app.ui.form
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.sp
+import me.timeto.app.H_PADDING
+import me.timeto.app.c
+
+@Composable
+fun FormHeader(
+    title: String,
+) {
+    Text(
+        title,
+        modifier = Modifier
+            .padding(horizontal = H_PADDING + H_PADDING)
+            .alpha(0.8f),
+        color = c.textSecondary,
+        fontSize = 12.sp,
+    )
+}
