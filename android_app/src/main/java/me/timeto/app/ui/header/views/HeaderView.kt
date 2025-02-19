8ce403c8c52e8c8150f9021769004e53a5de681d
hi@timeto.me <hi@timeto.me>
2025-02-19 12:10:40 +0300

    HeaderView.kt: Background on scroll

--- android_app/src/main/java/me/timeto/app/ui/header/views/HeaderView.kt
+++ android_app/src/main/java/me/timeto/app/ui/header/views/HeaderView.kt
@@ -1,15 +1,24 @@
 package me.timeto.app.ui.header.views
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import me.timeto.app.H_PADDING
 import me.timeto.app.MainActivity
 import me.timeto.app.ZStack
 import me.timeto.app.ui.DividerBgScroll
+
+private const val animRatio = 50f
+private val backgroundColor: Color = Color(red = 32, green = 32, blue = 34)
 
 @Composable
 fun HeaderView(
@@ -19,8 +28,45 @@ fun HeaderView(
 
     val mainActivity = LocalContext.current as MainActivity
 
+    val alphaValue = remember {
+
+        derivedStateOf {
+
+            when (scrollState) {
+
+                null -> 0f
+
+                is LazyListState -> {
+                    val offset = scrollState.firstVisibleItemScrollOffset
+                    when {
+                        scrollState.firstVisibleItemIndex > 0 -> 1f
+                        offset == 0 -> 0f
+                        offset > animRatio -> 1f
+                        else -> offset / animRatio
+                    }
+                }
+
+                is ScrollState -> {
+                    val offset = scrollState.value
+                    when {
+                        offset == 0 -> 0f
+                        offset > animRatio -> 1f
+                        else -> offset / animRatio
+                    }
+                }
+
+                else -> throw Exception("HeaderView.kt: invalid scrollState")
+            }
+        }
+    }
+
+    val alphaAnimate = animateFloatAsState(alphaValue.value)
+
     ZStack(
         modifier = Modifier
+            .drawBehind {
+                drawRect(color = backgroundColor.copy(alpha = alphaAnimate.value))
+            }
             .padding(top = mainActivity.statusBarHeightDp),
     ) {
 
@@ -29,8 +75,7 @@ fun HeaderView(
         DividerBgScroll(
             scrollState = scrollState,
             modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(horizontal = H_PADDING),
+                .align(Alignment.BottomCenter),
         )
     }
 }
