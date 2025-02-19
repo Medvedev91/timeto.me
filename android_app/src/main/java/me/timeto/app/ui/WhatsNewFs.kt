35b654bd6e5589b1a8fbe8a00acb781848994d81
hi@timeto.me <hi@timeto.me>
2025-02-19 21:16:52 +0300

    WhatsNewFs.kt:

--- android_app/src/main/java/me/timeto/app/ui/WhatsNewFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/WhatsNewFs.kt
@@ -1,12 +1,11 @@
 package me.timeto.app.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,6 +14,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.timeto.app.*
+import me.timeto.app.ui.header.Header
+import me.timeto.app.ui.header.HeaderCancelButton
 import me.timeto.app.ui.navigation.LocalNavigationLayer
 import me.timeto.shared.vm.ReadmeVm
 import me.timeto.shared.vm.WhatsNewVm
@@ -28,113 +29,111 @@ fun WhatsNewFs() {
         WhatsNewVm()
     }
 
-    VStack(
-        modifier = Modifier
-            .fillMaxHeight()
-            .background(c.bg),
-    ) {
+    Screen {
 
-        val scrollState = rememberScrollState()
+        val scrollState = rememberLazyListState()
 
-        Fs__HeaderTitle(
+        Header(
             title = state.headerTitle,
             scrollState = scrollState,
-            onClose = {
-                navigationLayer.close()
-            },
+            actionButton = null,
+            cancelButton = HeaderCancelButton(
+                text = "Close",
+                onClick = {
+                    navigationLayer.close()
+                },
+            ),
         )
 
-        VStack(
+        LazyColumn(
             modifier = Modifier
-                .verticalScroll(state = scrollState)
-                .weight(1f),
+                .navigationBarsPadding(),
+            state = scrollState,
+            contentPadding = PaddingValues(bottom = H_PADDING),
         ) {
 
-            state.historyItemsUi.forEach { historyItemUi ->
+            item {
 
-                VStack(
-                    modifier = Modifier
-                        .padding(top = 12.dp),
-                ) {
+                state.historyItemsUi.forEach { historyItemUi ->
 
-                    HStack(
+                    VStack(
                         modifier = Modifier
-                            .padding(horizontal = H_PADDING),
+                            .padding(top = 12.dp),
                     ) {
 
-                        Text(
-                            text = historyItemUi.dateText,
+                        HStack(
                             modifier = Modifier
-                                .weight(1f),
-                            color = c.textSecondary,
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Light,
-                        )
+                                .padding(horizontal = H_PADDING),
+                        ) {
+
+                            Text(
+                                text = historyItemUi.dateText,
+                                modifier = Modifier
+                                    .weight(1f),
+                                color = c.textSecondary,
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.Light,
+                            )
+
+                            Text(
+                                text = historyItemUi.timeAgoText,
+                                color = c.textSecondary,
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.Light,
+                            )
+                        }
 
                         Text(
-                            text = historyItemUi.timeAgoText,
-                            color = c.textSecondary,
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Light,
-                        )
-                    }
-
-                    Text(
-                        text = historyItemUi.title,
-                        modifier = Modifier
-                            .padding(top = 2.dp)
-                            .padding(horizontal = H_PADDING),
-                        color = c.text,
-                        fontWeight = FontWeight.Bold,
-                    )
-
-                    val text = historyItemUi.text
-                    if (text != null) {
-                        Text(
-                            text = text,
+                            text = historyItemUi.title,
                             modifier = Modifier
                                 .padding(top = 2.dp)
                                 .padding(horizontal = H_PADDING),
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Light,
-                            color = c.textSecondary,
+                            color = c.text,
+                            fontWeight = FontWeight.Bold,
                         )
-                    }
 
-                    val buttonUi = historyItemUi.buttonUi
-                    if (buttonUi != null) {
-                        Text(
-                            text = buttonUi.text,
-                            modifier = Modifier
-                                .padding(top = 2.dp)
-                                .padding(horizontal = H_PADDING_HALF)
-                                .clip(squircleShape)
-                                .clickable {
-                                    when (buttonUi) {
-                                        WhatsNewVm.HistoryItemUi.ButtonUi.pomodoro -> {
-                                            ReadmeSheet__show(ReadmeVm.DefaultItem.pomodoro)
+                        val text = historyItemUi.text
+                        if (text != null) {
+                            Text(
+                                text = text,
+                                modifier = Modifier
+                                    .padding(top = 2.dp)
+                                    .padding(horizontal = H_PADDING),
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.Light,
+                                color = c.textSecondary,
+                            )
+                        }
+
+                        val buttonUi = historyItemUi.buttonUi
+                        if (buttonUi != null) {
+                            Text(
+                                text = buttonUi.text,
+                                modifier = Modifier
+                                    .padding(top = 2.dp)
+                                    .padding(horizontal = H_PADDING_HALF)
+                                    .clip(squircleShape)
+                                    .clickable {
+                                        when (buttonUi) {
+                                            WhatsNewVm.HistoryItemUi.ButtonUi.pomodoro -> {
+                                                ReadmeSheet__show(ReadmeVm.DefaultItem.pomodoro)
+                                            }
                                         }
                                     }
-                                }
-                                .padding(horizontal = H_PADDING_HALF - halfDpFloor),
-                            color = c.blue,
-                        )
-                    }
+                                    .padding(horizontal = H_PADDING_HALF - halfDpFloor),
+                                color = c.blue,
+                            )
+                        }
 
-                    if (state.historyItemsUi.last() != historyItemUi)
-                        DividerBg(
-                            modifier = Modifier
-                                .padding(top = 12.dp)
-                                .padding(horizontal = H_PADDING),
-                        )
+                        if (state.historyItemsUi.last() != historyItemUi)
+                            DividerBg(
+                                modifier = Modifier
+                                    .padding(top = 12.dp)
+                                    .padding(start = H_PADDING),
+                            )
+                    }
                 }
             }
-
-            ZStack(
-                modifier = Modifier
-                    .navigationBarsPadding()
-                    .padding(bottom = H_PADDING),
-            ) {}
         }
     }
 }
