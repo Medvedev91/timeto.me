293518837eb73c61e5e91f3889e2add16f7c043a
hi@timeto.me <hi@timeto.me>
2025-02-21 11:15:01 +0300

    ui_utils.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/ui/ui_utils.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/ui_utils.kt
@@ -3,15 +3,15 @@ package me.timeto.shared.ui
 import me.timeto.shared.launchExIo
 
 fun <T> List<T>.moveIos(
-    from: Int,
-    to: Int,
+    fromIdx: Int,
+    toIdx: Int,
     action: suspend (List<T>) -> Unit,
 ) {
     val oldList: List<T> = this
     val newList: MutableList<T> = oldList.toMutableList()
-    val fromItem: T = oldList[from]
-    newList.removeAt(from)
-    newList.add(to, fromItem)
+    val fromItem: T = oldList[fromIdx]
+    newList.removeAt(fromIdx)
+    newList.add(toIdx, fromItem)
     launchExIo {
         action(newList)
     }
