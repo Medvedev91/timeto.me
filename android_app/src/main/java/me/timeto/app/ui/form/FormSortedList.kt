7a0dd808179dc494a134f5c07eae94b9d1709cfd
hi@timeto.me <hi@timeto.me>
2025-02-21 10:39:00 +0300

    FormSortedList()

---
+++ android_app/src/main/java/me/timeto/app/ui/form/FormSortedList.kt
@@ -0,0 +1,71 @@
+package me.timeto.app.ui.form
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import me.timeto.app.ZStack
+import me.timeto.app.ui.form.views.FormSortedItemView
+import me.timeto.app.ui.form.views.FormSortedState
+
+@Composable
+fun <Item>FormSortedList(
+    items: List<Item>,
+    itemId: (Item) -> Int,
+    itemTitle: (Item) -> String,
+    onItemClick: (Item) -> Unit,
+    scrollState: LazyListState,
+    onMove: (Int, Int) -> Unit, // from idx / to idx
+    onFinish: () -> Unit,
+) {
+
+    val sortedState = remember {
+        FormSortedState()
+    }
+    val sortedMovingIdx = remember {
+        mutableStateOf<Int?>(null)
+    }
+
+    LazyColumn(
+        state = scrollState,
+        userScrollEnabled = sortedMovingIdx.value == null,
+    ) {
+
+        item {
+            ZStack(Modifier.height(4.dp))
+        }
+
+        items.forEachIndexed { idx, item ->
+            item(key = "item_${itemId(item)}") {
+                FormSortedItemView(
+                    title = itemTitle(item),
+                    isFirst = items.first() == item,
+                    itemIdx = idx,
+                    sortedState = sortedState,
+                    sortedMovingIdx = sortedMovingIdx,
+                    onMove = { fromIdx, toIdx ->
+                        onMove(fromIdx, toIdx)
+                    },
+                    onFinish = {
+                        onFinish()
+                    },
+                    onClick = {
+                        onItemClick(item)
+                    },
+                )
+            }
+        }
+
+        item {
+            ZStack(
+                modifier = Modifier
+                    .navigationBarsPadding(),
+            )
+        }
+    }
+}
