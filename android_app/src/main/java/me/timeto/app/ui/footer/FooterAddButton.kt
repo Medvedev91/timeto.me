ec3e709022f4603742bd7a17a1a7450fbde2ee80
hi@timeto.me <hi@timeto.me>
2025-02-22 13:46:52 +0300

    FooterAddButton.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/footer/FooterAddButton.kt
@@ -0,0 +1,53 @@
+package me.timeto.app.ui.footer
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import me.timeto.app.HStack
+import me.timeto.app.H_PADDING_HALF
+import me.timeto.app.R
+import me.timeto.app.c
+import me.timeto.app.squircleShape
+
+@Composable
+fun FooterAddButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+
+    HStack(
+        modifier = Modifier
+            .clip(squircleShape)
+            .clickable {
+                onClick()
+            }
+            .padding(horizontal = H_PADDING_HALF, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Icon(
+            painter = painterResource(R.drawable.sf_plus_circle_fill_medium_bold),
+            contentDescription = text,
+            tint = c.blue,
+            modifier = Modifier
+                .size(20.dp),
+        )
+
+        Text(
+            text = text,
+            modifier = Modifier
+                .padding(start = 8.dp),
+            color = c.blue,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
