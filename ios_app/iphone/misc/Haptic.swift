ab8be5c6fe6a761e8a6ddb9ada414b0174355c5a
hi@timeto.me <hi@timeto.me>
2025-02-23 22:24:02 +0300

    Haptic.swift

---
+++ ios_app/iphone/misc/Haptic.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct Haptic {
+    
+    static func softShot() {
+        shot(.soft)
+    }
+    
+    static func success() {
+        notification(.success)
+    }
+}
+
+///
+
+private func shot(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    UIImpactFeedbackGenerator(style: style).impactOccurred()
+}
+
+private func notification(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+    UINotificationFeedbackGenerator().notificationOccurred(type)
+}
