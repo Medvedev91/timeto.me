704b52684d9b061c3523a8205a744ed7f6c28a0a
hi@timeto.me <hi@timeto.me>
2025-03-03 21:01:43 +0300

    ChecklistItemsFormFs.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/checklists/ChecklistItemsFormFs.kt
@@ -0,0 +1,108 @@
+package me.timeto.app.ui.checklists
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import me.timeto.app.H_PADDING_HALF
+import me.timeto.app.rememberVm
+import me.timeto.app.ui.Screen
+import me.timeto.app.ui.footer.Footer
+import me.timeto.app.ui.footer.FooterAddButton
+import me.timeto.app.ui.form.FormSortedList
+import me.timeto.app.ui.header.Header
+import me.timeto.app.ui.header.HeaderCancelButton
+import me.timeto.app.ui.header.HeaderSecondaryButton
+import me.timeto.app.ui.navigation.LocalNavigationFs
+import me.timeto.app.ui.navigation.LocalNavigationLayer
+import me.timeto.shared.db.ChecklistDb
+import me.timeto.shared.ui.checklists.ChecklistItemsFormVm
+
+@Composable
+fun ChecklistItemsFormFs(
+    checklistDb: ChecklistDb,
+    onDelete: () -> Unit,
+) {
+
+    val navigationFs = LocalNavigationFs.current
+    val navigationLayer = LocalNavigationLayer.current
+
+    val (vm, state) = rememberVm {
+        ChecklistItemsFormVm(checklistDb = checklistDb)
+    }
+
+    Screen {
+
+        val scrollState = rememberLazyListState()
+
+        Header(
+            title = state.checklistName,
+            scrollState = scrollState,
+            actionButton = null,
+            cancelButton = HeaderCancelButton(
+                text = "Close",
+                onClick = {
+                    navigationLayer.close()
+                },
+            ),
+            secondaryButtons = listOf(
+                HeaderSecondaryButton(
+                    text = "Settings",
+                    onClick = {
+                        navigationFs.push {
+                            ChecklistFormFs(
+                                checklistDb = checklistDb,
+                                onSave = {},
+                                onDelete = {
+                                    onDelete()
+                                    navigationLayer.close()
+                                },
+                            )
+                        }
+                    },
+                )
+            ),
+        )
+
+        FormSortedList(
+            items = state.checklistItemsDb,
+            itemId = { it.id },
+            itemTitle = { it.text },
+            onItemClick = { checklistItemDb ->
+                navigationFs.push {
+                    ChecklistItemFormFs(
+                        checklistDb = checklistDb,
+                        checklistItemDb = checklistItemDb,
+                    )
+                }
+            },
+            scrollState = scrollState,
+            modifier = Modifier
+                .weight(1f),
+            onMove = { fromIdx, toIdx ->
+                vm.moveAndroidLocal(fromIdx, toIdx)
+            },
+            onFinish = {
+                vm.moveAndroidSync()
+            },
+        )
+
+        Footer(
+            scrollState = scrollState,
+            contentModifier = Modifier
+                .padding(horizontal = H_PADDING_HALF),
+        ) {
+            FooterAddButton(
+                text = state.newItemText,
+                onClick = {
+                    navigationFs.push {
+                        ChecklistItemFormFs(
+                            checklistDb = checklistDb,
+                            checklistItemDb = null,
+                        )
+                    }
+                },
+            )
+        }
+    }
+}
