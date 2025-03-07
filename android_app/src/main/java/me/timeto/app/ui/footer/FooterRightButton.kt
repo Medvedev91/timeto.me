4037b46944b2a3a0987890d90630e2b2b46a75c9
hi@timeto.me <hi@timeto.me>
2025-03-07 10:40:58 +0300

    FooterRightButton()

---
+++ android_app/src/main/java/me/timeto/app/ui/footer/FooterRightButton.kt
@@ -0,0 +1,29 @@
+package me.timeto.app.ui.footer
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import me.timeto.app.H_PADDING_HALF
+import me.timeto.app.c
+import me.timeto.app.squircleShape
+
+@Composable
+fun FooterRightButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .clip(squircleShape)
+            .clickable {
+                onClick()
+            }
+            .padding(horizontal = H_PADDING_HALF, vertical = 4.dp),
+        color = c.blue,
+    )
+}
