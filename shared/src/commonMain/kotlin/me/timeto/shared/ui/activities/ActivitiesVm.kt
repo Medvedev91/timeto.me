a7831eb3399b6724c7120a96f2d08b20c51504d5
hi@timeto.me <hi@timeto.me>
2025-03-09 11:30:14 +0300

    ActivitiesVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/activities/ActivitiesVm.kt
@@ -0,0 +1,43 @@
+package me.timeto.shared.ui.activities
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.Cache
+import me.timeto.shared.db.ActivityDb
+import me.timeto.shared.onEachExIn
+import me.timeto.shared.textFeatures
+import me.timeto.shared.vm.__Vm
+
+class ActivitiesVm : __Vm<ActivitiesVm.State>() {
+
+    data class State(
+        val activitiesUi: List<ActivityUi>
+    )
+
+    override val state = MutableStateFlow(
+        State(
+            activitiesUi =
+                Cache.activitiesDbSorted.map { ActivityUi(it) },
+        )
+    )
+
+    init {
+        val scopeVm = scopeVm()
+        ActivityDb.selectAllSortedFlow().onEachExIn(scopeVm) { activitiesDb ->
+            state.update { state ->
+                state.copy(activitiesUi = activitiesDb.map { ActivityUi(it) })
+            }
+        }
+    }
+
+    ///
+
+    class ActivityUi(
+        val activityDb: ActivityDb,
+    ) {
+        val text: String =
+            activityDb.name.textFeatures().textUi()
+        val isActive: Boolean =
+            Cache.lastInterval.activity_id == activityDb.id
+    }
+}
