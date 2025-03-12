a6b0a4d81f4b2987245e5bd460c9899356c8758b
hi@timeto.me <hi@timeto.me>
2025-03-12 21:45:32 +0300

    ShortcutApp.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/shortcuts/apps/ShortcutApp.kt
@@ -0,0 +1,9 @@
+package me.timeto.app.ui.shortcuts.apps
+
+import android.graphics.drawable.Drawable
+
+class ShortcutApp(
+    val name: String,
+    val androidPackage: String,
+    val icon: Drawable,
+)
