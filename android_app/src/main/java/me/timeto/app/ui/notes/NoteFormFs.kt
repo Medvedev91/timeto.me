f343dcd4a0e3921a80e6b92af6915cf639977ee7
hi@timeto.me <hi@timeto.me>
2025-03-14 00:05:12 +0300

    NoteFormFs.kt: Delete Button

--- android_app/src/main/java/me/timeto/app/ui/notes/NoteFormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/notes/NoteFormFs.kt
@@ -1,11 +1,16 @@
 package me.timeto.app.ui.notes
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.input.ImeAction
+import me.timeto.app.c
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
+import me.timeto.app.ui.form.FormButton
 import me.timeto.app.ui.form.FormInput
 import me.timeto.app.ui.form.FormPaddingFirstItem
+import me.timeto.app.ui.form.FormPaddingSectionSection
 import me.timeto.app.ui.header.Header
 import me.timeto.app.ui.header.HeaderActionButton
 import me.timeto.app.ui.header.HeaderCancelButton
@@ -17,6 +22,7 @@ import me.timeto.shared.ui.notes.NoteFormVm
 @Composable
 fun NoteFormFs(
     noteDb: NoteDb?,
+    onDelete: () -> Unit,
 ) {
 
     val navigationFs = LocalNavigationFs.current
@@ -30,9 +36,11 @@ fun NoteFormFs(
 
     Screen {
 
+        val scrollState = rememberLazyListState()
+
         Header(
             title = state.title,
-            scrollState = null,
+            scrollState = scrollState,
             actionButton = HeaderActionButton(
                 text = state.saveText,
                 isEnabled = state.isSaveEnabled,
@@ -55,16 +63,45 @@ fun NoteFormFs(
 
         FormPaddingFirstItem()
 
-        FormInput(
-            initText = state.text,
-            placeholder = state.textPlaceholder,
-            onChange = { newText ->
-                vm.setText(newText)
-            },
-            isFirst = true,
-            isLast = true,
-            isAutoFocus = true,
-            imeAction = ImeAction.Done,
-        )
+        LazyColumn(
+            state = scrollState,
+        ) {
+
+            item {
+                FormInput(
+                    initText = state.text,
+                    placeholder = state.textPlaceholder,
+                    onChange = { newText ->
+                        vm.setText(newText)
+                    },
+                    isFirst = true,
+                    isLast = true,
+                    isAutoFocus = true,
+                    imeAction = ImeAction.Done,
+                )
+            }
+
+            if (noteDb != null) {
+                item {
+                    FormPaddingSectionSection()
+                    FormButton(
+                        title = "Delete Note",
+                        titleColor = c.red,
+                        isFirst = true,
+                        isLast = true,
+                        onClick = {
+                            vm.delete(
+                                noteDb = noteDb,
+                                dialogsManager = navigationFs,
+                                onDelete = {
+                                    onDelete()
+                                    navigationLayer.close()
+                                },
+                            )
+                        },
+                    )
+                }
+            }
+        }
     }
 }
