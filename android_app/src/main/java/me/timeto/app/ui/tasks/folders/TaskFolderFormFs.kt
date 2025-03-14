09a7428a0bed7ee2c25b6f91a8b28861a84225ac
hi@timeto.me <hi@timeto.me>
2025-03-14 12:53:58 +0300

    TaskFolderFormFs.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/tasks/folders/TaskFolderFormFs.kt
@@ -0,0 +1,109 @@
+package me.timeto.app.ui.tasks.folders
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import me.timeto.app.c
+import me.timeto.app.rememberVm
+import me.timeto.app.ui.Screen
+import me.timeto.app.ui.form.FormButton
+import me.timeto.app.ui.form.FormInput
+import me.timeto.app.ui.form.FormPaddingFirstItem
+import me.timeto.app.ui.form.FormPaddingSectionSection
+import me.timeto.app.ui.header.Header
+import me.timeto.app.ui.header.HeaderActionButton
+import me.timeto.app.ui.header.HeaderCancelButton
+import me.timeto.app.ui.navigation.LocalNavigationFs
+import me.timeto.app.ui.navigation.LocalNavigationLayer
+import me.timeto.shared.db.TaskFolderDb
+import me.timeto.shared.ui.tasks.TaskFolderFormVm
+
+@Composable
+fun TaskFolderFormFs(
+    initTaskFolderDb: TaskFolderDb?,
+) {
+
+    val navigationFs = LocalNavigationFs.current
+    val navigationLayer = LocalNavigationLayer.current
+
+    val (vm, state) = rememberVm {
+        TaskFolderFormVm(
+            folderDb = initTaskFolderDb,
+        )
+    }
+
+    Screen {
+
+        val scrollState = rememberLazyListState()
+
+        Header(
+            title = state.title,
+            scrollState = scrollState,
+            actionButton = HeaderActionButton(
+                text = state.saveText,
+                isEnabled = state.isSaveEnabled,
+                onClick = {
+                    vm.save(
+                        dialogsManager = navigationFs,
+                        onSuccess = {
+                            navigationLayer.close()
+                        },
+                    )
+                },
+            ),
+            cancelButton = HeaderCancelButton(
+                text = "Cancel",
+                onClick = {
+                    navigationLayer.close()
+                },
+            ),
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            state = scrollState,
+        ) {
+
+            item {
+
+                FormPaddingFirstItem()
+
+                FormInput(
+                    initText = state.name,
+                    placeholder = state.namePlaceholder,
+                    onChange = { newName ->
+                        vm.setName(newName)
+                    },
+                    isFirst = true,
+                    isLast = true,
+                    isAutoFocus = true,
+                    imeAction = ImeAction.Done,
+                )
+
+                val taskFolderDb: TaskFolderDb? = state.folderDb
+                if (taskFolderDb != null) {
+                    FormPaddingSectionSection()
+                    FormButton(
+                        title = state.deleteText,
+                        titleColor = c.red,
+                        isFirst = true,
+                        isLast = true,
+                        onClick = {
+                            vm.delete(
+                                folderDb = taskFolderDb,
+                                dialogsManager = navigationFs,
+                                onDelete = {
+                                    navigationLayer.close()
+                                },
+                            )
+                        },
+                    )
+                }
+            }
+        }
+    }
+}
