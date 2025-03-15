3a2ba7224f4a856ac03c1a42568667a39e983673
hi@timeto.me <hi@timeto.me>
2025-03-15 11:42:21 +0300

    FormSwitch.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/form/FormSwitch.kt
@@ -0,0 +1,48 @@
+package me.timeto.app.ui.form
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import me.timeto.app.c
+import me.timeto.app.halfDpFloor
+import me.timeto.app.ui.form.views.FormButtonView
+
+@Composable
+fun FormSwitch(
+    title: String,
+    isEnabled: Boolean,
+    isFirst: Boolean,
+    isLast: Boolean,
+    onChange: (Boolean) -> Unit,
+) {
+    FormButtonView(
+        title = title,
+        titleColor = c.text,
+        isFirst = isFirst,
+        isLast = isLast,
+        modifier = Modifier,
+        rightView = {
+            Switch(
+                checked = isEnabled,
+                onCheckedChange = { newValue ->
+                    onChange(newValue)
+                },
+                modifier = Modifier
+                    .padding(end = 9.dp, bottom = halfDpFloor),
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = c.blue,
+                    uncheckedThumbColor = Color.Gray,
+                    uncheckedTrackColor = Color.Gray,
+                ),
+            )
+        },
+        onClick = {
+            onChange(!isEnabled)
+        },
+        onLongClick = null,
+    )
+}
