9aa985df46175c4635b71ebdd4ff736367b094d2
hi@timeto.me <hi@timeto.me>
2025-03-22 17:16:07 +0300

    ShortcutsPickerVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/shortcuts/ShortcutsPickerVm.kt
@@ -0,0 +1,45 @@
+package me.timeto.shared.ui.shortcuts
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.db.ShortcutDb
+import me.timeto.shared.onEachExIn
+import me.timeto.shared.vm.__Vm
+
+class ShortcutsPickerVm(
+    initShortcutsDb: List<ShortcutDb>,
+) : __Vm<ShortcutsPickerVm.State>() {
+
+    data class State(
+        val shortcutsDb: List<ShortcutDb>,
+        val selectedIds: Set<Int>,
+    ) {
+
+        val title = "Shortcuts"
+        val doneText = "Done"
+
+        val shortcutsDbSorted: List<ShortcutDb> =
+            shortcutsDb.sortedByDescending { it.id in selectedIds }
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            shortcutsDb = initShortcutsDb,
+            selectedIds = initShortcutsDb.map { it.id }.toSet(),
+        )
+    )
+
+    init {
+        val scopeVm = scopeVm()
+        ShortcutDb.selectAscFlow().onEachExIn(scopeVm) { shortcutsDb ->
+            state.update { it.copy(shortcutsDb = shortcutsDb) }
+        }
+    }
+
+    fun setSelectedIds(ids: Set<Int>) {
+        state.update { it.copy(selectedIds = ids) }
+    }
+
+    fun getSelectedShortcutsDb(): List<ShortcutDb> =
+        state.value.shortcutsDb.filter { it.id in state.value.selectedIds }
+}
