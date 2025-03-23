99130e552b75c281e887d4cbeb20555514b14556
hi@timeto.me <hi@timeto.me>
2025-03-23 16:32:23 +0300

    ColorPickerExamplesData.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/color/ColorPickerExamplesData.kt
@@ -0,0 +1,7 @@
+package me.timeto.shared.ui.color
+
+data class ColorPickerExamplesData(
+    val mainExample: ColorPickerExampleData,
+    val secondaryHeader: String,
+    val secondaryExamples: List<ColorPickerExampleData>,
+)
