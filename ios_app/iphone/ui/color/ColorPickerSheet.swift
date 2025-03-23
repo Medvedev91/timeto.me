9e8db97d28aa8399d6b95f95eada6121f23f0824
hi@timeto.me <hi@timeto.me>
2025-03-23 16:36:03 +0300

    ColorPickerSheet.swift:

--- ios_app/iphone/ui/color/ColorPickerSheet.swift
+++ ios_app/iphone/ui/color/ColorPickerSheet.swift
@@ -4,13 +4,13 @@ import shared
 struct ColorPickerSheet: View {
     
     let title: String
-    let initColorRgba: ColorRgba
+    let examplesData: ColorPickerExamplesData
     let onPick: (ColorRgba) -> Void
     
     var body: some View {
         VmView({
             ColorPickerVm(
-                initColorRgba: initColorRgba
+                examplesData: examplesData
             )
         }) { vm, state in
             ColorPickerSheetInner(
@@ -22,6 +22,13 @@ struct ColorPickerSheet: View {
         }
     }
 }
+
+///
+
+private let circleSize: CGFloat = 42
+private let circlePadding: CGFloat = 4
+private let circleCellSize: CGFloat = circleSize + (circlePadding * 2.0)
+private let exampleShape = RoundedRectangle(cornerRadius: 10, style: .continuous)
 
 private struct ColorPickerSheetInner: View {
     
@@ -36,8 +43,61 @@ private struct ColorPickerSheetInner: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
+        
         VStack {
+            
+            HStack {
+                
+                ScrollView {
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(state.examplesData.mainExample.title)
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(.white)
+                            .lineLimit(1)
+                            .padding(.horizontal, 12)
+                            .frame(height: circleSize - 2)
+                            .background(exampleShape.fill(state.colorRgba.toColor()))
+                            .padding(.top, 1)
+                        
+                        let secondaryExamples = state.examplesData.secondaryExamples
+                        if !secondaryExamples.isEmpty {
+                            
+                            Text(state.examplesData.secondaryHeader)
+                                .foregroundColor(.secondary)
+                                .fontWeight(.medium)
+                                .font(.system(size: 13))
+                                .padding(.leading, 4)
+                                .padding(.top, 28)
+                            
+                            ForEach(secondaryExamples, id: \.self) { exampleData in
+                                Button(
+                                    action: {
+                                        vm.setColorRgba(colorRgba: exampleData.colorRgba)
+                                    },
+                                    label: {
+                                        Text(exampleData.title)
+                                            .font(.system(size: 15, weight: .semibold))
+                                            .foregroundColor(.white)
+                                            .lineLimit(1)
+                                            .padding(.horizontal, 12)
+                                            .padding(.top, 6)
+                                            .padding(.bottom, 6)
+                                            .background(exampleShape.fill(exampleData.colorRgba.toColor()))
+                                            .padding(.top, 8)
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+                .padding(.leading, H_PADDING)
+                
+                Spacer()
+            }
         }
+        .myFormContentMargins()
         .toolbar {
             ToolbarItem(placement: .cancellationAction) {
                 Button("Cancel") {
