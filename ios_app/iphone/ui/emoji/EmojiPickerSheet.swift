3e231701e70aafba4ceeefc3c40f494b604e1cbf
hi@timeto.me <hi@timeto.me>
2025-03-25 00:51:11 +0300

    EmojiPickerSheet.swift:

--- ios_app/iphone/ui/emoji/EmojiPickerSheet.swift
+++ ios_app/iphone/ui/emoji/EmojiPickerSheet.swift
@@ -20,8 +20,6 @@ struct EmojiPickerSheet: View {
 
 ///
 
-private let fgColor = Color(.secondarySystemBackground)
-private let fgCornerRadius: CGFloat = 8
 private let emojisRows: [GridItem] = Array(
     repeating: .init(.flexible(), spacing: 0),
     count: 8
@@ -37,131 +35,57 @@ private struct EmojiPickerSheetInner: View {
     ///
     
     @State private var searchText = ""
-    @FocusState private var isFocused: Bool
     
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         
-        VStack {
+        ScrollView {
             
-            HStack {
+            LazyVGrid(columns: emojisRows, spacing: 0) {
                 
-                ZStack(alignment: .trailing) {
+                ForEach(state.emojis, id: \.self.emoji) { emoji in
                     
-                    HStack {
-                        
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(.secondary)
-                            .padding(.trailing, 10)
-                        
-                        TextField(
-                            state.searchPlaceholder,
-                            text: $searchText
-                        )
-                        .focused($isFocused)
-                        .onChange(of: searchText) { _, text in
-                            vm.search(text: text)
+                    // Button() freezes on scroll // todo
+                    Text(emoji.emoji)
+                        .font(.system(size: 33))
+                        .padding(.bottom, 4)
+                        .onTapGesture {
+                            onPick(emoji.emoji)
+                            dismiss()
                         }
-                    }
-                    .padding(.leading, 12)
-                    .padding(.vertical, 8)
-                    .background(fgColor)
-                    .cornerRadius(fgCornerRadius)
-                    .frame(maxWidth: .infinity)
-                    .onTapGesture {
-                        isFocused = true
-                    }
-                    
-                    HStack {
-                        
-                        if isFocused {
-                            Button(
-                                action: {
-                                    isFocused = false
-                                },
-                                label: {
-                                    Image(systemName: "keyboard.chevron.compact.down")
-                                        .foregroundColor(.secondary.opacity(0.8))
-                                        .padding(.leading, 8)
-                                        .padding(.trailing, 4)
-                                        .padding(.vertical, 8)
-                                }
-                            )
-                            .transition(.opacity.animation(.easeInOut(duration: 0.3)))
-                            .padding(.top, 1)
-                        }
-                        
-                        if !searchText.isEmpty {
-                            ClearButton(
-                                text: $searchText,
-                                isFocused: $isFocused
-                            )
-                        }
-                    }
-                    .padding(.trailing, 4)
                 }
-                .padding(.leading, H_PADDING)
-                
-                Button("Cancel") {
-                    dismiss()
-                }
-                .padding(.leading, 12)
-                .padding(.trailing, H_PADDING)
             }
-            .padding(.top, 8)
-            
-            ZStack {
-                
-                ScrollView {
-                    
-                    LazyVGrid(columns: emojisRows, spacing: 0) {
-                        
-                        ForEach(state.emojis, id: \.self.emoji) { emoji in
-                            
-                            // Button() freezes on scroll // todo
-                            Text(emoji.emoji)
-                                .font(.system(size: 33))
-                                .padding(.bottom, 4)
-                                .onTapGesture {
-                                    onPick(emoji.emoji)
-                                    dismiss()
-                                }
-                        }
-                    }
-                    .padding(.horizontal, 8)
-                }
-                .contentMargins(.vertical, 10)
-                .background(fgColor)
-                .cornerRadius(fgCornerRadius)
-            }
-            .padding(.horizontal, H_PADDING)
-            .padding(.top, 10)
-            .padding(.bottom, 10)
+            .padding(.horizontal, 8)
         }
-    }
-}
-
-private struct ClearButton: View {
-    
-    @Binding var text: String
-    @FocusState.Binding var isFocused: Bool
-    
-    var body: some View {
-        Button(
-            action: {
-                withAnimation(.easeInOut(duration: 0.2)) {
-                    text = ""
+        .contentMargins(.vertical, 10)
+        .toolbar {
+            
+            ToolbarItem(placement: .principal) {
+                
+                HStack {
+                    
+                    SearchBar(
+                        text: $searchText,
+                        placeholder: "Search",
+                        returnKeyType: .done,
+                        onDone: {
+                            hideKeyboard()
+                        }
+                    )
+                    .onChange(of: searchText) { _, text in
+                        vm.search(text: text)
+                    }
+                    
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .buttonStyle(.plain)
+                    .foregroundColor(.blue)
+                    .padding(.leading, 14)
                 }
-                isFocused = true
-            },
-            label: {
-                Image(systemName: "xmark.circle.fill")
-                    .foregroundColor(Color(.tertiaryLabel))
             }
-        )
-        .padding(.leading, 8)
-        .padding(.trailing, 8)
-        .transition(.opacity.animation(.easeInOut(duration: 0.1)))
+        }
+        .toolbarTitleDisplayMode(.inline)
     }
 }
