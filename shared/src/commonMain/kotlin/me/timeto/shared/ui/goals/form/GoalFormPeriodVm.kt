6c6d1563760738f2b74d8247f61bd03140a1eddc
hi@timeto.me <hi@timeto.me>
2025-03-29 02:54:33 +0300

    GoalFormPeriodVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/goals/form/GoalFormPeriodVm.kt
@@ -0,0 +1,69 @@
+package me.timeto.shared.ui.goals.form
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.UnixTime
+import me.timeto.shared.db.GoalDb
+import me.timeto.shared.ui.DialogsManager
+import me.timeto.shared.ui.UiException
+import me.timeto.shared.vm.__Vm
+
+class GoalFormPeriodVm(
+    initGoalDbPeriod: GoalDb.Period?,
+) : __Vm<GoalFormPeriodVm.State>() {
+
+    data class State(
+        val selectedDaysOfWeek: Set<Int>,
+    ) {
+
+        val title = "Period"
+        val doneText = "Done"
+
+        fun buildPeriodOrNull(
+            dialogsManager: DialogsManager,
+        ): GoalDb.Period? = try {
+            GoalDb.Period.DaysOfWeek.buildWithValidation(selectedDaysOfWeek)
+        } catch (e: UiException) {
+            dialogsManager.alert(e.uiMessage)
+            null
+        }
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            selectedDaysOfWeek = when (initGoalDbPeriod) {
+                is GoalDb.Period.DaysOfWeek -> initGoalDbPeriod.days.toSet()
+                else -> (0..6).toSet()
+            },
+        )
+    )
+
+    ///
+
+    val daysOfWeek: List<DayOfWeek> = UnixTime.dayOfWeekNames
+        .mapIndexed { dayIdx, dayTitle ->
+            DayOfWeek(
+                id = dayIdx,
+                title = dayTitle,
+            )
+        }
+
+    fun toggleDayOfWeek(dayOfWeek: DayOfWeek) {
+        state.update { state ->
+            val newDayOfWeek: MutableSet<Int> =
+                state.selectedDaysOfWeek.toMutableSet()
+            if (newDayOfWeek.contains(dayOfWeek.id))
+                newDayOfWeek.remove(dayOfWeek.id)
+            else
+                newDayOfWeek.add(dayOfWeek.id)
+            state.copy(selectedDaysOfWeek = newDayOfWeek)
+        }
+    }
+
+    ///
+
+    data class DayOfWeek(
+        val id: Int,
+        val title: String,
+    )
+}
