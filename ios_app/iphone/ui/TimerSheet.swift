d7864542d38fc52a8fe3e59dceacee4ca442f2df
hi@timeto.me <hi@timeto.me>
2025-03-29 11:57:34 +0300

    TimerSheet.swift

---
+++ ios_app/iphone/ui/TimerSheet.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+import shared
+
+struct TimerSheet: View {
+    
+    let title: String
+    let doneTitle: String
+    let initSeconds: Int
+    let onDone: (Int) -> Void
+
+    var body: some View {
+        VmView({
+            TimerVm(initSeconds: initSeconds.toInt32())
+        }) { vm, state in
+            TimerSheetInner(
+                title: title,
+                doneTitle: doneTitle,
+                onDone: onDone,
+                pickerItems: vm.pickerItems,
+                selected: vm.initSelectedSeconds
+            )
+        }
+    }
+}
+
+private struct TimerSheetInner: View {
+    
+    let title: String
+    let doneTitle: String
+    let onDone: (Int) -> Void
+    let pickerItems: [TimerVm.PickerItem]
+    
+    @State var selected: Int32
+
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        
+        VStack {
+            
+            Spacer()
+            
+            Picker("", selection: $selected) {
+                ForEach(pickerItems, id: \.seconds) { item in
+                    Text(item.title)
+                }
+            }
+            .pickerStyle(.wheel)
+            .foregroundColor(.primary)
+            .padding(.bottom, 5)
+            
+            Spacer()
+        }
+        .presentationDetents([.height(350)])
+        .navigationTitle(title)
+        .toolbarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button(doneTitle) {
+                    onDone(selected.toInt())
+                    dismiss()
+                }
+                .fontWeight(.semibold)
+            }
+        }
+    }
+}
