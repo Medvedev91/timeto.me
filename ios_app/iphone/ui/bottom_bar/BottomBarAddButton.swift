612ee135bdecc5282f982a40202ceaf9cf50ae3e
hi@timeto.me <hi@timeto.me>
2025-03-29 03:10:12 +0300

    BottomBarAddButton.swift

---
+++ ios_app/iphone/ui/bottom_bar/BottomBarAddButton.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct BottomBarAddButton: View {
+    
+    let text: String
+    let action: () -> Void
+    
+    var body: some View {
+        
+        Button(
+            action: {
+                action()
+            },
+            label: {
+                
+                HStack(spacing: 8) {
+                    
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundStyle(.blue)
+                        .fontWeight(.bold)
+                    
+                    Text(text)
+                        .foregroundColor(.blue)
+                        .fontWeight(.semibold)
+                }
+            }
+        )
+        .buttonStyle(.plain)
+    }
+}
