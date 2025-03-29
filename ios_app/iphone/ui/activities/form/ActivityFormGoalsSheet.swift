76b337cae62ef89662c80b726114caf3c0f467da
hi@timeto.me <hi@timeto.me>
2025-03-29 17:22:30 +0300

    ActivityFormGoalsSheet.swift

---
+++ ios_app/iphone/ui/activities/form/ActivityFormGoalsSheet.swift
@@ -0,0 +1,102 @@
+import SwiftUI
+import shared
+
+struct ActivityFormGoalsSheet: View {
+    
+    let initGoalFormsData: [GoalFormData]
+    let onPick: ([GoalFormData]) -> Void
+    
+    var body: some View {
+        VmView({
+            ActivityFormGoalsVm(
+                initGoalFormsData: initGoalFormsData
+            )
+        }) { vm, state in
+            ActivityFormGoalsSheetInner(
+                vm: vm,
+                state: state,
+                onPick: onPick
+            )
+        }
+    }
+}
+
+private struct ActivityFormGoalsSheetInner: View {
+    
+    let vm: ActivityFormGoalsVm
+    let state: ActivityFormGoalsVm.State
+    
+    let onPick: ([GoalFormData]) -> Void
+    
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(Navigation.self) private var navigation
+
+    var body: some View {
+        List {
+            ForEach(Array(state.goalFormsData.enumerated()), id: \.offset) { idx, goalFormData in
+                NavigationLinkSheet(
+                    label: {
+                        HStack {
+                            Text(goalFormData.formListTitle)
+                                .foregroundColor(.primary)
+                            Spacer()
+                            Text(goalFormData.formListNote)
+                                .foregroundColor(.secondary)
+                        }
+                    },
+                    sheet: {
+                        GoalFormSheet(
+                            strategy: GoalFormStrategy.FormData(
+                                initGoalFormData: goalFormData,
+                                onDone: { newGoalFormData in
+                                    vm.updateGoalFormData(
+                                        idx: idx.toInt32(),
+                                        new: newGoalFormData
+                                    )
+                                }
+                            )
+                        )
+                    }
+                )
+            }
+        }
+        .navigationTitle("Goals")
+        .toolbarTitleDisplayMode(.inline)
+        .toolbar {
+            
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .primaryAction) {
+                Button("Done") {
+                    onPick(state.goalFormsData)
+                    dismiss()
+                }
+                .fontWeight(.semibold)
+            }
+            
+            ToolbarItemGroup(placement: .bottomBar) {
+                
+                BottomBarAddButton(
+                    text: state.newGoalTitle,
+                    action: {
+                        navigation.sheet {
+                            GoalFormSheet(
+                                strategy: vm.newGoalStrategy
+                            )
+                        }
+                    }
+                )
+                
+                Spacer()
+            }
+        }
+        .myFormContentMargins()
+        .interactiveDismissDisabled()
+    }
+}
