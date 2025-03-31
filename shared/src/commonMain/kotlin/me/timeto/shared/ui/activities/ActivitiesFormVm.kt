01892388bed2cc8812573d11fe2547d2adc8248e
hi@timeto.me <hi@timeto.me>
2025-03-31 03:01:21 +0300

    ActivitiesFormVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/activities/ActivitiesFormVm.kt
@@ -0,0 +1,54 @@
+package me.timeto.shared.ui.activities
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.Cache
+import me.timeto.shared.db.ActivityDb
+import me.timeto.shared.onEachExIn
+import me.timeto.shared.textFeatures
+import me.timeto.shared.ui.moveIos
+import me.timeto.shared.vm.__Vm
+
+class ActivitiesFormVm : __Vm<ActivitiesFormVm.State>() {
+
+    data class State(
+        val activitiesDb: List<ActivityDb>,
+    ) {
+
+        val title = "Activities"
+        val doneTitle = "Done"
+
+        val activitiesUi: List<ActivityUi> =
+            activitiesDb.map { ActivityUi(it) }
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            activitiesDb = Cache.activitiesDbSorted,
+        )
+    )
+
+    init {
+        val scopeVm = scopeVm()
+        ActivityDb.selectSortedFlow().onEachExIn(scopeVm) { activitiesDb ->
+            state.update { it.copy(activitiesDb = activitiesDb) }
+        }
+    }
+
+    ///
+
+    fun moveIos(fromIdx: Int, toIdx: Int) {
+        state.value.activitiesDb.moveIos(fromIdx, toIdx) { newActivitiesDb ->
+            ActivityDb.updateSortMany(newActivitiesDb)
+        }
+    }
+
+    ///
+
+    data class ActivityUi(
+        val activityDb: ActivityDb,
+    ) {
+        val title: String =
+            activityDb.name.textFeatures().textNoFeatures
+    }
+}
