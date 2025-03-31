6addaa25624febd3bdb79abd59bbf42046799dc2
hi@timeto.me <hi@timeto.me>
2025-03-31 12:06:42 +0300

    ActivitiesVm.TimerHintUi:

--- shared/src/commonMain/kotlin/me/timeto/shared/ui/activities/ActivitiesVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/activities/ActivitiesVm.kt
@@ -4,8 +4,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import me.timeto.shared.Cache
 import me.timeto.shared.db.ActivityDb
+import me.timeto.shared.launchExIo
 import me.timeto.shared.onEachExIn
 import me.timeto.shared.textFeatures
+import me.timeto.shared.toTimerHintNote
 import me.timeto.shared.vm.__Vm
 
 class ActivitiesVm : __Vm<ActivitiesVm.State>() {
@@ -35,9 +37,31 @@ class ActivitiesVm : __Vm<ActivitiesVm.State>() {
     class ActivityUi(
         val activityDb: ActivityDb,
     ) {
+
         val text: String =
             activityDb.name.textFeatures().textUi()
+
         val isActive: Boolean =
             Cache.lastInterval.activity_id == activityDb.id
+
+        val timerHintsUi: List<TimerHintUi> =
+            activityDb.timerHints.sorted().map { seconds ->
+                TimerHintUi(
+                    seconds = seconds,
+                    onTap = {
+                        launchExIo {
+                            activityDb.startInterval(seconds)
+                        }
+                    },
+                )
+            }
+    }
+
+    class TimerHintUi(
+        val seconds: Int,
+        val onTap: () -> Unit,
+    ) {
+        val title: String =
+            seconds.toTimerHintNote(isShort = true)
     }
 }
