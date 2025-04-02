d3896358fd8d0f8bd7945d2ae79a6ce0abeceae8
hi@timeto.me <hi@timeto.me>
2025-04-03 01:53:05 +0300

    ActivityFormFs.kt: Emoji Button

--- android_app/src/main/java/me/timeto/app/ui/activities/form/ActivityFormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/activities/form/ActivityFormFs.kt
@@ -3,16 +3,26 @@ package me.timeto.app.ui.activities.form
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.timeto.app.HStack
+import me.timeto.app.c
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
 import me.timeto.app.ui.form.FormInput
+import me.timeto.app.ui.form.FormPaddingSectionSection
 import me.timeto.app.ui.form.FormPaddingTop
+import me.timeto.app.ui.form.views.FormButtonArrowView
+import me.timeto.app.ui.form.views.FormButtonNoteView
+import me.timeto.app.ui.form.views.FormButtonView
 import me.timeto.app.ui.header.Header
 import me.timeto.app.ui.header.HeaderActionButton
 import me.timeto.app.ui.header.HeaderCancelButton
@@ -86,6 +96,41 @@ fun ActivityFormFs(
                     isLast = true,
                     isAutoFocus = false,
                     imeAction = ImeAction.Done,
+                )
+
+                FormPaddingSectionSection()
+
+                FormButtonView(
+                    title = state.emojiTitle,
+                    titleColor = null,
+                    isFirst = true,
+                    isLast = true,
+                    modifier = Modifier,
+                    rightView = {
+                        HStack(
+                            verticalAlignment = CenterVertically,
+                        ) {
+                            val emoji: String? = state.emoji
+                            if (emoji != null) {
+                                Text(
+                                    text = emoji,
+                                    fontSize = 20.sp,
+                                    modifier = Modifier
+                                        .padding(end = 8.dp)
+                                )
+                            } else {
+                                FormButtonNoteView(
+                                    note = state.emojiNotSelected,
+                                    color = c.red,
+                                    withArrow = true,
+                                )
+                            }
+                            FormButtonArrowView()
+                        }
+                    },
+                    onClick = {
+                    },
+                    onLongClick = null,
                 )
             }
         }
