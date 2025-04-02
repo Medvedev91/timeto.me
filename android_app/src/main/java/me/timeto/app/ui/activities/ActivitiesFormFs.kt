9d4678b9eba90bc04be92b61836aecf962983ccd
hi@timeto.me <hi@timeto.me>
2025-04-02 22:41:04 +0300

    ActivitiesFormFs.kt: List

--- android_app/src/main/java/me/timeto/app/ui/activities/ActivitiesFormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/activities/ActivitiesFormFs.kt
@@ -2,16 +2,20 @@ package me.timeto.app.ui.activities
 
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
+import me.timeto.app.ui.form.FormSortedList
 import me.timeto.app.ui.header.Header
 import me.timeto.app.ui.header.HeaderActionButton
+import me.timeto.app.ui.navigation.LocalNavigationFs
 import me.timeto.app.ui.navigation.LocalNavigationLayer
 import me.timeto.shared.ui.activities.ActivitiesFormVm
 
 @Composable
 fun ActivitiesFormFs() {
 
+    val navigationFs = LocalNavigationFs.current
     val navigationLayer = LocalNavigationLayer.current
 
     val (vm, state) = rememberVm {
@@ -33,6 +37,26 @@ fun ActivitiesFormFs() {
                 },
             ),
             cancelButton = null,
+        )
+
+        FormSortedList(
+            items = state.activitiesUi,
+            itemId = { it.activityDb.id },
+            itemTitle = { it.title },
+            onItemClick = { activityUi ->
+                navigationFs.push {
+                }
+            },
+            onItemDelete = null,
+            scrollState = scrollState,
+            modifier = Modifier
+                .weight(1f),
+            onMove = { fromIdx, toIdx ->
+                vm.moveAndroidLocal(fromIdx, toIdx)
+            },
+            onFinish = {
+                vm.moveAndroidSync()
+            },
         )
     }
 }
