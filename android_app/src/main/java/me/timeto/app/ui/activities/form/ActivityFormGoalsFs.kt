170e6a39b8c8b0f2561c341c6527c5fc1275d00e
hi@timeto.me <hi@timeto.me>
2025-04-05 22:46:16 +0300

    ActivityFormGoalsFs.kt: Button New

--- android_app/src/main/java/me/timeto/app/ui/activities/form/ActivityFormGoalsFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/activities/form/ActivityFormGoalsFs.kt
@@ -1,9 +1,15 @@
 package me.timeto.app.ui.activities.form
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import me.timeto.app.rememberVm
+import me.timeto.app.ui.goals.form.GoalFormFs
 import me.timeto.app.ui.Screen
+import me.timeto.app.ui.footer.Footer
+import me.timeto.app.ui.footer.FooterAddButton
+import me.timeto.app.ui.form.FormPaddingTop
 import me.timeto.app.ui.header.Header
 import me.timeto.app.ui.header.HeaderActionButton
 import me.timeto.app.ui.header.HeaderCancelButton
@@ -48,6 +54,34 @@ fun ActivityFormGoalsFs(
                     navigationLayer.close()
                 },
             ),
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f),
+            state = scrollState,
+        ) {
+
+            item {
+                FormPaddingTop()
+            }
+        }
+
+        Footer(
+            scrollState = scrollState,
+            contentModifier = Modifier,
+            content = {
+                FooterAddButton(
+                    text = state.newGoalTitle,
+                    onClick = {
+                        navigationFs.push {
+                            GoalFormFs(
+                                strategy = vm.newGoalStrategy,
+                            )
+                        }
+                    },
+                )
+            },
         )
     }
 }
