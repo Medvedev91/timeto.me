207b6935675328dc4c623b7bc63f11e518ef15e4
hi@timeto.me <hi@timeto.me>
2025-04-08 23:53:38 +0300

    to10.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/misc/to10.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/misc/to10.kt
@@ -1,8 +1,14 @@
 package me.timeto.shared.misc
 
-fun Boolean.toInt10(): Int = if (this) 1 else 0
-fun Int.toBoolean10(): Boolean = this != 0
-fun Boolean.toString10(): String = if (this) "1" else "0"
+fun Boolean.toInt10(): Int =
+    if (this) 1 else 0
+
+fun Int.toBoolean10(): Boolean =
+    this != 0
+
+fun Boolean.toString10(): String =
+    if (this) "1" else "0"
+
 fun String.toBoolean10(): Boolean = when (this) {
     "1" -> true
     "0" -> false
