66099021dc9b48f02fc98db806262962d1d9ba95
hi@timeto.me <hi@timeto.me>
2025-04-13 23:14:14 +0300

    SummaryChartVm.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/SummaryChartVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/SummaryChartVm.kt
@@ -2,46 +2,41 @@ package me.timeto.shared.vm
 
 import kotlinx.coroutines.flow.*
 import me.timeto.shared.*
+import me.timeto.shared.ui.summary.SummaryVm
 
 class SummaryChartVm(
-    val activitiesUI: List<SummarySheetVm.ActivityUI>,
+    activitiesUi: List<SummaryVm.ActivityUi>,
 ) : __Vm<SummaryChartVm.State>() {
 
     data class State(
-        val selectedId: String?,
         val pieItems: List<PieChart.ItemData>,
     )
 
     override val state = MutableStateFlow(
         State(
-            selectedId = null,
-            pieItems = listOf(),
+            pieItems = emptyList(),
         )
     )
 
-    override fun onAppear() {
+    init {
         scopeVm().launchEx {
-            val items = activitiesUI.map { activityUI ->
-                val activity = activityUI.activity
-                val seconds = activityUI.seconds
+            val items = activitiesUi.map { activityUi ->
+                val activity = activityUi.activity
+                val seconds = activityUi.seconds
                 PieChart.ItemData(
                     id = "${activity.id}",
                     value = seconds.toDouble(),
                     color = activity.colorRgba,
-                    title = activityUI.title,
+                    title = activityUi.title,
                     shortTitle = activity.emoji,
-                    subtitleTop = "${(activityUI.ratio * 100).toInt()}%",
-                    subtitleBottom = activityUI.totalTimeString,
-                    customData = activityUI.perDayString,
+                    subtitleTop = "${(activityUi.ratio * 100).toInt()}%",
+                    subtitleBottom = activityUi.totalTimeString,
+                    customData = activityUi.perDayString,
                 )
             }
             state.update {
                 it.copy(pieItems = items)
             }
         }
-    }
-
-    fun selectId(id: String?) {
-        state.update { it.copy(selectedId = id) }
     }
 }
