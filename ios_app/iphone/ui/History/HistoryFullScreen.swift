5b9c00860ac46c2c975b7cbe849e4ff54a37489a
hi@timeto.me <hi@timeto.me>
2025-04-16 12:43:15 +0300

    HistoryFullScreen.swift:

--- ios_app/iphone/ui/History/HistoryFullScreen.swift
+++ ios_app/iphone/ui/History/HistoryFullScreen.swift
@@ -27,107 +27,97 @@ private struct HistoryFullScreenInner: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(Navigation.self) private var navigation
     
-    @State private var scrollPosition = ScrollPosition()
-    
     var body: some View {
         
-        ScrollView(.vertical, showsIndicators: false) {
+        ScrollViewReader { scrollProxy in
             
-            LazyVStack(pinnedViews: [.sectionHeaders]) {
+            ScrollView(.vertical, showsIndicators: false) {
                 
-                ForEach(state.daysUi, id: \.unixDay) { dayUi in
+                LazyVStack(spacing: 16, pinnedViews: [.sectionHeaders]) {
                     
-                    Section(
-                        header: Button(
-                            action: {
-                                // todo dialog to move to day
-                                withAnimation {
-                                    scrollPosition.scrollTo(id: "day_20185", anchor: .top)
+                    ForEach(state.daysUi, id: \.unixDay) { dayUi in
+                        
+                        Section(
+                            header: Button(
+                                action: {
+                                    // todo dialog to move to day
+                                    withAnimation {
+                                        // scrollPosition.scrollTo(id: "day_20185", anchor: .top)
+                                    }
+                                },
+                                label: {
+                                    Text(dayUi.dayText)
+                                        .padding(.horizontal, 9)
+                                        .padding(.vertical, 6)
+                                        .background(Capsule().fill(.blue))
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 14))
                                 }
-                            },
-                            label: {
-                                Text(dayUi.dayText)
-                                    .padding(.horizontal, 9)
-                                    .padding(.vertical, 6)
-                                    .background(Capsule().fill(.blue))
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 14))
-                            }
-                        )
-                        .id("day_\(dayUi.unixDay)")
-                    ) {
-                        
-                        // Warning
-                        // VStack to fix https://developer.apple.com/forums/thread/741406
-                        
-                        VStack(spacing: 12) {
+                            )
+                            .id("day_\(dayUi.unixDay)")
+                        ) {
                             
-                            ForEach(dayUi.intervalsDb, id: \.id) { intervalDb in
+                            // Warning
+                            // VStack to fix https://developer.apple.com/forums/thread/741406
+                            
+                            VStack(spacing: 10) {
                                 
-                                let intervalUi: HistoryVm.IntervalUi = HistoryVm.IntervalUi.companion.build(
-                                    interval: intervalDb,
-                                    dayUi: dayUi
-                                )
-                                
-                                HStack(alignment: .top, spacing: 10) {
+                                ForEach(dayUi.intervalsDb, id: \.id) { intervalDb in
                                     
-                                    HStack(alignment: .center, spacing: 0) {
+                                    let intervalUi: HistoryVm.IntervalUi = HistoryVm.IntervalUi.companion.build(
+                                        interval: intervalDb,
+                                        dayUi: dayUi
+                                    )
+                                    
+                                    HStack(alignment: .top, spacing: 10) {
                                         
-                                        if !intervalUi.isStartsPrevDay {
+                                        HStack(alignment: .center) {
                                             
-                                            Text(intervalUi.periodString)
-                                                .foregroundColor(.primary)
-                                                .font(.system(size: 12, weight: .thin))
-                                                .frame(alignment: .leading)
-                                            
-                                            Spacer()
-                                            
-                                            Text(intervalUi.timeString)
-                                                .foregroundColor(.primary)
-                                                .font(.system(size: 15, weight: .medium, design: .monospaced))
-                                                .frame(alignment: .trailing)
+                                            if !intervalUi.isStartsPrevDay {
+                                                
+                                                Text(intervalUi.periodString)
+                                                    .foregroundColor(.secondary)
+                                                    .font(.system(size: 15, weight: .semibold, design: .monospaced))
+                                                    .frame(alignment: .trailing)
+                                            }
                                         }
-                                    }
-                                    .frame(width: 100, alignment: .leading)
-                                    .padding(.top, 3)
-                                    
-                                    VStack {
-                                        
-                                        RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                            .fill(intervalDb.selectActivityDbCached().colorRgba.toColor())
-                                            .frame(
-                                                width: 10,
-                                                height: Double(10.limitMin(intervalUi.secondsForBar.toInt() / 50))
-                                            )
-                                            .padding(.leading, 5)
-                                            .padding(.trailing, 5)
-                                    }
-                                    .frame(maxHeight: .infinity)
-                                    
-                                    VStack(spacing: 4) {
-                                        
-                                        if !intervalUi.isStartsPrevDay {
-                                            
-                                            Text(intervalUi.text)
-                                                .foregroundColor(.primary)
-                                                .font(.system(size: 16, weight: .medium))
-                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                        .frame(width: 120, alignment: .leading)
+
+                                        VStack {
+                                            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                                .fill(intervalDb.selectActivityDbCached().colorRgba.toColor())
+                                                .frame(
+                                                    width: 10,
+                                                    height: Double(10.limitMin(intervalUi.secondsForBar.toInt() / 50))
+                                                )
+                                                .padding(.leading, 5)
+                                                .padding(.trailing, 5)
                                         }
+                                        .frame(maxHeight: .infinity)
+                                        
+                                        VStack(spacing: 4) {
+                                            
+                                            if !intervalUi.isStartsPrevDay {
+                                                
+                                                Text(intervalUi.text)
+                                                    .foregroundColor(.primary)
+                                                    .font(.system(size: 16, weight: .medium))
+                                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                            }
+                                        }
+                                        .frame(maxWidth: .infinity)
                                     }
-                                    .frame(maxWidth: .infinity)
-                                    .padding(.top, 3)
+                                    .padding(.leading, 20)
+                                    .padding(.trailing, 20)
                                 }
-                                .padding(.leading, 20)
-                                .padding(.trailing, 20)
                             }
                         }
                     }
                 }
             }
+            .contentMargins(.bottom, 8)
+            .defaultScrollAnchor(.bottom)
         }
-        .contentMargins(.bottom, 8)
-        .defaultScrollAnchor(.bottom)
-        .scrollPosition($scrollPosition, anchor: .top)
         .toolbar {
             ToolbarItemGroup(placement: .bottomBar) {
                 BottomBarAddButton(
