6be99624ddc0679743aa6e356659de7959191e35
hi@timeto.me <hi@timeto.me>
2025-04-17 22:59:02 +0300

    HistoryFormVm.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/ui/history/HistoryFormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/history/HistoryFormVm.kt
@@ -1,8 +1,10 @@
 package me.timeto.shared.ui.history
 
 import kotlinx.coroutines.flow.MutableStateFlow
+import me.timeto.shared.Cache
 import me.timeto.shared.db.ActivityDb
 import me.timeto.shared.db.IntervalDb
+import me.timeto.shared.textFeatures
 import me.timeto.shared.vm.__Vm
 
 class HistoryFormVm(
@@ -10,14 +12,15 @@ class HistoryFormVm(
 ) : __Vm<HistoryFormVm.State>() {
 
     data class State(
-        val intervalDb: IntervalDb?,
-        val activityDb: ActivityDb?,
+        val initIntervalDb: IntervalDb?,
+        val initActivityDb: ActivityDb,
+        val activitiesUi: List<ActivityUi>,
     ) {
 
         val title: String =
-            if (intervalDb == null) "New Entry" else "Edit"
+            if (initIntervalDb == null) "New Entry" else "Edit"
         val saveText: String =
-            if (intervalDb == null) "Create" else "Save"
+            if (initIntervalDb == null) "Create" else "Save"
 
         val activityTitle = "Activity"
         val timeTitle = "Time Start"
@@ -25,8 +28,20 @@ class HistoryFormVm(
 
     override val state = MutableStateFlow(
         State(
-            intervalDb = initIntervalDb,
-            activityDb = initIntervalDb?.selectActivityDbCached()
+            initIntervalDb = initIntervalDb,
+            initActivityDb =
+                initIntervalDb?.selectActivityDbCached() ?: ActivityDb.selectOtherCached(),
+            activitiesUi =
+                Cache.activitiesDbSorted.map { ActivityUi(activityDb = it) }
         )
     )
+
+    ///
+
+    data class ActivityUi(
+        val activityDb: ActivityDb,
+    ) {
+        val title: String =
+            activityDb.name.textFeatures().textNoFeatures
+    }
 }
