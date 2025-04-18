108045f9ed64691d667b3dd7b482afed9955cc87
hi@timeto.me <hi@timeto.me>
2025-04-18 22:50:19 +0300

    HistoryFormVm.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/ui/history/HistoryFormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/history/HistoryFormVm.kt
@@ -1,9 +1,12 @@
 package me.timeto.shared.ui.history
 
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import me.timeto.shared.Cache
+import me.timeto.shared.UnixTime
 import me.timeto.shared.db.ActivityDb
 import me.timeto.shared.db.IntervalDb
+import me.timeto.shared.misc.time
 import me.timeto.shared.textFeatures
 import me.timeto.shared.vm.__Vm
 
@@ -13,7 +16,8 @@ class HistoryFormVm(
 
     data class State(
         val initIntervalDb: IntervalDb?,
-        val initActivityDb: ActivityDb,
+        val activityDb: ActivityDb,
+        val selectedTime: Int,
         val activitiesUi: List<ActivityUi>,
     ) {
 
@@ -23,18 +27,29 @@ class HistoryFormVm(
             if (initIntervalDb == null) "Create" else "Save"
 
         val activityTitle = "Activity"
+        val activityNote: String =
+            activityDb.name.textFeatures().textNoFeatures
+
         val timeTitle = "Time Start"
+        val timeNote: String =
+            prepTimeNote(selectedTime)
     }
 
     override val state = MutableStateFlow(
         State(
             initIntervalDb = initIntervalDb,
-            initActivityDb =
-                initIntervalDb?.selectActivityDbCached() ?: ActivityDb.selectOtherCached(),
-            activitiesUi =
-                Cache.activitiesDbSorted.map { ActivityUi(activityDb = it) },
+            activityDb = initIntervalDb?.selectActivityDbCached()
+                         ?: ActivityDb.selectOtherCached(),
+            selectedTime = initIntervalDb?.id ?: time(),
+            activitiesUi = Cache.activitiesDbSorted.map { ActivityUi(activityDb = it) },
         )
     )
+
+    ///
+
+    fun setActivityDb(activityDb: ActivityDb) {
+        state.update { it.copy(activityDb = activityDb) }
+    }
 
     ///
 
@@ -44,4 +59,31 @@ class HistoryFormVm(
         val title: String =
             activityDb.name.textFeatures().textNoFeatures
     }
+}
+
+///
+
+private fun prepTimeNote(
+    time: Int,
+): String {
+    val today: Int = UnixTime().localDay
+    val unixTime = UnixTime(time)
+    return if (today == unixTime.localDay)
+        "Today " + unixTime.getStringByComponents(
+            listOf(
+                UnixTime.StringComponent.hhmm24,
+            )
+        )
+    else unixTime.getStringByComponents(
+        listOf(
+            UnixTime.StringComponent.dayOfMonth,
+            UnixTime.StringComponent.space,
+            UnixTime.StringComponent.month3,
+            UnixTime.StringComponent.comma,
+            UnixTime.StringComponent.space,
+            UnixTime.StringComponent.dayOfWeek3,
+            UnixTime.StringComponent.space,
+            UnixTime.StringComponent.hhmm24,
+        )
+    )
 }
