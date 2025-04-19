608a87b989a00232caeb90002146cc20d0ae8b62
hi@timeto.me <hi@timeto.me>
2025-04-19 22:10:57 +0300

    HistoryFormUtils.deleteIntervalUi()

--- shared/src/commonMain/kotlin/me/timeto/shared/ui/history/form/HistoryFormUtils.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/history/form/HistoryFormUtils.kt
@@ -1,8 +1,35 @@
 package me.timeto.shared.ui.history.form
 
 import me.timeto.shared.UnixTime
+import me.timeto.shared.db.ActivityDb
+import me.timeto.shared.db.IntervalDb
+import me.timeto.shared.launchExIo
+import me.timeto.shared.textFeatures
+import me.timeto.shared.ui.DialogsManager
 
 object HistoryFormUtils {
+
+    fun deleteIntervalUi(
+        intervalDb: IntervalDb,
+        dialogsManager: DialogsManager,
+        onSuccess: suspend () -> Unit,
+    ) {
+        val activityDb: ActivityDb = intervalDb.selectActivityDbCached()
+        val intervalNote: String? = intervalDb.note?.takeIf { it.isNotBlank() }
+        val note: String = (intervalNote ?: activityDb.name)
+            .textFeatures()
+            .textNoFeatures
+        dialogsManager.confirmation(
+            message = "Are you sure you want to delete \"$note\"",
+            buttonText = "Delete",
+            onConfirm = {
+                launchExIo {
+                    intervalDb.delete()
+                    onSuccess()
+                }
+            },
+        )
+    }
 
     fun prepTimeNote(
         time: Int,
