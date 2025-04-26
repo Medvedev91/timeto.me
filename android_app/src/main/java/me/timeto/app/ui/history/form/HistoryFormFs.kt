3cd54b21b3f7760d6937a333c870724b0e244689
hi@timeto.me <hi@timeto.me>
2025-04-26 21:36:07 +0300

    HistoryFormFs.kt: Activity picker

--- android_app/src/main/java/me/timeto/app/ui/history/form/HistoryFormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/history/form/HistoryFormFs.kt
@@ -1,8 +1,19 @@
 package me.timeto.app.ui.history.form
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import me.timeto.app.c
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
+import me.timeto.app.ui.activities.ActivityPickerFs
+import me.timeto.app.ui.form.button.FormButton
+import me.timeto.app.ui.form.padding.FormPaddingTop
+import me.timeto.app.ui.header.Header
+import me.timeto.app.ui.header.HeaderActionButton
+import me.timeto.app.ui.header.HeaderCancelButton
+import me.timeto.app.ui.navigation.LocalNavigationFs
+import me.timeto.app.ui.navigation.LocalNavigationLayer
 import me.timeto.shared.db.IntervalDb
 import me.timeto.shared.ui.history.form.HistoryFormVm
 
@@ -11,6 +22,9 @@ fun HistoryFormFs(
     initIntervalDb: IntervalDb?,
 ) {
 
+    val navigationFs = LocalNavigationFs.current
+    val navigationLayer = LocalNavigationLayer.current
+
     val (vm, state) = rememberVm {
         HistoryFormVm(
             initIntervalDb = initIntervalDb,
@@ -18,5 +32,61 @@ fun HistoryFormFs(
     }
 
     Screen {
+
+        val scrollState = rememberLazyListState()
+
+        Header(
+            title = state.title,
+            scrollState = scrollState,
+            actionButton = HeaderActionButton(
+                text = state.saveText,
+                isEnabled = true,
+                onClick = {
+                    vm.save(
+                        dialogsManager = navigationFs,
+                        onSuccess = {
+                            navigationLayer.close()
+                        },
+                    )
+                },
+            ),
+            cancelButton = HeaderCancelButton(
+                text = "Cancel",
+                onClick = {
+                    navigationLayer.close()
+                },
+            ),
+        )
+
+        LazyColumn(
+            state = scrollState,
+        ) {
+
+            item {
+
+                FormPaddingTop()
+
+                FormButton(
+                    title = state.activityTitle,
+                    isFirst = true,
+                    isLast = false,
+                    note = state.activityNote,
+                    noteColor =
+                        if (state.activityDb == null) c.red
+                        else c.textSecondary,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            ActivityPickerFs(
+                                initActivityDb = state.activityDb,
+                                onDone = { newActivityDb ->
+                                    vm.setActivityDb(newActivityDb)
+                                },
+                            )
+                        }
+                    },
+                )
+            }
+        }
     }
 }
