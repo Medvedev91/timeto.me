57c2a440ec49f77703cff1c54189aa068d478dc9
hi@timeto.me <hi@timeto.me>
2025-04-26 22:02:29 +0300

    HistoryFormFs.kt: Time

--- android_app/src/main/java/me/timeto/app/ui/history/form/HistoryFormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/history/form/HistoryFormFs.kt
@@ -1,12 +1,25 @@
 package me.timeto.app.ui.history.form
 
+import android.widget.NumberPicker
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import me.timeto.app.c
+import me.timeto.app.dpToPx
+import me.timeto.app.isSDKQPlus
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
 import me.timeto.app.ui.activities.ActivityPickerFs
+import me.timeto.app.ui.form.FormItemView
 import me.timeto.app.ui.form.button.FormButton
 import me.timeto.app.ui.form.padding.FormPaddingTop
 import me.timeto.app.ui.header.Header
@@ -84,6 +97,43 @@ fun HistoryFormFs(
                                 },
                             )
                         }
+                    },
+                )
+
+                val timerItemsUi = state.timerItemsUi
+                val formTimeItemIdx: MutableState<Int> = remember {
+                    mutableStateOf(timerItemsUi.indexOfFirst { it.time == state.time })
+                }
+
+                val formTimeItemIdxValue: Int = formTimeItemIdx.value
+                LaunchedEffect(formTimeItemIdxValue) {
+                    vm.setTime(timerItemsUi[formTimeItemIdxValue].time)
+                }
+
+                FormItemView(
+                    isFirst = false,
+                    isLast = true,
+                    modifier = Modifier,
+                    content = {
+                        AndroidView(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 80.dp),
+                            factory = { context ->
+                                NumberPicker(context).apply {
+                                    setOnValueChangedListener { _, _, new ->
+                                        formTimeItemIdx.value = new
+                                    }
+                                    displayedValues = timerItemsUi.map { it.title }.toTypedArray()
+                                    if (isSDKQPlus())
+                                        textSize = dpToPx(18f).toFloat()
+                                    wrapSelectorWheel = false
+                                    minValue = 0
+                                    maxValue = timerItemsUi.size - 1
+                                    value = formTimeItemIdx.value // Set last
+                                }
+                            }
+                        )
                     },
                 )
             }
