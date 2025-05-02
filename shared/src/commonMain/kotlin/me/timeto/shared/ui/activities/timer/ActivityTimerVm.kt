2e03b70f8f4be1c3d21fcf419fe66f5a01f4afac
hi@timeto.me <hi@timeto.me>
2025-05-03 01:50:10 +0300

    ActivityTimerVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/activities/timer/ActivityTimerVm.kt
@@ -0,0 +1,111 @@
+package me.timeto.shared.ui.activities.timer
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import me.timeto.shared.db.ActivityDb
+import me.timeto.shared.launchExIo
+import me.timeto.shared.textFeatures
+import me.timeto.shared.toHms
+import me.timeto.shared.vm.__Vm
+
+class ActivityTimerVm(
+    private val activityDb: ActivityDb,
+    private val strategy: ActivityTimerStrategy,
+) : __Vm<ActivityTimerVm.State>() {
+
+    companion object {
+
+        suspend fun startInterval(
+            seconds: Int,
+            activityDb: ActivityDb,
+            strategy: ActivityTimerStrategy,
+        ) {
+            when (strategy) {
+                ActivityTimerStrategy.Simple ->
+                    activityDb.startInterval(seconds = seconds)
+//                is TimerContext.Task ->
+//                    timerContext.task.startInterval(timer, activity)
+//                is TimerContext.Interval ->
+//                    IntervalDb.insertWithValidation(timer, activity, timerContext.interval.note)
+            }
+        }
+    }
+
+    ///
+
+    data class State(
+        val title: String,
+        val note: String?,
+        val initSeconds: Int,
+        val timerItemsUi: List<TimerItemUi>,
+    )
+
+    override val state: MutableStateFlow<State>
+
+    init {
+        val initSeconds: Int = when (strategy) {
+            ActivityTimerStrategy.Simple -> activityDb.timer
+        }
+        state = MutableStateFlow(
+            State(
+                title = when (strategy) {
+                    ActivityTimerStrategy.Simple ->
+                        activityDb.name.textFeatures().textNoFeatures
+                },
+                note = when (strategy) {
+                    ActivityTimerStrategy.Simple -> null
+                },
+                initSeconds = initSeconds,
+                timerItemsUi = makeTimerItemsUi(initSeconds),
+            )
+        )
+    }
+
+    fun start(
+        seconds: Int,
+        onSuccess: () -> Unit,
+    ) {
+        launchExIo {
+            startInterval(
+                seconds = seconds,
+                activityDb = activityDb,
+                strategy = strategy,
+            )
+            onUi {
+                onSuccess()
+            }
+        }
+    }
+
+    ///
+
+    data class TimerItemUi(
+        val seconds: Int,
+    ) {
+        val title: String = run {
+            val (h, m, _) = seconds.toHms()
+            when {
+                h == 0 -> "$m min"
+                m == 0 -> "$h h"
+                else -> "$h : ${m.toString().padStart(2, '0')}"
+            }
+        }
+    }
+}
+
+private fun makeTimerItemsUi(
+    defSeconds: Int,
+): List<ActivityTimerVm.TimerItemUi> {
+
+    val secondsSet: Set<Int> = run {
+        (1..10).map { it * 60 } + // 1 - 10 min by 1 min
+        (1..10).map { (600 + (it * 300)) } + // 15 min - 1 hour by 5 min
+        (1..138).map { (3_600 + (it * 600)) } + // 1 hour+ by 10 min
+        defSeconds
+    }.toSet()
+
+    return secondsSet.sorted().map { seconds ->
+        ActivityTimerVm.TimerItemUi(
+            seconds = seconds,
+        )
+    }
+}
