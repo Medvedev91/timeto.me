0e7417724edaae95c093c6617b7ca232faf0f22d
hi@timeto.me <hi@timeto.me>
2025-05-03 01:22:51 +0300

    ActivityTimerSheet.swift

---
+++ ios_app/iphone/ui/Activities/Timer/ActivityTimerSheet.swift
@@ -0,0 +1,92 @@
+import SwiftUI
+import shared
+
+extension Navigation {
+    
+    func showActivityTimerSheet(
+        activityDb: ActivityDb,
+        strategy: ActivityTimerStrategy,
+        // false for nested sheet to speed up closing
+        hideOnStart: Bool,
+        onStart: @escaping () -> Void
+    ) {
+        self.sheet {
+            VmView({
+                ActivityTimerVm(
+                    activityDb: activityDb,
+                    strategy: strategy
+                )
+            }) { vm, state in
+                ActivityTimerSheet(
+                    vm: vm,
+                    state: state,
+                    selectedSeconds: state.initSeconds,
+                    onStart: onStart
+                )
+            }
+        }
+    }
+}
+ 
+private struct ActivityTimerSheet: View {
+    
+    let vm: ActivityTimerVm
+    let state: ActivityTimerVm.State
+    
+    @State var selectedSeconds: Int32
+    let onStart: () -> Void
+    
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        
+        VStack {
+            
+            // Plus padding from picker
+            if let note = state.note {
+                Text(note)
+                    .font(.system(size: 14))
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 6)
+            }
+            
+            Spacer()
+            
+            Picker("", selection: $selectedSeconds) {
+                ForEach(state.timerItemsUi, id: \.seconds) { itemUi in
+                    Text(itemUi.title)
+                }
+            }
+            .pickerStyle(.wheel)
+            .foregroundColor(.primary)
+            .padding(.bottom, state.note != nil ? 30 : 5)
+            
+            Spacer()
+        }
+        .presentationDetents([.height(400)])
+        .presentationDragIndicator(.visible)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .primaryAction) {
+                Button("Start") {
+                    vm.start(
+                        onSuccess: {
+                            onStart()
+                            dismiss()
+                        }
+                    )
+                }
+                .fontWeight(.semibold)
+            }
+        }
+        .navigationTitle(state.title)
+        .toolbarTitleDisplayMode(.inline)
+    }
+}
