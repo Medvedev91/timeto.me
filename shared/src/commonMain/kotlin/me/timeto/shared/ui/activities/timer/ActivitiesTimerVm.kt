a926492f31e31bad1fdf0cca625a6e529f7025c1
hi@timeto.me <hi@timeto.me>
2025-05-04 00:37:06 +0300

    ActivitiesTimerVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/activities/timer/ActivitiesTimerVm.kt
@@ -0,0 +1,30 @@
+package me.timeto.shared.ui.activities.timer
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.Cache
+import me.timeto.shared.db.IntervalDb
+import me.timeto.shared.onEachExIn
+import me.timeto.shared.vm.__Vm
+
+class ActivitiesTimerVm : __Vm<ActivitiesTimerVm.State>() {
+
+    data class State(
+        val lastIntervalId: Int,
+    )
+
+    override val state = MutableStateFlow(
+        State(
+            lastIntervalId = Cache.lastInterval.id,
+        )
+    )
+
+    init {
+        IntervalDb.selectLastOneOrNullFlow()
+            .filterNotNull()
+            .onEachExIn(scopeVm()) { newIntervalDb: IntervalDb ->
+                state.update { it.copy(lastIntervalId = newIntervalDb.id) }
+            }
+    }
+}
