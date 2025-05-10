d309212ce08f42ebab13a94d9f3bdb20aaa8c2b5
hi@timeto.me <hi@timeto.me>
2025-05-10 23:41:22 +0300

    RepeatingFormPeriodFs.kt:

--- android_app/src/main/java/me/timeto/app/ui/repeatings/form/RepeatingFormPeriodFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/repeatings/form/RepeatingFormPeriodFs.kt
@@ -1,10 +1,19 @@
 package me.timeto.app.ui.repeatings.form
 
+import android.widget.NumberPicker
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import me.timeto.app.H_PADDING
+import me.timeto.app.VStack
+import me.timeto.app.dpToPx
+import me.timeto.app.isSDKQPlus
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
 import me.timeto.app.ui.form.button.FormButton
@@ -95,6 +104,36 @@ fun RepeatingFormPeriodFs(
                 )
 
                 FormPaddingSectionSection()
+
+                when (state.activePeriodIdx) {
+                    0 -> {}
+                    1 -> {
+                        VStack(
+                            modifier = Modifier
+                                .padding(horizontal = H_PADDING + 4.dp),
+                        ) {
+                            val days: List<Int> = (2..666).toList()
+                            AndroidView(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                factory = { context ->
+                                    NumberPicker(context).apply {
+                                        setOnValueChangedListener { _, _, new ->
+                                            vm.setSelectedNDays(new)
+                                        }
+                                        displayedValues = days.map { "$it" }.toTypedArray()
+                                        if (isSDKQPlus())
+                                            textSize = dpToPx(18f).toFloat()
+                                        wrapSelectorWheel = false
+                                        minValue = 0
+                                        maxValue = days.size - 1
+                                        value = days.indexOf(state.selectedNDays) // Set last
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
 
                 FormPaddingBottom(withNavigation = true)
             }
