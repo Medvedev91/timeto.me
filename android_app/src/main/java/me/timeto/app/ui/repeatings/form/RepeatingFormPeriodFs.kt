ae3b49fc65ec38c8e17118e9d335a0b64e3a9d42
hi@timeto.me <hi@timeto.me>
2025-05-10 23:49:35 +0300

    RepeatingFormPeriodFs.kt:

--- android_app/src/main/java/me/timeto/app/ui/repeatings/form/RepeatingFormPeriodFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/repeatings/form/RepeatingFormPeriodFs.kt
@@ -4,19 +4,25 @@ import android.widget.NumberPicker
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import me.timeto.app.H_PADDING
+import me.timeto.app.R
 import me.timeto.app.VStack
+import me.timeto.app.c
 import me.timeto.app.dpToPx
 import me.timeto.app.isSDKQPlus
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
 import me.timeto.app.ui.form.button.FormButton
+import me.timeto.app.ui.form.button.FormButtonView
 import me.timeto.app.ui.form.padding.FormPaddingBottom
 import me.timeto.app.ui.form.padding.FormPaddingSectionSection
 import me.timeto.app.ui.form.padding.FormPaddingTop
@@ -59,6 +65,7 @@ fun RepeatingFormPeriodFs(
                         dialogsManager = navigationFs,
                         onSuccess = { newPeriod ->
                             onDone(newPeriod)
+                            navigationLayer.close()
                         }
                     )
                 },
@@ -130,6 +137,34 @@ fun RepeatingFormPeriodFs(
                                         value = days.indexOf(state.selectedNDays) // Set last
                                     }
                                 }
+                            )
+                        }
+                    }
+                    2 -> {
+                        val daysOfWeekUi = state.daysOfWeekUi
+                        daysOfWeekUi.forEachIndexed { idx, dayOfWeekUi ->
+                            FormButtonView(
+                                title = dayOfWeekUi.title,
+                                titleColor = null,
+                                isFirst = idx == 0,
+                                isLast = ((idx + 1) == daysOfWeekUi.size),
+                                modifier = Modifier,
+                                rightView = {
+                                    if (dayOfWeekUi.idx in state.selectedDaysOfWeek) {
+                                        Icon(
+                                            painterResource(R.drawable.sf_checkmark_medium_medium),
+                                            contentDescription = "Selected",
+                                            tint = c.white,
+                                            modifier = Modifier
+                                                .padding(end = H_PADDING)
+                                                .size(13.dp),
+                                        )
+                                    }
+                                },
+                                onClick = {
+                                    vm.toggleDayOfWeek(dayOfWeekUi.idx)
+                                },
+                                onLongClick = null,
                             )
                         }
                     }
