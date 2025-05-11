e53199e0e6bb9dbe7c454455ef1863d0811d6936
hi@timeto.me <hi@timeto.me>
2025-05-11 14:37:51 +0300

    Screen(bgColor)

--- android_app/src/main/java/me/timeto/app/ui/Screen.kt
+++ android_app/src/main/java/me/timeto/app/ui/Screen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import me.timeto.app.VStack
 import me.timeto.app.c
@@ -12,12 +13,13 @@ import me.timeto.app.c
 @Composable
 fun Screen(
     modifier: Modifier = Modifier,
+    bgColor: Color? = null,
     content: @Composable ColumnScope.() -> Unit,
 ) {
     VStack(
         modifier = modifier
             .fillMaxSize()
-            .background(c.bg)
+            .background(bgColor ?: c.bg)
             .pointerInput(Unit) {},
     ) {
         content()
