1709eaffef9e6db4f5a9acf46261c50406bbcefd
hi@timeto.me <hi@timeto.me>
2025-05-11 14:07:26 +0300

    NavigationDialog.kt:

--- android_app/src/main/java/me/timeto/app/ui/navigation/NavigationDialog.kt
+++ android_app/src/main/java/me/timeto/app/ui/navigation/NavigationDialog.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import me.timeto.app.H_PADDING
+import me.timeto.app.MainActivity
 import me.timeto.app.VStack
 import me.timeto.app.ZStack
 import me.timeto.app.c
@@ -25,8 +28,12 @@ fun NavigationDialog(
     innerPadding: PaddingValues = PaddingValues(H_PADDING),
     content: @Composable ColumnScope.(layer: NavigationLayer) -> Unit,
 ) {
+    val mainActivity = LocalContext.current as MainActivity
     ZStack(
         modifier = Modifier
+            .padding(top = mainActivity.statusBarHeightDp)
+            .padding(vertical = 8.dp)
+            .systemBarsPadding()
             .fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
