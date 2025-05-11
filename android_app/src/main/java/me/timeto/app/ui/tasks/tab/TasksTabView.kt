47ec78cdee93078b3d3ce8f8490230666d3661a5
hi@timeto.me <hi@timeto.me>
2025-05-11 22:40:36 +0300

    TasksTabView.kt:

---
+++ android_app/src/main/java/me/timeto/app/ui/tasks/tab/TasksTabView.kt
@@ -0,0 +1,9 @@
+package me.timeto.app.ui.tasks.tab
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import me.timeto.app.H_PADDING
+
+val TasksTabView__TAB_BUTTON_WIDTH: Dp = 32.dp
+val TasksTabView__PADDING_END: Dp = TasksTabView__TAB_BUTTON_WIDTH + H_PADDING
+val TasksTabView__LIST_SECTION_PADDING: Dp = 20.dp
