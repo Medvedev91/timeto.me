fc8cb66fcfa4b6eb38cd8ef80c51e27f8687f1aa
hi@timeto.me <hi@timeto.me>
2025-05-11 16:35:25 +0300

    RepeatingFormFs.kt:

--- android_app/src/main/java/me/timeto/app/ui/repeatings/form/RepeatingFormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/repeatings/form/RepeatingFormFs.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.text.input.ImeAction
 import me.timeto.app.c
 import me.timeto.app.rememberVm
 import me.timeto.app.ui.Screen
+import me.timeto.app.ui.activities.ActivityPickerFs
 import me.timeto.app.ui.daytime.DaytimePickerSheet
 import me.timeto.app.ui.form.FormInput
 import me.timeto.app.ui.form.button.FormButton
@@ -21,6 +22,7 @@ import me.timeto.app.ui.header.HeaderActionButton
 import me.timeto.app.ui.header.HeaderCancelButton
 import me.timeto.app.ui.navigation.LocalNavigationFs
 import me.timeto.app.ui.navigation.LocalNavigationLayer
+import me.timeto.app.ui.timer.TimerSheet
 import me.timeto.shared.db.RepeatingDb
 import me.timeto.shared.ui.repeatings.form.RepeatingFormVm
 
@@ -129,6 +131,48 @@ fun RepeatingFormFs(
                                     vm.setDaytime(newDaytime)
                                 },
                                 onRemove = {},
+                            )
+                        }
+                    },
+                )
+
+                FormPaddingSectionSection()
+
+                FormButton(
+                    title = state.activityTitle,
+                    isFirst = true,
+                    isLast = false,
+                    note = state.activityNote,
+                    noteColor = if (state.activityDb == null) c.red else c.textSecondary,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            ActivityPickerFs(
+                                initActivityDb = state.activityDb,
+                                onDone = { newActivityDb ->
+                                    vm.setActivity(newActivityDb)
+                                },
+                            )
+                        }
+                    },
+                )
+
+                FormButton(
+                    title = state.timerTitle,
+                    isFirst = false,
+                    isLast = true,
+                    note = state.timerNote,
+                    noteColor = if (state.timerSeconds == null) c.red else c.textSecondary,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            TimerSheet(
+                                title = state.timerTitle,
+                                doneTitle = "Done",
+                                initSeconds = state.timerPickerSeconds,
+                                onDone = { newTimerSeconds ->
+                                    vm.setTimerSeconds(newTimerSeconds)
+                                },
                             )
                         }
                     },
