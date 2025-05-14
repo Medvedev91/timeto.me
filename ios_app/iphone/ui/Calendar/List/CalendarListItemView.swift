50430af56aca9d401411baec5775ad66272df463
hi@timeto.me <hi@timeto.me>
2025-05-15 01:34:23 +0300

    CalendarListItemView.swift

---
+++ ios_app/iphone/ui/Calendar/List/CalendarListItemView.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+import shared
+
+struct CalendarListItemView: View {
+    
+    let eventUi: CalendarListVm.EventUi
+    let withTopDivider: Bool
+    
+    ///
+    
+    @Environment(Navigation.self) private var navigation
+    
+    var body: some View {
+        
+        ZStack(alignment: .top) {
+            
+            Button(
+                action: {
+                    navigation.fullScreen(withAnimation: false) {
+                        EventFormFullScreen(
+                            initEventDb: eventUi.eventDb,
+                            initText: nil,
+                            initTime: nil
+                        )
+                    }
+                },
+                label: {
+                    
+                    VStack {
+                        
+                        HStack {
+                            
+                            Text(eventUi.dateString)
+                                .font(.system(size: 14, weight: .light))
+                                .foregroundColor(.secondary)
+                            
+                            Spacer()
+                            
+                            Text(eventUi.dayLeftString)
+                                .font(.system(size: 14, weight: .light))
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        HStack {
+                            
+                            Text(eventUi.listText)
+                                .textAlign(.leading)
+                            
+                            Spacer()
+                            
+                            TriggersIconsView(
+                                checklistsDb: eventUi.textFeatures.checklists,
+                                shortcutsDb: eventUi.textFeatures.shortcuts
+                            )
+                        }
+                        .padding(.top, 4)
+                    }
+                    .foregroundColor(.primary)
+                    .padding(.top, 10)
+                    .padding(.bottom, 10)
+                }
+            )
+            
+            if withTopDivider {
+                Divider()
+            }
+        }
+    }
+}
