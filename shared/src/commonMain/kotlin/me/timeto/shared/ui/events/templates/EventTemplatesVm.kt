ca887ea73de6e210c1b8f33b617e96da1963e206
hi@timeto.me <hi@timeto.me>
2025-05-16 06:03:09 +0300

    EventTemplatesVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ui/events/templates/EventTemplatesVm.kt
@@ -0,0 +1,39 @@
+package me.timeto.shared.ui.events.templates
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.Cache
+import me.timeto.shared.db.EventTemplateDb
+import me.timeto.shared.onEachExIn
+import me.timeto.shared.vm.__Vm
+
+class EventTemplatesVm : __Vm<EventTemplatesVm.State>() {
+
+    data class State(
+        val templatesUi: List<EventTemplateUi>
+    ) {
+        val newTemplateText = "New Template"
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            templatesUi = Cache.eventTemplatesDbSorted.toTemplatesUi(),
+        )
+    )
+
+    init {
+        val scope = scopeVm()
+        EventTemplateDb.selectAscSortedFlow().onEachExIn(scope) { templatesDb ->
+            state.update {
+                it.copy(templatesUi = templatesDb.toTemplatesUi())
+            }
+        }
+    }
+}
+
+private fun List<EventTemplateDb>.toTemplatesUi(
+): List<EventTemplateUi> = this.reversed().map { eventTemplateDb ->
+    EventTemplateUi(
+        eventTemplateDb = eventTemplateDb,
+    )
+}
