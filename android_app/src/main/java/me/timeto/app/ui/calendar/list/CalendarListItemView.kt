be57169ff42193e3bea8382715da87434aec5138
hi@timeto.me <hi@timeto.me>
2025-05-16 23:30:15 +0300

    CalendarListItemView.kt

---
+++ android_app/src/main/java/me/timeto/app/ui/calendar/list/CalendarListItemView.kt
@@ -0,0 +1,90 @@
+package me.timeto.app.ui.calendar.list
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.timeto.app.HStack
+import me.timeto.app.H_PADDING_HALF
+import me.timeto.app.VStack
+import me.timeto.app.ZStack
+import me.timeto.app.c
+import me.timeto.app.ui.TriggersIconsView
+import me.timeto.shared.ui.calendar.CalendarListVm
+
+@Composable
+fun CalendarListItemView(
+    eventUi: CalendarListVm.EventUi,
+    withTopDivider: Boolean,
+    clip: Shape,
+    modifier: Modifier,
+) {
+
+    ZStack(
+        modifier = modifier,
+        contentAlignment = Alignment.TopCenter,
+    ) {
+
+        VStack(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(clip)
+                .clickable {
+                }
+                .padding(horizontal = H_PADDING_HALF)
+                .padding(vertical = 10.dp),
+        ) {
+
+            HStack {
+
+                Text(
+                    text = eventUi.dateString,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W300,
+                    color = c.textSecondary,
+                    modifier = Modifier.weight(1f),
+                )
+
+                Text(
+                    text = eventUi.dayLeftString,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W300,
+                    color = c.textSecondary,
+                )
+            }
+
+            HStack(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                Text(
+                    eventUi.listText,
+                    modifier = Modifier
+                        .weight(1f),
+                    color = c.text,
+                )
+
+                TriggersIconsView(
+                    checklistsDb = eventUi.textFeatures.checklists,
+                    shortcutsDb = eventUi.textFeatures.shortcuts,
+                )
+            }
+        }
+
+        if (withTopDivider) {
+            Divider(
+                modifier = Modifier
+                    .padding(horizontal = H_PADDING_HALF),
+            )
+        }
+    }
+}
