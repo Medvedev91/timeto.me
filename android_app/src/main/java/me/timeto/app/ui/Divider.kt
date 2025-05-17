a93573f3cb84f93acf3bd9ea27c5630d56fe3ad8
hi@timeto.me <hi@timeto.me>
2025-05-17 05:11:33 +0300

    Divider(color)

--- android_app/src/main/java/me/timeto/app/ui/Divider.kt
+++ android_app/src/main/java/me/timeto/app/ui/Divider.kt
@@ -5,18 +5,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import me.timeto.app.ZStack
 import me.timeto.app.c
 import me.timeto.app.onePx
 
 @Composable
 fun Divider(
+    color: Color? = null,
     modifier: Modifier = Modifier,
 ) {
     ZStack(
         modifier = modifier
             .height(onePx)
             .fillMaxWidth()
-            .background(c.divider),
+            .background(color ?: c.divider),
     )
 }
