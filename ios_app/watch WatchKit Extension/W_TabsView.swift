cfae70146243204de73e60045c6ec576c88c68bb
hi@timeto.me <hi@timeto.me>
2025-05-23 04:26:38 +0300

    W_TabsView.swift:

--- ios_app/watch WatchKit Extension/W_TabsView.swift
+++ ios_app/watch WatchKit Extension/W_TabsView.swift
@@ -3,45 +3,45 @@ import Combine
 import shared
 
 struct W_TabsView: View {
-
+    
     static var lastInstance: W_TabsView? = nil
-
+    
     static let TAB_ID_TIMER = 1
     static let TAB_ID_TASKS = 2
-
+    
     @State var tabSelection = TAB_ID_TIMER
-
+    
     private let alertPublisher: AnyPublisher<UIAlertData, Never> = Utils_kmpKt.uiAlertFlow.toPublisher()
     @State private var dialogErrorText: String?
     @State private var dialogErrorIsPresented = false
-
+    
     var body: some View {
-
+        
         let _ = W_TabsView.lastInstance = self
-
+        
         TabView(selection: $tabSelection) {
-
-            W_TabTimerView()
-                    .tag(W_TabsView.TAB_ID_TIMER)
-
+            
+            WatchTabTimerView()
+                .tag(W_TabsView.TAB_ID_TIMER)
+            
             W_TabTasksView()
-                    .tag(W_TabsView.TAB_ID_TASKS)
+                .tag(W_TabsView.TAB_ID_TASKS)
         }
-                /// TRICK. Otherwise the text does not updates
-                .onReceive(alertPublisher) { data in
-                    dialogErrorText = data.message
-                }
-                .onChange(of: dialogErrorText) { newValue in
-                    dialogErrorIsPresented = newValue != nil
-                }
-                //////
-                .sheet(
-                        isPresented: $dialogErrorIsPresented,
-                        onDismiss: { dialogErrorText = nil }
-                ) {
-                    Text(dialogErrorText ?? "")
-                            .font(.title2)
-                            .foregroundColor(.red)
-                }
+        /// TRICK. Otherwise the text does not updates
+        .onReceive(alertPublisher) { data in
+            dialogErrorText = data.message
+        }
+        .onChange(of: dialogErrorText) { _, newValue in
+            dialogErrorIsPresented = newValue != nil
+        }
+        ///
+        .sheet(
+            isPresented: $dialogErrorIsPresented,
+            onDismiss: { dialogErrorText = nil }
+        ) {
+            Text(dialogErrorText ?? "")
+                .font(.title2)
+                .foregroundColor(.red)
+        }
     }
 }
