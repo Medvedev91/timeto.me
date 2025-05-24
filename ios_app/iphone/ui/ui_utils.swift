ca8166816f2bb8db726759fd551d9f9e7a4142f5
hi@timeto.me <hi@timeto.me>
2025-05-24 21:28:41 +0300

    ui_utils.swift:

--- ios_app/iphone/ui/ui_utils.swift
+++ ios_app/iphone/ui/ui_utils.swift
@@ -1,8 +1,18 @@
 import SwiftUI
+import shared
 
-extension View {
-    
-    func myFormContentMargins() -> some View {
-        contentMargins(.top, 14)
-    }
+let H_PADDING = 16.0
+let H_PADDING_HALF = H_PADDING / 2
+
+private let scale: CGFloat = UIScreen.main.scale
+let onePx: CGFloat = 1 / scale
+let halfDpFloor: CGFloat = CGFloat(Int(scale / 2)) / scale // -=
+let halfDpCeil: CGFloat = 1 - halfDpFloor // +=
+
+func hideKeyboard() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+}
+
+func showOpenSource() {
+    UIApplication.shared.open(URL(string: Utils_kmpKt.OPEN_SOURCE_URL)!)
 }
