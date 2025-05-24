3d12b407cfd397ab27651f65d82afdab8e86af7d
hi@timeto.me <hi@timeto.me>
2025-05-25 01:45:24 +0300

    org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktor_version")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
                 implementation("app.cash.sqldelight:primitive-adapters:$sqldelight_version")
                 implementation("app.cash.sqldelight:coroutines-extensions:$sqldelight_version")
             }
