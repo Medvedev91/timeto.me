ecb04471ad5a9aa605bc0227ea9c158ccafca640
hi@timeto.me <hi@timeto.me>
2025-05-25 01:33:33 +0300

    androidx.compose.material:material:1.8.2

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -60,6 +60,6 @@ android {
 dependencies {
     implementation(project(":shared"))
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.material:material:1.8.0")
+    implementation("androidx.compose.material:material:1.8.2")
     implementation("com.google.android.material:material:1.12.0")
 }
