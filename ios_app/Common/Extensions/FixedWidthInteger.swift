7bfe749e3e62847a50bb4dee5f7a087043dbc8f5
hi@timeto.me <hi@timeto.me>
2025-05-25 17:40:36 +0300

    FixedWidthInteger.swift

---
+++ ios_app/Common/Extensions/FixedWidthInteger.swift
@@ -0,0 +1,34 @@
+import shared
+
+extension FixedWidthInteger {
+
+    func toInt() -> Int {
+        Int(self)
+    }
+
+    func toInt32() -> Int32 {
+        Int32(self)
+    }
+
+    func toDouble() -> Double {
+        Double(self)
+    }
+
+    func toKotlinInt() -> KotlinInt {
+        KotlinInt(integerLiteral: Int(self))
+    }
+
+    func toString() -> String {
+        String(self)
+    }
+
+    ///
+
+    func asUnixDayToDate() -> Date {
+        Date(timeIntervalSince1970: Double(UnixTime.companion.byLocalDay(localDay: Int32(self), utcOffset: Utils_kmpKt.localUtcOffset).time))
+    }
+
+    func asTimeToDate() -> Date {
+        Date(timeIntervalSince1970: Double(self))
+    }
+}
--- ios_app/utils_apple_todo_remove.swift
+++ ios_app/utils_apple_todo_remove.swift
@@ -1,39 +1,6 @@
 import SwiftUI
 import shared
 
-extension FixedWidthInteger {
-
-    func toInt() -> Int {
-        Int(self)
-    }
-
-    func toInt32() -> Int32 {
-        Int32(self)
-    }
-
-    func toDouble() -> Double {
-        Double(self)
-    }
-
-    func toKotlinInt() -> KotlinInt {
-        KotlinInt(integerLiteral: Int(self))
-    }
-
-    func toString() -> String {
-        String(self)
-    }
-
-    ///
-
-    func asUnixDayToDate() -> Date {
-        Date(timeIntervalSince1970: Double(UnixTime.companion.byLocalDay(localDay: Int32(self), utcOffset: Utils_kmpKt.localUtcOffset).time))
-    }
-
-    func asTimeToDate() -> Date {
-        Date(timeIntervalSince1970: Double(self))
-    }
-}
-
 extension Int {
 
     func limitMin(_ value: Int) -> Int {
