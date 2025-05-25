a1f788c98e3f2eccda9d3c305b5bd1594b442cc4
hi@timeto.me <hi@timeto.me>
2025-05-25 14:03:55 +0300

    utils_ios.swift:

--- ios_app/iphone/utils_ios.swift
+++ ios_app/iphone/utils_ios.swift
@@ -1,27 +1,6 @@
 import SwiftUI
 import shared
 
-func schedulePush(data: ScheduledNotificationData) {
-    let content = UNMutableNotificationContent()
-    content.title = data.title
-    content.body = data.text
-
-    if data.type == .break_ {
-        let soundFile = Utils_kmpKt.getSoundTimerExpiredFileName(withExtension: true)
-        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: soundFile))
-    } else {
-        content.sound = .default
-    }
-    content.badge = 1
-
-    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(data.inSeconds.toInt()), repeats: false)
-
-    let req = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-    let center = UNUserNotificationCenter.current()
-    center.add(req, withCompletionHandler: nil)
-}
-
 struct c {
 
     static let bg = ColorRgba.companion.bg.toColor()
