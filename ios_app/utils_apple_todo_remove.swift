f0698d0a2e1a5a45033e4e965338c9d5ad181454
hi@timeto.me <hi@timeto.me>
2025-05-25 17:23:49 +0300

    Date.swift

--- ios_app/utils_apple_todo_remove.swift
+++ ios_app/utils_apple_todo_remove.swift
@@ -1,27 +1,6 @@
 import SwiftUI
 import shared
 
-extension Date {
-
-    /// For device time zone
-    func startOfDay() -> Date {
-        Calendar.current.startOfDay(for: self)
-    }
-
-    func inSeconds(_ seconds: Int) -> Date {
-        var components = DateComponents()
-        components.second = seconds
-        return Calendar.current.date(
-            byAdding: components,
-            to: self
-        )!
-    }
-
-    func toUnixTime() -> UnixTime {
-        UnixTime(time: Int32(timeIntervalSince1970), utcOffset: Utils_kmpKt.localUtcOffset)
-    }
-}
-
 extension UnixTime {
 
     func toDate() -> Date {
