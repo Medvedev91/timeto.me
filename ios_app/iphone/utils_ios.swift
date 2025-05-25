b08e9fd6231ae945082f6cd66968a7e1cd6c3364
hi@timeto.me <hi@timeto.me>
2025-05-25 14:07:41 +0300

    utils_ios.swift:

--- ios_app/iphone/utils_ios.swift
+++ ios_app/iphone/utils_ios.swift
@@ -3,9 +3,6 @@ import shared
 
 struct c {
 
-    static let bg = ColorRgba.companion.bg.toColor()
-    static let fg = ColorRgba.companion.fg.toColor()
-
     static let dividerBg = ColorRgba.companion.dividerBg.toColor()
     static let dividerFg = ColorRgba.companion.dividerFg.toColor()
 
@@ -14,6 +11,4 @@ struct c {
     static let homeFontSecondary = ColorRgba.companion.homeFontSecondary.toColor()
     static let homeMenuTime = ColorRgba.companion.homeMenuTime.toColor()
     static let homeFg = ColorRgba.companion.homeFg.toColor()
-
-    static let tasksDropFocused = ColorRgba.companion.tasksDropFocused.toColor()
 }
