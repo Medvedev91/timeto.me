615f141498265bfcf167d8cbd3f040598bcef8e1
hi@timeto.me <hi@timeto.me>
2025-05-25 17:59:45 +0300

    time.swift

--- ios_app/Common/apple_utils.swift
+++ ios_app/Common/apple_utils.swift
@@ -2,10 +2,6 @@ import shared
 
 let Cache = shared.Cache.shared
 
-func time() -> Int {
-    TimeKt.time().toInt()
-}
-
 func reportApi(_ message: String) {
     Utils_kmpKt.reportApi(message: message, force: false)
 }
---
+++ ios_app/Common/time.swift
@@ -0,0 +1,5 @@
+import shared
+
+func time() -> Int {
+    TimeKt.time().toInt()
+}
