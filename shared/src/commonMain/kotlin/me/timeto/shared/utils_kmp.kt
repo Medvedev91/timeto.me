33ddecd0ecb91c35076e69ac5bfb694da0d21fac
hi@timeto.me <hi@timeto.me>
2025-05-30 01:39:48 +0300

    utils_kmp.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
@@ -217,21 +217,3 @@ class TimerPickerItem(
         }
     }
 }
-
-class TimerTimeParser(
-    val seconds: Int,
-    val match: String,
-) {
-
-    companion object {
-
-        // Using IGNORE_CASE, not lowercase() to set to the "match" real string
-        private val regex = "\\d+\\s?min".toRegex(RegexOption.IGNORE_CASE)
-
-        fun parse(text: String): TimerTimeParser? {
-            val match = regex.find(text)?.value ?: return null
-            val seconds = match.filter { it.isDigit() }.toInt() * 60
-            return TimerTimeParser(seconds, match)
-        }
-    }
-}
