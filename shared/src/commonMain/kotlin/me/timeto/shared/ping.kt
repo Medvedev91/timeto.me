6fa8c2dfdadabd7283c9bb1172d7bb35c5ab0048
hi@timeto.me <hi@timeto.me>
2025-05-29 22:40:38 +0300

    ping.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/ping.kt
@@ -0,0 +1,59 @@
+package me.timeto.shared
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import me.timeto.shared.db.KvDb
+import me.timeto.shared.db.KvDb.Companion.isSendingReports
+import me.timeto.shared.misc.getString
+
+var pingLastDay: Int? = null
+
+suspend fun ping(
+    force: Boolean = false,
+) {
+    try {
+
+        val today = UnixTime().localDay
+
+        if (!force) {
+            if (!KvDb.KEY.IS_SENDING_REPORTS.selectOrNull().isSendingReports())
+                return
+            if (pingLastDay == today)
+                return
+        }
+
+        HttpClient().use { client ->
+            val httpResponse = client.get("https://api.timeto.me/ping") {
+                val token = KvDb.selectTokenOrNullSafe()
+                val password = getsertTokenPassword()
+                url {
+                    parameters.append("password", password)
+                    appendSystemInfo(token)
+                }
+            }
+            val plainJson = httpResponse.bodyAsText()
+            val j = Json.parseToJsonElement(plainJson).jsonObject
+            if (j.getString("status") != "success")
+                throw Exception("status != success\n$plainJson")
+            val jData = j.jsonObject["data"]!!.jsonObject
+            KvDb.KEY.TOKEN.upsertString(jData.getString("token"))
+            KvDb.KEY.FEEDBACK_SUBJECT.upsertString(jData.getString("feedback_subject"))
+            pingLastDay = today // After success
+        }
+    } catch (e: Throwable) {
+        reportApi("ping() exception:\n$e")
+    }
+}
+
+private suspend fun getsertTokenPassword(): String {
+    val oldPassword = KvDb.KEY.TOKEN_PASSWORD.selectStringOrNull()
+    if (oldPassword != null)
+        return oldPassword
+    val chars = ('0'..'9') + ('a'..'z') + ('A'..'Z') + ("!@#%^&*()_+".toList())
+    val newPassword = (1..15).map { chars.random() }.joinToString("")
+    KvDb.KEY.TOKEN_PASSWORD.upsertString(newPassword)
+    return newPassword
+}
--- shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
@@ -1,18 +1,11 @@
 package me.timeto.shared
 
-import io.ktor.client.HttpClient
 import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.datetime.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.jsonObject
 import me.timeto.shared.db.*
 import me.timeto.shared.db.KvDb.Companion.asDayStartOffsetSeconds
-import me.timeto.shared.db.KvDb.Companion.isSendingReports
-import me.timeto.shared.misc.getString
 import me.timeto.shared.misc.SystemInfo
 import me.timeto.shared.misc.ioScope
 import me.timeto.shared.misc.time
@@ -23,57 +16,6 @@ const val OPEN_SOURCE_URL = "https://github.com/Medvedev91/timeto.me"
 const val HI_EMAIL = "hi@timeto.me"
 
 const val prayEmoji = "🙏"
-
-//
-// Ping
-
-var pingLastDay: Int? = null
-suspend fun ping(
-    force: Boolean = false,
-) {
-    try {
-
-        val today = UnixTime().localDay
-
-        if (!force) {
-            if (!KvDb.KEY.IS_SENDING_REPORTS.selectOrNull().isSendingReports())
-                return
-            if (pingLastDay == today)
-                return
-        }
-
-        HttpClient().use { client ->
-            val httpResponse = client.get("https://api.timeto.me/ping") {
-                val token = KvDb.selectTokenOrNullSafe()
-                val password = getsertTokenPassword()
-                url {
-                    parameters.append("password", password)
-                    appendSystemInfo(token)
-                }
-            }
-            val plainJson = httpResponse.bodyAsText()
-            val j = Json.parseToJsonElement(plainJson).jsonObject
-            if (j.getString("status") != "success")
-                throw Exception("status != success\n$plainJson")
-            val jData = j.jsonObject["data"]!!.jsonObject
-            KvDb.KEY.TOKEN.upsertString(jData.getString("token"))
-            KvDb.KEY.FEEDBACK_SUBJECT.upsertString(jData.getString("feedback_subject"))
-            pingLastDay = today // After success
-        }
-    } catch (e: Throwable) {
-        reportApi("AppVM ping() exception:\n$e")
-    }
-}
-
-private suspend fun getsertTokenPassword(): String {
-    val oldPassword = KvDb.KEY.TOKEN_PASSWORD.selectStringOrNull()
-    if (oldPassword != null)
-        return oldPassword
-    val chars = ('0'..'9') + ('a'..'z') + ('A'..'Z') + ("!@#%^&*()_+".toList())
-    val newPassword = (1..15).map { chars.random() }.joinToString("")
-    KvDb.KEY.TOKEN_PASSWORD.upsertString(newPassword)
-    return newPassword
-}
 
 //
 
