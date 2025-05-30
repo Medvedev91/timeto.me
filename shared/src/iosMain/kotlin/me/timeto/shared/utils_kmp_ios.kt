85ed3f6723f7b76b29f1a013531826934d5dda99
hi@timeto.me <hi@timeto.me>
2025-05-30 11:48:44 +0300

    utils_kmp_ios.kt:

--- shared/src/iosMain/kotlin/me/timeto/shared/utils_kmp_ios.kt
+++ shared/src/iosMain/kotlin/me/timeto/shared/utils_kmp_ios.kt
@@ -3,18 +3,19 @@
 package me.timeto.shared
 
 import kotlinx.cinterop.*
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
 import me.timeto.appdbsq.TimetomeDB
 import platform.Foundation.*
 import platform.UIKit.UIDevice
 import me.timeto.shared.db.*
 import me.timeto.shared.misc.SystemInfo
+import me.timeto.shared.misc.combine
 
 fun initKmpIos() {
     val systemInfo = SystemInfo(
         build = (NSBundle.mainBundle.infoDictionary!!["CFBundleVersion"] as String).toInt(),
         version = NSBundle.mainBundle.infoDictionary!!["CFBundleShortVersionString"] as String,
-        os =  SystemInfo.Os.Ios(UIDevice.currentDevice.systemVersion),
+        os = SystemInfo.Os.Ios(UIDevice.currentDevice.systemVersion),
         device = machineIdentifier(),
         flavor = null,
     )
@@ -35,8 +36,7 @@ actual fun getResourceContent(file: String, type: String): String {
 
 private fun listenForSyncWatch() = launchExIo {
     initKmpDeferred.join()
-    // todo refactor by combine()
-    listOf<Flow<*>>(
+    combine(
         ActivityDb.anyChangeFlow(),
         NoteDb.anyChangeFlow(),
         TaskFolderDb.anyChangeFlow(),
@@ -45,14 +45,7 @@ private fun listenForSyncWatch() = launchExIo {
         ChecklistDb.anyChangeFlow(),
         ChecklistItemDb.anyChangeFlow(),
         ShortcutDb.anyChangeFlow(),
-    ).forEach { diFlow ->
-        var isFirst = true
-        diFlow.onEachExIn(this) {
-            if (isFirst) {
-                isFirst = false
-                return@onEachExIn
-            }
-            IosToWatchSync.syncWatch()
-        }
-    }
+    ) { _, _, _, _, _, _, _, _ ->
+        IosToWatchSync.syncWatch()
+    }.launchIn(this)
 }
