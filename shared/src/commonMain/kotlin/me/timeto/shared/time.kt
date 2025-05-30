a5ef36f171731b5b7431ab5b4fb8c6b9647941ec
hi@timeto.me <hi@timeto.me>
2025-05-30 23:53:50 +0300

    time.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/time.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/time.kt
@@ -1,9 +1,25 @@
 package me.timeto.shared
 
 import kotlinx.datetime.*
+import me.timeto.shared.db.KvDb
+import me.timeto.shared.db.KvDb.Companion.asDayStartOffsetSeconds
 
 fun time(): Int =
     Clock.System.now().epochSeconds.toInt()
 
 fun timeMls(): Long =
     Clock.System.now().toEpochMilliseconds()
+
+/**
+ * todo
+ * Store as a constant and update for performance. Now 3+
+ * functions are being called and objects are being created.
+ */
+val localUtcOffset: Int
+    get() = Clock.System.now().offsetIn(TimeZone.currentSystemDefault()).totalSeconds
+
+val localUtcOffsetWithDayStart: Int
+    get() = localUtcOffset - dayStartOffsetSeconds()
+
+fun dayStartOffsetSeconds(): Int =
+    KvDb.KEY.DAY_START_OFFSET_SECONDS.selectOrNullCached().asDayStartOffsetSeconds()
--- shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
@@ -2,9 +2,6 @@ package me.timeto.shared
 
 import io.ktor.client.request.HttpRequestBuilder
 import kotlinx.coroutines.flow.*
-import kotlinx.datetime.*
-import me.timeto.shared.db.*
-import me.timeto.shared.db.KvDb.Companion.asDayStartOffsetSeconds
 import me.timeto.shared.misc.SystemInfo
 
 fun HttpRequestBuilder.appendSystemInfo(
@@ -35,20 +32,3 @@ data class ScheduledNotificationData(
         BREAK, OVERDUE
     }
 }
-
-///
-/// Time
-
-/**
- * todo
- * Store as a constant and update for performance. Now 3+
- * functions are being called and objects are being created.
- */
-val localUtcOffset: Int
-    get() = Clock.System.now().offsetIn(TimeZone.currentSystemDefault()).totalSeconds
-
-val localUtcOffsetWithDayStart: Int
-    get() = localUtcOffset - dayStartOffsetSeconds()
-
-fun dayStartOffsetSeconds(): Int =
-    KvDb.KEY.DAY_START_OFFSET_SECONDS.selectOrNullCached().asDayStartOffsetSeconds()
