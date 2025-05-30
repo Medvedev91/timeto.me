840e67953ec3b4f3cddfad86db1489e82a5889a7
hi@timeto.me <hi@timeto.me>
2025-05-31 00:57:18 +0300

    urlAppendSystemInfo.kt:

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/urlAppendSystemInfo.kt
@@ -0,0 +1,17 @@
+package me.timeto.shared
+
+import io.ktor.client.request.HttpRequestBuilder
+import me.timeto.shared.misc.SystemInfo
+
+fun HttpRequestBuilder.urlAppendSystemInfo(
+    token: String?,
+) {
+    url {
+        val systemInfo = SystemInfo.instance
+        parameters.append("__token", token ?: "")
+        parameters.append("__build", systemInfo.build.toString())
+        parameters.append("__os", systemInfo.os.fullVersion)
+        parameters.append("__device", systemInfo.device)
+        parameters.append("__flavor", systemInfo.flavor ?: "")
+    }
+}
--- shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/utils_kmp.kt
@@ -1,21 +1,6 @@
 package me.timeto.shared
 
-import io.ktor.client.request.HttpRequestBuilder
 import kotlinx.coroutines.flow.*
-import me.timeto.shared.misc.SystemInfo
-
-fun HttpRequestBuilder.urlAppendSystemInfo(
-    token: String?,
-) {
-    url {
-        val systemInfo = SystemInfo.instance
-        parameters.append("__token", token ?: "")
-        parameters.append("__build", systemInfo.build.toString())
-        parameters.append("__os", systemInfo.os.fullVersion)
-        parameters.append("__device", systemInfo.device)
-        parameters.append("__flavor", systemInfo.flavor ?: "")
-    }
-}
 
 ///
 /// Notifications Flow
