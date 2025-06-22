ef4befb8f2fc4a71fd1ca2a08b79064ca65e01c4
hi@timeto.me <hi@timeto.me>
2025-06-22 22:10:25 +0300

    HomeSettingsButtonsFs.kt:

---
+++ android_app/src/main/java/me/timeto/app/ui/home/settings/HomeSettingsButtonsFs.kt
@@ -0,0 +1,49 @@
+package me.timeto.app.ui.home.settings
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import me.timeto.app.ui.H_PADDING
+import me.timeto.app.ui.Screen
+import me.timeto.app.ui.SpacerW1
+import me.timeto.app.ui.ZStack
+import me.timeto.app.ui.rememberVm
+import me.timeto.shared.vm.home.settings.buttons.HomeSettingsButtonsVm
+
+private val rowHeight: Dp = 26.dp
+private val barHeight: Dp = 24.dp
+private val spacing: Dp = 10.dp
+
+private val buttonsHPadding: Dp = H_PADDING
+
+@Composable
+fun HomeSettingsButtonsFs() {
+
+    val configuration = LocalConfiguration.current
+
+    val (vm, state) = rememberVm {
+        HomeSettingsButtonsVm(
+            spacing = spacing.value,
+            rowHeight = rowHeight.value,
+            width = (configuration.screenWidthDp.dp.value - (buttonsHPadding.value * 2)),
+        )
+    }
+
+    Screen {
+
+        SpacerW1()
+
+        ZStack(
+            modifier = Modifier
+                .padding(horizontal = buttonsHPadding)
+                .fillMaxWidth()
+                .height(state.height.dp),
+        ) {
+        }
+    }
+}
