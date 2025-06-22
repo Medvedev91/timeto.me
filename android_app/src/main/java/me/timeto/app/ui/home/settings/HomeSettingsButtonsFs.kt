01f1c82a98873ae30d3b37134b20f31e0dc23810
hi@timeto.me <hi@timeto.me>
2025-06-22 22:54:20 +0300

    HomeSettingsButtonsFs.kt:

--- android_app/src/main/java/me/timeto/app/ui/home/settings/HomeSettingsButtonsFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/home/settings/HomeSettingsButtonsFs.kt
@@ -1,13 +1,18 @@
 package me.timeto.app.ui.home.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import me.timeto.app.toColor
 import me.timeto.app.ui.H_PADDING
 import me.timeto.app.ui.H_PADDING_HALF
 import me.timeto.app.ui.Screen
@@ -23,7 +28,9 @@ import me.timeto.app.ui.header.HeaderCancelButton
 import me.timeto.app.ui.navigation.LocalNavigationFs
 import me.timeto.app.ui.navigation.LocalNavigationLayer
 import me.timeto.app.ui.rememberVm
+import me.timeto.app.ui.roundedShape
 import me.timeto.shared.vm.goals.form.GoalFormStrategy
+import me.timeto.shared.vm.home.settings.buttons.HomeSettingsButtonUi
 import me.timeto.shared.vm.home.settings.buttons.HomeSettingsButtonsVm
 
 private val rowHeight: Dp = 26.dp
@@ -77,6 +84,15 @@ fun HomeSettingsButtonsFs() {
                 .fillMaxWidth()
                 .height(state.height.dp),
         ) {
+
+            state.buttonsData.emptyButtonsUi.forEach { buttonUi ->
+                ButtonView(
+                    buttonUi = buttonUi,
+                    extraLeftWidth = 0f,
+                    extraRightWidth = 0f,
+                    content = {},
+                )
+            }
         }
 
         Footer(
@@ -108,5 +124,31 @@ fun HomeSettingsButtonsFs() {
             )
             SpacerW1()
         }
+    }
+}
+
+@Composable
+private fun ButtonView(
+    buttonUi: HomeSettingsButtonUi,
+    extraLeftWidth: Float,
+    extraRightWidth: Float,
+    content: @Composable () -> Unit,
+) {
+
+    ZStack(
+        modifier = Modifier
+            .size(
+                width = (buttonUi.fullWidth + extraLeftWidth + extraRightWidth).dp,
+                height = rowHeight,
+            )
+            .height(barHeight)
+            .offset(
+                x = (buttonUi.offsetX - extraLeftWidth).dp,
+                y = buttonUi.offsetY.dp,
+            )
+            .clip(roundedShape)
+            .background(buttonUi.colorRgba.toColor())
+    ) {
+        content()
     }
 }
