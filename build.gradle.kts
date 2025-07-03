9b04e7b16dbf35c61d779022bde8f706321e939f
hi@timeto.me <hi@timeto.me>
2025-07-03 04:30:00 +0300

    Up Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.11.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
     }
 }
