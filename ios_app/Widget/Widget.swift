6cf8fc6f2375ed0a318a1dd147a63c712ce3fc0a
hi@timeto.me <hi@timeto.me>
2025-07-27 19:24:22 +0300

    Widget.swift:

--- ios_app/Widget/Widget.swift
+++ ios_app/Widget/Widget.swift
@@ -3,80 +3,7 @@ import WidgetKit
 import SwiftUI
 import shared
 
-struct Provider: TimelineProvider {
-    
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
-    }
-    
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
-        completion(entry)
-    }
-    
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-        
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-        
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-    
-    //    func relevances() async -> WidgetRelevances<Void> {
-    //        // Generate a list containing the contexts this widget is relevant in.
-    //    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let emoji: String
-}
-
-struct WidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        VStack {
-            Text("Time -")
-            
-            Text("00")
-                .hidden()
-                .overlay(alignment: .leading) {
-                    Text(entry.date, style: .timer)
-                }
-            
-            Text(entry.date, style: .timer)
-//                .textAlign(.center)
-                .frame(minWidth: 0, idealWidth: 0, maxWidth: 10, alignment: .center)
-                .lineLimit(1)
-                .background(.red)
-
-            Text("Emoji - \(Int(Date().timeIntervalSince1970) % 60)")
-            Text(entry.emoji)
-        }
-    }
-}
-
-
-struct Widget2: SwiftUI.Widget {
-    
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: "widget", provider: Provider()) { entry in
-            WidgetEntryView(entry: entry)
-                .containerBackground(.black, for: .widget)
-        }
-    }
-}
-
 struct Widget: SwiftUI.Widget {
-    
     
     var body: some WidgetConfiguration {
         
@@ -85,6 +12,7 @@ struct Widget: SwiftUI.Widget {
             content: { context in
                 HStack {
                     
+                    // todo
                     Button("Report") {
                         reportApi("test")
                     }
@@ -113,6 +41,7 @@ struct Widget: SwiftUI.Widget {
                     expanded: {
                         DynamicIslandExpandedRegion(.center) {
                             VStack {
+                                // todo
 //                                Tmp(state: context.state)
                                 Button("report") {
                                     reportApi("tap")
