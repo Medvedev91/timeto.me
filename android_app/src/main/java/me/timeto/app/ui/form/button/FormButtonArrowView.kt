ea157d542d3e4524b7b793a1b62ebb922d44bb01
hi@timeto.me <hi@timeto.me>
2025-08-07 20:30:55 +0300

    FormButtonArrowView(color)

--- android_app/src/main/java/me/timeto/app/ui/form/button/FormButtonArrowView.kt
+++ android_app/src/main/java/me/timeto/app/ui/form/button/FormButtonArrowView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import me.timeto.app.ui.H_PADDING
@@ -14,11 +15,13 @@ import me.timeto.app.ui.c
 import me.timeto.app.ui.halfDpFloor
 
 @Composable
-fun FormButtonArrowView() {
+fun FormButtonArrowView(
+    color: Color? = null,
+) {
     Icon(
         painter = painterResource(id = R.drawable.sf_chevron_right_medium_medium),
         contentDescription = "Expand",
-        tint = c.tertiaryText,
+        tint = color ?: c.tertiaryText,
         modifier = Modifier
             .offset(y = -halfDpFloor)
             .padding(end = H_PADDING - 2.dp - halfDpFloor)
