0bfb79612bbdb7759ad9a510a66a143e724e2573
hi@timeto.me <hi@timeto.me>
2025-08-09 13:04:06 +0300

    Build Version to 2025.08.09.581

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 580
-        versionName = "2025.06.26"
+        versionCode = 581
+        versionName = "2025.08.09"
     }
 
     buildTypes {
