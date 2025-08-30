7642d1df73ced40329b211d14b3ccbd8edc136c0
hi@timeto.me <hi@timeto.me>
2025-08-30 15:56:09 +0300

    ActivityDb.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/db/ActivityDb.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/db/ActivityDb.kt
@@ -124,7 +124,6 @@ data class ActivityDb(
 
         ///
 
-        // todo sync with fillInitData()
         // todo apple colors
         // attractiveness. In fillInitData() hardcode by indexes.
         // https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/color
