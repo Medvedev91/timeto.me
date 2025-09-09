031911f9d14884021205e86a667309af74224b0c
hi@timeto.me <hi@timeto.me>
2025-09-10 02:36:19 +0300

    README.md:

--- ios_app/iphone/UI/Readme/Readme2FullScreen.swift
+++ ios_app/iphone/UI/Readme/Readme2FullScreen.swift
@@ -20,7 +20,7 @@ struct Readme2FullScreen: View {
             TextView(text: Text("Timer is running ") +
                      Text("all the time").underline() +
                      Text(", 24/7, even for sleep or breakfast. ") +
-                     Text("No stop option").underline() +
+                     Text("There is no stop option").underline() +
                      Text("! To stop the current goal, you have to start the next one.")
             )
             
