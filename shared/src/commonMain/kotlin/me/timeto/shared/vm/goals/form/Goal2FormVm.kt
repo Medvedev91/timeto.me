8a1ac212cb991541366ad0e08374efef4c4204a2
hi@timeto.me <hi@timeto.me>
2025-09-25 16:23:32 +0300

    Goal2FormVm.State.secondsPickerItemsUi

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/goals/form/Goal2FormVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/goals/form/Goal2FormVm.kt
@@ -15,6 +15,7 @@ class Goal2FormVm(
         val initGoalDb: Goal2Db?,
         val name: String,
         val seconds: Int,
+        val secondsPickerItemsUi: List<SecondsPickerItemUi>,
     ) {
 
         val title: String =
@@ -36,11 +37,13 @@ class Goal2FormVm(
 
     init {
         val tf = (initGoalDb?.name ?: "").textFeatures()
+        val seconds: Int = initGoalDb?.seconds ?: 3_600
         state = MutableStateFlow(
             State(
                 initGoalDb = initGoalDb,
                 name = tf.textNoFeatures,
-                seconds = initGoalDb?.seconds ?: 3_600
+                seconds = seconds,
+                secondsPickerItemsUi = buildSecondsPickerItems(defSeconds = seconds),
             )
         )
     }
@@ -53,5 +56,40 @@ class Goal2FormVm(
 
     fun setSeconds(newSeconds: Int) {
         state.update { it.copy(seconds = newSeconds) }
+    }
+
+    ///
+
+    data class SecondsPickerItemUi(
+        val title: String,
+        val seconds: Int,
+    )
+}
+
+private fun buildSecondsPickerItems(
+    defSeconds: Int,
+): List<Goal2FormVm.SecondsPickerItemUi> {
+
+    val a: List<Int> =
+        (1..10).map { it * 60 } + // 1 - 10 min by 1 min
+                (1..10).map { (600 + (it * 300)) } + // 15 min - 1 hour by 5 min
+                (1..138).map { (3_600 + (it * 600)) } + // 1 hour + by 10 min
+                defSeconds
+
+    return a.toSet().sorted().map { seconds ->
+
+        val hours: Int = seconds / 3600
+        val minutes: Int = (seconds % 3600) / 60
+
+        val title: String = when {
+            hours == 0 -> "$minutes min"
+            minutes == 0 -> "$hours h"
+            else -> "$hours : ${minutes.toString().padStart(2, '0')}"
+        }
+
+        Goal2FormVm.SecondsPickerItemUi(
+            title = title,
+            seconds = seconds,
+        )
     }
 }
