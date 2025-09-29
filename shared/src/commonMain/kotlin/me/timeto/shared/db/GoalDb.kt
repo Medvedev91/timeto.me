206a0aae7a2fdd5d77560cceea4d58e0cec71e95
hi@timeto.me <hi@timeto.me>
2025-09-29 16:19:04 +0300

    Clean

--- shared/src/commonMain/kotlin/me/timeto/shared/db/GoalDb.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/db/GoalDb.kt
@@ -1,18 +1,13 @@
 package me.timeto.shared.db
 
-import app.cash.sqldelight.coroutines.asFlow
 import dbsq.GoalSq
 import kotlinx.coroutines.flow.Flow
 import kotlinx.serialization.json.*
-import me.timeto.shared.*
 import me.timeto.shared.backups.Backupable__Holder
 import me.timeto.shared.backups.Backupable__Item
 import me.timeto.shared.getInt
 import me.timeto.shared.getString
 import me.timeto.shared.toJsonArray
-import me.timeto.shared.UiException
-import me.timeto.shared.vm.goals.form.GoalFormData
-import me.timeto.shared.vm.home.buttons.homeButtonsCellsCount
 
 data class GoalDb(
     val id: Int,
@@ -28,12 +23,6 @@ data class GoalDb(
 
     companion object : Backupable__Holder {
 
-        fun anyChangeFlow(): Flow<*> =
-            db.goalQueries.anyChange().asFlow()
-
-        fun selectLastInsertedIdSync(): Int =
-            db.goalQueries.selectLastInsertId().executeAsOne().toInt()
-
         suspend fun selectAll(): List<GoalDb> = dbIo {
             selectAllSync()
         }
@@ -43,37 +32,6 @@ data class GoalDb(
 
         fun selectAllFlow(): Flow<List<GoalDb>> =
             db.goalQueries.selectAll().asListFlow { toDb() }
-
-        fun insertSync(
-            activityDb: ActivityDb,
-            goalFormData: GoalFormData,
-        ) {
-            db.goalQueries.insert(
-                activity_id = activityDb.id,
-                seconds = goalFormData.seconds,
-                period_json = goalFormData.period.toJson().toString(),
-                note = goalFormData.note.trim(),
-                finish_text = goalFormData.finishText.trim(),
-                home_button_sort = HomeButtonSort(rowIdx = 999, cellIdx = 0, size = homeButtonsCellsCount).string,
-                is_entire_activity = goalFormData.isEntireActivity.toInt10(),
-                timer = goalFormData.timer,
-            )
-        }
-
-        suspend fun insertAndGet(
-            activityDb: ActivityDb,
-            goalFormData: GoalFormData,
-        ): GoalDb = dbIo {
-            db.transactionWithResult {
-                insertSync(activityDb, goalFormData)
-                val lastId: Int = selectLastInsertedIdSync()
-                selectAllSync().first { it.id == lastId }
-            }
-        }
-
-        fun deleteByActivityDbSync(activityDb: ActivityDb) {
-            db.goalQueries.deleteByActivityId(activity_id = activityDb.id)
-        }
 
         //
         // Backupable Holder
@@ -98,75 +56,6 @@ data class GoalDb(
             )
         }
     }
-
-    ///
-
-    val isEntireActivity: Boolean =
-        is_entire_activity.toBoolean10()
-
-    suspend fun startInterval(
-        barsGoalStats: DayBarsUi.GoalStats,
-    ): IntervalDb {
-        val goalDb = this
-
-        val timer: Int =
-            barsGoalStats.calcTimer()
-
-        val noteTf: TextFeatures =
-            goalDb.note.textFeatures().copy(goalDb = goalDb)
-
-        TaskDb.selectAsc()
-            .filter { taskDb ->
-                val tf = taskDb.text.textFeatures()
-                taskDb.isToday && (tf.paused != null) && (tf.goalDb?.id == goalDb.id)
-            }
-            .forEach { taskDb ->
-                taskDb.delete()
-            }
-
-        return IntervalDb.insertWithValidation(
-            timer = timer,
-            activityDb = goalDb.getActivityDbCached(),
-            note = noteTf.textWithFeatures(),
-        )
-    }
-
-    suspend fun updateHomeButtonSort(
-        homeButtonSort: HomeButtonSort,
-    ): Unit = dbIo {
-        db.goalQueries.updateHomeButtonSortById(
-            home_button_sort = homeButtonSort.string,
-            id = id,
-        )
-    }
-
-    suspend fun update(
-        goalFormData: GoalFormData,
-    ): Unit = dbIo {
-        updateSync(goalFormData)
-    }
-
-    fun updateSync(
-        goalFormData: GoalFormData,
-    ) {
-        db.goalQueries.updateById(
-            activity_id = activity_id,
-            seconds = goalFormData.seconds,
-            period_json = goalFormData.period.toJson().toString(),
-            note = goalFormData.note.trim(),
-            finish_text = goalFormData.finishText.trim(),
-            home_button_sort = home_button_sort,
-            is_entire_activity = goalFormData.isEntireActivity.toInt10(),
-            timer = goalFormData.timer,
-            id = id,
-        )
-    }
-
-    fun buildPeriod(): Period =
-        Period.fromJson(Json.parseToJsonElement(period_json).jsonObject)
-
-    fun getActivityDbCached(): ActivityDb =
-        Cache.getActivityDbByIdOrNull(activity_id)!!
 
     suspend fun delete(): Unit = dbIo {
         deleteSync()
@@ -204,96 +93,6 @@ data class GoalDb(
 
     override fun backupable__delete() {
         db.goalQueries.deleteById(id)
-    }
-
-    ///
-
-    sealed interface Period {
-
-        val type: Type
-
-        fun isToday(): Boolean
-
-        fun note(): String
-
-        fun toJson(): JsonObject
-
-        ///
-
-        enum class Type(val id: Int) {
-            daysOfWeek(1), weekly(2),
-        }
-
-        companion object {
-
-            fun fromJson(json: JsonObject): Period {
-                val typeRaw: Int = json["type"]!!.jsonPrimitive.int
-                return when (typeRaw) {
-                    Type.daysOfWeek.id -> DaysOfWeek.fromJson(json)
-                    Type.weekly.id -> Weekly()
-                    else -> throw Exception("GoalDb.Period.fromJson() type: $typeRaw")
-                }
-            }
-        }
-
-        ///
-
-        class DaysOfWeek(
-            val days: Set<Int>,
-        ) : Period {
-
-            companion object {
-
-                fun fromJson(json: JsonObject) = DaysOfWeek(
-                    days = json["days"]!!.jsonArray.map { it.jsonPrimitive.int }.toSet(),
-                )
-
-                @Throws(UiException::class)
-                fun buildWithValidation(days: Set<Int>): DaysOfWeek {
-                    if (days.isEmpty())
-                        throw UiException("Days not selected")
-                    if (days.any { it !in 0..6 })
-                        throw UiException("Invalid days: $days")
-                    return DaysOfWeek(days)
-                }
-            }
-
-            ///
-
-            override val type = Type.daysOfWeek
-
-            override fun isToday(): Boolean =
-                UnixTime().dayOfWeek() in days
-
-            override fun note(): String {
-                if (days.size == 7)
-                    return "Every Day"
-                // todo if size is zero?
-                return days.sorted().joinToString(", ") { UnixTime.dayOfWeekNames2[it] }
-            }
-
-            override fun toJson() = JsonObject(
-                mapOf(
-                    "type" to JsonPrimitive(type.id),
-                    "days" to JsonArray(days.map { JsonPrimitive(it) }),
-                )
-            )
-        }
-
-        class Weekly() : Period {
-
-            override val type = Type.weekly
-
-            override fun isToday(): Boolean = true
-
-            override fun note(): String = "Weekly"
-
-            override fun toJson() = JsonObject(
-                mapOf(
-                    "type" to JsonPrimitive(type.id),
-                )
-            )
-        }
     }
 }
 
