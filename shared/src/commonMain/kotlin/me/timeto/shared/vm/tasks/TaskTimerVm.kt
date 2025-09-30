ff2aed84e1560a09f6472277387a833da8a0d7e0
hi@timeto.me <hi@timeto.me>
2025-09-30 20:24:31 +0300

    TaskTimerVm.kt:

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/tasks/TaskTimerVm.kt
@@ -0,0 +1,72 @@
+package me.timeto.shared.vm.tasks
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import me.timeto.shared.Cache
+import me.timeto.shared.DayBarsUi
+import me.timeto.shared.db.Goal2Db
+import me.timeto.shared.db.TaskDb
+import me.timeto.shared.launchExIo
+import me.timeto.shared.textFeatures
+import me.timeto.shared.toTimerHintNote
+import me.timeto.shared.vm.Vm
+
+class TaskTimerVm(
+    taskDb: TaskDb,
+) : Vm<TaskTimerVm.State>() {
+
+    data class State(
+        val goalsUi: List<GoalUi>
+    )
+
+    override val state = MutableStateFlow(
+        State(
+            goalsUi = Cache.goals2Db.map { goalDb ->
+                GoalUi(
+                    goalDb = goalDb,
+                    taskDb = taskDb,
+                )
+            },
+        )
+    )
+
+
+    ///
+
+    class GoalUi(
+        val goalDb: Goal2Db,
+        val taskDb: TaskDb,
+    ) {
+
+        val text: String =
+            goalDb.name.textFeatures().textUi()
+
+        val timerHintsUi: List<TimerHintUi> =
+            listOf(5 * 60, 15 * 60, 45 * 60).map { seconds ->
+                TimerHintUi(
+                    seconds = seconds,
+                    onTap = {
+                        launchExIo {
+                            taskDb.startInterval(
+                                timer = seconds,
+                                goalDb = goalDb,
+                            )
+                        }
+                    },
+                )
+            }
+
+        fun onTap() {
+            launchExIo {
+                goalDb.startInterval(DayBarsUi.buildToday().buildGoalStats(goalDb))
+            }
+        }
+    }
+
+    class TimerHintUi(
+        val seconds: Int,
+        val onTap: () -> Unit,
+    ) {
+        val title: String =
+            seconds.toTimerHintNote(isShort = true)
+    }
+}
