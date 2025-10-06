764cd3f7609b35093d8b96763db5d7978c6546eb
hi@timeto.me <hi@timeto.me>
2025-10-06 21:25:50 +0300

    Goal2FormFs.kt:

--- android_app/src/main/java/me/timeto/app/ui/goals/form/Goal2FormFs.kt
+++ android_app/src/main/java/me/timeto/app/ui/goals/form/Goal2FormFs.kt
@@ -7,7 +7,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import me.timeto.app.ui.Screen
+import me.timeto.app.ui.checklists.ChecklistsPickerFs
 import me.timeto.app.ui.form.FormInput
+import me.timeto.app.ui.form.button.FormButton
+import me.timeto.app.ui.form.padding.FormPaddingSectionSection
 import me.timeto.app.ui.form.padding.FormPaddingTop
 import me.timeto.app.ui.header.Header
 import me.timeto.app.ui.header.HeaderActionButton
@@ -15,6 +18,8 @@ import me.timeto.app.ui.header.HeaderCancelButton
 import me.timeto.app.ui.navigation.LocalNavigationFs
 import me.timeto.app.ui.navigation.LocalNavigationLayer
 import me.timeto.app.ui.rememberVm
+import me.timeto.app.ui.shortcuts.ShortcutsPickerFs
+import me.timeto.app.ui.timer.TimerSheet
 import me.timeto.shared.db.Goal2Db
 import me.timeto.shared.vm.goals.form.Goal2FormVm
 
@@ -82,6 +87,84 @@ fun Goal2FormFs(
                     isLast = false,
                     isAutoFocus = goalDb == null,
                     imeAction = ImeAction.Done,
+                )
+
+                FormButton(
+                    title = state.secondsTitle,
+                    isFirst = false,
+                    isLast = true,
+                    note = state.secondsNote,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            TimerSheet(
+                                title = state.secondsTitle,
+                                doneTitle = "Done",
+                                initSeconds = state.seconds,
+                                onDone = { newSeconds ->
+                                    vm.setSeconds(newSeconds = newSeconds)
+                                },
+                            )
+                        }
+                    },
+                )
+
+                FormPaddingSectionSection()
+
+                FormButton(
+                    title = "Checklists",
+                    isFirst = true,
+                    isLast = false,
+                    note = state.checklistsNote,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            ChecklistsPickerFs(
+                                initChecklistsDb = state.checklistsDb,
+                                onDone = { newChecklistsDb ->
+                                    vm.setChecklistsDb(newChecklistsDb)
+                                },
+                            )
+                        }
+                    },
+                )
+
+                FormButton(
+                    title = "Shortcuts",
+                    isFirst = false,
+                    isLast = true,
+                    note = state.shortcutsNote,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            ShortcutsPickerFs(
+                                initShortcutsDb = state.shortcutsDb,
+                                onDone = { newShortcutsDb ->
+                                    vm.setShortcutsDb(newShortcutsDb)
+                                },
+                            )
+                        }
+                    },
+                )
+
+                FormPaddingSectionSection()
+
+                FormButton(
+                    title = state.periodTitle,
+                    isFirst = true,
+                    isLast = true,
+                    note = state.periodNote,
+                    withArrow = true,
+                    onClick = {
+                        navigationFs.push {
+                            GoalFormPeriodFs(
+                                initGoalDbPeriod = state.period,
+                                onDone = { newPeriod ->
+                                    vm.setPeriod(newPeriod = newPeriod)
+                                },
+                            )
+                        }
+                    },
                 )
             }
         }
