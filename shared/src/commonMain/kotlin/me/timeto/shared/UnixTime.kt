9f3dd1dc1b320e94d62d4228c5b6c4de3216c504
hi@timeto.me <hi@timeto.me>
2025-10-13 09:28:29 +0300

    UnixTime.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/UnixTime.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/UnixTime.kt
@@ -70,8 +70,8 @@ data class UnixTime(
     fun lastUnixDayOfMonth(): UnixTime {
         val year = year()
         val lastDayOfMonth = when (month()) {
-            in setOf(4, 6, 9, 11) -> 30
-            in setOf(1, 3, 5, 7, 8, 10, 12) -> 31
+            4, 6, 9, 11 -> 30
+            1, 3, 5, 7, 8, 10, 12 -> 31
             2 -> when {
                 (year % 400) == 0 -> 29
                 (year % 100) == 0 -> 28
