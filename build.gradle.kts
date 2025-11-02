b293d960edd37f11f35a56bbd383445fb615225b
hi@timeto.me <hi@timeto.me>
2025-11-02 21:15:49 +0300

    org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.10.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
     }
 }
 
