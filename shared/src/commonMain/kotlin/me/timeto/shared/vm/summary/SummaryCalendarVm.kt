dca011be44e5c60c8640084b09511290c116e414
hi@timeto.me <hi@timeto.me>
2025-11-13 06:17:34 +0300

    SummaryCalendarVm.kt

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/summary/SummaryCalendarVm.kt
@@ -0,0 +1,75 @@
+package me.timeto.shared.vm.summary
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import me.timeto.shared.Cache
+import me.timeto.shared.UnixTime
+import me.timeto.shared.vm.Vm
+
+class SummaryCalendarVm : Vm<SummaryCalendarVm.State>() {
+
+    data class State(
+        val weeksUi: List<WeekUi>,
+    )
+
+    override val state = MutableStateFlow(
+        State(
+            weeksUi = buildCalendar()
+        )
+    )
+
+    data class DayUi(
+        val timeStart: UnixTime,
+        val isFirstDay: Boolean,
+    ) {
+        val title: String
+        val subtitle: String?
+
+        init {
+            val day: Int = timeStart.dayOfMonth()
+            title = day.toString()
+            subtitle = if (isFirstDay || day == 1) {
+                val year: Int = timeStart.year()
+                val yearNow: Int = UnixTime().year()
+                val yearString: String = if (yearNow != year) " ${year.toString().takeLast(2)}" else ""
+                timeStart.getStringByComponents(UnixTime.StringComponent.month3) + yearString
+            }
+            else null
+        }
+    }
+
+    data class WeekUi(
+        val daysUi: List<DayUi?>,
+    ) {
+        val id: Int = daysUi.first { it != null }!!.timeStart.time
+    }
+}
+
+private fun buildCalendar(): List<SummaryCalendarVm.WeekUi> {
+    val resList = mutableListOf<SummaryCalendarVm.WeekUi>()
+    val timeStart = UnixTime(Cache.firstIntervalDb.id)
+    val timeFinish = UnixTime()
+    val curWeekDaysUi: MutableList<SummaryCalendarVm.DayUi?> =
+        (0 until timeStart.dayOfWeek()).map { null }.toMutableList()
+    (timeStart.localDay..timeFinish.localDay).forEach { day ->
+        val unixDay = UnixTime.byLocalDay(day)
+        curWeekDaysUi.add(
+            SummaryCalendarVm.DayUi(
+                timeStart = unixDay,
+                isFirstDay = (day == timeStart.localDay)
+            )
+        )
+        // Sunday
+        if (unixDay.dayOfWeek() == 6) {
+            resList.add(SummaryCalendarVm.WeekUi(curWeekDaysUi.toList()))
+            curWeekDaysUi.clear()
+            return@forEach
+        }
+        // End of Calendar
+        if (timeFinish.localDay == day) {
+            curWeekDaysUi.addAll(((timeFinish.dayOfWeek() + 1)..6).map { null })
+            resList.add(SummaryCalendarVm.WeekUi(curWeekDaysUi.toList()))
+            return@forEach
+        }
+    }
+    return resList
+}
