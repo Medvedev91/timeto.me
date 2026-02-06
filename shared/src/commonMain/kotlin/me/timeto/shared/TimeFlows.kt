220d3eb8ce8cb34376da73c28e235d06ef85da0d
hi@timeto.me <hi@timeto.me>
2026-02-06 15:11:54 +0300

    TimeFlows.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/TimeFlows.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/TimeFlows.kt
@@ -1,7 +1,15 @@
 package me.timeto.shared
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.offsetIn
+import me.timeto.shared.db.KvDb
+import me.timeto.shared.db.KvDb.Companion.asDayStartOffsetSeconds
 
 object TimeFlows {
 
@@ -17,11 +25,33 @@ object TimeFlows {
         }
     }
 
-    ///
-
-    private fun calcLastMinuteTime(): Int {
-        val now = time()
-        val rem = now % 60
-        return now - rem
+    fun buildTodayWithDayStartOffsetFlow(): Flow<Int> = combine(
+        KvDb.KEY.DAY_START_OFFSET_SECONDS.selectOrNullFlow().distinctUntilChanged(),
+        eachMinuteSecondsFlow,
+    ) { dayStartOffsetSecondsKvDb, nowTime ->
+        calcDayWithDayStartOffset(
+            time = nowTime,
+            dayStartOffsetSeconds = dayStartOffsetSecondsKvDb.asDayStartOffsetSeconds(),
+        )
     }
+}
+
+private fun calcLastMinuteTime(): Int {
+    val now = time()
+    val rem = now % 60
+    return now - rem
+}
+
+private fun calcDayWithDayStartOffset(
+    time: Int,
+    dayStartOffsetSeconds: Int,
+): Int {
+    val localUtcOffset: Int =
+        Clock.System.now().offsetIn(TimeZone.currentSystemDefault()).totalSeconds
+    val localUtcOffsetWithDayStart: Int =
+        localUtcOffset - dayStartOffsetSeconds
+    return UnixTime(
+        time = time,
+        utcOffset = localUtcOffsetWithDayStart,
+    ).localDay
 }
