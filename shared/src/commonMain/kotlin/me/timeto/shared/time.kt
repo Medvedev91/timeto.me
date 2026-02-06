1e84f219f14dc5f6d5d6a1fb612d1d4486675562
hi@timeto.me <hi@timeto.me>
2026-02-06 17:55:26 +0300

    time.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/time.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/time.kt
@@ -1,8 +1,6 @@
 package me.timeto.shared
 
 import kotlinx.datetime.*
-import me.timeto.shared.db.KvDb
-import me.timeto.shared.db.KvDb.Companion.asDayStartOffsetSeconds
 
 fun time(): Int =
     Clock.System.now().epochSeconds.toInt()
@@ -17,9 +15,3 @@ fun timeMls(): Long =
  */
 val localUtcOffset: Int
     get() = Clock.System.now().offsetIn(TimeZone.currentSystemDefault()).totalSeconds
-
-val localUtcOffsetWithDayStart: Int
-    get() = localUtcOffset - dayStartOffsetSeconds()
-
-fun dayStartOffsetSeconds(): Int =
-    KvDb.KEY.DAY_START_OFFSET_SECONDS.selectOrNullCached().asDayStartOffsetSeconds()
