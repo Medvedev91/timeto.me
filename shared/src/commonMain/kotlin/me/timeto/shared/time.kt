f48fbffeb79ccefd2d3ddd8b58a436c75a573b83
hi@timeto.me <hi@timeto.me>
2026-02-07 11:58:02 +0300

    time.kt: Clean

--- shared/src/commonMain/kotlin/me/timeto/shared/time.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/time.kt
@@ -1,16 +1,6 @@
 package me.timeto.shared
 
 import kotlinx.datetime.Clock
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.offsetIn
 
 fun time(): Int =
     Clock.System.now().epochSeconds.toInt()
-
-/**
- * todo
- * Store as a constant and update for performance. Now 3+
- * functions are being called and objects are being created.
- */
-val localUtcOffset: Int
-    get() = Clock.System.now().offsetIn(TimeZone.currentSystemDefault()).totalSeconds
