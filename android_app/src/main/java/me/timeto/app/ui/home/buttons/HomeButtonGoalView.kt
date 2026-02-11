6ac42bd767d00ff6e41ee690784b3ba4169daf9d
hi@timeto.me <hi@timeto.me>
2026-02-11 15:17:07 +0300

    HomeButtonGoalView.kt:

--- android_app/src/main/java/me/timeto/app/ui/home/buttons/HomeButtonGoalView.kt
+++ android_app/src/main/java/me/timeto/app/ui/home/buttons/HomeButtonGoalView.kt
@@ -1,5 +1,6 @@
 package me.timeto.app.ui.home.buttons
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
@@ -7,13 +8,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import me.timeto.app.R
 import me.timeto.app.toColor
 import me.timeto.app.ui.HStack
 import me.timeto.app.ui.ZStack
@@ -154,36 +162,54 @@ fun HomeButtonGoalView(
                 ),
         ) {
 
+            val goalColor: Color =
+                goal.bgColor.toColor()
+
+            val progressRatioAnimate =
+                animateFloatAsState(if (goal.isCompletedAsChecklist) 1f else goal.progressRatio)
+
             ZStack(
                 modifier = Modifier
                     .fillMaxHeight()
-                    .fillMaxWidth(goal.progressRatio)
-                    .background(goal.bgColor.toColor())
+                    .fillMaxWidth(progressRatioAnimate.value)
+                    .background(goalColor)
                     .clip(roundedShape)
                     .align(Alignment.CenterStart),
             )
 
-            Text(
-                text = goal.leftText,
+            HStack(
                 modifier = Modifier
-                    .padding(start = HomeScreen__itemCircleHPadding)
                     .align(Alignment.CenterStart),
-                color = c.white,
-                fontSize = HomeScreen__itemCircleFontSize,
-                fontWeight = HomeScreen__itemCircleFontWeight,
-                lineHeight = 18.sp,
-            )
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
 
-            Text(
-                text = goal.rightText,
-                modifier = Modifier
-                    .padding(end = HomeScreen__itemCircleHPadding)
-                    .align(Alignment.CenterEnd),
-                color = c.white,
-                fontSize = HomeScreen__itemCircleFontSize,
-                fontWeight = HomeScreen__itemCircleFontWeight,
-                lineHeight = 18.sp,
-            )
+                Text(
+                    text = goal.leftText,
+                    modifier = Modifier
+                        .padding(start = HomeScreen__itemCircleHPadding)
+                        .weight(1f),
+                    color = c.white,
+                    fontSize = HomeScreen__itemCircleFontSize,
+                    fontWeight = HomeScreen__itemCircleFontWeight,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip,
+                    lineHeight = 18.sp,
+                )
+
+                Text(
+                    text = goal.rightText,
+                    modifier = Modifier
+                        .padding(end = if (goal.isCompletedAsChecklist) 0.dp else HomeScreen__itemCircleHPadding),
+                    color = c.white,
+                    fontSize = HomeScreen__itemCircleFontSize,
+                    fontWeight = HomeScreen__itemCircleFontWeight,
+                    lineHeight = 18.sp,
+                )
+
+                if (goal.isCompletedAsChecklist) {
+                    ChecklistIconView(goalColor)
+                }
+            }
         }
     }
 }
@@ -263,4 +289,27 @@ private sealed class ContextPickerItemType {
     object UntilTime : ContextPickerItemType()
     object RestOfGoal : ContextPickerItemType()
     object HomeScreenSettings : ContextPickerItemType()
+}
+
+
+@Composable
+private fun ChecklistIconView(
+    color: Color,
+) {
+    ZStack(
+        modifier = Modifier
+            .size(HomeScreen__itemCircleHeight)
+            .padding(3.dp)
+            .clip(roundedShape)
+            .background(c.white),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            painterResource(id = R.drawable.sf_checkmark_medium_semibold),
+            contentDescription = "Checklist completed",
+            tint = color,
+            modifier = Modifier
+                .size(8.dp),
+        )
+    }
 }
