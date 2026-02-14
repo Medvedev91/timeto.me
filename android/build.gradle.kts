f80863c9b740487e779a4ce31508e2439d0a4f16
Ivan Medvedev <ivan@timeto.me>
2026-02-14 15:10:41 +0300

    Bump version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "me.timeto.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 603
-        versionName = "2026.02.09"
+        versionCode = 607
+        versionName = "2026.02.15"
     }
 
     buildTypes {
