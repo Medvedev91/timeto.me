be98358f121403d416495771b33a337b3db1fb03
Ivan Medvedev <ivan@timeto.me>
2026-03-05 19:44:44 +0300

    widgetTimerRange.swift:

--- apple/Widget/widgetTimerRange.swift
+++ apple/Widget/widgetTimerRange.swift
@@ -2,8 +2,13 @@ import Foundation
 
 extension Date {
     
-    func widgetTimerRange() -> ClosedRange<Date> {
+    func widgetTimerRange(
+        isCountUpOrDown: Bool,
+    ) -> ClosedRange<Date> {
         let now = Date.now
+        if isCountUpOrDown {
+            return self...now.inSeconds(999_999)
+        }
         return now...(self > now ? self : now)
     }
 }
