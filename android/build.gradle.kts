f1d13da7a922ef32e0eab9d19f8e16640ef2f6e3
Ivan Medvedev <ivan@timeto.me>
2026-04-11 16:24:38 +0300

    build.gradle.kts:

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("android")
     id("com.android.application")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20"
 }
 
 android {
