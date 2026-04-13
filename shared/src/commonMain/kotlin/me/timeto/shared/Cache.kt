d590d481e96ea776bbf59e6ff4fe4d7096c72d8a
Ivan Medvedev <ivan@timeto.me>
2026-04-13 15:12:34 +0300

    Cache.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/Cache.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/Cache.kt
@@ -15,7 +15,6 @@ object Cache {
     var eventsDb = listOf<EventDb>()
     var eventTemplatesDbSorted = listOf<EventTemplateDb>()
     var repeatingsDb = listOf<RepeatingDb>()
-    var goals2Db = listOf<Goal2Db>()
     var activitiesDb = listOf<ActivityDb>()
 
     lateinit var firstIntervalDb: IntervalDb
@@ -75,9 +74,6 @@ object Cache {
 
         repeatingsDb = RepeatingDb.selectAsc()
         RepeatingDb.selectAscFlow().onEachExIn(scope) { repeatingsDb = it }
-
-        goals2Db = Goal2Db.selectAll()
-        Goal2Db.selectAllFlow().onEachExIn(scope) { goals2Db = it }
 
         activitiesDb = ActivityDb.selectAll()
         ActivityDb.selectAllFlow().onEachExIn(scope) { activitiesDb = it }
