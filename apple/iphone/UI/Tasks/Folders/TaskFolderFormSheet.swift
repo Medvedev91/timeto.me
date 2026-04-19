523a52a9c1560b30665d394e57f7daa0d1a8d176
Ivan Medvedev <ivan@timeto.me>
2026-04-19 12:01:58 +0300

    TaskFolderFormSheet.swift:

--- apple/iphone/UI/Tasks/Folders/TaskFolderFormSheet.swift
+++ apple/iphone/UI/Tasks/Folders/TaskFolderFormSheet.swift
@@ -8,13 +8,14 @@ struct TaskFolderFormSheet: View {
     var body: some View {
         VmView({
             TaskFolderFormVm(
-                folderDb: taskFolderDb
+                folderDb: taskFolderDb,
             )
         }) { vm, state in
             TaskFolderFormSheetInner(
                 vm: vm,
                 state: state,
-                name: state.name
+                name: state.name,
+                activityDb: state.activityDb,
             )
         }
     }
@@ -26,7 +27,8 @@ private struct TaskFolderFormSheetInner: View {
     let state: TaskFolderFormVm.State
     
     @State var name: String
-    
+    @State var activityDb: ActivityDb?
+
     ///
     
     @FocusState private var isFocused: Bool
@@ -37,13 +39,38 @@ private struct TaskFolderFormSheetInner: View {
         
         List {
             
-            TextField(
-                state.namePlaceholder,
-                text: $name
-            )
-            .focused($isFocused)
-            .onChange(of: name) { _, new in
-                vm.setName(name: new)
+            if state.activityDb == nil {
+                Section {
+                    TextField(
+                        state.namePlaceholder,
+                        text: $name
+                    )
+                    .focused($isFocused)
+                    .onChange(of: name) { _, new in
+                        vm.setName(name: new)
+                    }
+                }
+            }
+            
+            if state.isActivityAvailable {
+                Section {
+                    Picker(state.activityTitle, selection: $activityDb) {
+                        if activityDb == nil {
+                            Text("None")
+                                .tag(nil as ActivityDb?) // Support optional (nil) selection
+                        }
+                        ForEach(state.activitiesUi, id: \.activityDb) { activityUi in
+                            Text(activityUi.title)
+                                .tag(activityUi.activityDb as ActivityDb?) // Support optional (nil) selection
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    .accentColor(.secondary)
+                    .foregroundColor(.primary)
+                    .onChange(of: activityDb) { _, activityDb in
+                        vm.setActivity(activityDb: activityDb)
+                    }
+                }
             }
             
             if let folderDb = state.folderDb {
