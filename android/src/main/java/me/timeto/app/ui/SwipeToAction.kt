497ede1dffa288c0f5457df61e19287a1e05e4a2
Ivan Medvedev <ivan@timeto.me>
2026-05-15 16:49:07 +0300

    SwipeToAction.kt:

--- android/src/main/java/me/timeto/app/ui/SwipeToAction.kt
+++ android/src/main/java/me/timeto/app/ui/SwipeToAction.kt
@@ -1,26 +1,14 @@
 package me.timeto.app.ui
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.platform.ViewConfiguration
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 import me.timeto.app.Haptic
-import me.timeto.app.ui.navigation.LocalNavigationFs
 import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -119,102 +107,6 @@ fun SwipeToAction(
     }
 }
 
-@Composable
-fun SwipeToAction__StartView(
-    text: String,
-    bgColor: Color,
-) {
-    val bgAnimate = animateColorAsState(bgColor)
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(bgAnimate.value)
-    ) {
-        Text(
-            text,
-            modifier = Modifier
-                .padding(start = 19.dp, bottom = 1.dp)
-                .align(Alignment.CenterStart),
-            color = c.white,
-            fontSize = 15.sp
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun SwipeToAction__DeleteView(
-    state: DismissState,
-    note: String,
-    deletionConfirmationNote: String? = null,
-    onDelete: () -> Unit,
-) {
-    val scope = rememberCoroutineScope()
-
-    val navigationFs = LocalNavigationFs.current
-
-    Row(
-        modifier = Modifier
-            .fillMaxHeight()
-            .background(c.red),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        Text(
-            text = note,
-            color = c.white,
-            fontWeight = FontWeight.W300,
-            fontSize = 13.sp,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier
-                .padding(start = 16.dp, end = 8.dp)
-                .weight(1f)
-        )
-
-        Text(
-            "Cancel",
-            color = c.white,
-            modifier = Modifier
-                .padding(end = 8.dp)
-                .clip(squircleShape)
-                .clickable {
-                    // С launchEx can be canceled
-                    scope.launch {
-                        state.reset()
-                    }
-                }
-                .padding(horizontal = 10.dp, vertical = 4.dp)
-                .alpha(0.9f),
-            fontSize = 15.sp,
-        )
-
-        Text(
-            text = "Delete",
-            color = c.red,
-            modifier = Modifier
-                .padding(end = 10.dp)
-                .clip(squircleShape)
-                .background(c.white)
-                .clickable {
-                    if (deletionConfirmationNote != null) {
-                        navigationFs.confirmation(
-                            message = deletionConfirmationNote,
-                            buttonText = "Delete",
-                            onConfirm = {
-                                onDelete()
-                            },
-                        )
-                    } else {
-                        onDelete()
-                    }
-                }
-                .padding(start = 10.dp, end = 10.dp, top = 4.dp, bottom = 5.dp),
-            fontWeight = FontWeight.Bold,
-            fontSize = 15.sp
-        )
-    }
-}
 
 private class ActionsContainer(
     val onStart: () -> Boolean,
