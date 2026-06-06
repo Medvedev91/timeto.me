013fcce809268fae54f1e87972a9a52a3f91baf6
Ivan Medvedev <ivan@timeto.me>
2026-06-06 17:50:00 +0300

    doc_timetome_...

--- android/src/main/java/me/timeto/app/ui/doc/DocFs.kt
+++ android/src/main/java/me/timeto/app/ui/doc/DocFs.kt
@@ -706,7 +706,6 @@ fun DocFs() {
                         append(" you can set a ")
                         appendGreenSemiBold("DAY START TIME")
                         append(" setting, to track the working time right.")
-                        append(" I suggest set it for two hours before you wake up.")
                     }
                 )
             }
