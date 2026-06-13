c31c176512fcd48663f6c4d0b627c538613b2029
Ivan Medvedev <ivan@timeto.me>
2026-06-13 13:55:55 +0300

    TaskFormStrategy.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/task_form/TaskFormStrategy.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/task_form/TaskFormStrategy.kt
@@ -1,11 +1,13 @@
 package me.timeto.shared.vm.task_form
 
+import me.timeto.shared.db.ActivityDb
 import me.timeto.shared.db.TaskDb
 import me.timeto.shared.db.TaskFolderDb
 
 sealed class TaskFormStrategy {
 
     class NewTask(
+        val activityDb: ActivityDb,
         val taskFolderDb: TaskFolderDb,
     ) : TaskFormStrategy()
 
