d20ae62de3798ff1a1a39f31ab0f9f72950363dc
Ivan Medvedev <ivan@timeto.me>
2026-06-26 04:23:35 +0300

    DocFullScreen.swift:

---
+++ apple/iphone/UI/Doc/DocFullScreen.swift
@@ -0,0 +1,102 @@
+import SwiftUI
+import shared
+
+struct DocFullScreen: View {
+    
+    let forceRead: Bool
+    
+    var body: some View {
+        VmView({
+            DocVm()
+        }) { vm, state in
+            let state = vm.state.value as! DocVm.State
+            DocFullScreenInner(
+                vm: vm,
+                state: state,
+                forceRead: forceRead,
+            )
+        }
+    }
+}
+
+private struct DocFullScreenInner: View {
+    
+    let vm: DocVm
+    let state: DocVm.State
+    
+    let forceRead: Bool
+    
+    ///
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        
+        List {
+            
+            PView {
+                Text("I built this app to manage my productivity. Here, I will ") +
+                Text("SHARE")
+                    .greenSemiBold() +
+                Text(" my productivity system and how I use the app.")
+            }
+            
+            PView {
+                Text("My system ") +
+                Text("IS NOT")
+                    .redSemiBold() +
+                Text(" about time tracking, ") +
+                Text("IS NOT")
+                    .redSemiBold() +
+                Text(" about getting nice activity charts, ") +
+                Text("IS NOT")
+                    .redSemiBold() +
+                Text(" about reducing wasted time.")
+            }
+        }
+        .listStyle(.plain)
+        .navigationTitle("How to Use the App")
+        .toolbar {
+            if !forceRead {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+private struct PView<Content: View>: View {
+    
+    @ViewBuilder private let content: () -> Content
+    
+    init(
+        _ content: @escaping () -> Content,
+    ) {
+        self.content = content
+    }
+    
+    var body: some View {
+        ZStack {
+            content()
+        }
+        .listRowSeparator(.hidden)
+    }
+}
+
+private extension Text {
+    
+    func redSemiBold() -> Text {
+        foregroundColor(.red).fontWeight(.semibold)
+    }
+    
+    func greenSemiBold() -> Text {
+        foregroundColor(.green).fontWeight(.semibold)
+    }
+    
+    func blueSemiBold() -> Text {
+        foregroundColor(.blue).fontWeight(.semibold)
+    }
+}
