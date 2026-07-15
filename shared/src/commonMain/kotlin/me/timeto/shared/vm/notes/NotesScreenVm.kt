9a368aedda4ccce8453d5f06b47bbc510329d14e
Ivan Medvedev <ivan@timeto.me>
2026-07-16 01:40:14 +0300

    NotesScreenVm.kt:

---
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/notes/NotesScreenVm.kt
@@ -0,0 +1,34 @@
+package me.timeto.shared.vm.notes
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import me.timeto.shared.db.NoteFolderDb
+import me.timeto.shared.onEachExIn
+import me.timeto.shared.vm.Vm
+
+class NotesScreenVm(
+    initNoteFolderDb: NoteFolderDb,
+) : Vm<NotesScreenVm.State>() {
+
+    data class State(
+        val noteFolderDb: NoteFolderDb,
+    ) {
+        val title: String =
+            noteFolderDb.name
+    }
+
+    override val state = MutableStateFlow(
+        State(
+            noteFolderDb = initNoteFolderDb,
+        )
+    )
+
+    init {
+        val scopeVm = scopeVm()
+        NoteFolderDb.selectAllSortedFlow().onEachExIn(scopeVm) { noteFoldersDb ->
+            val noteFolderDb: NoteFolderDb =
+                noteFoldersDb.first { it.id == initNoteFolderDb.id }
+            state.update { it.copy(noteFolderDb = noteFolderDb) }
+        }
+    }
+}
