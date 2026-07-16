97ebc083034c743af834a205519ed36f12d53537
Ivan Medvedev <ivan@timeto.me>
2026-07-17 02:56:46 +0300

    homeBarIconSize.kt / homeBarLetterSize.kt

---
+++ android/src/main/java/me/timeto/app/ui/home/bar/homeBarLetterSize.kt
@@ -0,0 +1,5 @@
+package me.timeto.app.ui.home.bar
+
+import androidx.compose.ui.unit.sp
+
+val homeBarLetterSize = 23.sp
--- android/src/main/java/me/timeto/app/ui/home/tasks/homeTasksBarLetterSize.kt
+++
@@ -1,5 +0,0 @@
-package me.timeto.app.ui.home.tasks
-
-import androidx.compose.ui.unit.sp
-
-val homeTasksBarLetterSize = 23.sp
