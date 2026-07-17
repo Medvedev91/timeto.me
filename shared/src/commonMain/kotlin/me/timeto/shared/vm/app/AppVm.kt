028744f0590d14beaa80122f269b1846365024f9
Ivan Medvedev <ivan@timeto.me>
2026-07-17 21:44:59 +0300

    AppVm.kt:

--- shared/src/commonMain/kotlin/me/timeto/shared/vm/app/AppVm.kt
+++ shared/src/commonMain/kotlin/me/timeto/shared/vm/app/AppVm.kt
@@ -62,11 +62,6 @@ class AppVm : Vm<AppVm.State>() {
                 }
             }
 
-            // todo remove migration starts August 2026
-            val allNotesDb = NoteDb.selectAllSorted()
-            if (allNotesDb.isNotEmpty() && NoteFolderDb.selectAllSorted().isEmpty())
-                insertDefaultNoteFolder()
-
             state.update { it.copy(isAppReady = true) }
 
             ///
@@ -176,7 +171,13 @@ private suspend fun syncTomorrow(todayWithDayStartOffset: Int) {
 private suspend fun fillInitData(
     withDemoData: Boolean,
 ) {
-    insertDefaultNoteFolder()
+    NoteFolderDb.insertNoValidation(
+        id = 1,
+        sort = 0,
+        onHome = true,
+        symbol = Symbol.Icon.IconEnum.pencil_note.toIcon(),
+        name = "Notes",
+    )
 
     TaskFolderDb.insertNoValidation(
         id = TaskFolderDb.ID_TODAY,
@@ -409,14 +410,4 @@ private suspend fun addSleepActivity(): ActivityDb {
     )
     activityDb.updateHomeButtonSort(HomeButtonSort(rowIdx = 1, cellIdx = 4, size = 2))
     return activityDb
-}
-
-private suspend fun insertDefaultNoteFolder() {
-    NoteFolderDb.insertNoValidation(
-        id = 1,
-        sort = 0,
-        onHome = true,
-        symbol = Symbol.Icon.IconEnum.pencil_note.toIcon(),
-        name = "Notes",
-    )
 }
