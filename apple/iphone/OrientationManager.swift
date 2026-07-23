dc5788a337230aa500239a3da647140f67bd87ab
Ivan Medvedev <ivan@timeto.me>
2026-07-23 20:10:49 +0300

    OrientationManager.swift:

---
+++ apple/iphone/OrientationManager.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+import CoreMotion
+
+@Observable
+class OrientationManager {
+    
+    static let instance = OrientationManager()
+    
+    var orientationMask: UIInterfaceOrientationMask = .portrait
+    
+    private let motionManager = CMMotionManager()
+    
+    func start() {
+        stop()
+        
+        guard motionManager.isDeviceMotionAvailable else { return }
+        
+        // Update interval 4 times a second
+        motionManager.deviceMotionUpdateInterval = 0.25
+        
+        motionManager.startDeviceMotionUpdates(to: .main) {[weak self] motion, _ in
+            guard let self = self, let motion = motion else { return }
+            
+            let gravityX: Double = motion.gravity.x
+            let gravityY: Double = motion.gravity.y
+            
+            if abs(gravityY) > 0.3 {
+                self.updateOrientation(.portrait)
+            } else if gravityX < -0.80 {
+                self.updateOrientation(.landscapeRight)
+            } else if gravityX > 0.80 {
+                self.updateOrientation(.landscapeLeft)
+            }
+        }
+    }
+    
+    func stop() {
+        motionManager.stopDeviceMotionUpdates()
+    }
+    
+    private func updateOrientation(_ orientation: UIInterfaceOrientationMask) {
+        if UIDevice.current.userInterfaceIdiom != .phone {
+            return
+        }
+        guard let windowScene = (UIApplication.shared.connectedScenes.first as? UIWindowScene) else {
+            return
+        }
+        if orientationMask == orientation {
+            return
+        }
+        orientationMask = orientation
+        windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: orientation))
+        windowScene.keyWindow?.rootViewController?.setNeedsUpdateOfSupportedInterfaceOrientations()
+    }
+}
