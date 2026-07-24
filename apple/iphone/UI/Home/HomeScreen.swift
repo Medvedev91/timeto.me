6e133227c72d42fe6be186eda8662c0728a39c44
Ivan Medvedev <ivan@timeto.me>
2026-07-24 18:14:30 +0300

    ChecklistView.swift:

--- apple/iphone/UI/Home/HomeScreen.swift
+++ apple/iphone/UI/Home/HomeScreen.swift
@@ -98,6 +98,7 @@ private struct HomeScreenInner: View {
                                 ChecklistView(
                                     checklistDb: checklistDb,
                                     maxLines: 1,
+                                    fullHeight: true,
                                     withAddButton: false,
                                     onDelete: {},
                                 )
