4c42a239bf9caeef48c9aa766c5f08f825f46ada
Ivan Medvedev <ivan@timeto.me>
2026-07-24 18:50:12 +0300

    ZenModeView.swift:

--- apple/iphone/UI/ZenMode/ZenModeView.swift
+++ apple/iphone/UI/ZenMode/ZenModeView.swift
@@ -1,29 +1,64 @@
 import SwiftUI
 import shared
 
-private let timerFont = buildTimerFont(size: 72)
+private let timerFontFullScreen = buildTimerFont(size: 72)
+private let timerFontHalfScreen = buildTimerFont(size: 56)
 
 struct ZenModeView: View {
     
     var body: some View {
         VmView({ ZenModeVm() }) { vm, _ in
             let state = vm.state.value as! ZenModeVm.State
-            ZStack {
+            ZenModeViewLocal(
+                state: state,
+            )
+        }
+    }
+}
+
+private struct ZenModeViewLocal: View {
+    
+    let state: ZenModeVm.State
+    
+    var body: some View {
+        GeometryReader { geometry in
+            
+            let checklistDb: ChecklistDb? = state.checklistDb
+
+            let fullWidth: CGFloat = geometry.size.width
+            let checklistWidth: CGFloat = checklistDb == nil ? 0 : fullWidth * 0.35
+            let timerWidth: CGFloat = fullWidth - checklistWidth
+            
+            HStack {
+                
                 Button(
                     action: {
                         state.timerStateUi.togglePomodoro()
                     },
                     label: {
                         Text(state.timerStateUi.timerText)
-                            .font(timerFont)
+                            .font(checklistDb == nil ? timerFontFullScreen : timerFontHalfScreen)
                             .foregroundColor(state.timerStateUi.timerColor.toColor())
                             .lineLimit(1)
                             .fixedSize()
                     },
                 )
+                .frame(width: timerWidth)
+                
+                if let checklistDb = checklistDb {
+                    ChecklistView(
+                        checklistDb: checklistDb,
+                        maxLines: 1,
+                        fullHeight: false,
+                        withAddButton: false,
+                        onDelete: {},
+                    )
+                    .frame(width: checklistWidth)
+                }
             }
-            .fillMaxSize()
-            .background(.black)
+            .frame(height: geometry.size.height)
         }
+        .fillMaxSize()
+        .background(.black)
     }
 }
