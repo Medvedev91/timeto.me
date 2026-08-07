726ab3b0cceeaa0a6f186a16ac4a8886cae4e427
Ivan Medvedev <ivan@timeto.me>
2026-08-07 22:30:10 +0300

    MyWidgetHomeBarCalendarButton.kt

---
+++ android/src/main/java/me/timeto/app/widget/ui/home_bar/MyWidgetHomeBarCalendarButton.kt
@@ -0,0 +1,34 @@
+package me.timeto.app.widget.ui.home_bar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.glance.ColorFilter
+import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
+import androidx.glance.layout.size
+import me.timeto.app.R
+import me.timeto.app.toGlanceColorProvider
+
+@Composable
+fun MyWidgetHomeBarCalendarButton(
+    color: Color,
+    onClick: () -> Unit,
+) {
+    MyWidgetHomeBarIconButton(
+        onClick = {
+            onClick()
+        },
+        glanceModifier = GlanceModifier,
+        content = {
+            Image(
+                provider = ImageProvider(R.drawable.sf_calendar_medium_light),
+                contentDescription = "New Task",
+                modifier = GlanceModifier
+                    .size(18.dp),
+                colorFilter = ColorFilter.tint(color.toGlanceColorProvider()),
+            )
+        },
+    )
+}
