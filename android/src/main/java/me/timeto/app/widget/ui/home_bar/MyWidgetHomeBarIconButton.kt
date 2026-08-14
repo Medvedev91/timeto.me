b67132de6f660dd59e1364b6e35ba2502bd238c9
Ivan Medvedev <ivan@timeto.me>
2026-08-15 00:56:58 +0300

    MyWidgetHomeBarIconButton.kt:

--- android/src/main/java/me/timeto/app/widget/ui/home_bar/MyWidgetHomeBarIconButton.kt
+++ android/src/main/java/me/timeto/app/widget/ui/home_bar/MyWidgetHomeBarIconButton.kt
@@ -3,6 +3,7 @@ package me.timeto.app.widget.ui.home_bar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
+import androidx.glance.action.Action
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.layout.Alignment
@@ -12,7 +13,7 @@ import me.timeto.app.widget.ui.myWidgetItemHeight
 
 @Composable
 fun MyWidgetHomeBarIconButton(
-    onClick: () -> Unit,
+    onClickAction: Action,
     glanceModifier: GlanceModifier,
     content: @Composable () -> Unit,
 ) {
@@ -20,9 +21,7 @@ fun MyWidgetHomeBarIconButton(
         modifier = glanceModifier
             .size(myWidgetItemHeight)
             .cornerRadius(99.dp)
-            .clickable {
-                onClick()
-            },
+            .clickable(onClickAction),
         contentAlignment = Alignment.Center,
     ) {
         content()
