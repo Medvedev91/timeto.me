68887df7b001d6fc61f0d77db513d24e40b0b050
Ivan Medvedev <ivan@timeto.me>
2026-08-16 07:33:20 +0300

    MyWidgetView.kt:

--- android/src/main/java/me/timeto/app/widget/ui/MyWidgetView.kt
+++ android/src/main/java/me/timeto/app/widget/ui/MyWidgetView.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalContext
 import androidx.glance.action.clickable
 import androidx.glance.background
 import androidx.glance.layout.Alignment
@@ -19,6 +20,7 @@ import androidx.glance.text.TextStyle
 import me.timeto.app.toColor
 import me.timeto.app.toGlanceColorProvider
 import me.timeto.app.ui.c
+import me.timeto.app.widget.MyWidgetOpenApp
 import me.timeto.app.widget.ui.buttons.MyWidgetButtonsView
 import me.timeto.app.widget.ui.home_bar.MyWidgetHomeBarView
 import me.timeto.app.widget.ui.tasks.MyWidgetTasksView
@@ -41,32 +43,44 @@ fun MyWidgetView(
         Box(
             modifier = GlanceModifier
                 .fillMaxWidth()
-                .padding(top = 8.dp)
-                .clickable {
-                    widgetUi.timerStateUi.togglePomodoro()
-                },
+                .clickable(
+                    MyWidgetOpenApp.buildAction(
+                        context = LocalContext.current,
+                        appAction = null,
+                    )
+                ),
             contentAlignment = Alignment.TopCenter,
         ) {
 
-            Text(
-                text = widgetUi.timerStateUi.note,
-                style = TextStyle(
-                    color = widgetUi.timerStateUi.noteColor.toColor().toGlanceColorProvider(),
-                    fontSize = myWidgetPrimaryFontSize,
-                    fontWeight = FontWeight.Medium,
-                ),
-            )
-
-            Text(
-                text = widgetUi.timerStateUi.timerText,
+            Box(
                 modifier = GlanceModifier
-                    .padding(top = 12.dp),
-                style = TextStyle(
-                    color = widgetUi.timerStateUi.timerColor.toColor().toGlanceColorProvider(),
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Bold,
-                ),
-            )
+                    .padding(top = 8.dp)
+                    .clickable {
+                        widgetUi.timerStateUi.togglePomodoro()
+                    },
+                contentAlignment = Alignment.TopCenter,
+            ) {
+
+                Text(
+                    text = widgetUi.timerStateUi.note,
+                    style = TextStyle(
+                        color = widgetUi.timerStateUi.noteColor.toColor().toGlanceColorProvider(),
+                        fontSize = myWidgetPrimaryFontSize,
+                        fontWeight = FontWeight.Medium,
+                    ),
+                )
+
+                Text(
+                    text = widgetUi.timerStateUi.timerText,
+                    modifier = GlanceModifier
+                        .padding(top = 12.dp),
+                    style = TextStyle(
+                        color = widgetUi.timerStateUi.timerColor.toColor().toGlanceColorProvider(),
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
+                )
+            }
         }
 
         Row(
