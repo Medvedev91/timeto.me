4dbd35b44cd9c09fdcb79b6df1a6429c9065fd74
Ivan Medvedev <ivan@timeto.me>
2026-08-19 07:26:55 +0300

    AppDelegate.swift:

--- apple/iphone/AppDelegate.swift
+++ apple/iphone/AppDelegate.swift
@@ -8,6 +8,34 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
+        
+        // todo remove, migration starts 1 Sep 2026
+        do {
+            let fileManager = FileManager.default
+            var fileURLs: [URL] = []
+            fileURLs.append(contentsOf: try fileManager.contentsOfDirectory(at: URL.applicationSupportDirectory, includingPropertiesForKeys: nil))
+            fileURLs.append(contentsOf: try fileManager.contentsOfDirectory(at: URL.applicationSupportDirectory.appendingPathComponent("databases"), includingPropertiesForKeys: nil))
+            try fileURLs.forEach { oldDbUrl in
+                let name: String = oldDbUrl.lastPathComponent
+                if name == "timetome.db" {
+                    print("timetome.db: move \(oldDbUrl.lastPathComponent) \(oldDbUrl)")
+                    
+                    let newDbFolderUrl: URL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.me.timeto.app")!
+                    let newDbUrl: URL = newDbFolderUrl.appendingPathComponent(name)
+                    print("timetome.db: New DB Url \(newDbUrl)")
+                    
+                    if FileManager.default.fileExists(atPath: newDbUrl.path) {
+                        print("timetome.db: File Exists")
+                        try FileManager.default.removeItem(atPath: newDbUrl.path)
+                    }
+                    try FileManager.default.moveItem(atPath: oldDbUrl.path, toPath: newDbUrl.path)
+                    print("timetome.db: Migration Complete")
+                }
+            }
+        } catch {
+            reportApi("AppDelegate.application() move timetome.db error \(error)")
+        }
+        
         InitKmpIosKt.doInitKmpIos()
         setupWCSession(self)
         return true
