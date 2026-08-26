92440592353fe523737ada4a42b9474cb4117b3d
Ivan Medvedev <ivan@timeto.me>
2026-08-27 00:15:15 +0300

    WidgetFull.swift:

---
+++ apple/Widget/Full/WidgetFull.swift
@@ -0,0 +1,17 @@
+import WidgetKit
+import SwiftUI
+
+struct WidgetFull: Widget {
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: "full_widget", provider: WidgetFullProvider()) { entry in
+            if let widgetUi = entry.widgetUi {
+                WidgetFullView(widgetUi: widgetUi)
+                    .containerBackground(.black, for: .widget)
+            } else {
+                WidgetFullPlaceholderView()
+            }
+        }
+        .supportedFamilies([.systemLarge])
+    }
+}
