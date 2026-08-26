68d7bc39b9bf2c20eff1f71469943bda35269224
Ivan Medvedev <ivan@timeto.me>
2026-08-27 00:15:45 +0300

    WidgetFullView.swift:

---
+++ apple/Widget/Full/WidgetFullView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import shared
+
+struct WidgetFullView : View {
+    
+    let widgetUi: WidgetUi
+    
+    var body: some View {
+        VStack {
+            
+            let timerStateUi: TimerStateUi = widgetUi.timerStateUi
+            let widgetTimerData = WidgetTimerData.build(timerType: timerStateUi.timerType)
+            
+            Link(destination: WidgetLink.Toggle.buildUrl()) {
+                ZStack(alignment: .top) {
+                    
+                    Text(timerStateUi.note)
+                        .foregroundColor(timerStateUi.noteColor.toColor())
+                        .font(.system(size: 16, weight: .semibold))
+                    
+                    Text(
+                        timerInterval: widgetTimerData.date.widgetTimerRange(isTimerOrStopwatch: widgetTimerData.isTimerOrStopwatch),
+                        countsDown: widgetTimerData.isTimerOrStopwatch,
+                    )
+                    .textAlign(.center)
+                    .foregroundColor(timerStateUi.timerColor.toColor())
+                    .font(.system(size: 40, weight: .bold))
+                    .padding(.top, 16)
+                }
+            }
+            
+            Spacer()
+        }
+    }
+}
