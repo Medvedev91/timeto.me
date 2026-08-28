21d7f52fda015d51c954b96e4d77eb8be6971f54
Ivan Medvedev <ivan@timeto.me>
2026-08-28 14:58:09 +0300

    WidgetFullButtonsActivityView.swift:

---
+++ apple/Widget/Full/Buttons/WidgetFullButtonsActivityView.swift
@@ -0,0 +1,108 @@
+import SwiftUI
+import shared
+
+private let fgColor: Color = Palette.companion.gray5.dark.toColor()
+
+struct WidgetFullButtonsActivityView: View {
+    
+    let activity: HomeButtonType.Activity
+    
+    ///
+    
+    var body: some View {
+        
+        Link(destination: WidgetLink.ActivityButton(activityId: activity.activityDb.id.toInt()).buildUrl()) {
+            
+            ZStack {
+                
+                ZStack {
+                    
+                    GeometryReader { geometry in
+                        let width = geometry.size.width
+                        let progressRatio: CGFloat = Double(activity.progressRatio)
+                        let progressWidth: CGFloat = width * progressRatio
+                        VStack {
+                            ZStack {
+                            }
+                            .fillMaxHeight()
+                            .frame(width: progressWidth)
+                            .background(activity.bgColor.toColor())
+                        }
+                    }
+                    .fillMaxWidth()
+                    .clipShape(roundedShape)
+                    
+                    if activity.sort.size == 1 {
+                        ZStack {
+                            
+                            HStack {
+                                // todo calc once
+                                let symbol = activity.activityDb.symbolOrDefault()
+                                
+                                SymbolView(
+                                    symbol: symbol,
+                                    color: .white,
+                                    letterSize: widgetFullItemCircleFontSize,
+                                    iconSize: 11,
+                                    emojiSize: widgetFullItemCircleFontSize,
+                                )
+                                .padding(.leading, symbol is Symbol.Emoji ? 4 : 6)
+                                Spacer()
+                            }
+                            
+                            HStack {
+                                Spacer()
+                                RightBarView(
+                                    activity: activity,
+                                )
+                            }
+                        }
+                        .fillMaxSize()
+                    }
+                    else {
+                        HStack {
+                            
+                            Text(activity.leftText)
+                                .padding(.leading, widgetFullItemCircleHPadding)
+                                .foregroundColor(.white)
+                                .font(.system(size: widgetFullItemCircleFontSize, weight: widgetFullItemCircleFontWeight))
+                            
+                            Spacer()
+                            
+                            RightBarView(
+                                activity: activity,
+                            )
+                        }
+                    }
+                }
+                .frame(height: widgetFullItemCircleHeight, alignment: .center)
+                .background(roundedShape.fill(fgColor))
+            }
+            .frame(height: widgetFullItemHeight, alignment: .center)
+        }
+    }
+}
+
+private struct RightBarView: View {
+    
+    let activity: HomeButtonType.Activity
+    
+    var body: some View {
+        
+        if activity.isCompleted {
+            ZStack {
+                Image(systemName: "checkmark")
+                    .foregroundColor(activity.bgColor.toColor())
+                    .font(.system(size: 10, weight: .bold))
+            }
+            .frame(width: widgetFullItemCircleHeight - 6, height: widgetFullItemCircleHeight - 6)
+            .background(Circle().fill(.white))
+            .padding(.trailing, 3)
+        } else {
+            Text(activity.rightText)
+                .padding(.trailing, widgetFullItemCircleHPadding)
+                .foregroundColor(.white)
+                .font(.system(size: widgetFullItemCircleFontSize, weight: widgetFullItemCircleFontWeight))
+        }
+    }
+}
